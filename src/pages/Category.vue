9298360282d8643647cc1d7489e543dae96890d7
FIlip Rakowski <frakowskI@divante.pl>
2017-09-21 10:27:08 +0200

    Products in category page displayed

--- src/pages/Category.vue
+++ src/pages/Category.vue
@@ -5,11 +5,26 @@
 </template>
 
 <script>
+let bodybuilder = require('bodybuilder')
+
 export default {
   name: 'category',
+  beforeMount () {
+    const self = this
+    this.$store.dispatch('catalog/quickSearchByQuery',
+      bodybuilder().query('match', 'name', 'Bag').aggregation('terms', 'category.id').build()
+    ).then(function (res) {
+      self.products = res.items
+    })
+  },
   data () {
     return {
-      categoryName: this.$route.params.name
+      products: {},
+      categoryName: this.$route.params.name,
+      filters: {
+        color: ['red', 'blue'],
+        size: ['XS', 'S', 'M', 'L', 'XL']
+      }
     }
   }
 }
--- src/themes/default/pages/Category.vue
+++ src/themes/default/pages/Category.vue
@@ -2,16 +2,21 @@
   <div id="category">
     <header>
         <div class="container">
+            <!-- There will be breadcrumbs -->
             <h1> {{ categoryName }} </h1>
         </div>
     </header>
-    <div class="container">
+    <div class="container pb60">
         <div class="row">
             <div class="col-md-3">
                 Sidebar
             </div>
             <div class="col-md-9">
-                Content
+                <div class="row">
+                    <div v-for="product in products" class="col-md-4 p15">
+                        <product-tile :product="product" />
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -19,10 +24,15 @@
 </template>
 
 <script>
-// TO-DO configure routing to show specific category not whole catalog, pass category via route param
+// TO-DO @Piotr jak będziesz sie tym bawił to w route param w core jest nazwa kategorii, trzebaby jej użyć jako parametr do dispatchera żeby wyświetlało odpowiednią kategorie zamiast wszystkich produktów,
+//  jak opiszesz jak ma wygladać API dla filtrowania produktów po parametrach to zrobię jak wróce
 import { corePage } from 'lib/themes'
+import ProductTile from '../components/core/ProductTile.vue'
 
 export default {
+  components: {
+    ProductTile
+  },
   mixins: [corePage('Category')]
 }
 </script>
