5bfec45915d2e54698c9151710ae7420c933130b
FIlip Rakowski <frakowskI@divante.pl>
2017-10-08 08:18:43 +0200

    Refactor css fix

--- src/themes/default/components/core/blocks/MainSlider/MainSlider.vue
+++ src/themes/default/components/core/blocks/MainSlider/MainSlider.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="main-slider bg-darkgray clr-white">
+  <section class="main-slider bg-darkgray c-white">
       <div class="container">
           <!-- TO-DO: Use v-for and make slides moving from right to left -->
           <div class="row middle-md">
