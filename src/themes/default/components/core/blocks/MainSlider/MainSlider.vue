3a847b70346ba894df259a7cdecc2234594d7607
Patryk Mazur <pmazur@divante.pl>
2017-10-17 15:22:40 +0200

    Hero carousel added
    Relates to: #82

--- src/themes/default/components/core/blocks/MainSlider/MainSlider.vue
+++ src/themes/default/components/core/blocks/MainSlider/MainSlider.vue
@@ -1,27 +1,33 @@
 <template>
-  <section class="main-slider bg-darkgray c-white" :style="{ backgroundImage: 'url(' + slides[currentSlide].image + ')' }">
-      <div class="container">
-          <!-- TO-DO: Use v-for and make slides moving from right to left -->
-          <div class="row middle-md">
-            <div class="col-md-12 px10p">
-                <p class="mb0 serif uppercase h3 align-center">{{ slides[currentSlide].subtitle }}</p>
-                <h1 class="mt0 mb30 align-center">{{ slides[currentSlide].title }}</h1>
-                <div class="align-center">
-                    <button-outline :text="slides[currentSlide].button_text" color="light" />
+  <section class="main-slider bg-darkgray c-white">
+    <carousel :perPage='1'>
+        <slide v-for='slide in slides'>
+            <div class="container" :style="{ backgroundImage: 'url(' + slide.image + ')' }">
+                <div class="row middle-md">
+                    <div class="col-md-12 px10p">
+                        <p class="mb0 serif uppercase h3 align-center">{{ slide.subtitle }}</p>
+                        <h1 class="mt0 mb30 align-center">{{ slide.title }}</h1>
+                        <div class="align-center">
+                            <button-outline :text="slide.button_text" color="light" />
+                        </div>
+                    </div>
                 </div>
             </div>
-          </div>
-      </div>
+        </slide>
+    </carousel>
   </section>
 </template>
 
 <script>
 import { coreComponent } from 'lib/themes'
+import { Carousel, Slide } from 'vue-carousel'
 import ButtonOutline from '../../../theme/ButtonOutline.vue'
 
 export default {
   components: {
-    ButtonOutline
+    ButtonOutline,
+    Carousel,
+    Slide
   },
   mixins: [coreComponent('core/blocks/MainSlider/MainSlider')]
 }
@@ -34,6 +40,9 @@ h1 {
 .main-slider {
     width: 100%;
     height: 640px;
+}
+.container {
+    width: 100%;
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
