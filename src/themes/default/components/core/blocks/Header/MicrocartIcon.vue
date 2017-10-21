9122357ae3645502a8a3a7722f495954550aa1f1
Adam Marcinkowski <kontakt@mercs600.pl>
2017-10-21 19:12:03 +0200

    [#44] Fix minicart counter

--- src/themes/default/components/core/blocks/Header/MicrocartIcon.vue
+++ src/themes/default/components/core/blocks/Header/MicrocartIcon.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="microcart-icon" @click="toggleMicrocart">
     <i class="material-icons md-18">shopping_cart</i>
+    <span class="minicart-count l-center-vh brdr-circle fs-10 weight-700 bg-gray c-white" v-cloak>{{totals.quantity}}</span>
   </div>
 </template>
 
