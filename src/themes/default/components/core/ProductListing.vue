045a4f25ac2bdd3319feb85d57ceadf122e18b3b
Patryk Mazur <pmazur@divante.pl>
2017-11-07 07:01:14 +0100

    Paddings between products
    Closes #253

--- src/themes/default/components/core/ProductListing.vue
+++ src/themes/default/components/core/ProductListing.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="product-listing row center-xs">
-    <div v-for="product in products" :key="product.id" class="p15" :class="'col-md-' + (12/columns)%10">
+    <div v-for="product in products" :key="product.id" :class="'col-md-' + (12/columns)%10">
       <product-tile :product="product" />
     </div>
   </div>
