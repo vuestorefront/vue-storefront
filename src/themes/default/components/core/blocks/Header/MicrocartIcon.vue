c2e7a7dd798ae43a3bd3084f7a55e37b21727ed2
FIlip Rakowski <frakowskI@divante.pl>
2017-12-05 12:50:15 +0100

    Small css refactoring

--- src/themes/default/components/core/blocks/Header/MicrocartIcon.vue
+++ src/themes/default/components/core/blocks/Header/MicrocartIcon.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="microcart-icon inline-flex" @click="openMicrocart">
     <i class="material-icons md-18">shopping_cart</i>
-    <span class="minicart-count brdr-circle fs-10 weight-700 bg-gray c-white" v-cloak>{{totals.quantity}}</span>
+    <span class="minicart-count brdr-circle h4 weight-700 bg-gray c-white" v-cloak>{{totals.quantity}}</span>
   </div>
 </template>
 
