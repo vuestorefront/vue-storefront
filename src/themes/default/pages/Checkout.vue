428b1fb0296865c01d997dd0c0b06cbf2df59d64
Akbar Abdrakhmanov <kz.akbar@gmail.com>
2017-12-08 11:06:06 +0600

    Added destroyed to Checkout component in order to remove network.status listener. This fixes the issue 'Fix the offline message on the checkout #298'

--- src/themes/default/pages/Checkout.vue
+++ src/themes/default/pages/Checkout.vue
@@ -108,6 +108,9 @@ export default {
       this.activateSection(section)
     })
   },
+  destroyed () {
+    this.$bus.$off('network.status')
+  },
   computed: {
     isValid () {
       let isValid = true
