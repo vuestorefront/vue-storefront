4bd7f32648db7bcb4e0be4949ff8aa22db8b4650
Adam Marcinkowski <kontakt@mercs600.pl>
2017-12-28 20:54:47 +0100

    [#145] Basic modal test

---
+++ test/unit/specs/components/core/Modal.spec.js
@@ -0,0 +1,34 @@
+import Vue from 'vue/dist/vue.common.js'
+import Vuex from 'vuex/dist/vuex.js'
+import store from 'src/store'
+import { EventBusPlugin as EventBus } from 'src/event-bus'
+import Modal from 'src/components/core/Modal.vue'
+
+Vue.use(EventBus)
+Vue.use(Vuex)
+
+describe('Modal.vue', () => {
+  var vm = new Vue({
+    template: '<div><modal name="my-modal"><div class="test-modal">Modal content</div></modal></div>',
+    store,
+    components: {
+      Modal
+    }
+  }).$mount()
+
+  describe('When you open the modal', () => {
+    vm.$bus.$emit('modal.toggle', 'my-modal')
+
+    it('should be visible and hidden after toggle again', (done) => {
+      Vue.nextTick(() => {
+        expect(vm.$el.querySelectorAll('.test-modal').length.should.equal(1))
+
+        vm.$bus.$emit('modal.toggle', 'my-modal')
+
+        Vue.nextTick().then(() => {
+          expect(vm.$el.querySelectorAll('.test-modal').length.should.equal(0))
+        }).then(done, done)
+      })
+    })
+  })
+})
