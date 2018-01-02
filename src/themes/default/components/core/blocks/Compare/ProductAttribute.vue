f2ec2d0e51760114a6152b5688ae74e2150172c7
talalus <talal.suhail@snow.dog>
2018-01-02 21:39:08 +0200

    #322: compare - extend productAttribute component DRY

--- src/themes/default/components/core/blocks/Compare/ProductAttribute.vue
+++ src/themes/default/components/core/blocks/Compare/ProductAttribute.vue
@@ -1,40 +1,11 @@
 <template>
-    <span>{{value|htmlDecode}}</span>
+  <span>{{value|htmlDecode}}</span>
 </template>
 
 <script>
-  import { coreComponent } from 'lib/themes'
+  import ProductAttribute from '../../ProductAttribute.vue'
 
   export default {
-    data () {
-      return {
-        value: ''
-      }
-    },
-    beforeMount () {
-      let parsedValues = this.product[this.attribute.attribute_code]
-
-      if (!parsedValues) {
-        this.value = this.emptyPlaceholder
-      } else {
-        parsedValues = parsedValues.split(',')
-        let results = []
-        for (let parsedVal of parsedValues) {
-          if (this.attribute.options) {
-            let option = this.attribute.options.find(av => {
-              /* eslint eqeqeq: "off" */
-              return av.value == parsedVal
-            })
-            if (option) {
-              results.push(option.label)
-            }
-          } else {
-            results.push(parsedVal)
-          }
-        }
-        this.value = results.join(', ')
-      }
-    },
-    mixins: [coreComponent('core/blocks/Compare/ProductAttribute')]
+    mixins: [ProductAttribute]
   }
 </script>
