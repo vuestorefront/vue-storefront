6c80c192e2f4c5b946be5839374e161431cd4df6
Pawel Szafranski <pawel.szafranski@snow.dog>
2018-01-05 01:12:00 +0100

    #482: wishlist - removed useless div and added correct tag for ul > li

--- src/themes/default/components/core/blocks/Wishlist/Product.vue
+++ src/themes/default/components/core/blocks/Wishlist/Product.vue
@@ -1,30 +1,30 @@
 <template>
-  <div>
-    <div class="row pr55 pt20 pb20">
+  <li class="row pr55 pt20 pb20">
+    <div>
+      <img v-lazy="thumbnail" />
+    </div>
+    <div class="col-xs flex pl40 pb15 pt15">
       <div>
-        <img v-lazy="thumbnail" />
-      </div>
-      <div class="col-xs flex pl40 pb15 pt15">
-        <div>
-          <router-link :to="{ name: product.type_id + '-product', params: { parentSku: product.parentSku ? product.parentSku : product.sku, slug: product.slug, childSku: product.sku }}">
-            {{ product.name | htmlDecode}}
-           </router-link>
-          <div class="h6 c-lightgray pt5">{{ product.sku }}</div>
-        </div>
-      </div>
-      <div class="col-xs flex pb15 pt15 align-right">
-        <div>
-          <span class="price-special" v-if="product.special_price">{{ product.priceInclTax | price }}</span>&nbsp;
-          <span class="price-original" v-if="product.special_price" >{{ product.originalPriceInclTax | price }}</span>
-
-          <span v-if="!product.special_price" >{{ product.priceInclTax | price }}</span>
-        </div>
-        <div>
-          <div class="mt5"><span @click="removeItem"><remove-button class="c-darkgray" /></span></div>
-        </div>
+        <router-link :to="{ name: product.type_id + '-product', params: { parentSku: product.parentSku ? product.parentSku : product.sku, slug: product.slug, childSku: product.sku }}">
+          {{ product.name | htmlDecode}}
+        </router-link>
+        <div class="h6 c-lightgray pt5">{{ product.sku }}</div>
       </div>
     </div>
-  </div>
+    <div class="col-xs flex pb15 pt15 align-right">
+      <div>
+        <span class="price-special" v-if="product.special_price">{{ product.priceInclTax | price }}</span>&nbsp;
+        <span class="price-original" v-if="product.special_price" >{{ product.originalPriceInclTax | price }}</span>
+
+        <span v-if="!product.special_price">
+          {{ product.priceInclTax | price }}
+        </span>
+      </div>
+      <div>
+        <div class="mt5"><span @click="removeItem"><remove-button class="c-darkgray" /></span></div>
+      </div>
+    </div>
+  </li>
 </template>
 
 <script>
