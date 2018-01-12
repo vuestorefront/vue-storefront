f8b77c33e0eb83b6100a4722f7eb0d90a5c25625
Pawel Szafranski <pawel.szafranski@snow.dog>
2018-01-12 15:42:24 +0100

    #485: - microcart - defined new margin classes, added base fixes for template structure

--- src/themes/default/components/core/blocks/Microcart/Microcart.vue
+++ src/themes/default/components/core/blocks/Microcart/Microcart.vue
@@ -1,19 +1,34 @@
 <template>
-  <div class="microcart bg-white c-black" :class="{ active: isOpen }">
-    <div class="row">
-      <div class="col-md-12 end-xs">
-        <i class="material-icons p15 close c-black" @click="closeMicrocart">close</i>
+  <div class="microcart bg-lightgray c-black" :class="{ active: isOpen }">
+    <div class="row middle-xs bg-white top-sm">
+      <div class="col-xs-10">
+        <h2 v-if="items.length" class="c-black mt60 mb35 ml40 microcart-heading">
+          Shopping cart
+        </h2>
+      </div>
+      <div class="col-xs-2 end-xs">
+        <button type="button" class="p0 close-button">
+          <i class="material-icons p15 close c-black" @click="closeMicrocart">
+            close
+          </i>
+        </button>
       </div>
     </div>
-    <h2 v-if="items.length" class="c-black ml30">Shopping cart</h2>
-    <h4 v-if="!items.length" class="c-black ml30">Your shopping cart is empty.</h4>
-    <div v-if="!items.length" class="ml30" @click="closeMicrocart">Don't hesitate and <router-link to="/">browse our catalog</router-link> to find something beatufiul for You!</div>
-    <ul class="products">
+
+    <h4 v-if="!items.length" class="c-black ml30">
+      Your shopping cart is empty.
+    </h4>
+    <div v-if="!items.length" class="ml30" @click="closeMicrocart">
+      Don't hesitate and <router-link to="/">browse our catalog</router-link> to find something beatufiul for You!
+    </div>
+    <ul class="bg-white m0 products">
       <product v-for="product in items" :key="product.id" :product="product" />
     </ul>
-    <div v-if="items.length" class="checkout bg-lightgray pt10">
-      <h3 class="ml30 c-black">Shopping summary</h3>
-      <div class="row pt15 pb20 pl30 pr55">
+    <div v-if="items.length" class="checkout pt10 serif">
+      <h3 class="m40 c-black weight-400">
+        Shopping summary
+      </h3>
+      <div class="row pt15 pb20 pl40 pr55">
         <div class="col-xs c-black">
           Subtotal inc. tax
         </div>
@@ -21,7 +36,7 @@
           {{ subtotalInclTax | price }}
         </div>
       </div>
-      <div class="row pt20 pb20 pl30 pr55">
+      <div class="row pt20 pb20 pl40 pr55">
         <div class="col-xs c-black">
           Shipping ({{ shipping.name }})
         </div>
@@ -29,7 +44,7 @@
           {{ shipping.costInclTax | price }}
         </div>
       </div>
-      <div class="row pt20 pb20 pl30 pr55">
+      <div class="row pt20 pb20 pl40 pr55">
         <div class="col-xs c-black">
           Payment ({{ payment.name }})
         </div>
@@ -37,7 +52,7 @@
           {{ payment.costInclTax | price }}
         </div>
       </div>
-      <div class="row pt20 pb20 pl30 pr55">
+      <div class="row pt20 pb20 pl40 pr55">
         <div class="col-xs weight-400 c-black">
           Total inc. tax
         </div>
@@ -45,9 +60,13 @@
           {{ totalInclTax | price }}
         </div>
       </div>
-      <div class="row pt20 pb20 pl30 pr55" v-if="!isCheckoutMode">
+      <div class="row pt20 pb20 pl40 pr55" v-if="!isCheckoutMode">
         <div class="col-xs align-right">
-              <router-link class="no-underline" :to="{ name: 'checkout' }"><button class="ripple checkout-button bg-black brdr-none c-white pb20 pt20 pl70 pr70" @click="closeMicrocart">CHECKOUT</button></router-link>
+          <router-link class="no-underline" :to="{ name: 'checkout' }">
+            <button class="ripple checkout-button bg-black brdr-none c-white pb20 pt20 pl70 pr70" @click="closeMicrocart">
+              CHECKOUT
+            </button>
+          </router-link>
         </div>
       </div>
     </div>
@@ -69,7 +88,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "../../../../css/transitions.scss";
+  @import "../../../../css/transitions.scss";
 
   .microcart {
       height: 100vh;
@@ -84,16 +103,28 @@ export default {
       overflow-y: auto;
       overflow-x: hidden;
   }
-  .microcart.active {
-      transform: translateX(0)
+
+  .microcart-heading {
+    @media (max-width: 767px) {
+      margin: 12px 0 12px 15px;
+      font-size: 24px;
+    }
+
+    i {
+      opacity: 0.6;
+      &:hover {
+        opacity: 1;
+      }
+    }
   }
-  .close {
+
+  .close-button {
+    background: transparent;
+    border: none;
     cursor: pointer;
   }
-  i {
-    opacity: 0.6;
-  }
-  i:hover {
-    opacity: 1;
+
+  .microcart.active {
+    transform: translateX(0)
   }
 </style>
