96a91796728be7021f1a18ebb61648089c468cd2
Pawel Szafranski <pawel.szafranski@snow.dog>
2018-01-14 19:40:06 +0100

    #486: microcart - product component formatting

--- src/themes/default/components/core/blocks/Microcart/Product.vue
+++ src/themes/default/components/core/blocks/Microcart/Product.vue
@@ -6,31 +6,62 @@
       </div>
       <div class="col-xs flex pl40 pb15 pt15">
         <div>
-          <div>{{ product.name | htmlDecode }}</div>
-          <div class="h6 c-lightgray pt5">{{ product.sku }}</div>
-          <div class="h6 pt5 error" v-if="product.warning_message">{{ product.warning_message }}</div>
-          <div class="h6 pt5 info" v-if="product.info_message">{{ product.info_message }}</div>
-
+          <div>
+            {{ product.name | htmlDecode }}
+          </div>
+          <div class="h6 c-lightgray pt5">
+            {{ product.sku }}
+          </div>
+          <div class="h6 pt5 error" v-if="product.warning_message">
+            {{ product.warning_message }}
+          </div>
+          <div class="h6 pt5 info" v-if="product.info_message">
+            {{ product.info_message }}
+          </div>
         </div>
         <div>
-          <div><span class="h6 c-darkgray">Qty </span>
-          <span class="h6 weight-400" :class="{ hidden: isEditing }">{{ product.qty }}</span>
-          <span :class="{ hidden: !isEditing }">
-            <input class="h6" type="number" autofocus v-model.number="qty" @change="updateQuantity">
-          </span>
+          <div>
+            <span class="h6 c-darkgray">
+              Qty
+            </span>
+            <span class="h6 weight-400" :class="{ hidden: isEditing }">
+              {{ product.qty }}
+            </span>
+            <span :class="{ hidden: !isEditing }">
+              <input
+                class="h6"
+                type="number"
+                autofocus
+                v-model.number="qty"
+                @change="updateQuantity"
+              >
+            </span>
           </div>
         </div>
       </div>
       <div class="col-xs flex pb15 pt15 align-right">
         <div>
-          <span class="price-special" v-if="product.special_price">{{ product.priceInclTax | price }}</span>&nbsp;
-          <span class="price-original" v-if="product.special_price" >{{ product.originalPriceInclTax | price }}</span>
-
-          <span v-if="!product.special_price" >{{ product.priceInclTax | price }}</span>
+          <span class="price-special" v-if="product.special_price">
+            {{ product.priceInclTax | price }}
+          </span>&nbsp;
+          <span class="price-original" v-if="product.special_price" >
+            {{ product.originalPriceInclTax | price }}
+          </span>
+          <span v-if="!product.special_price">
+            {{ product.priceInclTax | price }}
+          </span>
         </div>
         <div>
-          <div class="c-darkgray"><span @click="switchEdit"><edit-button class="c-darkgray" /></span></div>
-          <div class="mt5"><span @click="removeItem"><remove-button class="c-darkgray" /></span></div>
+          <div class="c-darkgray">
+            <span @click="switchEdit">
+              <edit-button class="c-darkgray" />
+            </span>
+          </div>
+          <div class="mt5">
+            <span @click="removeItem">
+              <remove-button class="c-darkgray" />
+            </span>
+          </div>
         </div>
       </div>
     </div>
@@ -82,27 +113,27 @@ export default {
 </script>
 
 <style scoped>
-.error {
-  color: red
-}
-.info {
-  color: green
-}
-.price-special {
-  color: red
-}
-.price-original {
-  text-decoration: line-through;
-  font-size: smaller
-}
-.col-xs {
-  flex-direction: column;
-  justify-content: space-between;
-}
-.hidden {
-  display: none;
-}
-input {
-  width: 30px;
-}
+  .error {
+    color: red
+  }
+  .info {
+    color: green
+  }
+  .price-special {
+    color: red
+  }
+  .price-original {
+    text-decoration: line-through;
+    font-size: smaller
+  }
+  .col-xs {
+    flex-direction: column;
+    justify-content: space-between;
+  }
+  .hidden {
+    display: none;
+  }
+  input {
+    width: 30px;
+  }
 </style>
