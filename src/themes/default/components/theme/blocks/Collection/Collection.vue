dcceea500661e8ec3a7993ca6bacfff1e58296ed
Pawel Szafranski <pawel.szafranski@snow.dog>
2018-01-22 02:15:06 +0100

    Added Products Slider component, called on homepage and product page

--- src/themes/default/components/theme/blocks/Collection/Collection.vue
+++ src/themes/default/components/theme/blocks/Collection/Collection.vue
@@ -1,44 +1,25 @@
 <template>
-  <div class="collection">
-
-    <div class="container">
-      <div class="row center-xs">
-        <header class="col-md-12 pt40 pb15">
-          <h2 class="align-center c-black">{{title}}</h2>
-        </header>
-      </div>
-    </div>
-
-
-  <div class="bg-lightgray collection-slider">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="pb20 pt20 center-xs cool-stuff-collection">
-          <no-ssr>
-            <carousel :perPage="6" :paginationEnabled="false" :loop="true" ref="carousel">
-              <slide v-for='(product, index) in products' v-bind:key='product.id'>
-                <product-tile class="collection-product" :product="product"  v-bind:class="{'is-muted': (currentIndex == index || index == currentIndex + 5)}"/>
-              </slide>
-            </carousel>
-          </no-ssr>
-        </div>
-      </div>
-    </div>
-  </div>
-  </div>
+  <products-slider 
+    :products="products" 
+    :config="sliderConfig" 
+    :title="title" 
+  />
 </template>
 <script>
-  import NoSSR from 'vue-no-ssr'
-  import { Carousel, Slide } from 'vue-carousel'
   import builder from 'bodybuilder'
-  import ProductTile from 'theme/components/core/ProductTile.vue'
+  import ProductsSlider from 'theme/components/core/ProductsSlider.vue'
 
   export default {
     name: 'Collection',
     props: ['title', 'coverImage', 'category'],
     data () {
       return {
-        products: []
+        products: [],
+        sliderConfig: {
+          perPage: 6,
+          paginationEnabled: false,
+          loop: true
+        }
       }
     },
     computed: {
@@ -64,46 +45,7 @@
       })
     },
     components: {
-      Slide,
-      Carousel,
-      ProductTile,
-      'no-ssr': NoSSR
+      ProductsSlider
     }
   }
 </script>
-<style lang="scss">
-.collection-slider {
-  overflow: hidden;
-}
-//TO-DO: Clean blending mode mess on products!
-.product {
-  &.collection-product {
-     background-color: #f2f2f2;
-     padding: 15px;
-  }
-
-  &.is-muted {
-    @media (min-width: 1024px) {
-      opacity: 0.5;
-    }
-  }
-}
-
-.collection-product .product-image {
-  //TO-DO: Should be global
-  mix-blend-mode: darken;
-  height: auto;
-
-  img {
-    max-width: 100%;
-    max-height: 100%;
-    height: auto;
-  }
-}
-
-.cool-stuff-collection {
-  @media (min-width: 1024px) {
-    margin: 0 -130px;
-  }
-}
-</style>
