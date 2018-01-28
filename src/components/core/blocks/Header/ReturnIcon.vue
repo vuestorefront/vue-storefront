0e9ec5b6de8edd595fc9218e504aec590e7f112b
Pawel Szafranski <pawel.szafranski@snow.dog>
2018-01-28 16:20:05 +0100

    #593 - header - added for core and theme 'Return Icon' component

---
+++ src/components/core/blocks/Header/ReturnIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="return-icon">
+    <!--
+        methods:
+        goBack - opens previous page
+    -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'return-icon',
+  methods: {
+    goBack () {
+      this.$router.back()
+    }
+  }
+}
+</script>
---
+++ src/themes/default/components/core/blocks/Header/ReturnIcon.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="hamburger-icon inline-flex" @click="goBack">
+    <i class="material-icons md-18">keyboard_backspace</i>
+  </div>
+</template>
+
+<script>
+  import { coreComponent } from 'lib/themes'
+
+  export default {
+    mixins: [coreComponent('core/blocks/Header/ReturnIcon')]
+  }
+</script>
