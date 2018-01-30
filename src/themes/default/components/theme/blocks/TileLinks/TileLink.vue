226ee29ceb6c815a65589885e51fad20e4d56077
Pawel Szafranski <pawel.szafranski@snow.dog>
2018-01-31 00:39:16 +0100

    Formatting

--- src/themes/default/components/theme/blocks/TileLinks/TileLink.vue
+++ src/themes/default/components/theme/blocks/TileLinks/TileLink.vue
@@ -1,11 +1,14 @@
 <template>
-    <div class="tile-link">
-        <a :href="tile.account_link">
-          <div class="tile-image bg-lightgray" v-lazy:background-image="tile.background_image">
-            <div class="overlay flex bg-white h4">@{{ tile.account_name }}</div>
-          </div>
-        </a>
-    </div>
+  <div class="tile-link">
+    <a :href="tile.account_link">
+      <div
+        class="tile-image bg-lightgray"
+        v-lazy:background-image="tile.background_image"
+      >
+        <div class="overlay flex bg-white h4">@{{ tile.account_name }}</div>
+      </div>
+    </a>
+  </div>
 </template>
 
 <script>
@@ -16,32 +19,34 @@
 </script>
 
 <style lang="scss" scoped>
-@import '~src/themes/default/css/transitions';
+  @import '~src/themes/default/css/transitions';
 
-.tile-link {
-  width: 322px;
-  height: 280px;
-
-  @media (max-width: 767px) {
-    width: 100%;
+  .tile-link {
+    width: 322px;
+    height: 280px;
+    @media (max-width: 767px) {
+      width: 100%;
+    }
   }
-}
-.tile-image {
-  width: 100%;
-  height: 100%;
-  background-position: center;
-  background-size: cover;
-  background-repeat: no-repeat;
-}
-.overlay {
-  width: 100%;
-  height: 100%;
-  align-items: center;
-  justify-content: center;
-  opacity: 0;
-  transition: 0.3s all $motion-main;
-}
-.overlay:hover {
-  opacity: 0.75;
-}
+
+  .tile-image {
+    width: 100%;
+    height: 100%;
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+
+  .overlay {
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+    opacity: 0;
+    transition: 0.3s all $motion-main;
+    &:hover,
+    &:focus {
+      opacity: 0.75;
+    }
+  }
 </style>
