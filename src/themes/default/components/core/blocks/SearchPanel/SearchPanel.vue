a622016f6d07f31945fa6aefa7ba5d2e239e09a8
Damian Fiałkiewicz <damian.fialkiewicz@snow.dog>
2018-02-06 00:43:10 +0100

    Move css properties to atomic classes in MyAccount, SearchPanel, SidebarMenu

--- src/themes/default/components/core/blocks/SearchPanel/SearchPanel.vue
+++ src/themes/default/components/core/blocks/SearchPanel/SearchPanel.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="searchpanel bg-white c-black" :class="{ active: isOpen }">
+  <div class="searchpanel fixed bg-white c-black" :class="{ active: isOpen }">
     <div class="row">
       <div class="col-md-12 end-xs">
         <i class="material-icons p15 close c-black" @click="closeSearchpanel">close</i>
       </div>
     </div>
     <div class="col-md-12 end-xs">
-      <input ref="search" v-model="search" @input="makeSearch" class="form-control input-lg search-input" :placeholder="placeholder" type="text" >
+      <input
+        ref="search"
+        v-model="search"
+        @input="makeSearch"
+        class="mr20 py10 brdr-none brdr-bottom brdr-c-lightgray-secondary no-outline h4"
+        :placeholder="placeholder"
+        type="text"
+      >
     </div>
-    <div class="col-md-12 product-listing row">
+    <div class="col-md-12 product-listing pl35 pt20 row">
       <product-tile @click.native="closeSearchpanel" :key="product.id" v-for="product in products" :product="product"/>
       <transition name="fade">
-        <div v-if="emptyResults" class="center-text no-results">No results were found.</div>
+        <div v-if="emptyResults" class="no-results relative center-xs h4">No results were found.</div>
       </transition>
     </div>
   </div>
@@ -48,65 +55,53 @@ export default {
 <style lang="scss" scoped>
 @import "../../../../css/transitions.scss";
 
-  .searchpanel {
-      height: 100vh;
-      width: 800px;
-      top: 0;
-      right: 0;
-      max-width: 100%;
-      position: fixed;
-      z-index: 3;
-      transform: translateX(100%);
-      transition: transform 300ms $motion-main;
-      overflow-y: auto;
-      overflow-x: hidden;
+.searchpanel {
+  height: 100vh;
+  width: 800px;
+  top: 0;
+  right: 0;
+  max-width: 100%;
+  z-index: 3;
+  transform: translateX(100%);
+  transition: transform 300ms $motion-main;
+  overflow-y: auto;
+  overflow-x: hidden;
 
-      .product-listing{
-        padding-left: 35px;
-        padding-top: 20px;
-
-        .product{
-          width: 30%;
-          padding: 10px;
-        }
-
-      }
-
-      input[type=text], input[type=email], input[type=tel] {
-        padding: 10px 0;
-        border: none;
-        border-bottom: 1px solid #BDBDBD;
-        width: calc(100% - 40px);
-        font-size: 18px;
-        outline: none;
-        margin-right:20px;
-       }
-
-       .no-results{
-          position: relative;
-          top: 80px;
-          display: inline-block;
-          text-align: center;
-          width: calc(100% - 40px);
-          font-size: 1.3rem;
-       }
-  }
-  .searchpanel.active {
-      transform: translateX(0)
-  }
-  .close {
-    cursor: pointer;
-  }
-  i {
-    opacity: 0.6;
-  }
-  i:hover {
-    opacity: 1;
+  &.active {
+    transform: translateX(0)
   }
 
-  @media only screen and (max-width:50em) {
-    .searchpanel .product-listing .product{
-      width:auto;
-    }
+  .product{
+    width: 30%;
+    padding: 10px;
   }
+
+  input {
+    width: calc(100% - 40px);
+  }
+
+  .no-results{
+    top: 80px;
+    display: inline-block;
+    width: calc(100% - 40px);
+  }
+}
+
+.close {
+  cursor: pointer;
+}
+
+i {
+  opacity: 0.6;
+}
+
+i:hover {
+  opacity: 1;
+}
+
+@media only screen and (max-width:50em) {
+  .searchpanel .product {
+    width: auto;
+  }
+}
 </style>
--- src/themes/default/components/core/blocks/SidebarMenu/SidebarMenu.vue
+++ src/themes/default/components/core/blocks/SidebarMenu/SidebarMenu.vue
@@ -1,38 +1,60 @@
 <template>
-  <div class="sidebar-menu bg-lightgray" :class="{ active: isOpen }">
+  <div class="sidebar-menu fixed bg-lightgray" :class="{ active: isOpen }">
     <div class="row between-xs">
-      <div @click="closeMenu" class="flex-start px10 bg-white brdr-bottom brdr-c-lightgray ">
+      <div @click="closeMenu" class="flex-start px10 bg-white brdr-bottom brdr-c-lightgray">
         <sub-btn type="back" v-if="submenu.depth" />
         <search-icon class="p15 icon hidden-md" />
         <wishlist-icon class="p15 icon hidden-md" />
         <account-icon class="p15 icon hidden-md" />
       </div>
-      <div class="flex-end col-xs close bg-white align-right end-xs brdr-bottom brdr-c-lightgray" @click="closeMenu">
+      <div
+        class="flex-end col-xs inline-flex close bg-white align-right end-xs brdr-bottom brdr-c-lightgray"
+        @click="closeMenu"
+      >
         <i class="material-icons p15">close</i>
       </div>
     </div>
     <div class="row">
       <div class="col-xs-12 h4 serif">
-        <ul class="p0 m0 sidebar-menu__list" :style="mainListStyles">
+        <ul class="p0 m0 relative sidebar-menu__list" :style="mainListStyles">
           <li @click="closeMenu" class="brdr-bottom brdr-c-lightgray bg-white">
             <router-link class="px25 py20 c-black no-underline" to="/" exact>Home</router-link>
           </li>
-          <li class="brdr-bottom brdr-c-lightgray bg-white flex" :key="category.slug" @click="closeMenu" v-for="category in categories" v-if="category.product_count >0 || category.children_data.length>0" >
-            <router-link class="px25 py20 c-black no-underline col-xs" :to="{ name: 'category', params: { id: category.id, slug: category.slug }}">{{ category.name }}</router-link>
+          <li
+            class="brdr-bottom brdr-c-lightgray bg-white flex"
+            :key="category.slug"
+            @click="closeMenu"
+            v-for="category in categories"
+            v-if="category.product_count > 0 || category.children_data.length > 0"
+          >
+            <router-link
+              class="px25 py20 c-black no-underline col-xs"
+              :to="{ name: 'category', params: { id: category.id, slug: category.slug }}"
+            >
+              {{ category.name }}
+            </router-link>
             <sub-btn class="flex-end center-self" :id="category.id"/>
             <sub-category :category-links="category.children_data" :id="category.id"/>
           </li>
           <li @click="closeMenu">
-            <router-link class="px25 py20 brdr-bottom brdr-c-alto c-black no-underline" to="/magazine" exact>Magazine</router-link>
+            <router-link class="px25 py20 brdr-bottom brdr-c-alto c-black no-underline" to="/magazine" exact>
+              Magazine
+            </router-link>
           </li>
           <li @click="closeMenu">
-            <router-link class="px25 py20 brdr-bottom brdr-c-alto c-black no-underline" to="/sale" exact>Sale</router-link>
+            <router-link class="px25 py20 brdr-bottom brdr-c-alto c-black no-underline" to="/sale" exact>
+              Sale
+            </router-link>
           </li>
           <li @click="closeMenu">
-            <router-link class="px25 py20 brdr-bottom brdr-c-alto c-black no-underline" to="/order-tracking" exact>Track my order</router-link>
+            <router-link class="px25 py20 brdr-bottom brdr-c-alto c-black no-underline" to="/order-tracking" exact>
+              Track my order
+            </router-link>
           </li>
           <li @click="closeMenu" class="brdr-bottom brdr-c-alto flex">
-            <router-link v-if="currentUser" class="px25 py20 c-black no-underline col-xs" to="/my-account" exact>My account</router-link>
+            <router-link v-if="currentUser" class="px25 py20 c-black no-underline col-xs" to="/my-account" exact>
+              My account
+            </router-link>
             <sub-btn v-if="currentUser" class="flex-end center-self"/>
             <sub-category v-if="currentUser" :my-account-links="myAccountLinks" :id="'foo'"/>
             <a v-if="!currentUser" href="#" @click="login" class="px25 py20 c-black no-underline">My account</a>
@@ -119,56 +141,56 @@ export default {
 
 <style lang="scss" scoped>
 @import "../../../../css/transitions.scss";
+
 ul {
-    list-style-type: none;
+  list-style-type: none;
 }
 
 .sidebar-menu {
-    height: 100vh;
-    width: 350px;
-    max-width: 100%;
-    top: 0;
-    left: 0;
-    overflow: hidden;
-    overflow-y: auto;
-    position: fixed;
-    transform: translateX(-100%);
-    z-index: 3;
-    transition: transform $duration-main $motion-main;
-}
+  height: 100vh;
+  width: 350px;
+  max-width: 100%;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+  overflow-y: auto;
+  transform: translateX(-100%);
+  z-index: 3;
+  transition: transform $duration-main $motion-main;
 
-.sidebar-menu.active {
-  transform: translateX(0);
-}
+  &.active {
+    transform: translateX(0);
+  }
 
-.sidebar-menu__list {
-    position: relative;
+  &__list {
     transition: transform $duration-main $motion-main;
 
     a {
-        display: block;
+      display: block;
     }
+  }
+
 }
 
 .close {
-    cursor: pointer;
-    display: inline-flex;
+  cursor: pointer;
 }
 </style>
+
 <style lang="scss">
-    .sidebar-menu {
-      li {
-        &:hover {
-           background-color: #F2F2F2;
-         }
-      }
-
-      i {
-        opacity: 0.6;
-
-        &:hover{
-          opacity: 1;
-        }
-      }
+.sidebar-menu {
+  li {
+    &:hover {
+      background-color: #F2F2F2;
     }
+  }
+
+  i {
+    opacity: 0.6;
+
+    &:hover{
+      opacity: 1;
+    }
+  }
+}
 </style>
