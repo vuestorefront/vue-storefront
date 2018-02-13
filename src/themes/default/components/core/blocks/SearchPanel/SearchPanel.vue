3298cf0ff70ac21005fcb928868c23dc4a043b73
Pawel Szafranski <pawel.szafranski@snow.dog>
2018-02-14 00:42:03 +0100

    #a11y - added visually hidden class and styles

--- src/themes/default/components/core/blocks/SearchPanel/SearchPanel.vue
+++ src/themes/default/components/core/blocks/SearchPanel/SearchPanel.vue
@@ -6,8 +6,10 @@
       </div>
     </div>
     <div class="col-md-12 end-xs">
+      <label for="search" class="visually-hidden">Search</label>
       <input
         ref="search"
+        id="search"
         v-model="search"
         @input="makeSearch"
         class="mr20 py10 brdr-none brdr-bottom brdr-c-lightgray-secondary no-outline h4"
