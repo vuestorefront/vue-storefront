9f280bb72de02f189d6cd270ced7565c9dbc200f
Damian Fiałkiewicz <damian.fialkiewicz@snow.dog>
2018-02-13 12:25:16 +0100

    Add translations to pages

--- src/themes/default/pages/Static.vue
+++ src/themes/default/pages/Static.vue
@@ -30,6 +30,7 @@
 import Breadcrumbs from '../components/core/Breadcrumbs'
 import Meta from 'src/lib/meta'
 import staticContent from 'theme/components/theme/StaticContent'
+import i18n from 'lib/i18n'
 
 export default {
   components: {
@@ -55,13 +56,13 @@ export default {
   data () {
     return {
       navigation: [
-        { title: 'About us', link: '/about-us' },
-        { title: 'Customer service', link: '/customer-service' },
-        { title: 'Store locator', link: '/store-locator' },
-        { title: 'Delivery', link: '/delivery' },
-        { title: 'Return policy', link: '/returns' },
-        { title: 'Privacy policy', link: '/privacy' },
-        { title: 'Contact us', link: '/contact' }
+        { title: i18n.t('About us'), link: '/about-us' },
+        { title: i18n.t('Customer service'), link: '/customer-service' },
+        { title: i18n.t('Store locator'), link: '/store-locator' },
+        { title: i18n.t('Delivery'), link: '/delivery' },
+        { title: i18n.t('Return policy'), link: '/returns' },
+        { title: i18n.t('Privacy policy'), link: '/privacy' },
+        { title: i18n.t('Contact us'), link: '/contact' }
       ]
     }
   },
