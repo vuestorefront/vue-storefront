78a064d447956933795fb416ea8057880730271f
Damian Fiałkiewicz <damian.fialkiewicz@snow.dog>
2018-02-18 00:47:38 +0100

    Change ButtonFull to handle dynamic tag

--- src/themes/default/components/theme/ButtonFull.vue
+++ src/themes/default/components/theme/ButtonFull.vue
@@ -1,23 +1,36 @@
 <template>
-  <button
-    class="button-full block brdr-none w-100 px10 py20 bg-darkgray ripple weight-400 h4 c-white"
-    :type="type"
-    v-focus-clean="{class: 'no-outline'}"
+  <component
+    :is="tag"
+    :type="tag === 'button' ? type : false"
+    :to="link"
+    class="no-outline button-full block brdr-none w-100 px10 py20 bg-darkgray ripple weight-400 h4 c-white"
+    :class="{ 'no-underline pointer align-center border-box': tag != 'button' }"
+    v-focus-clean="{ class: 'no-outline' }"
   >
     <slot>
       Button
     </slot>
-  </button>
+  </component>
 </template>
 
 <script>
 export default {
   name: 'ButtonFull',
   props: {
+    tag: {
+      type: String,
+      required: false,
+      default: 'button'
+    },
     type: {
       type: String,
       required: false,
       default: 'button'
+    },
+    link: {
+      type: Object,
+      required: false,
+      default: null
     }
   }
 }
