bfd7a6d948f0f08b12841ccf47ebd05240e82826
Akbar Abdrakhmanov <kz.akbar@gmail.com>
2018-02-18 23:17:38 +0600

    Moved logic from theme to core components for ColorButton, PriceButton and SizeButton, changed docs accordingly

--- src/components/core/ColorButton.vue
+++ src/components/core/ColorButton.vue
@@ -24,6 +24,41 @@ export default {
       type: null,
       default: ''
     }
+  },
+  data () {
+    return {
+      active: false
+    }
+  },
+  beforeDestroy () {
+    this.$bus.$off('filter-reset')
+    this.$bus.$off('filter-changed-' + this.context)
+  },
+  beforeMount () {
+    if (this.$route.name !== 'product') {
+      this.$bus.$on('filter-reset', (filterOption) => {
+        this.active = false
+      })
+      this.$bus.$on('filter-changed-' + this.context, (filterOption) => {
+        if (filterOption.attribute_code === this.code) {
+          if (filterOption.id === this.id) {
+            if (this.active) {
+              this.active = false
+            } else {
+              this.active = true
+            }
+          } else {
+            this.active = false
+          }
+          // filterOption.id === this.id ? this.active = true : this.active = false
+        }
+      })
+    }
+  },
+  methods: {
+    switchFilter (id, label) {
+      this.$bus.$emit('filter-changed-' + this.context, { attribute_code: this.code, id: id, label: label })
+    }
   }
 }
 </script>
