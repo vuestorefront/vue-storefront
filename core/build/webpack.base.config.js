bd9c6abf2544550bb40bcdcd05d1c6e83ec281a2
Filip Rakowski <f.rakowski@hotmail.com>
2018-02-22 13:33:08 +0100

    Corrected webpack pathes on build

--- core/build/webpack.base.config.js
+++ core/build/webpack.base.config.js
@@ -2,17 +2,17 @@ const path = require('path')
 const config = require('config')
 const fs = require('fs')
 
-fs.writeFileSync(path.resolve(__dirname, '../build/config.json'), JSON.stringify(config))
+fs.writeFileSync(path.resolve(__dirname, './config.json'), JSON.stringify(config))
 
 const vueConfig = require('./vue-loader.config')
 
-const theme = require('../build/config.json').theme
-const themeComponents = '../src/themes/' + theme + '/components'
-const themePages = '../src/themes/' + theme + '/pages'
-const themePlugins = '../src/themes/' + theme + '/plugins'
-const themeResources = '../src/themes/' + theme + '/resource'
-const themeCSS = '../src/themes/' + theme + '/css'
-const themeApp = '../src/themes/' + theme + '/App.vue'
+const theme = require('./config.json').theme
+const themeComponents = '../../src/themes/' + theme + '/components'
+const themePages = '../../src/themes/' + theme + '/pages'
+const themePlugins = '../../src/themes/' + theme + '/plugins'
+const themeResources = '../../src/themes/' + theme + '/resource'
+const themeCSS = '../../src/themes/' + theme + '/css'
+const themeApp = '../../src/themes/' + theme + '/App.vue'
 
 module.exports = {
   devtool: '#source-map',
@@ -25,25 +25,25 @@ module.exports = {
     extensions: ['.js', '.vue'],
     alias: {
       // Main aliases
-      config: path.resolve(__dirname, '../build/config.json'),
-      lib: path.resolve(__dirname, '../src/lib'),
-      'src': path.resolve(__dirname, '../src'),
-      'assets': path.resolve(__dirname, '../src/assets'),
-      'themes': path.resolve(__dirname, '../src/themes/' + theme),
+      config: path.resolve(__dirname, './config.json'),
+      lib: path.resolve(__dirname, '../../src/lib'),
+      'src': path.resolve(__dirname, '../../src'),
+      'assets': path.resolve(__dirname, '../../src/assets'),
+      'themes': path.resolve(__dirname, '../../src/themes/' + theme),
       // Core aliases
       'core/components': path.resolve(__dirname, '../core/components'),
-      'components': path.resolve(__dirname, '../src/components'),
-      'core/pages': path.resolve(__dirname, '../core/pages'),
-      'core/resource': path.resolve(__dirname, '../core/resource'),
-      'core/plugins': path.resolve(__dirname, '../core/plugins'),
+      'components': path.resolve(__dirname, '../../src/components'),
+      'core/pages': path.resolve(__dirname, '../pages'),
+      'core/resource': path.resolve(__dirname, '../resource'),
+      'core/plugins': path.resolve(__dirname, '../plugins'),
       'core/api': path.resolve(__dirname, '../core/api'),
-      'core/lib': path.resolve(__dirname, '../src/lib'),
-      'core/helpers': path.resolve(__dirname, '../core/helpers'),
-      'core/filters': path.resolve(__dirname, '../core/filters'),
-      'core/models': path.resolve(__dirname, '../core/models'),
-      'core/router': path.resolve(__dirname, '../core/router'),
-      'core/store': path.resolve(__dirname, '../core/store'),
-      'core/mixins': path.resolve(__dirname, '../core/mixins'),
+      'core/lib': path.resolve(__dirname, '../../src/lib'),
+      'core/helpers': path.resolve(__dirname, '../helpers'),
+      'core/filters': path.resolve(__dirname, '../filters'),
+      'core/models': path.resolve(__dirname, '../models'),
+      'core/router': path.resolve(__dirname, '../router'),
+      'core/store': path.resolve(__dirname, '../store'),
+      'core/mixins': path.resolve(__dirname, '../mixins'),
       // Theme aliases
       'theme/resource': path.resolve(__dirname, themeResources),
       'theme/components': path.resolve(__dirname, themeComponents),
--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const serve = (path, cache, options) => express.static(resolve(path), Object.ass
   maxAge: cache && isProd ? 60 * 60 * 24 * 30 : 0
 }, options))
 
-const theme = require('./build/config.json').theme
+const theme = require('./core/build/config.json').theme
 
 app.use('/dist', serve('./dist', true))
 app.use('/assets', serve(path.resolve(__dirname, 'src/assets'), true))
