8611dfd4734c931096c8378152b031038b81982b
Bartek Igielski <igloczek@gmail.com>
2018-03-17 12:46:02 +0100

    Simplified import paths of extensions

--- src/extensions/index.js
+++ src/extensions/index.js
@@ -1,9 +1,8 @@
 export default [
-  require('src/extensions/custom_extension/index.js').default,
-  require('src/extensions/payment-cash-on-delivery/index.js').default,
-  require('src/extensions/payment-backend-methods/index.js').default,
-  require('src/extensions/mailchimp-subscribe/index.js').default,
-  require('src/extensions/google-analytics/index.js').default,
-
-  require('vue-storefront-stripe/index.js').default
+  require('src/extensions/custom_extension'),
+  require('src/extensions/payment-cash-on-delivery'),
+  require('src/extensions/payment-backend-methods'),
+  require('src/extensions/mailchimp-subscribe'),
+  require('src/extensions/google-analytics'),
+  require('vue-storefront-stripe')
 ]
