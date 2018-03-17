9acf43adcf9b48b84090f629026bb367181b63e6
Bartek Igielski <igloczek@gmail.com>
2018-03-17 12:50:57 +0100

    Missing extensions added

--- src/extensions/index.js
+++ src/extensions/index.js
@@ -1,8 +1,9 @@
 export default [
-  require('src/extensions/custom_extension'),
-  require('src/extensions/payment-cash-on-delivery'),
-  require('src/extensions/payment-backend-methods'),
-  require('src/extensions/mailchimp-subscribe'),
+  require('src/extensions/droppoint-shipping'),
   require('src/extensions/google-analytics'),
+  require('src/extensions/mailchimp-subscribe'),
+  require('src/extensions/payment-backend-methods'),
+  require('src/extensions/payment-cash-on-delivery'),
+  require('src/extensions/template'),
   require('vue-storefront-stripe')
 ]
