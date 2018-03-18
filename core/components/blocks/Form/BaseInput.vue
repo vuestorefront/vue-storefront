755707e8d436717219ec2ff40cebca33c9d5abd3
Damian Fiałkiewicz <damian.fialkiewicz@snow.dog>
2018-03-18 17:43:44 +0100

    #640 - Add BaseInput component

---
+++ core/components/blocks/Form/BaseInput.vue
@@ -0,0 +1,62 @@
+<script>
+export default {
+  name: 'BaseInput',
+  data () {
+    return {
+      passType: 'password',
+      iconActive: false,
+      icon: 'visibility'
+    }
+  },
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: String,
+      required: true
+    },
+    placeholder: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    autocomplete: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    validationIf: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    validationText: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+  methods: {
+    togglePassType () {
+      if (this.passType === 'password') {
+        this.passType = 'text'
+        this.icon = 'visibility_off'
+      } else {
+        this.passType = 'password'
+        this.icon = 'visibility'
+      }
+    }
+  },
+  created () {
+    if (this.type === 'password') {
+      this.iconActive = true
+    }
+  }
+}
+</script>
---
+++ src/themes/default/components/core/blocks/Form/BaseInput.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="relative">
+    <input
+      class="py10 w-100 border-box brdr-none brdr-bottom brdr-cl-primary h4 weight-200 sans-serif"
+      :type="type === 'password' ? passType : type"
+      :name="name"
+      :placeholder="placeholder"
+      :autocomplete="autocomplete"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+      @blur="$emit('blur')"
+    >
+    <i
+      v-if="iconActive"
+      class="icon material-icons absolute cl-brdr-secondary pointer"
+      @click="togglePassType()"
+    >
+      {{ icon }}
+    </i>
+    <span
+      v-if="validationIf"
+      class="block cl-error h6"
+    >
+      {{ validationText }}
+    </span>
+  </div>
+
+</template>
+
+<script>
+import { coreComponent } from 'core/lib/themes'
+export default {
+  mixins: [coreComponent('blocks/Form/BaseInput')]
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '~theme/css/variables/colors';
+  @import '~theme/css/helpers/functions/color';
+  $color-tertiary: color(tertiary);
+  $color-black: color(black);
+  $color-hover: color(tertiary, $colors-background);
+
+  input {
+    transition: 0.3s all;
+    &::-webkit-input-placeholder {
+      color: $color-tertiary;
+    }
+    &::-moz-placeholder {
+      color: $color-tertiary;
+    }
+    &:hover,
+    &:focus {
+      outline: none;
+      border-color: $color-black;
+    }
+  }
+
+  .icon {
+    right: 0;
+    top: 10px;
+    &:hover {
+      color: $color-hover;
+    }
+  }
+</style>
