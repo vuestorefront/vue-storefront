26621856c54504324283391d6bcc3dac23baa385
Bartek Igielski <igloczek@gmail.com>
2018-03-19 01:36:16 +0100

    Independent versioning of extensions

--- src/extensions/index.js
+++ src/extensions/index.js
@@ -1,9 +1,12 @@
+// if you need to have BABEL applied to extensions inside node_modules
+// (by default excluded from BABEL) please add ".js" file ext inside require
+
 export default [
-  require('src/extensions/droppoint-shipping'),
-  require('src/extensions/google-analytics'),
-  require('src/extensions/mailchimp-subscribe'),
-  require('src/extensions/payment-backend-methods'),
-  require('src/extensions/payment-cash-on-delivery'),
-  require('src/extensions/template'),
-  require('vue-storefront-stripe/index.js') // if you need to have BABEL applied to extensions inside node_modules (by default excluded from BABEL) please add ".js" file ext inside require
+  require('@vue-storefront/extension-droppoint-shipping/index.js'),
+  require('@vue-storefront/extension-google-analytics/index.js'),
+  require('@vue-storefront/extension-mailchimp-subscribe/index.js'),
+  require('@vue-storefront/extension-payment-backend-methods/index.js'),
+  require('@vue-storefront/extension-payment-cash-on-delivery/index.js'),
+  require('@vue-storefront/extension-template/index.js'),
+  require('vue-storefront-stripe/index.js')
 ]
