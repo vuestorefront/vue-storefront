d3399657f4bee250e7a342445155a8ab97e6ea8b
Damian Fiałkiewicz <damian.fialkiewicz@snow.dog>
2018-03-20 01:52:16 +0100

    #640 - Replace inputs in MyAccount with BaseInput and refactor code

--- src/themes/default/components/core/blocks/MyAccount/MyNewsletter.vue
+++ src/themes/default/components/core/blocks/MyAccount/MyNewsletter.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="my-newsletter mb35">
+  <div class="mb35">
     <!-- My newsletter header -->
     <div class="row mb15">
-      <div class="col-xs-12 col-md-6" :class="{ 'cl-accent' : !isActive }">
+      <div class="col-xs-12 col-sm-6" :class="{ 'cl-accent' : !isActive }">
         <h3 class="m0 mb5">
           {{ $t('My newsletter') }}
         </h3>
       </div>
-      <div class="col-xs-12 col-md-6 pr30">
+      <div class="col-xs-12 col-sm-6">
         <div class="lh30 flex end-md" v-if="!isActive && editMode">
           <a href="#" class="cl-tertiary flex" @click.prevent="edit">
             <span class="pr5">
@@ -26,6 +26,7 @@
           {{ $t('General agreement') }}
         </h4>
       </div>
+
       <base-checkbox
         class="col-xs-12 mb15"
         id="generalAgreement"
@@ -35,13 +36,15 @@
       >
         {{ $t('I want to receive a newsletter, and agree to its terms') }}
       </base-checkbox>
+
       <div class="col-xs-12">
         <h4>
           {{ $t('Preferences') }}
         </h4>
       </div>
+
       <base-checkbox
-        class="col-xs-6 col-md-3 preferences mb25"
+        class="col-xs-6 col-md-2 mb25"
         id="men"
         v-model="newsletterPreferences.men"
         @click="isActive ? newsletterPreferences.men = !newsletterPreferences.men : null"
@@ -49,8 +52,9 @@
       >
         {{ $t('Men') }}
       </base-checkbox>
+
       <base-checkbox
-        class="col-xs-6 col-md-3 mb25"
+        class="col-xs-6 col-md-2 mb25"
         id="women"
         v-model="newsletterPreferences.women"
         @click="isActive ? newsletterPreferences.women = !newsletterPreferences.women : null"
@@ -58,9 +62,9 @@
       >
         {{ $t('Women') }}
       </base-checkbox>
-      <div class="hidden-xs hidden-sm col-md-6"/>
+
       <base-checkbox
-        class="col-xs-6 col-md-3 preferences mb25"
+        class="col-xs-6 col-md-2 mb25"
         id="kids"
         v-model="newsletterPreferences.kids"
         @click="isActive ? newsletterPreferences.kids = !newsletterPreferences.kids : null"
@@ -68,8 +72,9 @@
       >
         {{ $t('Kids') }}
       </base-checkbox>
+
       <base-checkbox
-        class="col-xs-6 col-md-3 mb25"
+        class="col-xs-6 col-md-2 mb25"
         id="home"
         v-model="newsletterPreferences.home"
         @click="isActive ? newsletterPreferences.home = !newsletterPreferences.home : null"
@@ -77,14 +82,15 @@
       >
         {{ $t('Home') }}
       </base-checkbox>
-      <div class="hidden-xs hidden-sm col-md-6"/>
-      <div class="col-xs-12 col-sm-6 mt10 bottom-button" v-show="isActive">
+
+      <div class="col-xs-12 col-sm-6" v-if="isActive">
         <button-full @click.native="updateNewsletter">
           {{ $t('Update my preferences') }}
         </button-full>
       </div>
-      <div class="col-xs-12 col-sm-6 mt25 bottom-button" v-show="isActive">
-        <a href="#" @click="exitSection" class="link no-underline fs16 cl-accent">
+
+      <div class="col-xs-12 col-sm-6 flex middle-xs py10" v-if="isActive">
+        <a href="#" @click="exitSection" class="h4 cl-accent">
           {{ $t('Cancel') }}
         </a>
       </div>
@@ -105,11 +111,3 @@ export default {
   mixins: [coreComponent('blocks/MyAccount/MyNewsletter')]
 }
 </script>
-
-<style lang="scss" scoped>
-  @media (max-width: 767px) {
-    .preferences {
-      padding-left: 15px;
-    }
-  }
-</style>
