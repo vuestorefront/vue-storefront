26d0659465c6c809e7022e0d2793a204cb0bf36c
Irene <irene.iaccio@gmail.com>
2018-04-02 16:40:00 +0200

    #99 fix zoom mobile and add no-ssr

--- src/themes/default/components/core/ProductGallery.vue
+++ src/themes/default/components/core/ProductGallery.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="media-gallery relative" :class="{ 'open fixed bg-cl-primary': isZoomOpen }">
-    <div :class="{ 'container product-zoom py30': isZoomOpen }">
+    <div :class="{ 'container product-zoom py40': isZoomOpen }">
       <div :class="{ row: isZoomOpen }">
         <i v-if="isZoomOpen" class="material-icons modal-close p15 cl-bg-tertiary pointer" @click="toggleZoom">close</i>
         <div class="col-md-2 thumbnails p0"
@@ -15,27 +15,29 @@
           </div>
         </div>
         <div :class="{ 'col-md-10' : isZoomOpen}">
-          <carousel
-            :per-page="1"
-            mouse-drag="false"
-            navigation-enabled="true"
-            pagination-active-color="transparent"
-            pagination-color="#828282"
-            navigation-next-label="<i class='material-icons p15 cl-bg-tertiary pointer'>keyboard_arrow_right</i>"
-            navigation-prev-label="<i class='material-icons p15 cl-bg-tertiary pointer'>keyboard_arrow_left</i>"
-            ref="carousel">
-            <slide
-              v-for="(images, key) in gallery"
-              :key="key">
-              <div class="bg-cl-secondary">
-                <img
-                  class="product-image inline-flex pointer mw-100"
-                  v-lazy="images.path"
-                  ref="images.path"
-                  @dblclick="toggleZoom">
-              </div>
-            </slide>
-          </carousel>
+          <no-ssr>
+            <carousel
+              :per-page="1"
+              mouse-drag="false"
+              navigation-enabled="true"
+              pagination-active-color="transparent"
+              pagination-color="#828282"
+              navigation-next-label="<i class='material-icons p15 cl-bg-tertiary pointer'>keyboard_arrow_right</i>"
+              navigation-prev-label="<i class='material-icons p15 cl-bg-tertiary pointer'>keyboard_arrow_left</i>"
+              ref="carousel">
+              <slide
+                v-for="(images, key) in gallery"
+                :key="key">
+                <div class="bg-cl-secondary">
+                  <img
+                    class="product-image inline-flex pointer mw-100"
+                    v-lazy="images.path"
+                    ref="images.path"
+                    @dblclick="toggleZoom">
+                </div>
+              </slide>
+            </carousel>
+          </no-ssr>
           <i v-if="isZoomOpen==false" class="zoom-in material-icons p15 cl-bg-tertiary pointer" @click="toggleZoom">zoom_in</i>
         </div>
       </div>
