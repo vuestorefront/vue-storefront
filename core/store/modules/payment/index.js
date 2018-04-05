ae3c1b003c8f211ae4e5ba2ce667557063ffa3b1
Thomas Thune Hansen <th@improving.dk>
2018-04-05 13:56:40 +0200

    Use payment store for paymentmethods

--- core/store/modules/payment/index.js
+++ core/store/modules/payment/index.js
@@ -2,5 +2,20 @@ export default {
   namespaced: true,
   state: {
     methods: []
+  },
+  mutations: {
+    addMethod (state, paymentMethod) {
+      state.methods.push(paymentMethod)
+    }
+  },
+  actions: {
+    addMethod ({commit}, paymentMethod) {
+      commit('addMethod', paymentMethod)
+    }
+  },
+  getters: {
+    paymentMethods (state) {
+      return state.methods
+    }
   }
 }
