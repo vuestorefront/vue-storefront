b2cad2b465ee84bb48b1ad6ab16619b13a0e88cc
filrak <frakowski@divante.pl>
2018-04-10 15:21:38 +0200

    Vue-offline new version updates

--- src/themes/default/components/core/OfflineBadge.vue
+++ src/themes/default/components/core/OfflineBadge.vue
@@ -1,7 +1,7 @@
 <template>
   <no-ssr>
     <div
-      v-offline
+      v-show="OfflineOnly"
       class="offline-badge fixed w-100 p10 bg-cl-th-error cl-white center-xs"
     >
       {{ $t('You are offline, some of the functionalities are limited') }}
