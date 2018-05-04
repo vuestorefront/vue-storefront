a4cdceba24e2304fdf1a0c4768c85086f6cedf1a
Andrea Righi <theblackpaul@hotmail.it>
2018-05-05 00:19:26 +0200

    #1117

--- src/themes/default/components/core/blocks/Form/BaseInput.vue
+++ src/themes/default/components/core/blocks/Form/BaseInput.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="relative">
-    <input
-      class="
-        py10 w-100 border-box brdr-none brdr-bottom
-        brdr-cl-primary h4 sans-serif
-      "
-      :class="{pr30: type === 'password', empty: value === ''}"
-      :type="type === 'password' ? passType : type"
-      :name="name"
-      :autocomplete="autocomplete"
-      :value="value"
-      :autofocus="autofocus"
-      :ref="focus ? name : false"
-      @input="$emit('input', $event.target.value)"
-      @blur="$emit('blur')"
-      @keyup.enter="$emit('keyup.enter', $event.target.value)"
-      @keyup="$emit('keyup', $event)"
-    >
-    <label>{{ placeholder }}</label>
+    <div class="relative">
+      <input
+        class="
+         py10 w-100 border-box brdr-none brdr-bottom
+         brdr-cl-primary h4 sans-serif
+       "
+        :class="{pr30: type === 'password', empty: value === ''}"
+        :type="type === 'password' ? passType : type"
+        :name="name"
+        :autocomplete="autocomplete"
+        :value="value"
+        :autofocus="autofocus"
+        :ref="focus ? name : false"
+        @input="$emit('input', $event.target.value)"
+        @blur="$emit('blur')"
+        @keyup.enter="$emit('keyup.enter', $event.target.value)"
+        @keyup="$emit('keyup', $event)"
+      >
+      <label>{{ placeholder }}</label>
+    </div>
     <button
       v-if="iconActive"
       type="button"
