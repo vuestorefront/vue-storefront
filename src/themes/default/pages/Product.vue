5157de356e444cae494ec15afedd419b1af02d94
Damian Fiałkiewicz <damian.fialkiewicz@snow.dog>
2018-05-14 10:22:07 +0200

    #974 - Adjust product variants, change fonts and colors in product page

--- src/themes/default/pages/Product.vue
+++ src/themes/default/pages/Product.vue
@@ -2,17 +2,17 @@
   <div id="product">
     <section class="bg-cl-secondary px20 product-top-section">
       <div class="container">
-        <section class="row m0 data-wrapper">
-          <div class="col-xs-12 col-md-6 px15 center-xs middle-xs image">
+        <section class="row m0 between-xs">
+          <div class="col-xs-12 col-md-6 center-xs middle-xs image">
             <product-gallery
               :gallery="gallery"
               :offline="offlineImage"
               :configuration="configuration"
             />
           </div>
-          <div class="col-xs-12 col-md-6 px20 data">
+          <div class="col-xs-12 col-md-5 data">
             <breadcrumbs
-              class="pt40 pb20"
+              class="pt40 pb20 hidden-xs"
               :routes="breadcrumbs.routes"
               :active-route="breadcrumbs.name"
             />
@@ -49,7 +49,7 @@
               v-if="product.type_id =='configurable' && !loading"
             >
               <div
-                class="h4"
+                class="h5"
                 v-for="(option, index) in product.configurable_options"
                 :key="index"
               >
@@ -101,8 +101,8 @@
                     to="/size-guide"
                     v-if="option.label == 'Size'"
                     class="
-                      p0 ml30 inline-flex middle-xs weight-700 uppercase
-                      no-underline action size-guide pointer cl-tertiary
+                      p0 ml30 inline-flex middle-xs no-underline h5
+                      action size-guide pointer cl-secondary
                     "
                   >
                     <i class="pr5 material-icons">accessibility</i>
@@ -131,13 +131,13 @@
                 class="col-xs-12 col-sm-4 col-md-6"
               />
             </div>
-            <div class="row pt45 add-to-buttons">
-              <div class="col-xs-12 col-sm-5">
+            <div class="row py40 add-to-buttons">
+              <div class="col-xs-6 col-sm-3 col-md-6">
                 <button
                   @click="addToFavorite"
                   class="
                     p0 inline-flex middle-xs bg-cl-transparent brdr-none
-                    action weight-700 h5 uppercase pointer cl-tertiary
+                    action h5 pointer cl-secondary
                   "
                   type="button"
                 >
@@ -150,12 +150,12 @@
                   </template>
                 </button>
               </div>
-              <div class="hidden-xs col-md-7">
+              <div class="col-xs-6 col-sm-3 col-md-6">
                 <button
                   @click="addToCompare"
                   class="
                     p0 inline-flex middle-xs bg-cl-transparent brdr-none
-                    action weight-700 h5 uppercase pointer cl-tertiary
+                    action h5 pointer cl-secondary
                   "
                   type="button"
                 >
@@ -277,12 +277,6 @@ $bg-secondary: color(secondary, $colors-background);
   }
 }
 
-.data-wrapper {
-  @media (max-width: 767px) {
-    padding: 0;
-  }
-}
-
 .data {
   @media (max-width: 767px) {
     border-bottom: 1px solid $bg-secondary;
@@ -292,13 +286,11 @@ $bg-secondary: color(secondary, $colors-background);
 .image {
   @media (max-width: 1023px) {
     margin-bottom: 20px;
-    padding: 20px 0 30px 0;
   }
 }
 
 .product-name {
   @media (max-width: 767px) {
-    margin-top: 10px;
     font-size: 36px;
   }
 }
@@ -322,14 +314,14 @@ $bg-secondary: color(secondary, $colors-background);
 
  .sizes {
     @media (max-width: 767px) {
-      width: 60%;
+      width: 100%;
     }
   }
 
   .size-guide {
     height: 40px;
     @media (max-width: 767px) {
-      width: 40%;
+      width: 100%;
       margin-left: 0;
     }
   }
@@ -400,7 +392,7 @@ $bg-secondary: color(secondary, $colors-background);
 
 .action {
   &:hover {
-    color: $color-secondary;
+    color: $color-tertiary;
   }
 }
 
