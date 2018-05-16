c3a8b8606f9eb78ee884adfa3117db56c469c1ed
David Rouyer <rouyer.david@gmail.com>
2018-05-16 22:34:29 +0200

    feat: add vue-storefront ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,8 @@ module.exports = {
     'standard'
   ],
   plugins: [
-    'vue'
+    'vue',
+    'vue-storefront'
   ],
   // add your custom rules here
   rules: {
@@ -28,6 +29,10 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    "vue-storefront/no-corecomponent-import": "error",
+    "vue-storefront/no-corecomponent": "error",
+    "vue-storefront/no-corepage-import": "error",
+    "vue-storefront/no-corepage": "error"
   }
 }
