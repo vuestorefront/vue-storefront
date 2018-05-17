4031f5e5aa635d6f8c9f25c76c7f389abd1c65d3
David Rouyer <rouyer.david@gmail.com>
2018-05-17 22:59:22 +0200

    chore: replace process.env.NODE_ENV with mode where possible

--- core/build/webpack.client.config.js
+++ core/build/webpack.client.config.js
@@ -18,7 +18,6 @@ const config = merge(base, {
   plugins: [
     // strip dev-only code in Vue source
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
       'process.env.VUE_ENV': '"client"'
     }),
     // generate output HTML
--- core/build/webpack.server.config.js
+++ core/build/webpack.server.config.js
@@ -19,7 +19,6 @@ module.exports = merge(base, {
   externals: Object.keys(require('../../package.json').dependencies),
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
       'process.env.VUE_ENV': '"server"'
     }),
     new VueSSRPlugin()
