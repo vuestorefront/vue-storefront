ebda70d9c5fd9b6ccdedca96d12573c8c0eb0184
Bartosz Dominiak <bartosz.dominiak@snow.dog>
2018-05-18 15:22:04 +0200

    added v-html for parsing html tags in langs

--- src/themes/default/components/core/blocks/Checkout/ThankYouPage.vue
+++ src/themes/default/components/core/blocks/Checkout/ThankYouPage.vue
@@ -18,21 +18,15 @@
             <h4>
               {{ $t('Your purchase') }}
             </h4>
-            <p v-online>
-              {{ $t('You have successfuly placed the order #121213 You can check status of your order by using our delivery status feature. You will receive an order confirmation e-mail with details of your order and a link to track its progress.') }}
-            </p>
-            <p v-online>
-              {{ $t('E-mail us at demo@vuestorefront.io with any questions, seuggestions how we could improve products or shopping experience') }}
-            </p>
+            <p v-online v-html="this.$t('You have successfuly placed the order <b>#121213</b> You can check status of your order by using our <b>delivery status</b> feature. You will receive an order confirmation e-mail with details of your order and a link to track its progress.')" />
+            <p v-online v-html="this.$t('E-mail us at <b>demo@vuestorefront.io</b> with any questions, seuggestions how we could improve products or shopping experience')"/>
             <p v-offline>
               {{ $t('Your order will be sent to the server as soon as you connect to the Internet and then confirmed regarding the stock quantities of selected items') }}
             </p>
             <h4>
               {{ $t('Your Account') }}
             </h4>
-            <p>
-              {{ $t('You can log to your account using e-mail and password defined earlier. On your account you can edit you\'r profile data, check history of transactions, edit subscription to newsletter.') }}
-            </p>
+            <p v-html="this.$t('You can log to your account using e-mail and password defined earlier. On your account you can <b>edit you\'r profile data,</b> check <b>history of transactions,</b> edit <b>subscription to newsletter.</b>')"/>
           </div>
           <div class="col-md-6 bg-cl-secondary thank-you-improvment">
             <h4>
