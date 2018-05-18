ffc663fd3f50ffaaa975e8e9e385683e13cfc0cf
David Rouyer <rouyer.david@gmail.com>
2018-05-18 23:40:15 +0200

    feat: disallow full lodash import

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    "no-restricted-imports": [2, { "paths": ["lodash"] }]
   }
 }
