5daaa2d8a4aedac41e021c4dfb9fba274bbf4b02
Bartek Igielski <igloczek@gmail.com>
2018-05-18 18:00:51 +0200

    Sidebar components imports fix

--- src/themes/default/components/core/blocks/Category/Sidebar.vue
+++ src/themes/default/components/core/blocks/Category/Sidebar.vue
@@ -80,10 +80,10 @@
 <script>
 import sidebar from 'core/components/blocks/Category/sidebar'
 
-import ColorSelector from '../../ColorSelector.vue'
-import SizeSelector from '../../SizeSelector.vue'
-import PriceSelector from '../../PriceSelector.vue'
-import GenericSelector from 'core/components/genericSelector'
+import ColorSelector from 'theme/components/core/ColorSelector'
+import SizeSelector from 'theme/components/core/SizeSelector'
+import PriceSelector from 'theme/components/core/PriceSelector'
+import GenericSelector from 'theme/components/core/GenericSelector'
 
 export default {
   components: {
