601f98becdc373d22ba3c88980c2086b922d0dc1
Alina Bugaj <abugaj@divante.pl>
2018-06-18 14:53:07 +0200

    Home-page test edited

--- cypress/integration/home-page.js
+++ cypress/integration/home-page.js
@@ -17,6 +17,8 @@ describe("home page", () => {
     cy.get(
       ".VueCarousel-inner .VueCarousel-slide:nth-of-type(3) .subtitle"
     ).should("contain", "What's new");
+    cy.get(".material-icons.icon").click();
+    cy.get(".pl0").should("be.visible");
     cy.get(".newsletter-button > .button-outline")
       .click()
       .should("contain", "Subscribe");
