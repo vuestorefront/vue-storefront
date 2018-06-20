7ecfd7ca3b7f282d0ca8f3f9da46dd9ba00c47bb
Filip Rakowski <frakowski@divante.pl>
2018-06-20 23:46:18 +0200

    Added destriyed hook to component, minor fixes

--- core/components/ContentBlock.js
+++ core/components/ContentBlock.js
@@ -3,16 +3,25 @@ export default {
     blockIdentifier: {
       required: true,
       type: String
+    },
+    clearStoreOnDestroy: {
+      type: Boolean,
+      default: () => true
     }
   },
   computed: {
     content () {
-      return this.$store.getters.getBlock(blockIdentifier) ? this.content = this.$store.getters.getBlock(blockIdentifier) : null
+      return this.$store.getters.getBlock(blockIdentifier) ? this.$store.getters.getBlock(blockIdentifier) : null
     }
   },
   created () {
     if (content === null) {
       this.$store.dispatch('content/addBlock', this.blockIdentifier)
+    }
+  },
+  destroyed () {
+    if (this.clearStoreOnDestroy) {
+      this.$store.commit('content/removeBlock', this.blockIdentifier)
     }
   }
 }
