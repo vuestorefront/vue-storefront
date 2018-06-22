079a2f002999dc0b3ced64a4c06e9b5c64aaecea
pkarw <pkarwatka@divante.pl>
2018-06-22 15:29:00 +0200

    Fixes

--- core/components/blocks/SearchPanel/SearchPanel.js
+++ core/components/blocks/SearchPanel/SearchPanel.js
@@ -28,11 +28,11 @@ export default {
       let size = 18
 
       let query = bodybuilder()
-        .orQuery('match', 'name', { query: queryText, boost: 3 })
-        .orQuery('match', 'category.name', { query: queryText, boost: 1 })
-        .orQuery('match', 'short_description', { query: queryText, boost: 2 })
-        .orQuery('match', 'description', { query: queryText, boost: 1 })
-        .filter('range', 'visibility', { 'gte': 3, 'lte': 4 })
+        .query('range', 'visibility', { 'gte': 3, 'lte': 4 })
+        .andQuery('bool', b => b.orQuery('match', 'name', { query: queryText, boost: 3 })
+          .orQuery('match', 'category.name', { query: queryText, boost: 1 })
+          .orQuery('match', 'short_description', { query: queryText, boost: 2 })
+          .orQuery('match', 'description', { query: queryText, boost: 1 }))
         .build()
 
       this.$store.dispatch('product/list', { query, start, size, updateState: false }).then((resp) => {
