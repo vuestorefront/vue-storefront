2c5b952d4f032187d4fa5b823c97c46f22e2217f
Ania Karon <anna.karon@snow.dog>
2018-06-27 10:36:26 +0200

    1142 catch error & add message when cms api module is not installed

--- src/extensions/cms/store.js
+++ src/extensions/cms/store.js
@@ -33,6 +33,9 @@ const actions = {
           context.commit(`setCms${type}`, data.result)
         }
       })
+      .catch(function () {
+        console.error('You need to install a custom Magento module from Snow.dog to make the CMS magick happen. Please go to https://github.com/SnowdogApps/magento2-cms-api and follow the instructions')
+      })
   }
 }
 
