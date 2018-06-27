ee0ef1041de8c3127ca17383742545f6a6d45dab
David Rouyer <rouyer.david@gmail.com>
2018-06-28 01:33:57 +0200

    chore: move service worker config

--- core/build/webpack.prod.client.config.js
+++ core/build/webpack.prod.client.config.js
@@ -6,6 +6,9 @@ const extendedConfig = require(path.join(themeRoot, '/webpack.config.js'))
 const SWPrecachePlugin = require('sw-precache-webpack-plugin')
 
 const prodClientConfig = merge(baseClientConfig, {
+  entry: {
+    'core-service-worker': path.resolve(__dirname, '../service-worker/index.js')
+  },
   plugins: [
     // auto generate service worker
     new SWPrecachePlugin({
