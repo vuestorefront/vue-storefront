e01da6d7cce2b41163ef3ebccdcb0d351dd624d4
NataliaTepluhina <tarya.se@gmail.com>
2018-08-07 13:24:25 +0300

    Revert "Revert "Feature/typescript""
    This reverts commit 1bfc485010195065dee64557a2bc7cd3d06d7194.

--- core/build/webpack.base.config.js
+++ core/build/webpack.base.config.js
@@ -71,7 +71,7 @@ module.exports = {
       path.resolve(__dirname, extensionsRoot),
       path.resolve(__dirname, themesRoot)
     ],
-    extensions: ['.js', '.vue'],
+    extensions: ['.js', '.vue', 'ts'],
     alias: {
       // Main aliases
       'config': path.resolve(__dirname, './config.json'),
@@ -124,6 +124,13 @@ module.exports = {
           optimizeSSR: false,
           preserveWhitespace: false,
           postcss: [autoprefixer()],
+        }
+      },
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        options: {
+          appendTsSuffixTo: [/\.vue$/]
         }
       },
       {
