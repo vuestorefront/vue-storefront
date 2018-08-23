7c9bdee7800a82e2126078ba957865cdf32788cd
Bartosz Liburski <bartosz.liburski@gmail.com>
2018-08-23 10:09:06 +0200

    fix v3

--- core/api/wishlist/features/closeWishlist.js
+++ core/api/wishlist/features/closeWishlist.js
@@ -2,7 +2,7 @@
  * [Helper] Optional UI functionality for closing wishlist.
  *
  * #### Methods
- * - **`closeWishlist`** sets `wishlist` and `overlay` property from features state to `false`. Dispatches `ui/setWishlist'` Vuex action
+ * - **`closeWishlist`** sets `wishlist` and `overlay` property from ui state to `false`. Dispatches `ui/setWishlist'` Vuex action
  *
  * Part of [Wishlist API Module](https://github.com/DivanteLtd/vue-storefront/tree/master/doc/api-modules)
  */
--- core/api/wishlist/features/openWishlist.js
+++ core/api/wishlist/features/openWishlist.js
@@ -2,7 +2,7 @@
  * [Helper] Optional UI functionality for opening wishlist.
  *
  * #### Methods
- * - **`openWishlist`** sets `wishlist` and `overlay` property from features state to `true`. Dispatches `ui/setWishlist'` Vuex action
+ * - **`openWishlist`** sets `wishlist` and `overlay` property from ui state to `true`. Dispatches `ui/setWishlist'` Vuex action
  *
  * Part of [Wishlist API Module](https://github.com/DivanteLtd/vue-storefront/tree/master/doc/api-modules)
  */
