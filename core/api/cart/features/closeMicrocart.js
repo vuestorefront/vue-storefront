7fa05f5d01ddb07889183de5353db01f7a52e90f
Bartosz Liburski <bartosz.liburski@gmail.com>
2018-08-23 14:24:19 +0200

    fix v4

--- core/api/cart/features/closeMicrocart.js
+++ core/api/cart/features/closeMicrocart.js
@@ -2,7 +2,7 @@
  * [Helper] Optional UI functionality for closing microcart.
  *
  * #### Methods
- * - **`closeMicrocart`** sets `microcart` and `overlay` property from features state to `false`. Dispatches `ui/setMicrocart'` Vuex action
+ * - **`closeMicrocart`** sets `microcart` and `overlay` property from ui state to `false`. Dispatches `ui/setMicrocart'` Vuex action
  *
  * Part of [Cart API Module](https://github.com/DivanteLtd/vue-storefront/tree/master/doc/api-modules)
  */
