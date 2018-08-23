cc33e1bae954c6c370db0e5b0ea3a22f0ef864e9
Bartosz Liburski <bartosz.liburski@gmail.com>
2018-08-23 10:06:40 +0200

    fix v3

--- core/api/wishlist/features/openWishlist.js
+++ core/api/wishlist/features/openWishlist.js
@@ -2,14 +2,14 @@
  * [Helper] Optional UI functionality for opening wishlist.
  *
  * #### Methods
- * - **`openWishlist`** sets `wishlist` and `overlay` property from features state to `true`. Dispatches `features/setWishlist'` Vuex action
+ * - **`openWishlist`** sets `wishlist` and `overlay` property from features state to `true`. Dispatches `ui/setWishlist'` Vuex action
  *
  * Part of [Wishlist API Module](https://github.com/DivanteLtd/vue-storefront/tree/master/doc/api-modules)
  */
 export const openWishlist = {
   methods: {
     openWishlist () {
-      this.$store.commit('features/setWishlist', true)
+      this.$store.commit('ui/setWishlist', true)
     }
   }
 }
