779cd3099ddc8594ac51a85d9a589952d44595ba
pkarw <pkarwatka@divante.pl>
2018-08-29 14:49:49 +0200

    Feature #1551

--- core/modules/wishlist/features/addToWishlist.ts
+++ core/modules/wishlist/features/addToWishlist.ts
@@ -6,9 +6,10 @@
  *
  * Part of [Wishlist API Module](https://github.com/DivanteLtd/vue-storefront/tree/master/doc/api-modules)
  */
+import Product from '@vue-storefront/store/types/product/Product'
 export const addToWishlist = {
   methods: {
-    addToWishlist (product) {
+    addToWishlist (product: Product) {
       return this.$store.state['wishlist'] ? this.$store.dispatch('wishlist/addItem', product) : false
     }
   }
