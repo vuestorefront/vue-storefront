4401a5de525f6e60b74e0a796b566f2c9457b3fd
pkarw <pkarwatka@divante.pl>
2018-08-29 15:55:03 +0200

    Root store config must be state after hydrating the store from __INITIAL_STATE__ for the case when __INITIAL_STATE__.config == null

--- core/client-entry.ts
+++ core/client-entry.ts
@@ -23,6 +23,7 @@ const config = store.state.config
 let storeCode = null // select the storeView by prefetched vuex store state (prefetched serverside)
 if (window.__INITIAL_STATE__) {
   store.replaceState(window.__INITIAL_STATE__)
+  store.state.config = config
 }
 if (config.storeViews.multistore === true) {
   if ((storeCode = store.state.user.current_storecode)) {
