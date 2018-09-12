b183b3db7912413435dc0ceef68d59425c434148
NataliaTepluhina <tarya.se@gmail.com>
2018-09-12 18:22:02 +0300

    Scaffolded vuepress docs structure

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  base: '/docs/',
+  themeConfig: {
+    editLinks: true,
+    editLinkText: 'Help us improve this page!',
+    nav: [],
+    sidebar: [],
+  },
+  title: 'Vue Storefront',
+  description: 'Headless PWA for eCommerce',
+};
