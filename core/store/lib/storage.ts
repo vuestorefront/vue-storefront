ca3a17e2db4a5a6e984c7fd779978a3c7386809d
pkarw <pkarwatka@divante.pl>
2018-09-14 20:46:01 +0200

    Revert "Cache errors count moved to Vue.prototype for persistence"
    This reverts commit e5ee11c7ed514cae7ed97c51a43059b6b0586007.

--- core/store/lib/storage.ts
+++ core/store/lib/storage.ts
@@ -13,16 +13,17 @@ class LocalForageCacheDriver {
   private _localForageCollection: any;
   private _persistenceErrorNotified: boolean;
   private _useLocalCacheByDefault: boolean;
+  private cacheErrorsCount: any;
   private localCache: any;
 
   constructor (collection, useLocalCacheByDefault = true) {
     const collectionName = collection._config.storeName
     const dbName = collection._config.name
-    if (typeof Vue.prototype.$cacheErrorsCount === 'undefined') {
-      Vue.prototype.$cacheErrorsCount = {}
+    if (typeof this.cacheErrorsCount === 'undefined') {
+      this.cacheErrorsCount = {}
     }
-    if (typeof Vue.prototype.$cacheErrorsCount[collectionName] === 'undefined') {
-      Vue.prototype.$cacheErrorsCount[collectionName] = 0
+    if (typeof this.cacheErrorsCount[collectionName] === 'undefined') {
+      this.cacheErrorsCount[collectionName] = 0
     }
     if (typeof Vue.prototype.$localCache === 'undefined') {
       Vue.prototype.$localCache = {}
@@ -81,7 +82,7 @@ class LocalForageCacheDriver {
     }
 
     if (!Vue.prototype.$isServer) {
-      if (Vue.prototype.$cacheErrorsCount[this._collectionName] >= DISABLE_PERSISTANCE_AFTER && this._useLocalCacheByDefault) {
+      if (this.cacheErrorsCount[this._collectionName] >= DISABLE_PERSISTANCE_AFTER && this._useLocalCacheByDefault) {
         if (!this._persistenceErrorNotified) {
           console.error('Persistent cache disabled becasue of previous errors [get]', key)
           this._persistenceErrorNotified = true
@@ -126,7 +127,7 @@ class LocalForageCacheDriver {
               this._persistenceErrorNotified = true
               this.recreateDb()
             }
-            Vue.prototype.$cacheErrorsCount[this._collectionName] = Vue.prototype.$cacheErrorsCount[this._collectionName] ? Vue.prototype.$cacheErrorsCount[this._collectionName] + 1 : 1
+            this.cacheErrorsCount[this._collectionName] = this.cacheErrorsCount[this._collectionName] ? this.cacheErrorsCount[this._collectionName] + 1 : 1
             if (isCallbackCallable) callback(null, typeof this._localCache[key] !== 'undefined' ? this._localCache[key] : null)
           }
         }, CACHE_TIMEOUT)
@@ -184,11 +185,11 @@ class LocalForageCacheDriver {
     setTimeout(() => {
       if (!isResolved) { // this is cache time out check
         if (!this._persistenceErrorNotified) {
-          console.error('Cache not responding within ' + CACHE_TIMEOUT_ITERATE + ' ms for [iterate]', Vue.prototype.$cacheErrorsCount[this._collectionName])
+          console.error('Cache not responding within ' + CACHE_TIMEOUT_ITERATE + ' ms for [iterate]', this.cacheErrorsCount[this._collectionName])
           this._persistenceErrorNotified = true
           this.recreateDb()
         }
-        Vue.prototype.$cacheErrorsCount[this._collectionName] = Vue.prototype.$cacheErrorsCount[this._collectionName] ? Vue.prototype.$cacheErrorsCount[this._collectionName] + 1 : 1
+        this.cacheErrorsCount[this._collectionName] = this.cacheErrorsCount[this._collectionName] ? this.cacheErrorsCount[this._collectionName] + 1 : 1
         if (isCallbackCallable) callback(null, null)
       }
     }, CACHE_TIMEOUT_ITERATE)
@@ -225,7 +226,7 @@ class LocalForageCacheDriver {
     const isCallbackCallable = (typeof callback !== 'undefined' && callback)
     this._localCache[key] = value
     if (!Vue.prototype.$isServer) {
-      if (Vue.prototype.$cacheErrorsCount[this._collectionName] >= DISABLE_PERSISTANCE_AFTER && this._useLocalCacheByDefault) {
+      if (this.cacheErrorsCount[this._collectionName] >= DISABLE_PERSISTANCE_AFTER && this._useLocalCacheByDefault) {
         if (!this._persistenceErrorNotified) {
           console.error('Persistent cache disabled becasue of previous errors [set]', key)
           this._persistenceErrorNotified = true
@@ -252,7 +253,7 @@ class LocalForageCacheDriver {
               this._persistenceErrorNotified = true
               this.recreateDb()
             }
-            Vue.prototype.$cacheErrorsCount[this._collectionName] = Vue.prototype.$cacheErrorsCount[this._collectionName] ? Vue.prototype.$cacheErrorsCount[this._collectionName] + 1 : 1
+            this.cacheErrorsCount[this._collectionName] = this.cacheErrorsCount[this._collectionName] ? this.cacheErrorsCount[this._collectionName] + 1 : 1
             if (isCallbackCallable) callback(null, null)
           }
         }, CACHE_TIMEOUT)
