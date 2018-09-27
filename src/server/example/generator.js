7b4736eba0d5e54e15edda32e4a3addb4ad0216c
pkarw <pkarwatka@divante.pl>
2018-09-27 21:20:35 +0200

    Example minor fix

--- src/server/example/generator.js
+++ src/server/example/generator.js
@@ -4,7 +4,7 @@ module.exports = (expressApp) => {
    * This is an example on how You can bind Your own Express.js server routes to SSR server running Vue Storefront.
    * It may be usefull to avoid all the Vue.js processing and context - and useful for example for large XML/binary file generation
    */
-  expressApp.get('/file.xml', (req, res) => {
-    res.end('<content />')
+  expressApp.get('/vue-storefront.xml', (req, res) => {
+    res.end('<content>Vue Storefront custom XML generator example</content>')
   })
 }
