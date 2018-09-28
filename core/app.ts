ca65482904ed12f37589e656773a96a6dbb65789
pkarw <pkarwatka@divante.pl>
2018-09-28 11:07:07 +0200

    Server context removed from Vue.prototype

--- core/app.ts
+++ core/app.ts
@@ -110,9 +110,6 @@ export function createApp (serverContext = null): { app: Vue, router: any, store
     provide: apolloProvider,
     render: h => h(App)
   })
-  
-  Vue.prototype.$serverContext = serverContext // this is context passed from Expressjs
-
   registerExtensions(
     union(extensionEntryPoints, themeExtensionEntryPoints),
     app,
