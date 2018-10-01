897958db37f12d7a1b04c67977a31e99f8684374
Filip Rakowski <frakowski@divante.pl>
2018-10-01 19:33:53 +0200

    Product.js cleared

--- core/components/blocks/Microcart/Product.js
+++ core/components/blocks/Microcart/Product.js
@@ -2,15 +2,18 @@ import Product from '@vue-storefront/core/modules/cart/components/Product.ts'
 
 export default {
   data () {
+    // depreciated
     return {
       qty: 0,
       isEditing: false
     }
   },
   beforeMount () {
+    // depreciated, will be partially moved to theme in the near future
     this.$bus.$on('cart-after-itemchanged', this.onProductChanged)
   },
   beforeDestroy () {
+    // depreciated, will be partially moved to theme in the near future
     this.$bus.$off('cart-after-itemchanged', this.onProductChanged)
   },
   methods: {
@@ -24,11 +27,13 @@ export default {
       this.isEditing = !this.isEditing
     },
     onProductChanged (event) {
+      // depreciated
       if (event.item.sku === this.product.sku) {
         this.$forceUpdate()
       }
     },
     switchEdit () {
+      // depreciated, will be partially moved to theme in the near future
       this.isEditing ? this.updateQuantity() : this.qty = this.product.qty
       this.isEditing = !this.isEditing
     }
