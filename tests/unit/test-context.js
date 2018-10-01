bbb10735ed234935f08ab002cc613ff2a23c23ff
Filip Rakowski <frakowski@divante.pl>
2018-10-01 18:39:32 +0200

    changed path to tests to fit modules

--- tests/unit/test-context.js
+++ tests/unit/test-context.js
@@ -1,2 +1,5 @@
 var context = require.context('./', true, /\.spec\.js$/)
 context.keys().forEach(context)
+
+var modulesContext = require.context('./../../core/modules/', true, /\.spec\.js$/)
+context.keys().forEach(modulesContext)
