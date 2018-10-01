2e4c3141d3ed8a486f50c89694d23608f3f8111f
Filip Rakowski <frakowski@divante.pl>
2018-10-01 18:57:46 +0200

    tweaks for tests

--- tests/unit/test-context.js
+++ tests/unit/test-context.js
@@ -1,5 +1,3 @@
 var context = require.context('./', true, /\.spec\.js$/)
 context.keys().forEach(context)
 
-var modulesContext = require.context('./../../core/modules/', true, /\.spec\.js$/)
-context.keys().forEach(modulesContext)
