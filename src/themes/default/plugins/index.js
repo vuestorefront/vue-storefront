64c3e53e38b356d0d209275b58c45a1e5c66a2d9
Tomasz Duda <tom@mart.best>
2018-10-02 19:36:09 +0200

    Stage 2

--- src/themes/default/plugins/index.js
+++ src/themes/default/plugins/index.js
@@ -1,6 +1,6 @@
 // Add your own plugins in this folder with Vue.use()
-// import Vue from 'vue'
-//
-// import VueProgressBar from 'vue-progressbar'
-//
-// Vue.use(VueProgressBar)
+import Vue from 'vue'
+
+import VueProgressBar from 'vue-progressbar'
+
+Vue.use(VueProgressBar)
