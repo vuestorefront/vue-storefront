cb510354b8e6c490cc67d0ece1d06ee7e5dbc51a
pkarw <pkarwatka@divante.pl>
2018-10-10 09:51:03 +0200

    Force SSR disable for my-account and checkout pages

--- core/pages/MyAccount.js
+++ core/pages/MyAccount.js
@@ -68,6 +68,7 @@ export default {
   asyncData ({ store, route, context }) { // this is for SSR purposes to prefetch data
     return new Promise((resolve, reject) => {
       if (context) context.output.cacheTags.add(`my-account`)
+      if (context) context.server.response.redirect('/')
       resolve()
     })
   }
