9e80426288b2d47e00718bddce9871e06ef01d37
Filip Rakowski <frakowski@divante.pl>
2018-10-21 11:15:56 +0200

    Removed getters from wishlist

--- core/modules/wishlist/store/index.ts
+++ core/modules/wishlist/store/index.ts
@@ -1,6 +1,5 @@
 import { Module } from 'vuex'
 import actions from './actions'
-import getters from './getters'
 import mutations from './mutations'
 import RootState from '@vue-storefront/store/types/RootState'
 import WishlistState from '../types/WishlistState'
@@ -10,7 +9,6 @@ export const module:Module<WishlistState, RootState> = {
   state: {
     items: []
   },
-  getters,
   actions,
   mutations
 }
