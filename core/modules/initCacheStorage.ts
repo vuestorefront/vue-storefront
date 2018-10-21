4b85bb5e3f96fbd65acf78702f6345a0bd57db33
Filip Rakowski <frakowski@divante.pl>
2018-10-21 13:17:48 +0200

    cache storage inits with local storage by default

--- core/modules/initCacheStorage.ts
+++ core/modules/initCacheStorage.ts
@@ -1,15 +1,28 @@
 import * as localForage from 'localforage'
 import UniversalStorage from '@vue-storefront/core/store/lib/storage'
 import { currentStoreView } from '@vue-storefront/store/lib/multistore'
-// use it to initialize cache storage in modules
-export function initCacheStorage(key) {
+import rootStore from '@vue-storefront/store'
+
+
+export function initCacheStorage(key, localised = true) {
   const storeView = currentStoreView()
   const dbNamePrefix = storeView.storeCode ? storeView.storeCode + '-' : ''
+  const config = rootStore.state.config
+  const cacheDriver = config.localForage && config.localForage.defaultDrivers['orders'] || 'LOCALSTORAGE'
 
-  const cacheStorage = new UniversalStorage(localForage.createInstance({
-    name: dbNamePrefix + 'shop',
-    storeName: key
-  }))
-
-  return cacheStorage
+  if (localised) {
+    const cacheStorage = new UniversalStorage(localForage.createInstance({
+      name: dbNamePrefix + 'shop',
+      storeName: key,
+      driver: localForage[cacheDriver]
+    }))
+    return cacheStorage
+  } else {
+    const cacheStorage = new UniversalStorage(localForage.createInstance({
+      name: 'shop',
+      storeName: key,
+      driver: localForage[cacheDriver]
+    }))
+    return cacheStorage
+  }
 }
