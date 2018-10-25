50f3d26e61a6e39ef98a9ded306fbfae17465fdd
Filip Rakowski <frakowski@divante.pl>
2018-10-25 19:56:55 +0200

    Moved all ready modules registration to theme

--- core/modules-entry.ts
+++ core/modules-entry.ts
@@ -1,18 +1,10 @@
 
 import { VueStorefrontModule } from './modules'
-import { Cart } from './modules/cart'
-import { Review } from './modules/review'
 import { Wishlist } from './modules/wishlist'
 import { Mailer } from './modules/mailer'
 import { registerModules } from 'theme/modules'
 
-
-
-// This modules will be registered in the app
 export const enabledModules: VueStorefrontModule[] = [
-  Cart,
-  Review,
-  Mailer,
   Wishlist,
   ...registerModules
 ]
