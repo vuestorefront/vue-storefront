7bb86a4dc4f4d87d9466b8cd836b6a9eafa20e04
Filip Rakowski <frakowski@divante.pl>
2018-10-25 19:13:52 +0200

    Added posibility to extend currently existing vuex stores from modules

--- core/modules-entry.ts
+++ core/modules-entry.ts
@@ -1,12 +1,24 @@
 
-import { VueStorefrontModule } from './modules'
+import { VueStorefrontModule, VueStorefrontModuleConfig } from './modules'
 import { Cart } from './modules/cart'
 import { Review } from './modules/review'
 import { Wishlist } from './modules/wishlist'
 import { Mailchimp } from './modules/mailchimp'
 import { Mailer } from './modules/mailer'
-// import { Example } from './modules/module-template'
+import { Example } from './modules/module-template'
 
+
+// You can extend any module before registration like this
+const extendedExample: VueStorefrontModuleConfig = {
+  key: 'extend',
+  afterRegistration: function(Vue, config) {
+    console.info('Hello, im extended now')
+  }
+}
+
+Example.extend(extendedExample)
+
+// This modules will be registered in the app
 export const enabledModules: VueStorefrontModule[] = [
   Cart,
   Review,
