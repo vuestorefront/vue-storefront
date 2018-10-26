22ef24d235595213cce11caa494f6b0e775e9153
PhoenixDev - JS <jane.sotnykova@phoenix-media.eu>
2018-10-26 16:40:07 +0200

    #854 added config option

--- core/components/blocks/Microcart/Product.js
+++ core/components/blocks/Microcart/Product.js
@@ -1,3 +1,4 @@
+import rootStore from '@vue-storefront/store'
 import { MicrocartProduct } from '@vue-storefront/core/modules/cart/components/Product.ts'
 import i18n from '../../../i18n'
 
@@ -21,14 +22,18 @@ export default {
   },
   methods: {
     removeItem () {
-      this.$bus.$emit('notification', {
-        type: 'warning',
-        item: this.product,
-        message: i18n.t('Are you sure you would like to remove this item from the shopping cart?'),
-        action2: { label: i18n.t('OK'), action: 'itemremoved' },
-        action1: { label: i18n.t('Cancel'), action: 'close' },
-        hasNoTimeout: true
-      })
+      if (rootStore.state.config.cart.askBeforeRemoveProduct) {
+        this.$bus.$emit('notification', {
+          type: 'warning',
+          item: this.product,
+          message: i18n.t('Are you sure you would like to remove this item from the shopping cart?'),
+          action2: { label: i18n.t('OK'), action: 'itemremoved' },
+          action1: { label: i18n.t('Cancel'), action: 'close' },
+          hasNoTimeout: true
+        })
+      } else {
+        this.removeFromCart()
+      }
     },
     updateQuantity () {
       // additional logic will be moved to theme
