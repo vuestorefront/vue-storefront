4fa947b1981afb155d77f28a1c203730edbd8793
Pawel Szafranski <pawel.szafranski@snow.dog>
2018-10-30 00:50:21 +0100

    #1903 - removed useless files

--- core/modules/compare/components/IsOnCompare.ts
+++
@@ -1,9 +0,0 @@
-
-export const IsOnCompare = {
-  name: 'IsOnCompare',
-  computed: {
-    isOnCompare () {
-      return !!this.$store.state.compare.items.find(p => p.sku === this.product.sku)
-    }
-  }
-}
