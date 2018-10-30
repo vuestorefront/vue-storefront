5a180d8a8a50440f56c34321f5fcc45d166c703b
Filip Rakowski <frakowski@divante.pl>
2018-10-30 14:52:20 +0100

    removed setOpenMyAccound, moved user-related logic from client-entry

--- core/modules/user/hooks/afterRegistration.ts
+++ core/modules/user/hooks/afterRegistration.ts
@@ -1,10 +1,21 @@
 export function afterRegistration(Vue, config, store) {
   if (!Vue.prototype.$isServer) {
     store.dispatch('user/startSession')
+
     Vue.prototype.$bus.$on('user-before-logout', () => {
       store.dispatch('user/logout', { silent: false })
+      //TODO: Move it to theme
       store.commit('ui/setSubmenu', {
         depth: 0
+      })
+    })
+
+    Vue.prototype.$bus.$on('user-after-loggedin', receivedData => {
+      //TODO: Make independent of checkout module
+      store.dispatch('checkout/savePersonalDetails', {
+        firstName: receivedData.firstname,
+        lastName: receivedData.lastname,
+        emailAddress: receivedData.email
       })
     })
   }
