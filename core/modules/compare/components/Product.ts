29e322453736b55b26f43fbd666dc340699a31c4
Pawel Szafranski <pawel.szafranski@snow.dog>
2018-10-30 02:28:25 +0100

    #1903 - updated wrong var and removed prev compare store modules

--- core/modules/compare/components/Product.ts
+++ core/modules/compare/components/Product.ts
@@ -1,7 +1,7 @@
 import Product from '@vue-storefront/store/types/product/Product'
 
-export const Product = {
-  name: 'Product',
+export const CompareProduct = {
+  name: 'CompareProduct',
   computed: {
     isOnCompare () : boolean {
       return !!this.$store.state.compare.items.find(p => p.sku === this.product.sku)
