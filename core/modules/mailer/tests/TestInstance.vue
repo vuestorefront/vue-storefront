da7cba50e340b9143f126848cbb1f46d8a2ae07c
Akbar Abdrakhmanov <kz.akbar@gmail.com>
2018-10-30 21:22:49 +0100

    tests for Mailer and Notification modules, edited docs for Notification module

---
+++ core/modules/mailer/tests/TestInstance.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button id="send-email" @click="sendEmail(letter)">Send email</button>
+  </div>
+</template>
+
+<script>
+import { EmailForm } from '../components/EmailForm'
+
+export default {
+  data () {
+    return {
+      letter: {
+        sourceAddress: 'address1@domain.com',
+        targetAddress: 'address2@domain.com',
+        subject: 'Test subject',
+        emailText: 'Test email body'
+      }
+    }
+  },
+  // And register as a mixin
+  mixins: [
+    EmailForm
+  ]
+}
+</script>
