417584da0c888dbeec6cedec2e465c938fe9b97e
Pawel Szafranski <pawel.szafranski@snow.dog>
2018-10-31 00:02:53 +0100

    1903 - removed useless semicolon

--- core/modules/compare/store/index.ts
+++ core/modules/compare/store/index.ts
@@ -2,7 +2,7 @@ import { Module } from 'vuex'
 import actions from './actions'
 import getters from './getters'
 import mutations from './mutations'
-import RootState from "@vue-storefront/store/types/RootState";
+import RootState from "@vue-storefront/store/types/RootState"
 import CompareState from '../types/CompareState'
 
 export const module: Module<CompareState, RootState> = {
