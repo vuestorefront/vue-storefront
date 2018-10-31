a2283919224471d797a0b6f87580af36911490a3
Filip Rakowski <frakowski@divante.pl>
2018-10-31 22:12:44 +0100

    UserSingleOrder refactored

--- core/modules/order/components/UserSingleOrder.ts
+++ core/modules/order/components/UserSingleOrder.ts
@@ -3,26 +3,6 @@
  */
 export const UserSingleOrder = {
   name: 'UserSingleOrder',
-  data () {
-    return {
-      shippingAddress: {
-        'firstname': '',
-        'lastname': '',
-        'street': '',
-        'postcode': '',
-        'city': '',
-        'country': ''
-      },
-      billingAddress: {
-        'firstname': '',
-        'lastname': '',
-        'street': '',
-        'postcode': '',
-        'city': '',
-        'country': ''
-      },
-    }
-  },
   computed: {
     ordersHistory () {
       return this.$store.state.user.orders_history.items
@@ -34,23 +14,15 @@ export const UserSingleOrder = {
     },
     paymentMethod () {
       return this.order.payment.additional_information[0]
+    },
+    billingAddress () {
+      return this.order.billing_address
+    },
+    shippingAddress () {
+      return this.order.extension_attributes.shipping_assignments[0].shipping.address
     }
   },
-  beforeMount () {
-    this.$bus.$on('user-after-loaded-orders', this.onOrdersLoaded)
-  },
-  beforeDestroy () {
-    this.$bus.$off('user-after-loaded-orders', this.onOrdersLoaded)
-  },
-  mounted () {
-    this.billingAddress = this.getAddress(this.order.billing_address)
-    this.shippingAddress = this.getAddress(this.order.extension_attributes.shipping_assignments[0].shipping.address)
-  },
   methods: {
-    onOrdersLoaded () {
-      this.billingAddress = this.getAddress(this.order.billing_address)
-      this.shippingAddress = this.getAddress(this.order.extension_attributes.shipping_assignments[0].shipping.address)
-    },
     remakeOrder (items) {
       items.forEach(item => {
         this.$store.dispatch('product/single', { options: { sku: item.sku }, setCurrentProduct: false, selectDefaultVariant: false }).then((product) => {
@@ -58,16 +30,6 @@ export const UserSingleOrder = {
           this.$store.dispatch('cart/addItem', { productToAdd: product }).then(() => { })
         })
       })
-    },
-    getAddress (addressObject) {
-      return {
-        'firstname': addressObject.firstname,
-        'lastname': addressObject.lastname,
-        'street': addressObject.street,
-        'postcode': addressObject.postcode,
-        'city': addressObject.city,
-        'country': addressObject.country_id
-      }
     },
     skipGrouped (items) {
       return items.filter((item) => {
