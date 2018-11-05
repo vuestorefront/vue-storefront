a8084cf67d7c00d5a0663b330f80521fa2b2b5a0
Filip Rakowski <frakowski@divante.pl>
2018-11-05 11:37:59 +0100

    Changed pathes to point to @vue-storefront/module package

--- core/modules/cart/index.ts
+++ core/modules/cart/index.ts
@@ -1,5 +1,5 @@
 import { module } from './store'
-import { VueStorefrontModule, VueStorefrontModuleConfig } from '@vue-storefront/core/modules'
+import { VueStorefrontModule, VueStorefrontModuleConfig } from '@vue-storefront/module'
 import { afterRegistration } from './hooks/afterRegistration'
 
 export const KEY = 'cart'
--- core/modules/notification/index.ts
+++ core/modules/notification/index.ts
@@ -1,5 +1,5 @@
 import { module } from './store'
-import { VueStorefrontModule, VueStorefrontModuleConfig } from '@vue-storefront/core/modules'
+import { VueStorefrontModule, VueStorefrontModuleConfig } from '@vue-storefront/module'
 
 export const KEY = 'notification'
 
