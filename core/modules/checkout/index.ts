570b31a1d9f79682eec2ae595ad47b3d858d3a84
Filip Rakowski <frakowski@divante.pl>
2018-11-14 20:01:27 +0100

    Moved shipping to checkout module

--- core/modules/checkout/index.ts
+++ core/modules/checkout/index.ts
@@ -1,5 +1,6 @@
 import { checkoutModule } from './store/checkout'
 import { paymentModule } from './store/payment'
+import { shippingModule } from './store/shipping'
 
 import { VueStorefrontModule, VueStorefrontModuleConfig } from '@vue-storefront/module'
 
@@ -9,6 +10,7 @@ const moduleConfig: VueStorefrontModuleConfig = {
   key: KEY,
   store: { modules: [
     { key: 'payment', module: paymentModule },
+    { key: 'shipping', module: shippingModule },
     { key: 'checkout', module: checkoutModule },
   ] },
 }
--- core/store/modules/index.ts
+++ core/store/modules/index.ts
@@ -1,10 +1,8 @@
-import shipping from './shipping'
 import ui from './ui-store'
 import sync from './sync'
 import promoted from './promoted-offers'
 
 export default {
-  shipping,
   ui,
   sync,
   promoted
