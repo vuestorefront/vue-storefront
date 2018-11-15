b89e00942b2e85e721c8032383e7d789e0d96ec7
Filip Rakowski <frakowski@divante.pl>
2018-11-15 18:54:32 +0100

    Lint fix

--- core/lib/themes.js
+++ core/lib/themes.js
@@ -1,10 +1,3 @@
-import merge from 'lodash-es/merge'
-
-/** DEPRECIATED will be removed in 1.7 */
-export function extendStore (coreStore, extendStore) {
-  return merge(coreStore, extendStore)
-}
-
 export function registerTheme (themeName, app, routes, store, config, ssrContext) {
   let themeEntryPoint = require('theme/index.js')
   if (themeEntryPoint != null && themeEntryPoint.hasOwnProperty('default')) {
