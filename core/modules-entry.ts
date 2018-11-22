d575879a5c5e8b82221ebca5e2464a1674b417ac
PhoenixDev - YB <yuri.boyko@phoenix-media.eu>
2018-11-22 17:57:30 +0200

    Revert "Add Data Manager module"
    This reverts commit e5d2c1c8e5c9865d87dc949f4a219e6ddaa1afb3.

--- core/modules-entry.ts
+++ core/modules-entry.ts
@@ -2,7 +2,6 @@
 import { VueStorefrontModule } from '@vue-storefront/module'
 import { Wishlist } from './modules/wishlist'
 import { Cms } from './modules/cms'
-import { DataManager } from './modules/data-manager'
 import { Order } from './modules/order'
 import { User } from './modules/user'
 import { registerModules } from 'src/modules'
@@ -11,7 +10,6 @@ export const enabledModules: VueStorefrontModule[] = [
   Wishlist,
   Cms,
   Order,
-  DataManager,
   User,
   ...registerModules
 ]
--- core/modules/data-manager/types/DataManagerState.ts
+++
@@ -1,3 +0,0 @@
-export default interface DataManagerState {
-  fetchOperations: any[]
-}
