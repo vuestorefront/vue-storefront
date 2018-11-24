bdf39d59390905f323bc25c90a9758f36a8141f0
Filip Rakowski <frakowski@divante.pl>
2018-11-24 16:32:09 +0100

    VS logger final polishments

--- core/modules/catalog/store/product/actions.ts
+++ core/modules/catalog/store/product/actions.ts
@@ -656,7 +656,7 @@ const actions: ActionTree<ProductState, RootState> = {
     } else {
       context.state.productLoadPromise = new Promise((resolve, reject) => {
         context.state.productLoadStart = Date.now()
-        Logger.info('Calling fetchAsync for Product Page (core)' ,  { tag: 'cart', context: { label: 'Original cart item', value: {parentSku, childSku} }})
+        Logger.info('Fetching product data asynchronously' ,  { tag: 'product', context: { label: 'Parent and child SKUs', value: {parentSku, childSku} }})
         Vue.prototype.$bus.$emit('product-before-load', { store: rootStore, route: route })
         context.dispatch('reset').then(() => {
           rootStore.dispatch('attribute/list', { // load attributes to be shown on the product details
