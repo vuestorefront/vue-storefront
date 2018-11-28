4acee54d27f4be37d6674c8551df06429b2acba6
Filip Rakowski <frakowski@divante.pl>
2018-11-28 11:24:38 +0100

    Extensions entry temp removed from modules

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -31,12 +31,6 @@ const extendedExample = new VueStorefrontModule({
 
 Example.extend(extendedExample)
 
-/** 
- * If you have some extensions that are not yet ported to modules you can register them here
- * Keep in mind that extensions will be depreciated in next versions of Vue Storefront and replaced by modules
- */
-export const registerExtensions = []
-
 export const registerModules: VueStorefrontModule[] = [
   Checkout,
   Catalog,
