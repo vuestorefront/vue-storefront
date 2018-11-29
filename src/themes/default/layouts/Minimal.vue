29238e83d282fd1a0536bb4581b4c599e52abb2a
pkarw <pkarwatka@divante.pl>
2018-11-29 13:18:24 +0100

    Error page fixes

--- src/themes/default/layouts/Minimal.vue
+++ src/themes/default/layouts/Minimal.vue
@@ -2,8 +2,6 @@
   <div id="app">
     <div id="viewport" class="w-100 relative">
       <minimal-header/>
-      <search-panel/>
-      <wishlist/>
       <router-view/>
       <minimal-footer/>
     </div>
@@ -16,8 +14,6 @@ import MinimalHeader from 'theme/components/core/blocks/Header/MinimalHeader.vue
 import MinimalFooter from 'theme/components/core/blocks/Footer/MinimalFooter.vue'
 
 import Head from 'theme/resource/head'
-import SearchPanel from '../components/core/blocks/SearchPanel/SearchPanel'
-import Wishlist from '../components/core/blocks/Wishlist/Wishlist'
 
 export default {
   data () {
@@ -38,8 +34,6 @@ export default {
   },
   metaInfo: Head,
   components: {
-    Wishlist,
-    SearchPanel,
     MinimalHeader,
     MinimalFooter
   }
