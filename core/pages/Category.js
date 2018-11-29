eb8b8bd9dd859142238f893d806f86465111a9b1
pkarw <pkarwatka@divante.pl>
2018-11-29 13:38:06 +0100

    Missing import added

--- core/pages/Category.js
+++ core/pages/Category.js
@@ -5,7 +5,7 @@ import store from '@vue-storefront/store'
 import EventBus from '@vue-storefront/core/compatibility/plugins/event-bus'
 import { baseFilterProductsQuery, buildFilterProductsQuery } from '@vue-storefront/store/helpers'
 import { htmlDecode } from '@vue-storefront/core/filters/html-decode'
-import { localizedRoute } from '@vue-storefront/store/lib/multistore'
+import { currentStoreView, localizedRoute } from '@vue-storefront/store/lib/multistore'
 import Composite from '@vue-storefront/core/mixins/composite'
 import { Logger } from '@vue-storefront/core/lib/logger'
 
