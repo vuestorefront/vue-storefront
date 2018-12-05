2531e7df81a373e065aba118189c1b9a1be227e4
pkarw <pkarwatka@divante.pl>
2018-12-05 14:54:56 +0100

    Shamefull space.

--- core/mixins/thumbnail.js
+++ core/mixins/thumbnail.js
@@ -12,6 +12,7 @@ export const thumbnail = {
       if (store.state.config.images.useExactUrlsNoProxy) {
         return relativeUrl // this is exact url mode
       } else {
+        let resultUrl
         if (relativeUrl && (relativeUrl.indexOf('://') > 0 || relativeUrl.indexOf('?') > 0 || relativeUrl.indexOf('&') > 0)) relativeUrl = encodeURIComponent(relativeUrl)
         let baseUrl = store.state.config.images.baseUrl
         if (baseUrl.indexOf('{{') >= 0) {
@@ -20,7 +21,7 @@ export const thumbnail = {
           baseUrl = baseUrl.replace('{{height}}', height)
           resultUrl = baseUrl
         } else {
-          resultUrl = `${baseUrl}${parseInt(width)}/${parseInt(height)}/resize${relativeUrl}` 
+          resultUrl = `${baseUrl}${parseInt(width)}/${parseInt(height)}/resize${relativeUrl}`
         }
         return relativeUrl && relativeUrl.indexOf('no_selection') < 0 ? resultUrl : store.state.config.images.productPlaceholder || ''
       }
