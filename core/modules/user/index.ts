2ee838184130386990558c5f636c7d667076828d
Javier Villanueva <javiervd@gmail.com>
2018-12-09 20:49:49 +0000

    Move auth guard to user module

--- core/modules/user/index.ts
+++ core/modules/user/index.ts
@@ -1,13 +1,17 @@
 import { module } from './store'
 import { VueStorefrontModule, VueStorefrontModuleConfig } from '@vue-storefront/core/lib/module'
+import { beforeEach } from './router/beforeEach'
 import { afterRegistration } from './hooks/afterRegistration'
+import { initCacheStorage } from '@vue-storefront/core/helpers/initCacheStorage'
 
 export const KEY = 'user'
+export const cacheStorage = initCacheStorage(KEY)
 
 const moduleConfig: VueStorefrontModuleConfig = {
   key: KEY,
   store: { modules: [{ key: KEY, module }] },
-  afterRegistration
+  afterRegistration,
+  router: { beforeEach }
 }
 
 export const User = new VueStorefrontModule(moduleConfig)
