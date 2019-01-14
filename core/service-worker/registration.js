6222878d8857979f311091b32605a36dca7d66a2
Patryk Tomczyk <13100280+patzick@users.noreply.github.com>
2019-01-14 16:19:18 +0100

    delay serviceWorker registration

--- core/service-worker/registration.js
+++ core/service-worker/registration.js
@@ -2,25 +2,29 @@ import { register } from 'register-service-worker'
 import { server } from 'config'
 
 if (process.env.NODE_ENV === 'production' || server.devServiceWorker) {
-  register(`/service-worker.js`, {
-    ready () {
-      console.log(
-        'App is being served from cache by a service worker.'
-      )
-    },
-    cached () {
-      console.log('Content has been cached for offline use.')
-    },
-    updated (registration) {
-      console.log('New content is available, please refresh.')
-    },
-    offline () {
-      console.log(
-        'No internet connection found. App is running in offline mode.'
-      )
-    },
-    error (error) {
-      console.error('Error during service worker registration:', error)
-    }
+  window.addEventListener('load', () => {
+    setTimeout(() => {
+      register(`/service-worker.js`, {
+        ready () {
+          console.log(
+            'App is being served from cache by a service worker.'
+          )
+        },
+        cached () {
+          console.log('Content has been cached for offline use.')
+        },
+        updated (registration) {
+          console.log('New content is available, please refresh.')
+        },
+        offline () {
+          console.log(
+            'No internet connection found. App is running in offline mode.'
+          )
+        },
+        error (error) {
+          console.error('Error during service worker registration:', error)
+        }
+      })
+    }, 500)
   })
 }
