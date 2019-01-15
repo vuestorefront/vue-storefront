b45e70a3c2e84289e573203aebc3e8fed03f5d25
Filip Rakowski <frakowski@divante.pl>
2019-01-15 14:23:59 +0100

    remove module template from module entry

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -7,7 +7,6 @@ import { Compare } from '@vue-storefront/core/modules/compare'
 import { Review } from '@vue-storefront/core/modules/review'
 import { Mailer } from '@vue-storefront/core/modules/mailer'
 import { Mailchimp } from '../modules/mailchimp'
-import { Example } from './module-template'
 import { Notification } from '@vue-storefront/core/modules/notification'
 import { RecentlyViewed } from '@vue-storefront/core/modules/recently-viewed'
 import { Homepage } from "./homepage"
@@ -67,5 +66,4 @@ export const registerModules: VueStorefrontModule[] = [
   PaymentCashOnDelivery,
   RawOutputExample,
   AmpRenderer,
-  Example
 ]
