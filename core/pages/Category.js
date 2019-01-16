2ab48553cf25547a6ddba50a05aee774079b5977
pkarw <pkarwatka@divante.pl>
2019-01-16 23:16:42 +0100

    Improved offline mode handlers

--- core/pages/Category.js
+++ core/pages/Category.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import toString from 'lodash-es/toString'
 
+import i18n from '@vue-storefront/i18n'
 import store from '@vue-storefront/store'
 import EventBus from '@vue-storefront/core/compatibility/plugins/event-bus'
 import { baseFilterProductsQuery, buildFilterProductsQuery } from '@vue-storefront/core/helpers'
@@ -241,6 +242,15 @@ export default {
           }
           this.$store.dispatch('category/products', this.$store.state.category.current_product_query)
           EventBus.$emitFilter('category-after-load', { store: this.$store, route: this.$route })
+        }
+      }).catch(err => {
+        if (err.message.indexOf('query returned empty result') > 0) {
+          this.$store.dispatch('notification/spawnNotification', {
+            type: 'error',
+            message: i18n.t('The product, category or CMS page is not available in Offline mode. Redirecting to Home.'),
+            action1: { label: i18n.t('OK') }
+          })
+          this.$router.push(localizedRoute('/', currentStoreView().storeCode))
         }
       })
     },
