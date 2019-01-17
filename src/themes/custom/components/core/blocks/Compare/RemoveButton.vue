579b54efecfb6247f2f5f0936026cf5a6fdd7423
Filip Rakowski <frakowski@divante.pl>
2019-01-17 10:41:35 +0100

    remove custom layout

--- src/themes/custom/components/core/blocks/Compare/RemoveButton.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <button
-    class="brdr-none bg-cl-transparent p15"
-    :aria-label="$t('Remove')"
-    :title="$t('Remove')"
-  >
-    <i class="material-icons h4">close</i>
-  </button>
-</template>
