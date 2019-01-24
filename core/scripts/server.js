1b359f3450b8602ea6774f90b4b6eabe9a0c359b
Filip Rakowski <frakowski@divante.pl>
2019-01-24 10:31:42 +0100

    compilation html

--- core/scripts/server.js
+++ core/scripts/server.js
@@ -138,20 +138,80 @@ app.get('*', (req, res, next) => {
       next()
     }
   }
+  const HTMLContent = `
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <title>Compiling</title>
+      <meta http-equiv="refresh" content="10">
+    </head>
+    <body>
+      <div class="lds-css ng-scope"><div style="width:100%;height:100%" class="lds-ball"><div></div></div><style type="text/css">@keyframes lds-ball {
+        0%, 100% {
+          animation-timing-function: cubic-bezier(0.45, 0, 0.9, 0.55);
+        }
+        0% {
+          -webkit-transform: translate(0, 0);
+          transform: translate(0, 0);
+        }
+        50% {
+          -webkit-transform: translate(0, 108px);
+          transform: translate(0, 108px);
+          animation-timing-function: cubic-bezier(0, 0.45, 0.55, 0.9);
+        }
+        100% {
+          -webkit-transform: translate(0, 0);
+          transform: translate(0, 0);
+        }
+      }
+      @-webkit-keyframes lds-ball {
+        0%, 100% {
+          animation-timing-function: cubic-bezier(0.45, 0, 0.9, 0.55);
+        }
+        0% {
+          -webkit-transform: translate(0, 0);
+          transform: translate(0, 0);
+        }
+        50% {
+          -webkit-transform: translate(0, 108px);
+          transform: translate(0, 108px);
+          animation-timing-function: cubic-bezier(0, 0.45, 0.55, 0.9);
+        }
+        100% {
+          -webkit-transform: translate(0, 0);
+          transform: translate(0, 0);
+        }
+      }
+      .lds-ball {
+        position: absolute;
+        top: 45%;
+        left: 45%;
+      }
+      .lds-ball div {
+        position: absolute;
+        width: 52px;
+        height: 52px;
+        border-radius: 50%;
+        background: #00db6e;
+        left: 74px;
+        top: 20px;
+        -webkit-animation: lds-ball 1s linear infinite;
+        animation: lds-ball 1s linear infinite;
+      }
+      .lds-ball {
+        width: 200px !important;
+        height: 200px !important;
+        -webkit-transform: translate(-100px, -100px) scale(1) translate(100px, 100px);
+        transform: translate(-100px, -100px) scale(1) translate(100px, 100px);
+      }
+      </style></div>
+    </body>
+    </html> `
 
   const dynamicRequestHandler = renderer => {
     if (!renderer) {
       res.setHeader('Content-Type', 'text/html')
-      res.status(202).end('<html lang="en">\n' +
-          '    <head>\n' +
-          '      <meta charset="utf-8">\n' +
-          '      <title>Loading</title>\n' +
-          '      <meta http-equiv="refresh" content="10">\n' +
-          '    </head>\n' +
-          '    <body>\n' +
-          '      Vue Storefront: waiting for compilation... refresh in 30s :-) Thanks!\n' +
-          '    </body>\n' +
-          '  </html>')
+      res.status(202).end(HTMLContent)
       return next()
     }
     const context = {
