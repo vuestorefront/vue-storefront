87f50c4fd3e9e94d0f0108451b9edaf9b45b234d
patzick <tomczyk.patryk@gmail.com>
2019-01-31 22:48:14 +0100

    Fix Compare module, to not fetch localStorage data before hydration

--- core/modules/compare/components/AddToCompare.ts
+++ core/modules/compare/components/AddToCompare.ts
@@ -1,7 +1,9 @@
 import Product from '@vue-storefront/core/modules/catalog/types/Product'
+import moduleComponentMounted from '@vue-storefront/core/modules/compare/mixins/moduleComponentMounted'
 
 export const AddToCompare = {
   name: 'AddToCompare',
+  mixins: [moduleComponentMounted],
   methods: {
     addToCompare (product: Product) {
       return this.$store.state['compare']
--- core/modules/compare/components/CompareButton.ts
+++ core/modules/compare/components/CompareButton.ts
@@ -1,5 +1,8 @@
+import moduleComponentMounted from '@vue-storefront/core/modules/compare/mixins/moduleComponentMounted'
+
 export const CompareButton = {
   name: 'CompareButton',
+  mixins: [moduleComponentMounted],
   computed: {
     isEmpty () : boolean {
       return this.$store.getters['compare/isEmpty']
