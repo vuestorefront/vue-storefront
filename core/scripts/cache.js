2c609ce7b3c09a498cd9bbef1b2a9eab88215fb8
Bartek Igielski <igloczek@gmail.com>
2019-02-04 15:58:15 +0100

    Logger removed from cache.js

--- core/scripts/cache.js
+++ core/scripts/cache.js
@@ -1,17 +1,16 @@
-import { Logger } from '@vue-storefront/core/lib/logger'
-
 const program = require('commander')
 const config = require('config')
 const cache = require('./utils/cache-instance')
+
 program
   .command('clear')
   .option('-t|--tag <tag>', 'tag name, available tags: ' + config.server.availableCacheTags.join(', '), '*')
   .action((cmd) => { // TODO: add parallel processing
     if (!cmd.tag) {
-      Logger.error('error: tag must be specified')()
+      console.error('error: tag must be specified')
       process.exit(1)
     } else {
-      Logger.log(`Clear cache request for [${cmd.tag}]`)()
+      console.log(`Clear cache request for [${cmd.tag}]`)
       let tags = []
       if (cmd.tag === '*') {
         tags = config.server.availableCacheTags
@@ -24,17 +23,17 @@ program
           return tag.indexOf(t) === 0
         })) {
           subPromises.push(cache.invalidate(tag).then(() => {
-            Logger.log(`Tags invalidated successfully for [${tag}]`)()
+            console.log(`Tags invalidated successfully for [${tag}]`)
           }))
         } else {
-          Logger.error(`Invalid tag name ${tag}`)()
+          console.error(`Invalid tag name ${tag}`)
         }
       })
       Promise.all(subPromises).then(r => {
-        Logger.log(`All tags invalidated successfully [${cmd.tag}]`)()
+        console.log(`All tags invalidated successfully [${cmd.tag}]`)
         process.exit(0)
       }).catch(error => {
-        Logger.error(error)()
+        console.error(error)
       })
     }
   })
