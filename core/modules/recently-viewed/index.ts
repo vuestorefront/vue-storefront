6f8276538d504ed85a7308826e2f580ce995ea6f
Filip Rakowski <frakowski@divante.pl>
2019-02-14 16:32:11 +0100

    core modules and module template refactor

--- core/modules/recently-viewed/index.ts
+++ core/modules/recently-viewed/index.ts
@@ -1,16 +1,13 @@
 import { module } from './store'
 import { plugin } from './store/plugin'
-import { VueStorefrontModule, VueStorefrontModuleConfig } from '@vue-storefront/core/lib/module'
+import { createModule } from '@vue-storefront/core/lib/module'
 import { initCacheStorage } from '@vue-storefront/core/helpers/initCacheStorage'
 import { afterRegistration } from './hooks/afterRegistration'
 
 export const KEY = 'recently-viewed'
 export const cacheStorage = initCacheStorage(KEY)
-
-const moduleConfig: VueStorefrontModuleConfig = {
+export const RecentlyViewed = createModule({
   key: KEY,
   store: { modules: [{ key: KEY, module }], plugin },
   afterRegistration
-}
-
-export const RecentlyViewed = new VueStorefrontModule(moduleConfig)
+})
