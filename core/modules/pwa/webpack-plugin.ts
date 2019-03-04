fc9da500a14916e67e5d24cac5a0dd7d54ef56ff
Filip Rakowski <frakowski@divante.pl>
2019-03-04 09:25:43 +0100

    wb config improvements

--- core/modules/pwa/webpack-plugin.ts
+++ core/modules/pwa/webpack-plugin.ts
@@ -2,13 +2,5 @@ import { InjectManifest } from 'workbox-webpack-plugin'
 
 export default new InjectManifest({
   swDest: "sw.js",
-  swSrc: "core/modules/pwa/service-worker.js",
-  globDirectory: "dist/",
-  globPatterns: [
-    '**/*.js',
-    'assets/**.*',
-    'assets/ig/**.*',
-    'index.html',
-  ],
-  globIgnores: ['**/*..map']
+  swSrc: "core/modules/pwa/service-worker.js"
 })
