b340a7a2e380501de982a4b16b9db1be4e05bc78
Patryk Tomczyk <13100280+patzick@users.noreply.github.com>
2019-03-08 13:26:32 +0100

    add error fallback for images

--- core/modules/catalog/helpers/index.ts
+++ core/modules/catalog/helpers/index.ts
@@ -531,6 +531,7 @@ export function getMediaGallery (product) {
               mediaGallery.push({
                 'src': getThumbnailPath(mediaItem.image, rootStore.state.config.products.gallery.width, rootStore.state.config.products.gallery.height),
                 'loading': getThumbnailPath(mediaItem.image, 310, 300),
+                'error': getThumbnailPath(mediaItem.image, 310, 300),
                 'video': mediaItem.vid
               })
           }
@@ -556,6 +557,7 @@ export function configurableChildrenImages(product) {
                 configurableChildrenImages.push({
                     'src': getThumbnailPath(groupedByAttribute[confChild][0].image, rootStore.state.config.products.gallery.width, rootStore.state.config.products.gallery.height),
                     'loading': getThumbnailPath(groupedByAttribute[confChild][0].image, 310, 300),
+                    'error': getThumbnailPath(groupedByAttribute[confChild][0].image, 310, 300),
                     'id': confChild
                 })
             }
@@ -577,7 +579,8 @@ export function attributeImages(product) {
             if(product[attribute]) {
                 attributeImages.push({
                     'src': getThumbnailPath(product[attribute], rootStore.state.config.products.gallery.width, rootStore.state.config.products.gallery.height),
-                    'loading': getThumbnailPath(product[attribute], 310, 300)
+                    'loading': getThumbnailPath(product[attribute], 310, 300),
+                    'error': getThumbnailPath(product[attribute], 310, 300)
                 })
             }
         }
