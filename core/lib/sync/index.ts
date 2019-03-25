d22148fe158c38ff32c46ea0db3e59c086c60c1c
Patryk Tomczyk <13100280+patzick@users.noreply.github.com>
2019-03-25 09:21:47 +0100

    userToken as getter (https://github.com/DivanteLtd/vue-storefront/pull/2650/files#r268512710)

--- core/lib/sync/index.ts
+++ core/lib/sync/index.ts
@@ -60,10 +60,9 @@ function execute (task) { // not offline task
           if (!currentCartId && rootStore.state.cart.cartServerToken) { // this is workaround; sometimes after page is loaded indexedb returns null despite the cart token is properly set
             currentCartId = rootStore.state.cart.cartServerToken
           }
-          if (!currentToken && rootStore.state.user.token) { // this is workaround; sometimes after page is loaded indexedb returns null despite the cart token is properly set
-            currentToken = rootStore.state.user.token
-          }
-          taskExecute(task, currentToken, currentCartId).then((result) => {
+          const token = currentToken ? currentToken : rootStore.getters['user/getUserToken']
+
+          taskExecute(task, token, currentCartId).then((result) => {
             resolve(result)
           }).catch(err => {
             reject(err)
