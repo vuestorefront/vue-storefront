0c0680592ffdddd45d533ced7624995c418547d8
lukecollymore <luke@develodesign.co.uk>
2019-04-27 11:49:24 +0100

    Updated to async await, checking total requested against in stock and in cart to respond if not enough stock

--- core/modules/catalog/store/stock/actions.ts
+++ core/modules/catalog/store/stock/actions.ts
@@ -13,25 +13,27 @@ const actions: ActionTree<StockState, RootState> = {
   /**
    * Reset current configuration and selected variatnts
    */
-  check (context, { product, qty = 1 }) {
-    return new Promise((resolve, reject) => {
-      if (rootStore.state.config.stock.synchronize) {
-        TaskQueue.queue({ url: rootStore.state.config.stock.endpoint + '/check?sku=' + encodeURIComponent(product.sku),
-          payload: {
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json' },
-            mode: 'cors'
-          },
-          product_sku: product.sku,
-          callback_event: 'store:stock/stockAfterCheck'
-        }).then((task:any) => {
-          resolve({ qty: product.stock ? product.stock.qty : 0, status: product.stock ? (product.stock.is_in_stock ? 'ok' : 'out_of_stock') : 'ok', onlineCheckTaskId: task.task_id }) // if online we can return ok because it will be verified anyway
-        })
-      } else {
-        resolve({ qty: product.stock ? product.stock.qty : 0, status: product.stock ? (product.stock.is_in_stock ? 'ok' : 'out_of_stock') : 'volatile' }) // if not online, cannot check the source of true here
-      }
-    })
+  async check (context, { product, qty = 1 }) {
+    if (rootStore.state.config.stock.synchronize) {
+      const task:any = await TaskQueue.execute({ url: rootStore.state.config.stock.endpoint + '/check?sku=' + encodeURIComponent(product.sku),
+        payload: {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' },
+          mode: 'cors'
+        },
+        product_sku: product.sku,
+        callback_event: 'store:stock/stockAfterCheck'
+      })
+      let result = task.result;
+      const cartProduct = await rootStore.dispatch('cart/getItem', product.sku)
+      let totalQty = cartProduct ? cartProduct.qty + product.qty : product.qty
+      const status = result.is_in_stock ? totalQty > result.qty ? 'not_enough_stock' : 'ok' : 'out_of_stock'
+      return Object.assign({}, task, {qty: task.result.qty, status, onlineCheckTaskId: task.task_id})
+    } else {
+      return { qty: product.stock ? product.stock.qty : 0, status: product.stock ? (product.stock.is_in_stock ? 'ok' : 'out_of_stock') : 'volatile' } // if not online, cannot check the source of true here
+    }
   },
+  
   /**
    * Reset current configuration and selected variatnts
    */
