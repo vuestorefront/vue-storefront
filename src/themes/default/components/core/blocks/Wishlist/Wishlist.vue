6c9ddcfb132be1a2d2f959cf7fc49bacbaf05a85
Anna Musial <ania.musial13@gmail.com>
2019-05-20 17:10:38 +0200

    Update src/themes/default/components/core/blocks/Wishlist/Wishlist.vue
    Co-Authored-By: Patryk Tomczyk <13100280+patzick@users.noreply.github.com>

--- src/themes/default/components/core/blocks/Wishlist/Wishlist.vue
+++ src/themes/default/components/core/blocks/Wishlist/Wishlist.vue
@@ -9,7 +9,7 @@
       <h2 v-if="productsInWishlist.length" class="col-xs-12 col-sm cl-accent">
         {{ $t('Wishlist') }}
       </h2>
-      <clear-wishlist-button v-if="productsInWishlist.length" @click.native="clearWishlist" class="col-xs-12 col-sm mt35 mb35 end-sm"/>
+      <clear-wishlist-button v-if="productsInWishlist.length" @click="clearWishlist" class="col-xs-12 col-sm mt35 mb35 end-sm"/>
     </div>
     <h4 v-if="!productsInWishlist.length" class="cl-accent ml30">
       {{ $t('Your wishlist is empty.') }}
