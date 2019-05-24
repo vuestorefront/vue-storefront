4db57bb9abb7d1d4cefcb4a27476a4da96d2d3cd
Benjamin Klein <benjamin.klein@mdm.de>
2019-05-24 11:52:21 +0200

    #2637 Added the add to cart and quantity button to wishlist and product lsiting

--- src/themes/default/components/core/ProductTile.vue
+++ src/themes/default/components/core/ProductTile.vue
@@ -47,16 +47,47 @@
         {{ product.priceInclTax | price }}
       </span>
     </router-link>
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-6 py10"
+           v-if="product.type_id !== 'grouped' && product.type_id !== 'bundle'">
+        <base-input-number
+          :name="$t('Quantity')"
+          v-model="product.qty"
+          :min="1"
+          @blur="$v.$touch()"
+          :validations="[
+          {
+            condition: $v.product.qty.$error && !$v.product.qty.minValue,
+            text: $t('Quantity must be above 0')
+          }
+        ]"
+        />
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-6 py10">
+        <add-to-cart
+          :product="product"
+          :disabled="$v.product.qty.$error && !$v.product.qty.minValue"
+          class="addToCart"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import {minValue} from 'vuelidate/lib/validators'
+import AddToCart from 'theme/components/core/AddToCart.vue'
+import BaseInputNumber from 'theme/components/core/blocks/Form/BaseInputNumber'
 import rootStore from '@vue-storefront/core/store'
-import { ProductTile } from '@vue-storefront/core/modules/catalog/components/ProductTile.ts'
-import { isServer } from '@vue-storefront/core/helpers'
+import {ProductTile} from '@vue-storefront/core/modules/catalog/components/ProductTile.ts'
+import {isServer} from '@vue-storefront/core/helpers'
 import config from 'config'
 
 export default {
+  components: {
+    AddToCart,
+    BaseInputNumber
+  },
   mixins: [ProductTile],
   data () {
     return {
@@ -91,10 +122,31 @@ export default {
               }
             }
             if (skus.length > 0) {
-              rootStore.dispatch('stock/list', { skus: skus }) // store it in the cache
+              rootStore.dispatch('stock/list', {skus: skus}) // store it in the cache
             }
           }
         }
+      }
+    },
+    notifyOutStock () {
+      this.$store.dispatch('notification/spawnNotification', {
+        type: 'error',
+        message: this.$t('The product is out of stock and cannot be added to the cart!'),
+        action1: {label: this.$t('OK')}
+      })
+    },
+    notifyWrongAttributes () {
+      this.$store.dispatch('notification/spawnNotification', {
+        type: 'warning',
+        message: this.$t('No such configuration for the product. Please do choose another combination of attributes.'),
+        action1: {label: this.$t('OK')}
+      })
+    }
+  },
+  validations: {
+    product: {
+      qty: {
+        minValue: minValue(1)
       }
     }
   },
@@ -108,122 +160,128 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '~theme/css/animations/transitions';
-@import '~theme/css/variables/colors';
-@import '~theme/css/helpers/functions/color';
+  @import '~theme/css/animations/transitions';
+  @import '~theme/css/variables/colors';
+  @import '~theme/css/helpers/functions/color';
 
-$bg-secondary: color(secondary, $colors-background);
-$border-secondary: color(secondary, $colors-border);
-$color-white: color(white);
+  $bg-secondary: color(secondary, $colors-background);
+  $border-secondary: color(secondary, $colors-border);
+  $color-white: color(white);
 
-.product {
-  @media (max-width: 767px) {
-    padding-bottom: 10px;
-  }
-}
-
-.price-original {
-  text-decoration: line-through;
-}
-
-%label {
-  position: absolute;
-  top: 0;
-  left: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 40px;
-  height: 40px;
-  background-color: $border-secondary;
-  text-transform: uppercase;
-  color: $color-white;
-  font-size: 12px;
-}
-
-.product-image {
-  width: 100%;
-  overflow: hidden;
-  max-height: 300px;
-  height: 100%;
-  min-height: 155px;
-  display: flex;
-  align-items: flex-end;
-  background-image: url('/assets/placeholder.svg');
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: 60% auto;
-
-  @media (min-width: 768px) {
-    min-height: 190px;
-  }
-  @media (min-width: 1200px) {
-    min-height: 300px;
-  }
-
-  &__content {
-    display: none;
-  }
-
-  &--loaded {
-    background-image: none;
-
-    .product-image__content {
-      display: block;
-      transform: translateZ(0);
+  .addToCart {
+    &.button-full {
+      min-width: inherit;
     }
   }
 
-  &:hover {
-    img {
-      opacity: 1;
-      transform: scale(1.1);
-    }
-
-    &.sale::after,
-    &.new::after {
-      opacity: 0.8;
+  .product {
+    @media (max-width: 767px) {
+      padding-bottom: 10px;
     }
   }
 
-  img {
-    max-height: 100%;
-    max-width: 100%;
-    width: auto;
-    height: auto;
-    margin: auto;
-    mix-blend-mode: darken;
-    opacity: 0.8;
-    transform: scale(1);
-    transition: 0.3s opacity $motion-main, 0.3s transform $motion-main;
+  .price-original {
+    text-decoration: line-through;
+  }
 
-    &[lazy="loaded"] {
-      animation: products-loaded;
-      animation-duration: 0.3s;
+  %label {
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 40px;
+    height: 40px;
+    background-color: $border-secondary;
+    text-transform: uppercase;
+    color: $color-white;
+    font-size: 12px;
+  }
+
+  .product-image {
+    width: 100%;
+    overflow: hidden;
+    max-height: 300px;
+    height: 100%;
+    min-height: 155px;
+    display: flex;
+    align-items: flex-end;
+    background-image: url('/assets/placeholder.svg');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 60% auto;
+
+    @media (min-width: 768px) {
+      min-height: 190px;
+    }
+    @media (min-width: 1200px) {
+      min-height: 300px;
     }
 
-    @keyframes products-loaded {
-      from {
-        opacity: 0;
+    &__content {
+      display: none;
+    }
+
+    &--loaded {
+      background-image: none;
+
+      .product-image__content {
+        display: block;
+        transform: translateZ(0);
       }
-      to {
+    }
+
+    &:hover {
+      img {
+        opacity: 1;
+        transform: scale(1.1);
+      }
+
+      &.sale::after,
+      &.new::after {
         opacity: 0.8;
       }
     }
-  }
 
-  &.sale {
-    &::after {
-      @extend %label;
-      content: 'Sale';
+    img {
+      max-height: 100%;
+      max-width: 100%;
+      width: auto;
+      height: auto;
+      margin: auto;
+      mix-blend-mode: darken;
+      opacity: 0.8;
+      transform: scale(1);
+      transition: 0.3s opacity $motion-main, 0.3s transform $motion-main;
+
+      &[lazy="loaded"] {
+        animation: products-loaded;
+        animation-duration: 0.3s;
+      }
+
+      @keyframes products-loaded {
+        from {
+          opacity: 0;
+        }
+        to {
+          opacity: 0.8;
+        }
+      }
+    }
+
+    &.sale {
+      &::after {
+        @extend %label;
+        content: 'Sale';
+      }
+    }
+
+    &.new {
+      &::after {
+        @extend %label;
+        content: 'New';
+      }
     }
   }
-
-  &.new {
-    &::after {
-      @extend %label;
-      content: 'New';
-    }
-  }
-}
 </style>
--- src/themes/default/components/core/blocks/Wishlist/Product.vue
+++ src/themes/default/components/core/blocks/Wishlist/Product.vue
@@ -6,7 +6,7 @@
         fullPath: product.url_path,
         params: { parentSku: product.parentSku ? product.parentSku : product.sku, slug: product.slug, childSku: product.sku }
       })">
-        <img v-lazy="thumbnail" >
+        <img v-lazy="thumbnail">
       </router-link>
     </div>
     <div class="col-xs between-xs flex pl40 py15">
@@ -21,39 +21,94 @@
         <div class="h6 cl-bg-tertiary pt5 sku">{{ product.sku }}</div>
       </div>
     </div>
+    <div class="col-xs between-xs flex pl40 py15" v-if="product.type_id !== 'grouped' && product.type_id !== 'bundle'">
+      <base-input-number
+        :name="$t('Quantity')"
+        v-model="product.qty"
+        :min="1"
+        @blur="$v.$touch()"
+        :validations="[
+          {
+            condition: $v.product.qty.$error && !$v.product.qty.minValue,
+            text: $t('Quantity must be above 0')
+          }
+        ]"
+      />
+    </div>
+    <div class="col-xs between-xs flex pl40 py15">
+      <add-to-cart
+        :product="product"
+        :disabled="$v.product.qty.$error && !$v.product.qty.minValue"
+        class="col-xs-12 col-sm-4 col-md-6"
+      />
+    </div>
     <div class="col-xs flex py15 align-right">
       <div>
         <span class="price-special" v-if="product.special_price">{{ product.priceInclTax | price }}</span>&nbsp;
-        <span class="price-original" v-if="product.special_price" >{{ product.originalPriceInclTax | price }}</span>
+        <span class="price-original" v-if="product.special_price">{{ product.originalPriceInclTax | price }}</span>
 
         <span v-if="!product.special_price">
           {{ product.priceInclTax | price }}
         </span>
       </div>
       <div>
-        <div class="mt5"><span @click="removeFromWishlist(product)"><remove-button class="cl-accent" /></span></div>
+        <div class="mt5"><span @click="removeFromWishlist(product)"><remove-button class="cl-accent"/></span></div>
       </div>
     </div>
   </li>
 </template>
 
 <script>
+import {minValue} from 'vuelidate/lib/validators'
+import AddToCart from 'theme/components/core/AddToCart.vue'
 import Product from '@vue-storefront/core/compatibility/components/blocks/Wishlist/Product'
+import BaseInputNumber from 'theme/components/core/blocks/Form/BaseInputNumber'
 import RemoveButton from './RemoveButton'
 
 export default {
   components: {
+    AddToCart,
+    BaseInputNumber,
     RemoveButton
   },
-  mixins: [Product]
+  mixins: [Product],
+
+  methods: {
+    showDetails (event) {
+      this.detailsOpen = true
+      event.target.classList.add('hidden')
+    },
+    notifyOutStock () {
+      this.$store.dispatch('notification/spawnNotification', {
+        type: 'error',
+        message: this.$t('The product is out of stock and cannot be added to the cart!'),
+        action1: {label: this.$t('OK')}
+      })
+    },
+    notifyWrongAttributes () {
+      this.$store.dispatch('notification/spawnNotification', {
+        type: 'warning',
+        message: this.$t('No such configuration for the product. Please do choose another combination of attributes.'),
+        action1: {label: this.$t('OK')}
+      })
+    }
+  },
+  validations: {
+    product: {
+      qty: {
+        minValue: minValue(1)
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
-.col-xs {
-  flex-direction: column;
-}
-input {
-  width: 30px;
-}
+  .col-xs {
+    flex-direction: column;
+  }
+
+  input {
+    width: 30px;
+  }
 </style>
