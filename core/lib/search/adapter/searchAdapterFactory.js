fdaa2aa0c701f0bc79126dbc05beb85fef7498d7
abratko <abratko@gotrg.com>
2019-06-04 16:16:31 +0300

    issue 2960: test added, searchAdapterInterface added

--- core/lib/search/adapter/searchAdapterFactory.js
+++ core/lib/search/adapter/searchAdapterFactory.js
@@ -2,25 +2,28 @@ import { server } from 'config'
 
 let instances = {}
 
+const isImplementingSearchAdapterInterface = (obj) => {
+  return typeof obj.search === 'function' && typeof obj.registerEntityType === 'function'
+}
+
 export const getSearchAdapter = async (adapterName = server.api) => {
   const SearchAdapterModule = await import(/* webpackChunkName: "vsf-search-adapter-" */ `./${adapterName}/searchAdapter`)
   const SearchAdapter = SearchAdapterModule.SearchAdapter
 
   if (!SearchAdapter) {
     throw new Error('Search adapter class is not provided')
-  } else {
-    if (instances[adapterName]) {
-      return instances[adapterName]
-    }
+  }
 
-    instances[adapterName] = new SearchAdapter()
-
-    if (typeof instances[adapterName].isValidFor === 'function' && typeof instances[adapterName].search === 'function' && typeof instances[adapterName].handleResult === 'function') {
-      throw new Error('Not valid search adapter class provided. Search Adapter must have search() and handleResult() methods')
-    }
-
+  if (instances[adapterName]) {
     return instances[adapterName]
   }
+
+  const searchAdapter = new SearchAdapter()
+  if (!isImplementingSearchAdapterInterface(searchAdapter)) {
+    throw new Error('Not valid search adapter class provided. Search Adapter must implements SearchAdapterInterfaces')
+  }
+  instances[adapterName] = searchAdapter;
+  return instances[adapterName];
 }
 
 export default {
