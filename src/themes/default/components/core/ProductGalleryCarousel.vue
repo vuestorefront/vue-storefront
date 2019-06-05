22d6d542b2896a715b5a7f18a5f51f10539f393f
przspa <pspaczek@divante.co>
2019-06-05 12:14:13 +0200

    Fixed animation behaviour for color attribute

--- src/themes/default/components/core/ProductGalleryCarousel.vue
+++ src/themes/default/components/core/ProductGalleryCarousel.vue
@@ -96,6 +96,7 @@ export default {
   data () {
     return {
       carouselTransitionSpeed: 0,
+      currentColor: 0,
       currentPage: 0,
       hideImageAtIndex: null,
       lowerQualityImagesLoadedMap: {},
@@ -136,16 +137,10 @@ export default {
   },
   mounted () {
     this.selectVariant()
-    if (this.$refs.carousel) {
-      let navigation = this.$refs.carousel.$children.find(c => c.$el.className === 'VueCarousel-navigation')
-      let pagination = this.$refs.carousel.$children.find(c => c.$el.className === 'VueCarousel-pagination')
-      if (navigation !== undefined) {
-        navigation.$on('navigationclick', this.increaseCarouselTransitionSpeed)
-      }
-      if (pagination !== undefined) {
-        pagination.$on('paginationclick', this.increaseCarouselTransitionSpeed)
-      }
-    }
+
+    const {color} = this.configuration
+    this.currentColor = color.id
+
     this.$emit('loaded')
   },
   beforeDestroy () {
@@ -171,10 +166,18 @@ export default {
       const currentSlide = this.$refs.carousel.currentPage
       this.$emit('toggle', currentSlide)
     },
-    increaseCarouselTransitionSpeed () {
-      this.carouselTransitionSpeed = 500
+    switchCarouselSpeed () {
+      const {color} = this.configuration
+      if (color && this.currentColor !== color.id) {
+        this.currentColor = color.id
+        this.carouselTransitionSpeed = 0
+      } else {
+        this.carouselTransitionSpeed = 500
+      }
     },
     pageChange (index) {
+      this.switchCarouselSpeed()
+
       this.currentPage = index
       this.hideImageAtIndex = null
     },
