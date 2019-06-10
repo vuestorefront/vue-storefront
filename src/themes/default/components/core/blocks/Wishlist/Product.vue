2ee0a5e220c8d38d4dcbcc78d90c2a3aa33c58d1
miluka <mlukasik@divante.co>
2019-06-10 09:56:17 +0200

    Fixes formatting

--- src/themes/default/components/core/blocks/Wishlist/Product.vue
+++ src/themes/default/components/core/blocks/Wishlist/Product.vue
@@ -1,57 +1,50 @@
 <template>
   <li class="row pr55 py20">
     <div @click="closeWishlist">
-      <router-link
-        :to="localizedRoute({
+      <router-link :to="localizedRoute({
         name: 'configurable-product',
         params: { parentSku: product.parentSku ? product.parentSku : product.sku, slug: product.slug, childSku: product.sku }
-      })"
-      >
-        <img v-lazy="thumbnail">
+      })">
+        <img v-lazy="thumbnail" >
       </router-link>
     </div>
     <div class="col-xs between-xs flex pl40 py15">
       <div @click="closeWishlist">
-        <router-link
-          :to="localizedRoute({
+        <router-link :to="localizedRoute({
           name: 'configurable-product',
           params: { parentSku: product.parentSku ? product.parentSku : product.sku, slug: product.slug, childSku: product.sku }
-        })"
-        >{{ product.name | htmlDecode }}</router-link>
+        })">
+          {{ product.name | htmlDecode }}
+        </router-link>
         <div class="h6 cl-bg-tertiary pt5 sku">{{ product.sku }}</div>
       </div>
     </div>
     <div class="col-xs flex py15 align-right">
       <div>
         <span class="price-special" v-if="product.special_price">{{ product.priceInclTax | price }}</span>&nbsp;
-        <span
-          class="price-original"
-          v-if="product.special_price"
-        >{{ product.originalPriceInclTax | price }}</span>
+        <span class="price-original" v-if="product.special_price" >{{ product.originalPriceInclTax | price }}</span>
 
-        <span v-if="!product.special_price">{{ product.priceInclTax | price }}</span>
+        <span v-if="!product.special_price">
+          {{ product.priceInclTax | price }}
+        </span>
       </div>
       <div>
-        <div class="mt5">
-          <span @click="removeFromWishlist(product)">
-            <remove-button class="cl-accent"/>
-          </span>
-        </div>
+        <div class="mt5"><span @click="removeFromWishlist(product)"><remove-button class="cl-accent" /></span></div>
       </div>
     </div>
   </li>
 </template>
 
 <script>
-import Product from "@vue-storefront/core/compatibility/components/blocks/Wishlist/Product";
-import RemoveButton from "./RemoveButton";
+import Product from '@vue-storefront/core/compatibility/components/blocks/Wishlist/Product'
+import RemoveButton from './RemoveButton'
 
 export default {
   components: {
     RemoveButton
   },
   mixins: [Product]
-};
+}
 </script>
 
 <style scoped>
