7657b1fc34aa774d879bd2975e1a27dbce90d63a
PX-FE1 VS <valetina.smirnova@phoenix-media.eu>
2019-06-21 11:35:27 +0300

    if the robots.txt exists in theme use it, otherwise output default values

--- src/server/robots.js
+++ src/server/robots.js
@@ -1,6 +1,14 @@
+const fs = require('fs')
+const themePath = require('../../core/build/theme-path')
+const robots = `${themePath}/assets/robots.txt`
 
-module.exports = (expressApp) => {
+module.exports = expressApp => {
   expressApp.get('/robots.txt', (req, res) => {
-    res.end('User-agent: *\nDisallow: ')
+    if (fs.existsSync(robots)) {
+      res.type('text/plain')
+      res.sendFile(robots)
+    } else {
+      res.end('User-agent: *\nDisallow: ')
+    }
   })
 }
