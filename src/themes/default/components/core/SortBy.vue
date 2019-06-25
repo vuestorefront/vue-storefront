dee2ead0814840c4c6d312001c0e1ba60325ef64
patzick <tomczyk.patryk@gmail.com>
2019-06-25 14:34:04 +0200

    lnter

--- src/themes/default/components/core/SortBy.vue
+++ src/themes/default/components/core/SortBy.vue
@@ -39,7 +39,7 @@ export default {
   },
   watch: {
     value: {
-      handler() {
+      handler () {
         const defaultVariant = this.value && this.value.length ? this.value : products.defaultSortBy.attribute
         this.sortby = this.sortingVariants.find(variant => variant.id === defaultVariant)
       },
