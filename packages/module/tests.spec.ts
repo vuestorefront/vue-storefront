716377f033eda43fefb8ad0ba21407738e71e069
Filip Rakowski <frakowski@divante.pl>
2019-07-02 13:00:00 +0200

    add unit tests

---
+++ packages/module/tests.spec.ts
@@ -0,0 +1,18 @@
+import { refs, injectReferences } from "./index"
+
+describe('Modules', () => {
+
+  it('Sets proper references', () => {
+    const appMock = {}
+    const storeMock = {}
+    const routerMock = {}
+    const configMock = {}
+
+    injectReferences(appMock, storeMock, routerMock, configMock)
+
+    expect(refs.app).toBe(appMock)
+    expect(refs.store).toBe(storeMock)
+    expect(refs.router).toBe(routerMock)
+    expect(refs.config).toBe(configMock)
+  })
+})
