970bf7f0a8c86885664098b96f7273f8c8aee365
Filip Rakowski <frakowski@divante.pl>
2019-07-02 14:19:01 +0200

    remove demo module

--- src/themes/default/index.js
+++ src/themes/default/index.js
@@ -6,9 +6,7 @@ import VueProgressBar from 'vue-progressbar'
 import '@vue-storefront/core/lib/passive-listeners'
 import { RouterManager } from '@vue-storefront/core/lib/router-manager'
 import { once } from '@vue-storefront/core/helpers'
-import { registerModule } from '@vue-storefront/module'
-// temp
-import { ExampleModule } from 'packages/cli/boilerplates/module'
+
 once('__VUE_EXTEND_DROPPOINT_VPB__', () => {
   Vue.use(VueProgressBar)
 })
@@ -19,9 +17,6 @@ function initTheme (app, router, store, config, ssrContext) {
   // You can do it on your own and then be able to customize the components used for example for German storeView checkout
   // To do so please execlude the desired storeView from the config.storeViews.mapStoreUrlsFor and map the urls by Your own like:
   // { name: 'de-checkout', path: '/checkout', component: CheckoutCustomized },
-  registerModule(ExampleModule, {
-    message: 'Hello World!'
-  })
   setupMultistoreRoutes(config, router, routes)
   RouterManager.addRoutes(routes, router)
 }
