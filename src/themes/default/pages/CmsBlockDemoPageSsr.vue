0f6cef4eac6e8d3db9dcb47401a7d8653836f7b9
pkarw <pkarwatka@divante.pl>
2019-07-05 16:29:36 +0200

    Revert "Removed`magento-2-cms` module in favor of the `core/modules/cms"
    This reverts commit a70c354f2ac3291468a9ba3ec5f436dd23b24814.

---
+++ src/themes/default/pages/CmsBlockDemoPageSsr.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <h2>
+      Cms page content ssr
+    </h2>
+    <cms-block :identifier="'footer_about_menu'" />
+    <cms-block :identifier="'no_reviews___'" />
+    <!-- <cms-block :id="29"/> -->
+  </div>
+</template>
+<script>
+
+import CmsBlock from '../components/core/blocks/Cms/Block'
+
+export default {
+  components: {
+    CmsBlock
+  },
+  mixins: [CmsBlock]
+}
+</script>
+
+<style lang="scss">
+@import '~theme/css/variables/colors';
+@import '~theme/css/helpers/functions/color';
+$color-secondary: color(secondary);
+
+.cms-content {
+  & * {
+    font-size: 18px;
+  }
+
+  a {
+    color: $color-secondary;
+    text-decoration: underline;
+  }
+
+}
+</style>
