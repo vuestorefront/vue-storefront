7b1707e2837212c3d8ff4d54777715b11cf9e902
Michal-Dziedzinski <michal.marcin.dziedzinski@gmail.com>
2019-07-05 08:42:01 +0200

    Refactor visibilityCHanged method (number of nasted if)

--- src/themes/default/components/core/ProductTile.vue
+++ src/themes/default/components/core/ProductTile.vue
@@ -104,27 +104,23 @@ export default {
       }
     },
     visibilityChanged (isVisible, entry) {
-      if (isVisible) {
-        if (
-          config.products.configurableChildrenStockPrefetchDynamic &&
-          rootStore.products.filterUnavailableVariants
-        ) {
-          const skus = [this.product.sku]
-          if (
-            this.product.type_id === 'configurable' &&
-            this.product.configurable_children &&
-            this.product.configurable_children.length > 0
-          ) {
-            for (const confChild of this.product.configurable_children) {
-              const cachedItem = rootStore.state.stock.cache[confChild.id]
-              if (typeof cachedItem === 'undefined' || cachedItem === null) {
-                skus.push(confChild.sku)
-              }
-            }
-            if (skus.length > 0) {
-              rootStore.dispatch('stock/list', { skus: skus }) // store it in the cache
-            }
+      if (
+        isVisible &&
+        config.products.configurableChildrenStockPrefetchDynamic &&
+        rootStore.products.filterUnavailableVariants &&
+        this.product.type_id === 'configurable' &&
+        this.product.configurable_children &&
+        this.product.configurable_children.length > 0
+      ) {
+        const skus = [this.product.sku]
+        for (const confChild of this.product.configurable_children) {
+          const cachedItem = rootStore.state.stock.cache[confChild.id]
+          if (typeof cachedItem === 'undefined' || cachedItem === null) {
+            skus.push(confChild.sku)
           }
+        }
+        if (skus.length > 0) {
+          rootStore.dispatch('stock/list', { skus: skus }) // store it in the cache
         }
       }
     }
