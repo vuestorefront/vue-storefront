4e01d628c97a1e824a14a7976a3873e31d27e94a
Filip Rakowski <frakowski@divante.pl>
2019-07-08 12:43:33 +0200

    afterappInit removed from checkoutmodule

--- core/modules/checkout/index.ts
+++ core/modules/checkout/index.ts
@@ -13,31 +13,31 @@ export const CheckoutModule: StorefrontModule = function (app, store, router, mo
   store.registerModule('payment', paymentModule)
   store.registerModule('checkout', checkoutModule)
 
-    store.subscribe((mutation, state) => {
-      const type = mutation.type
+  store.subscribe((mutation, state) => {
+    const type = mutation.type
 
-      if (
-        type.endsWith(types.CHECKOUT_SAVE_PERSONAL_DETAILS)
-      ) {
-        Vue.prototype.$db.checkoutFieldsCollection.setItem('personal-details', state.checkout.personalDetails).catch((reason) => {
-          console.error(reason) // it doesn't work on SSR
-        }) // populate cache
-      }
+    if (
+      type.endsWith(types.CHECKOUT_SAVE_PERSONAL_DETAILS)
+    ) {
+      Vue.prototype.$db.checkoutFieldsCollection.setItem('personal-details', state.checkout.personalDetails).catch((reason) => {
+        console.error(reason) // it doesn't work on SSR
+      }) // populate cache
+    }
 
-      if (
-        type.endsWith(types.CHECKOUT_SAVE_SHIPPING_DETAILS)
-      ) {
-        Vue.prototype.$db.checkoutFieldsCollection.setItem('shipping-details', state.checkout.shippingDetails).catch((reason) => {
-          console.error(reason) // it doesn't work on SSR
-        }) // populate cache
-      }
+    if (
+      type.endsWith(types.CHECKOUT_SAVE_SHIPPING_DETAILS)
+    ) {
+      Vue.prototype.$db.checkoutFieldsCollection.setItem('shipping-details', state.checkout.shippingDetails).catch((reason) => {
+        console.error(reason) // it doesn't work on SSR
+      }) // populate cache
+    }
 
-      if (
-        type.endsWith(types.CHECKOUT_SAVE_PAYMENT_DETAILS)
-      ) {
-        Vue.prototype.$db.checkoutFieldsCollection.setItem('payment-details', state.checkout.paymentDetails).catch((reason) => {
-          console.error(reason) // it doesn't work on SSR
-        }) // populate cache
-      }
-    })
+    if (
+      type.endsWith(types.CHECKOUT_SAVE_PAYMENT_DETAILS)
+    ) {
+      Vue.prototype.$db.checkoutFieldsCollection.setItem('payment-details', state.checkout.paymentDetails).catch((reason) => {
+        console.error(reason) // it doesn't work on SSR
+      }) // populate cache
+    }
+  })
 }
