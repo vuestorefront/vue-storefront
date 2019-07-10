3551b2b3ab47c795b2efecf1a17004d50f228a0b
Filip Rakowski <frakowski@divante.pl>
2019-07-10 18:46:32 +0200

    add descriptions for storeView

--- core/hooks.ts
+++ core/hooks.ts
@@ -25,12 +25,12 @@ const coreHooksExecutors = {
 const coreHooks = {
   /** Hook is fired right after whole application is initialized. Modules are registered and theme setted up */
   afterAppInit: afterAppInitHook,
-  /** Hook is fired directly before sending order to the server, after all client-side validations
-   * @param orderMutator Inside this function you have access to order object that you can access and modify. It should return order object.
+  /** Hook is fired directly before changing current storeView (multistrore)
+   * @param storeView Inside this function you have access to order object that you can access and modify. It should return order object.
   */
   beforeStoreViewChange: beforeStoreViewChangeHook,
-  /** Hook is fired right after order has been sent to server
-   * @param result `{ order, task }` task is a result of sending order to backend and order is order that has been sent there
+  /** Hook is fired right after storeView (multistore) is changed
+   * @param storeVire current storeVire
   */
   afterStoreViewChange: afterStoreViewChangeHook
 }
