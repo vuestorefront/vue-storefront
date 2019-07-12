32113358454185ae13b18ba3965ccebfae522a64
Bartosz Żądłowski <bzadlowski@divante.pl>
2019-07-12 14:51:11 +0200

    cr fixes

--- core/modules/compare/index.ts
+++ core/modules/compare/index.ts
@@ -1,14 +1,27 @@
+
 import { compareStore } from './store'
 import { initCacheStorage } from '@vue-storefront/core/helpers/initCacheStorage';
 import { plugin } from './store/plugin'
 import { StorefrontModule } from '@vue-storefront/module';
-import Vue from 'vue';
+// import * as localForage from 'localforage'
+// import UniversalStorage from '@vue-storefront/core/store/lib/storage'
+// import { currentStoreView } from '@vue-storefront/core/lib/multistore'
+// import { StorageManager } from '@vue-storefront/core/store/lib/storage-manager'
 
-const KEY = 'compare'
-export const cacheStorage = initCacheStorage(KEY)
+export const cacheStorage = initCacheStorage('compare')
 export const CompareModule: StorefrontModule = function (app, store, router, moduleConfig, appConfig) {
-  Vue.prototype.$db.cartsCollection = initCacheStorage('compare')
+  // Vue.prototype.$db.compareCollection = initCacheStorage('compare')
+  // const storeView = currentStoreView()
+  // const dbNamePrefix = storeView.storeCode ? storeView.storeCode + '-' : ''
 
-  store.registerModule(KEY, compareStore)
+  // StorageManager.set('compareCollection', new UniversalStorage(localForage.createInstance({
+  //   name: dbNamePrefix + 'shop',
+  //   storeName: 'compare',
+  //   driver: localForage[appConfig.localForage.defaultDrivers['compare']]
+  // })))
+
+  initCacheStorage('compare')
+
+  store.registerModule('compare', compareStore)
   store.subscribe(plugin)
 }
