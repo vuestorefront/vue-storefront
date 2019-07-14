63ce1a6ce2ddf82eec4eeaff7a407ac7e8de95e2
Filip Rakowski <frakowski@divante.pl>
2019-07-14 23:04:06 +0200

    fix correct boilerplate and unit test

--- packages/cli/boilerplates/module/src/index.ts
+++ packages/cli/boilerplates/module/src/index.ts
@@ -1,5 +1,5 @@
 import { StorefrontModule } from '@vue-storefront/module'
-import { afterAppInit } from '@vue-storefront/module/hooks'
+import { coreHooks } from '@vue-storefront/core/hooks'
 import { extendStore } from '@vue-storefront/module/helpers'
 import { ExampleStore, ExtendProductStore } from './store'
 
@@ -10,5 +10,5 @@ export const ExampleModule: StorefrontModule = function (app, store, router, mod
   // This is how you override properties of currently existing Vuex modules
   extendStore('product', ExtendProductStore)
   // This is how you can hook into various palces of the application
-  afterAppInit(() => console.log('Do something when application is initialized!'))
+  coreHooks.afterAppInit(() => console.log('Do something when application is initialized!'))
 }
