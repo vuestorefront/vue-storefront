c61d84ed4d908efc4afbccf8bc6e997362243bc1
Luke Romanowicz <lromanowicz1@gmail.com>
2019-07-15 09:00:16 +0200

    fix modules tests

--- core/modules/cart/test/unit/index.spec.ts
+++ core/modules/cart/test/unit/index.spec.ts
@@ -2,6 +2,9 @@ import { CartModule } from '../../index'
 
 jest.mock('../../store', () => ({}));
 jest.mock('@vue-storefront/module', () => ({ createModule: jest.fn(() => ({ module: 'cart' })) }));
+jest.mock('../../helpers/cartCacheHandler', () => ({ cartCacheHandlerFactory: jest.fn() }))
+jest.mock('@vue-storefront/core/helpers', () => ({ isServer: false }))
+jest.mock('@vue-storefront/core/helpers/initCacheStorage', () => ({ initCacheStorage: jest.fn() }));
 
 describe('Cart Module', () => {
   it('can be initialized', () => {
