b517a820c0a76f598e8a01424c02ede74ca18a87
Bartosz Żądłowski <bzadlowski@divante.pl>
2019-07-15 10:02:50 +0200

    Fixed wrong key in catalog-next module.

--- core/modules/catalog-next/index.ts
+++ core/modules/catalog-next/index.ts
@@ -2,5 +2,5 @@ import { categoryModule } from './store/category'
 import { StorefrontModule } from '@vue-storefront/module';
 
 export const CatalogNextModule: StorefrontModule = function (app, store, router, moduleConfig, appConfig) {
-  store.registerModule('catalog-next', categoryModule)
+  store.registerModule('category-next', categoryModule)
 }
