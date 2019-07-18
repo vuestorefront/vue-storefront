76b7192322b84bd7297acea05483733796699cfb
Łukasz <lukasz.jedrasik@icloud.com>
2019-07-18 13:41:07 +0200

    Script has been injected and locale was set on 'es'

--- src/modules/vsf-zend-chat/hooks/beforeRegistration.ts
+++ src/modules/vsf-zend-chat/hooks/beforeRegistration.ts
@@ -1,3 +1,5 @@
+declare const zE;
+
 export function beforeRegistration({ Vue, config, store, isServer }) {
   if (!isServer) {
     var head = document.getElementsByTagName("head")[0],
@@ -7,7 +9,21 @@ export function beforeRegistration({ Vue, config, store, isServer }) {
       "https://static.zdassets.com/ekr/snippet.js?key=e6e4724a-324b-4839-a578-6c5d07bf3ba5";
     script.id = "ze-snippet";
     script.async = true;
-    script.defer = true;
+    script.onload = function() {
+      var headSetLocale = document.getElementsByTagName("head")[0],
+        scriptSetLocale = document.createElement("script");
+
+      const setLocaleSource = `//https://developer.zendesk.com/embeddables/docs/widget/api
+    zE(function() {
+      zE.setLocale("es");
+     })`;
+
+      scriptSetLocale.text = setLocaleSource;
+      scriptSetLocale.type = "text/javascript";
+
+      headSetLocale.append(scriptSetLocale);
+    };
+
     head.appendChild(script);
   }
 }
