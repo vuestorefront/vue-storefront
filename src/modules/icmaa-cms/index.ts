7737b658e9b8c60332dfd7e6985e6ce8bc32a6f7
cewald <c.ewald@impericon.com>
2019-07-18 14:49:45 +0200

    Update initCacheStorage to StorageManager.init()

--- src/modules/icmaa-cms/index.ts
+++ src/modules/icmaa-cms/index.ts
@@ -1,11 +1,11 @@
 import { VueStorefrontModule, VueStorefrontModuleConfig } from '@vue-storefront/core/lib/module'
-import { initCacheStorage } from '@vue-storefront/core/helpers/initCacheStorage'
+import { StorageManager } from '@vue-storefront/core/store/lib/storage-manager'
 import { blockModule, cmsBlockStateKey } from './store/block'
 import { pageModule, cmsPageStateKey } from './store/page'
 import { categoryExtrasModule, cmsCategoryExtrasStateKey } from './store/category-extras'
 
 export const KEY = 'icmaa-cms'
-export const cacheStorage = initCacheStorage(KEY)
+export const cacheStorage = StorageManager.init(KEY)
 
 const moduleConfig: VueStorefrontModuleConfig = {
   key: KEY,
