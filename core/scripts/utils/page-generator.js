c32abe5961fa41d1513da59fe565294b6d5ec9a9
pkarw <pkarwatka@divante.pl>
2019-07-18 13:47:40 +0200

    core/server.js refactored for a preparation to external generate command

---
+++ core/scripts/utils/page-generator.js
@@ -0,0 +1,15 @@
+function saveRenderedPage (destPath, url, output) {
+}
+
+function saveAssets (destPath) {
+}
+function saveIndex (destPath) {
+}
+function saveSW (destPath) {
+}
+module.exports = {
+  saveRenderedPage,
+  saveAssets,
+  saveIndex,
+  saveSW
+}
---
+++ core/scripts/utils/ssr-renderer.js
@@ -0,0 +1,75 @@
+const fs = require('fs')
+const rootPath = require('app-root-path').path
+const resolve = file => path.resolve(rootPath, file)
+const path = require('path')
+const compile = require('lodash.template')
+
+function createRenderer (bundle, clientManifest, template) {
+  // https://github.com/vuejs/vue/blob/dev/packages/vue-server-renderer/README.md#why-use-bundlerenderer
+  return require('vue-server-renderer').createBundleRenderer(bundle, {
+    clientManifest,
+    // runInNewContext: false,
+    cache: require('lru-cache')({
+      max: 1000,
+      maxAge: 1000 * 60 * 15
+    })
+  })
+}
+
+function applyAdvancedOutputProcessing (context, output, templatesCache, isProd = true) {
+  const contentPrepend = (typeof context.output.prepend === 'function') ? context.output.prepend(context) : '';
+  const contentAppend = (typeof context.output.append === 'function') ? context.output.append(context) : '';
+  output = contentPrepend + output + contentAppend;
+  if (context.output.template) { // case when we've got the template name back from vue app
+    if (!isProd) { context.output.template = 'default'; } // in dev mode we can not use pre-rendered HTML templates
+    if (templatesCache[context.output.template]) { // please look at: https://github.com/vuejs/vue/blob/79cabadeace0e01fb63aa9f220f41193c0ca93af/src/server/template-renderer/index.js#L87 for reference
+      output = templatesCache[context.output.template](context).replace('<!--vue-ssr-outlet-->', output);
+    } else {
+      throw new Error(`The given template name ${context.output.template} does not exist`);
+    }
+  }
+  return output;
+}
+
+function initTemplatesCache (config, compileOptions) {
+  const templatesCache = {}
+  for (const tplName of Object.keys(config.ssr.templates)) {
+    const fileName = resolve(config.ssr.templates[tplName]);
+    if (fs.existsSync(fileName)) {
+      const template = fs.readFileSync(fileName, 'utf-8');
+      templatesCache[tplName] = compile(template, compileOptions);
+    }
+  }
+  return templatesCache
+}
+
+function initSSRRequestContext (app, req, res, config) {
+  return {
+    url: decodeURI(req.url),
+    output: {
+      prepend: (context) => { return ''; },
+      append: (context) => { return ''; },
+      appendHead: (context) => { return ''; },
+      template: 'default',
+      cacheTags: null
+    },
+    server: {
+      app: app,
+      response: res,
+      request: req
+    },
+    meta: null,
+    vs: {
+      config: config,
+      storeCode: req.header('x-vs-store-code') ? req.header('x-vs-store-code') : process.env.STORE_CODE
+    }
+  };
+}
+
+module.exports = {
+  createRenderer,
+  initTemplatesCache,
+  initSSRRequestContext,
+  applyAdvancedOutputProcessing,
+  compileTemplate: compile
+}
