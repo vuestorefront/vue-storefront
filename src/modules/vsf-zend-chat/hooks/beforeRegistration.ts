aade32230550f895bf2f0e7e121dfbef65530101
Łukasz <lukasz.jedrasik@icloud.com>
2019-07-23 08:56:23 +0200

    Zendesk integrated

--- src/modules/vsf-zend-chat/hooks/beforeRegistration.ts
+++ src/modules/vsf-zend-chat/hooks/beforeRegistration.ts
@@ -1,7 +1,10 @@
+import EventBus from "@vue-storefront/core/compatibility/plugins/event-bus/index";
+
 declare const zE;
 
 export function beforeRegistration({ Vue, config, store, isServer }) {
   if (!isServer) {
+    // Inject Zendesk widget
     var head = document.getElementsByTagName("head")[0],
       script = document.createElement("script");
 
@@ -9,19 +12,65 @@ export function beforeRegistration({ Vue, config, store, isServer }) {
       "https://static.zdassets.com/ekr/snippet.js?key=e6e4724a-324b-4839-a578-6c5d07bf3ba5";
     script.id = "ze-snippet";
     script.async = true;
+
     script.onload = function() {
-      var headSetLocale = document.getElementsByTagName("head")[0],
-        scriptSetLocale = document.createElement("script");
+      // Widget language
+      const detectedLang = window.location.pathname;
 
-      const setLocaleSource = `//https://developer.zendesk.com/embeddables/docs/widget/api
-    zE(function() {
-      zE.setLocale("es");
-     })`;
+      let widgetLang;
 
-      scriptSetLocale.text = setLocaleSource;
-      scriptSetLocale.type = "text/javascript";
+      switch (detectedLang) {
+        case "/eu":
+          widgetLang = "en-US";
+          break;
 
-      headSetLocale.append(scriptSetLocale);
+        case "/us":
+          widgetLang = "en-US";
+          break;
+
+        case "/uk":
+          widgetLang = "en-US";
+          break;
+
+        case "/de":
+          widgetLang = "de";
+          break;
+
+        case "/fr":
+          widgetLang = "fr";
+          break;
+
+        case "/it":
+          widgetLang = "it";
+          break;
+
+        default:
+          widgetLang = "es";
+      }
+
+      zE(() => {
+        zE.setLocale(widgetLang);
+      });
+
+      // User identify;
+
+      EventBus.$on("user-after-loggedin", ({ firstname, lastname, email }) => {
+        zE(() => {
+          zE.identify({
+            name: firstname + " " + lastname,
+            email
+          });
+        });
+      });
+
+      EventBus.$on("user-after-logout", () => {
+        zE(() => {
+          zE.identify({
+            name: " ",
+            email: "example@example.com"
+          });
+        });
+      });
     };
 
     head.appendChild(script);
