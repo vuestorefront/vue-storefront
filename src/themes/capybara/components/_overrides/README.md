9cf554c9274b28fe8adafdcf0ebc971b43fec719
Filip Rakowski <frakowski@divante.pl>
2019-07-28 15:50:03 +0200

    basic SfUI compinents override

---
+++ src/themes/capybara/components/_overrides/README.md
@@ -0,0 +1,4 @@
+Use this path to override Storefront UI components. 
+To override a given component just create a new one here with the same name.
+
+**NOTE** Adding a new file requires dev server restart for webpack to recognize it.
--- src/themes/capybara/webpack.config.js
+++ src/themes/capybara/webpack.config.js
@@ -1,15 +1,23 @@
 // You can extend default webpack build here. Read more on docs: https://github.com/DivanteLtd/vue-storefront/blob/master/doc/Working%20with%20webpack.md
-const AliasRegexhOverridePlugin = require('alias-regex-webpack-plugin')
 const webpack = require('webpack')
+const fs = require('fs');
+
+let componentsToOverride;
+
+fs.readdir('./src/themes/capybara/components/_overrides', (_err, files) => {
+  componentsToOverride = files
+});
 
 module.exports = function (config, { isClient, isDev }) {
+  // Sufficient for prod, not for dev due to requirement of development server restart after adding new file
   // eslint-disable-next-line prefer-arrow-callback
   config.default.plugins.push(new webpack.NormalModuleReplacementPlugin(/(.*)Sf(\.*)/, function (resource) {
+    // Can be added as a part of regexp
     const folders = ['atoms', 'molecules', 'organisms']
-    // todo scan dir _overrides
-    const files = ['SfProductCard']
+    const files = componentsToOverride
     folders.forEach(folder => {
       files.forEach(file => {
+        file = file.split('.')[0]
         if (resource.request.includes('./src/components/' + folder + '/' + file + '/' + file + '.vue')) {
           resource.request = resource.request.replace('./src/components/' + folder + '/' + file + '/' + file + '.vue', '../../../src/themes/capybara/components/_overrides/' + file + '.vue')
         }
