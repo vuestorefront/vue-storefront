28d8c72d0f6f9f6b5e2f900aa9bd0aa593425a85
patzick <tomczyk.patryk@gmail.com>
2019-08-04 20:41:07 +0200

    fix product loading

--- src/themes/default/pages/Product.vue
+++ src/themes/default/pages/Product.vue
@@ -60,7 +60,7 @@
                   {{ getCurrentProduct.qty > 0 ? getCurrentProduct.price_incl_tax * getCurrentProduct.qty : getCurrentProduct.price_incl_tax | price }}
                 </div>
               </div>
-              <div class="cl-primary variants" v-if="getCurrentProduct.type_id =='configurable' && !isProductLoading">
+              <div class="cl-primary variants" v-if="getCurrentProduct.type_id =='configurable'">
                 <div
                   class="error"
                   v-if="getCurrentProduct.errors && Object.keys(getCurrentProduct.errors).length > 0"
@@ -117,15 +117,15 @@
               </div>
             </div>
             <product-links
-              v-if="getCurrentProduct.type_id =='grouped' && !isProductLoading"
+              v-if="getCurrentProduct.type_id =='grouped'"
               :products="getCurrentProduct.product_links"
             />
             <product-bundle-options
-              v-if="getCurrentProduct.bundle_options && getCurrentProduct.bundle_options.length > 0 && !isProductLoading"
+              v-if="getCurrentProduct.bundle_options && getCurrentProduct.bundle_options.length > 0"
               :product="getCurrentProduct"
             />
             <product-custom-options
-              v-else-if="getCurrentProduct.custom_options && getCurrentProduct.custom_options.length > 0 && !isProductLoading"
+              v-else-if="getCurrentProduct.custom_options && getCurrentProduct.custom_options.length > 0"
               :product="getCurrentProduct"
             />
             <div
@@ -327,7 +327,7 @@ export default {
       return false
     },
     getInputName () {
-      if (this.isSimpleOrConfigurable) { return this.$i18n.t('Quantity available', { qty: this.quantity }) }
+      if (this.isSimpleOrConfigurable && !this.isProductLoading) { return this.$i18n.t('Quantity available', { qty: this.quantity }) }
       return this.$i18n.t('Quantity')
     }
   },
