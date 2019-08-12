7802c42d330a52b01b5fa5bc7bbdf4a646de1212
cewald <c.ewald@impericon.com>
2019-08-12 15:14:26 +0200

    #3332 Use Vue.set in CATEGORY_ADD_CATEGORY mutators, otherwise getCategories won't be updated at changes

--- core/modules/catalog-next/helpers/categoryHelpers.ts
+++ core/modules/catalog-next/helpers/categoryHelpers.ts
@@ -22,7 +22,7 @@ export const getSearchOptionsFromRouteParams = (params: { [key: string]: string 
 
   Object.keys(params)
     .filter(key => filterableKeys.includes(key))
-    .forEach(key => filters[key] = params[key])
-  
+    .forEach(key => { filters[key] = params[key] })
+
   return filters
 }
--- core/modules/catalog-next/store/category/getters.ts
+++ core/modules/catalog-next/store/category/getters.ts
@@ -14,14 +14,14 @@ import { parseCategoryPath } from '@vue-storefront/core/modules/breadcrumbs/help
 import { _prepareCategoryPathIds, getSearchOptionsFromRouteParams } from '../../helpers/categoryHelpers';
 
 const getters: GetterTree<CategoryState, RootState> = {
-  getCategories: (state) => (): Category[] => Object.values(state.categoriesMap),
+  getCategories: (state): Category[] => Object.values(state.categoriesMap),
   getCategoriesMap: (state): { [id: string]: Category} => state.categoriesMap,
   getCategoryProducts: (state) => state.products,
   getCategoryFrom: (state, getters) => (path: string = '') => {
-    return getters.getCategories().find(category => path.replace(/^(\/)/gm, '') === category.url_path) || {}
+    return getters.getCategories.find(category => path.replace(/^(\/)/gm, '') === category.url_path) || {}
   },
   getCategoryByParams: (state, getters, rootState) => (params: { [key: string]: string } = {}) => {
-    return getters.getCategories().find(category => {
+    return getters.getCategories.find(category => {
       let valueCheck = []
       const searchOptions = getSearchOptionsFromRouteParams(params)
       forEach(searchOptions, (value, key) => valueCheck.push(category[key] && category[key] === (category[key].constructor)(value)))
