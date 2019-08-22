0dc637eb19c577fb024219a101dcba026d05dff8
cewald <c.ewald@impericon.com>
2019-08-22 05:24:57 +0000

    Undo our changes in icmaa-category to overwrite category-next which are now been merged

--- src/modules/icmaa-category/index.ts
+++ src/modules/icmaa-category/index.ts
@@ -1,11 +1,8 @@
 import { StorefrontModule } from '@vue-storefront/core/lib/modules'
-import { extendStore } from '@vue-storefront/core/helpers'
 import { CategoryStore } from './store'
-import { ExtendedCategoryStore } from './store/category'
 
 const KEY = 'icmaa-category'
 
 export const IcmaaCategoryModule: StorefrontModule = function (app, store, router, moduleConfig, appConfig) {
   store.registerModule('icmaaCategory', CategoryStore)
-  extendStore('category-next', ExtendedCategoryStore)
 }
--- src/modules/icmaa-category/store/category/index.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from 'vuex'
-import getters from './getters'
-import mutations from './mutations'
-import CategoryState from '@vue-storefront/core/modules/catalog-next/store/category/CategoryState'
-
-export const ExtendedCategoryStore: Module<CategoryState, any> = {
-  getters,
-  mutations
-}
