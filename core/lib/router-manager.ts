2b945341e0a7e5d56825462e7911f3659b1e0071
patzick <tomczyk.patryk@gmail.com>
2019-08-23 15:16:44 +0200

    add route locker

--- core/lib/router-manager.ts
+++ core/lib/router-manager.ts
@@ -1,9 +1,10 @@
 import { Logger } from '@vue-storefront/core/lib/logger'
 import { router } from '@vue-storefront/core/app'
-import VueRouter, { RouteConfig } from 'vue-router'
+import VueRouter, { RouteConfig, Route } from 'vue-router'
 
 const RouterManager = {
   _registeredRoutes: new Array<RouteConfig>(),
+  _lockedRoutes: new Map(),
   addRoutes : function (routes: RouteConfig[], routerInstance: VueRouter = router): void {
     this._registeredRoutes.push(...routes)
     router.addRoutes(routes)
@@ -13,7 +14,24 @@ const RouterManager = {
   },
   findByPath: function (fullPath: string): RouteConfig {
     return this._registeredRoutes.find(r => r.fullPath === fullPath)
-  }  
+  },
+  lockRoute: function (route: Route) {
+    let resolver
+    this._lockedRoutes.set(route.path, {
+      lockPromise: new Promise(resolve => { resolver = resolve }),
+      resolver
+    })
+  },
+  getRouteLock: function (route: Route) {
+    return this._lockedRoutes.get(route.path) && this._lockedRoutes.get(route.path).lockPromise
+  },
+  unlockRoute: function (route: Route) {
+    const locker = this._lockedRoutes.get(route.path)
+    if (locker) {
+      locker.resolver()
+      this._lockedRoutes.delete(route.path)
+    }
+  }
 }
 
 export { RouterManager }
