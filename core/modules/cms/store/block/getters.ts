bbde288aa3f7522698b2cee3d6a5d8cc4422a508
andrzejewsky <vox3r69@gmail.com>
2019-08-25 13:46:16 +0200

    refactoring vuex cms module

--- core/modules/cms/store/block/getters.ts
+++ core/modules/cms/store/block/getters.ts
@@ -1,14 +1,35 @@
 import { GetterTree } from 'vuex'
 import CmsBlockState from '../../types/CmsBlockState'
 import RootState from '@vue-storefront/core/types/RootState'
+import { Logger } from '@vue-storefront/core/lib/logger'
 
 const getters: GetterTree<CmsBlockState, RootState> = {
-  cmsBlocks: (state) => state.items,
-  cmsBlockIdentifier: (state) => (identifier) => {
-    return state.items.find(item => typeof item === 'object' && item.identifier === identifier)
+  cmsBlocks: (state, getters) => {
+    Logger.error('The getter cmsBlock/addMethod has been deprecated please change to cmsBlock/getCmsBlocks')()
+
+    return getters.getCmsBlocks
   },
-  cmsBlockId: (state) => (id) => {
-    return state.items.find(item => item.id === id)
+  cmsBlockIdentifier: (state, getters) => (identifier) => {
+    Logger.error('The getter cmsBlock/cmsBlockIdentifier has been deprecated please change to cmsBlock/getCmsBlockIdentifier')()
+
+    return getters.cmsBlockIdentifier(identifier)
+  },
+  cmsBlockId: (state, getters) => (id) => {
+    Logger.error('The getter cmsBlock/cmsBlockId has been deprecated please change to cmsBlock/getCmsBlockId')()
+
+    return getters.getCmsBlockId(id)
+  },
+  getCmsBlockIdentifier: (state) => (identifier) =>
+    state.items.find(item => typeof item === 'object' && item.identifier === identifier),
+  getCmsBlockId: (state) => (id) => state.items.find(item => item.id === id),
+  getCmsBlocks: (state) => state.items,
+  hasItems: (state) => state.items && state.items.length > 0,
+  findCmsBlocks: (state, getters) => ({ key, value }) => {
+    if (getters.hasItems) {
+      return state.items.filter(item => item[key] === value)
+    }
+
+    return []
   }
 }
 
--- src/themes/default/components/core/blocks/Cms/Block.vue
+++ src/themes/default/components/core/blocks/Cms/Block.vue
@@ -59,9 +59,9 @@ export default {
   computed: {
     getCmsData () {
       if (this.id) {
-        return this.$store.getters[`cmsBlock/cmsBlockId`](this.id)
+        return this.$store.getters[`cmsBlock/getCmsBlockId`](this.id)
       } else if (this.identifier) {
-        return this.$store.getters[`cmsBlock/cmsBlockIdentifier`](this.identifier)
+        return this.$store.getters[`cmsBlock/getCmsBlockIdentifier`](this.identifier)
       }
       return null
     }
