b88309ff53439510558a7f9af7e943ac6da56905
andrzejewsky <vox3r69@gmail.com>
2019-08-25 12:48:13 +0200

    refactoring vuex whishlist module

--- core/modules/wishlist/store/actions.ts
+++ core/modules/wishlist/store/actions.ts
@@ -1,42 +1,31 @@
-import Vue from 'vue'
 import { ActionTree } from 'vuex'
 import * as types from './mutation-types'
-import i18n from '@vue-storefront/i18n'
-import { htmlDecode } from '@vue-storefront/core/lib/store/filters'
-import rootStore from '@vue-storefront/core/store'
 import RootState from '@vue-storefront/core/types/RootState'
 import WishlistState from '../types/WishlistState'
-import { cacheStorage } from '../'
-import { Logger } from '@vue-storefront/core/lib/logger'
+import { StorageManager } from '@vue-storefront/core/lib/storage-manager'
+import { notifications } from '@vue-storefront/core/modules/wishlist/helpers'
 
 const actions: ActionTree<WishlistState, RootState> = {
   clear (context) {
     context.commit(types.WISH_LOAD_WISH, [])
   },
-  load ({ commit, getters }, force: boolean = false) {
+  async load ({ commit, getters, dispatch }, force: boolean = false) {
     if (!force && getters.isWishlistLoaded) return
     commit(types.SET_WISHLIST_LOADED)
-    cacheStorage.getItem('current-wishlist', (err, storedItems) => {
-      if (err) throw new Error(err)
-      commit(types.WISH_LOAD_WISH, storedItems)
-      Logger.info('Wishlist state loaded from browser cache. ', 'cache', storedItems)()
-    })
+    const storedItems = await dispatch('loadFromCache')
+    commit(types.WISH_LOAD_WISH, storedItems)
   },
-  addItem ({ commit }, product) {
+  loadFromCache () {
+    const wishlistStorage = StorageManager.get('wishlist')
+    return wishlistStorage.getItem('current-wishlist')
+  },
+  addItem ({ commit, dispatch }, product) {
     commit(types.WISH_ADD_ITEM, { product })
-    rootStore.dispatch('notification/spawnNotification', {
-      type: 'success',
-      message: i18n.t('Product {productName} has been added to wishlist!', { productName: htmlDecode(product.name) }),
-      action1: { label: i18n.t('OK') }
-    })
+    dispatch('notification/spawnNotification', notifications.productAddedToWhishList(product.name), { root: true })
   },
-  removeItem ({ commit }, product) {
+  removeItem ({ commit, dispatch }, product) {
     commit(types.WISH_DEL_ITEM, { product })
-    rootStore.dispatch('notification/spawnNotification', {
-      type: 'success',
-      message: i18n.t('Product {productName} has been removed from wishlit!', { productName: htmlDecode(product.name) }),
-      action1: { label: i18n.t('OK') }
-    })
+    dispatch('notification/spawnNotification', notifications.productRemovedFromWhishList(product.name), { root: true })
   }
 }
 
