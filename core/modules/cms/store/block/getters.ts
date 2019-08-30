b285e06df1e49aa4cf5229de4c4f1364b6d099fa
andrzejewsky <vox3r69@gmail.com>
2019-08-30 12:09:07 +0200

    upgrade notes, change getters

--- core/modules/cms/store/block/getters.ts
+++ core/modules/cms/store/block/getters.ts
@@ -1,36 +1,20 @@
 import { GetterTree } from 'vuex'
 import CmsBlockState from '../../types/CmsBlockState'
 import RootState from '@vue-storefront/core/types/RootState'
-import { Logger } from '@vue-storefront/core/lib/logger'
 
 const getters: GetterTree<CmsBlockState, RootState> = {
-  cmsBlocks: (state, getters) => {
-    Logger.error('The getter cmsBlock/addMethod has been deprecated please change to cmsBlock/getCmsBlocks')()
-
-    return getters.getCmsBlocks
-  },
-  cmsBlockIdentifier: (state, getters) => (identifier) => {
-    Logger.error('The getter cmsBlock/cmsBlockIdentifier has been deprecated please change to cmsBlock/getCmsBlockByIdentifier')()
-
-    return getters.cmsBlockIdentifier(identifier)
-  },
-  cmsBlockId: (state, getters) => (id) => {
-    Logger.error('The getter cmsBlock/cmsBlockId has been deprecated please change to cmsBlock/getCmsBlockById')()
-
-    return getters.getCmsBlockById(id)
-  },
+  // @deprecated
+  cmsBlocks: (state, getters) => getters.getCmsBlocks,
+  // @deprecated
+  cmsBlockIdentifier: (state, getters) => (identifier) => getters.cmsBlockIdentifier(identifier),
+  // @deprecated
+  cmsBlockId: (state, getters) => (id) => getters.getCmsBlockById(id),
   getCmsBlockByIdentifier: (state) => (identifier) =>
     state.items.find(item => typeof item === 'object' && item.identifier === identifier),
   getCmsBlockById: (state) => (id) => state.items.find(item => item.id === id),
   getCmsBlocks: (state) => state.items,
   hasItems: (state) => state.items && state.items.length > 0,
-  findCmsBlocks: (state, getters) => ({ key, value }) => {
-    if (getters.hasItems) {
-      return state.items.filter(item => item[key] === value)
-    }
-
-    return []
-  }
+  findCmsBlocks: (state) => ({ key, value }) => state.items.filter(item => item[key] === value)
 }
 
 export default getters
