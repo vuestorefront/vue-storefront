fda75e12798e186e0e79d67a98eb642134fbaf60
Marcin Drwiega <mdrwiega@divante.pl>
2019-09-05 14:48:24 +0200

    fix: Removed blank line at the top of csv

--- core/scripts/utils/sort-translations.js
+++ core/scripts/utils/sort-translations.js
@@ -4,7 +4,14 @@ const { EOL } = require('os');
 const fixTranslation = (filename) => {
   const content = readFileSync(filename, 'utf8');
 
-  writeFileSync(filename, content.split(EOL).sort().join(EOL));
+  writeFileSync(
+    filename,
+    content
+      .split(EOL)
+      .sort()
+      .filter(line => line.length > 0)
+      .join(EOL) + EOL
+  );
 }
 
 process.argv.map(filename => {
