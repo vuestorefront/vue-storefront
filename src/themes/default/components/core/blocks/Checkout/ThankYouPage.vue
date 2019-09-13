5bb4e845990a5c4676901a265fa06a6303fa6413
jakubmakielkowski <36307776+jakubmakielkowski@users.noreply.github.com>
2019-09-13 07:59:16 +0200

    Update ThankYouPage
    Co-Authored-By: Patryk Tomczyk <13100280+patzick@users.noreply.github.com>

--- src/themes/default/components/core/blocks/Checkout/ThankYouPage.vue
+++ src/themes/default/components/core/blocks/Checkout/ThankYouPage.vue
@@ -31,7 +31,7 @@
               {{ $t("You can allow us to remind you about the order via push notification after coming back online. You'll only need to click on it to confirm.") }}
             </p>
              <p v-if="OfflineOnly && isNotificationSupported && !isPermissionGranted">
-              {{ $t("Or if you will stay on \"Order confirmation\" page, the order will be placed automatically without confirmation, once the internet connection will be back.") }}
+              {{ $t(`Or if you will stay on "Order confirmation" page, the order will be placed automatically without confirmation, once the internet connection will be back.`) }}
             </p>
             <p v-if="OfflineOnly && isNotificationSupported && isPermissionGranted">
               <strong>{{ $t('You will receive Push notification after coming back online. You can confirm the order by clicking on it') }}</strong>
