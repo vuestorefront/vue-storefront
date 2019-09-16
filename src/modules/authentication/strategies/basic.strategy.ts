d3d42c537e4fc22ea6772038d4aad3c54b322413
Marcin Drwiega <mdrwiega@divante.pl>
2019-09-16 15:09:45 +0200

    Change export to a default for basic auth strategy

--- src/modules/authentication/strategies/basic.strategy.ts
+++ src/modules/authentication/strategies/basic.strategy.ts
@@ -1,7 +1,6 @@
 import { Strategy } from 'passport';
 import { BasicStrategy } from 'passport-http';
-import { GenerateStrategy } from '../common';
 
-export const generateStrategy: GenerateStrategy = (config): Strategy => new BasicStrategy((user, password, done) => {
+export default (config): Strategy => new BasicStrategy((user, password, done) => {
   // TODO: Credentials
 });
