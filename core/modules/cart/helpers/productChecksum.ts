318cc8a8637d13d46ee1c689f549264b7ff0ff0d
ResuBaka <resubaka@gmail.com>
2019-10-01 06:15:16 +0200

    Made the product hash a bit less aggressive
    Change the has to use the sku and product_option.

--- core/modules/cart/helpers/productChecksum.ts
+++ core/modules/cart/helpers/productChecksum.ts
@@ -1,7 +1,14 @@
 import CartItem from '@vue-storefront/core/modules/cart/types/CartItem'
 import { sha3_224 } from 'js-sha3'
 
+const getDataToHash = (product: CartItem): any => {
+  return {
+    product_option: product.product_option,
+    sku: product.sku
+  }
+}
+
 const productChecksum = (product: CartItem): string =>
-  sha3_224(JSON.stringify(product))
+  sha3_224(JSON.stringify(getDataToHash(product)))
 
 export default productChecksum
