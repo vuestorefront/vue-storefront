4992a7ee247bb5f73456ccab6da56e0283eb801f
ResuBaka <resubaka@gmail.com>
2019-10-08 14:37:46 +0200

    Fix: possible problem when two products have no server_item_id and item_id
    This is a problem because then they are equal but should not so now we
    check if one is undefined and then return false in that case.

--- core/modules/cart/helpers/productsEquals.ts
+++ core/modules/cart/helpers/productsEquals.ts
@@ -15,8 +15,12 @@ const getProductType = (product: CartItem): string =>
 const getServerItemId = (product: CartItem): string | number =>
   product.server_item_id || product.item_id
 
-const isServerIdsEquals = (product1: CartItem, product2: CartItem): boolean =>
-  getServerItemId(product1) === getServerItemId(product2)
+const isServerIdsEquals = (product1: CartItem, product2: CartItem): boolean => {
+  if (getServerItemId(product1) === undefined || getServerItemId(product2) === undefined) {
+    return false
+  }
+  return getServerItemId(product1) === getServerItemId(product2)
+}
 
 const isChecksumEquals = (product1: CartItem, product2: CartItem): boolean =>
   getChecksum(product1) === getChecksum(product2)
