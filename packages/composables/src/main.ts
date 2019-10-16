7ce3d140e4b12bdb10590ee8a84dba304fe33308
Filip Rakowski <frakowski@divante.pl>
2019-10-16 15:12:26 +0200

    build basic core

--- packages/composables/src/main.ts
+++ packages/composables/src/main.ts
@@ -1,11 +1,5 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueCompositionApi from '@vue/composition-api';
+import { useProduct } from './hooks/useProduct'
 
-Vue.config.productionTip = false
-
-Vue.use(VueCompositionApi);
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+export {
+  useProduct
+}
