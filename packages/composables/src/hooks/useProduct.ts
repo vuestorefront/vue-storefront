325c0ac6362c150da5142d2383309ee8254071d9
Filip Rakowski <frakowski@divante.pl>
2019-10-16 18:58:38 +0200

    update hook

--- packages/composables/src/hooks/useProduct.ts
+++ packages/composables/src/hooks/useProduct.ts
@@ -5,12 +5,19 @@ type Product = Ref<string>
 type Configuration = Ref<string>
 type Configure = () => void
 
-export function useProduct (): UseProduct<Product, Configuration, Configure> {
-  const product = ref('productFromHook')
-  const currentConfiguration = ref('configurationFromHook')
+/**
+ * Hook that returns specific product(s) data.
+ * @param sku product sku
+ * @returns product - whole product with all configurations
+ * @returns currentcConfiguration - currently selected configuration (via `configure`)
+ * @returns configure - function that modfies `currentConfiguration` object with currently selected product configuration
+ */
+export function useProduct (sku: string): UseProduct<Product, Configuration, Configure> {
+  const product = ref('productFromHook' + sku)
+  const currentConfiguration = ref('configurationFromHook' + sku)
 
   const configure = () => { 
-    currentConfiguration.value = 'updatedConfigurationFromHook' 
+    currentConfiguration.value = 'updatedConfigurationFromHook' + sku
   }
 
   return {
