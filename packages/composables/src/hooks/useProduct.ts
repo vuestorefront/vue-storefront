481e202ac12da90efcb0fc76ad7f43382310b350
Filip Rakowski <frakowski@divante.pl>
2019-10-16 15:00:12 +0200

    add typings to demo

--- packages/composables/src/hooks/useProduct.ts
+++ packages/composables/src/hooks/useProduct.ts
@@ -1,10 +1,10 @@
 import { ref } from '@vue/composition-api'
 import { UseProduct } from '@vue-storefront/core'
 
-export function useProduct (): UseProduct<any, any, any> {
+export function useProduct (): UseProduct<string, string, () => void> {
   const product = ref('productFromHook')
   const configuration = ref('configurationFromHook')
-  
+
   const configure = () => { 
     configuration.value = 'updatedConfigurationFromHook' 
   }
