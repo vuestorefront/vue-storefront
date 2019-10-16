b14a333a6b4c5d277b7eed0061333c697ffd08d9
Filip Rakowski <frakowski@divante.pl>
2019-10-16 14:09:02 +0200

    add composables basic setup

---
+++ packages/composables/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    Hello
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'app',
+});
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
