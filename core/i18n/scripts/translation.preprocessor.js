37de6a0646f7c487186c1afdc34d5a5a3b35ee49
tkostuch <tkostuch@divante.pl>
2019-10-18 13:02:06 +0200

    remove unused dayjs locales

--- core/i18n/scripts/translation.preprocessor.js
+++ core/i18n/scripts/translation.preprocessor.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const dsvFormat = require('d3-dsv').dsvFormat
 const dsv = dsvFormat(',')
+const { currentBuildLocales } = require('../helpers')
 
 /**
  *  Converts an Array to an Object
@@ -15,14 +16,18 @@ function convertToObject (array) {
 }
 
 module.exports = function (csvDirectories, config = null) {
+  const currentLocales = currentBuildLocales()
+  const fallbackLocale = 'en-US'
   let messages = {}
   let languages = []
 
+  // get messages from CSV files
   csvDirectories.forEach(directory => {
     fs.readdirSync(directory).forEach(file => {
       const fullFileName = path.join(directory, file)
       const extName = path.extname(fullFileName)
       const baseName = path.posix.basename(file, extName)
+
       if (extName === '.csv') {
         const fileContent = fs.readFileSync(fullFileName, 'utf8')
         if (languages.indexOf(baseName) === -1) {
@@ -34,24 +39,27 @@ module.exports = function (csvDirectories, config = null) {
     })
   })
 
-  languages.forEach((language) => {
-    if (!config || !config.i18n.bundleAllStoreviewLanguages || (config.i18n.bundleAllStoreviewLanguages && language === 'en-US')) {
-      console.debug(`Writing JSON file: ${language}.json`)
-      fs.writeFileSync(path.join(__dirname, '../resource/i18n', `${language}.json`), JSON.stringify(messages[language]))
-    }
-  })
+  // create fallback
+  console.debug(`Writing JSON file fallback: ${fallbackLocale}.json`)
+  fs.writeFileSync(path.join(__dirname, '../resource/i18n', `${fallbackLocale}.json`), JSON.stringify(messages[fallbackLocale]))
 
+  // bundle all messages in one file
   if (config && config.i18n.bundleAllStoreviewLanguages) {
-    const bundledLanguages = { 'en-US': messages['en-US'] } // fallback locale
+    const bundledLanguages = { [fallbackLocale]: messages[fallbackLocale] } // fallback locale
     bundledLanguages[config.i18n.defaultLocale] = messages[config.i18n.defaultLocale] // default locale
-    Object.keys(config.storeViews).forEach((storeCode) => {
-      const store = config.storeViews[storeCode]
-      if (store && typeof store === 'object' && store.i18n) {
-        bundledLanguages[store.i18n.defaultLocale] = messages[store.i18n.defaultLocale]
-      }
+    currentLocales.forEach((locale) => {
+      bundledLanguages[locale] = messages[locale]
     })
+
+    console.debug(`Writing JSON file multistoreLanguages`)
     fs.writeFileSync(path.join(__dirname, '../resource/i18n', `multistoreLanguages.json`), JSON.stringify(bundledLanguages))
   } else {
+    currentLocales.forEach((language) => {
+      if (language !== fallbackLocale) return // it's already loaded
+      const filePath = path.join(__dirname, '../resource/i18n', `${language}.json`)
+      console.debug(`Writing JSON file: ${language}.json`)
+      fs.writeFileSync(filePath, JSON.stringify(messages[language]))
+    })
     fs.writeFileSync(path.join(__dirname, '../resource/i18n', `multistoreLanguages.json`), JSON.stringify({})) // fix for webpack compilation error in case of `bundleAllStoreviewLanguages` = `false` (#3188)
   }
 }
