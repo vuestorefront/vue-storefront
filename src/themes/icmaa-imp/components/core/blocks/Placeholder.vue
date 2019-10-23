31799e5a1a69bd5d452cc04a97ef0d416c9d17d1
cewald <c.ewald@impericon.com>
2019-10-23 10:31:01 +0000

    Add placeholder for teaser

---
+++ src/themes/icmaa-imp/components/core/blocks/Placeholder.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="t-w-full" :class="{ 't-bg-white': !plain }" :style="{ paddingTop: `${height}%` }" />
+</template>
+
+<script>
+export default {
+  props: {
+    ratio: {
+      type: String,
+      required: true
+    },
+    plain: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    height () {
+      const ratio = this.ratio.split(':')
+      return ratio[1] * 100 / ratio[0]
+    }
+  }
+}
+</script>
--- src/themes/icmaa-imp/components/core/blocks/Teaser/Teaser.vue
+++ src/themes/icmaa-imp/components/core/blocks/Teaser/Teaser.vue
@@ -1,16 +1,28 @@
 <template>
   <div id="teaser">
-    <template v-if="showLarge && teaserLarge">
-      <teaser-large :teaser="teaserLarge" class="t-mb-8" />
-    </template>
-    <template v-if="teaserSmall && teaserSmall.length > 0">
-      <template v-if="showSmallInRow">
-        <div class="t-flex t-flex-wrap t--mx-4 t-px-4">
-          <teaser-small-row v-for="(teaser, index) in teaserSmall" :teaser="teaser" :index="index" :key="'small_' + index" />
+    <div class="placeholder" v-if="loading">
+      <div class="xs:t-px-0 sm:t-px-4 t-mb-8" v-if="showLarge">
+        <placeholder :ratio="viewport === 'sm' ? '133:78' : '12:7'" />
+      </div>
+      <div class="t-flex t-px-4 t-flex-wrap t--mx-4">
+        <div v-for="(n, i) in 4" :key="'placeholder-' + i" class="t-px-4" :class="{ 't-w-1/2 lg:t-w-1/4 t-mb-8': showSmallInRow, 't-w-full': !showSmallInRow, 't-mb-8': !showSmallInRow && i !== 3 }">
+          <placeholder :ratio="showSmallInRow ? '32:35' : '2:1'" />
         </div>
+      </div>
+    </div>
+    <template v-else>
+      <template v-if="showLarge && teaserLarge">
+        <teaser-large :teaser="teaserLarge" class="t-mb-8" />
       </template>
-      <template v-else>
-        <teaser-small v-for="(teaser, index) in teaserSmall" :teaser="teaser" :index="index" :key="'small_' + index" :class="{ 't-mb-8': index !== (teaserSmall.length - 1) }" />
+      <template v-if="teaserSmall && teaserSmall.length > 0">
+        <template v-if="showSmallInRow">
+          <div class="t-flex t-flex-wrap t--mx-4 t-px-4">
+            <teaser-small-row v-for="(teaser, index) in teaserSmall" :teaser="teaser" :index="index" :key="'small_' + index" />
+          </div>
+        </template>
+        <template v-else>
+          <teaser-small v-for="(teaser, index) in teaserSmall" :teaser="teaser" :index="index" :key="'small_' + index" :class="{ 't-mb-8': index !== (teaserSmall.length - 1) }" />
+        </template>
       </template>
     </template>
   </div>
@@ -19,6 +31,7 @@
 <script>
 import { mapGetters } from 'vuex';
 
+import Placeholder from 'theme/components/core/blocks/Placeholder'
 import TeaserLarge from 'theme/components/core/blocks/Teaser/TeaserLarge'
 import TeaserSmall from 'theme/components/core/blocks/Teaser/TeaserSmall'
 import TeaserSmallRow from 'theme/components/core/blocks/Teaser/TeaserSmallRow'
@@ -43,13 +56,20 @@ export default {
       default: 4
     }
   },
+  data () {
+    return {
+      loading: true
+    }
+  },
   components: {
+    Placeholder,
     TeaserLarge,
     TeaserSmall,
     TeaserSmallRow
   },
   computed: {
     ...mapGetters('icmaaTeaser', ['getSmallTeaser', 'getLargeTeaser']),
+    ...mapGetters({ viewport: 'ui/getViewport' }),
     teaserLarge () {
       return this.getLargeTeaser(this.tags)[0]
     },
@@ -58,8 +78,9 @@ export default {
       return teaser.slice(0, this.TeaserSmallRow ? 4 : this.limit)
     }
   },
-  mounted () {
-    this.$store.dispatch('icmaaTeaser/list', this.tags)
+  async mounted () {
+    await this.$store.dispatch('icmaaTeaser/list', this.tags)
+    this.loading = false
   }
 }
 </script>
