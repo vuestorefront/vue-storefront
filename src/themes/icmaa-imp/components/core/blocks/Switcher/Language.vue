60af4eee1dca159da350be04a2e26397f84883b0
simonchill <s.chill@impericon.com>
2019-11-11 20:06:45 +0100

    #189107 Style language-selector modal (#144)

--- src/themes/icmaa-imp/components/core/blocks/Switcher/Language.vue
+++ src/themes/icmaa-imp/components/core/blocks/Switcher/Language.vue
@@ -1,59 +1,37 @@
 <template>
-  <modal name="modal-switcher" :width="650">
-    <p slot="header">
-      {{ $t('Choose your country') }}
-    </p>
-    <div slot="content">
-      <div :class="{ 'columns': enableColumns }">
-        <div class="country country-current">
-          <h3>{{ $t(config.i18n.fullCountryName) }}</h3>
-          <ul>
-            <li><a href="/">{{ $t(config.i18n.fullLanguageName) }}</a></li>
-          </ul>
-        </div>
-        <div class="country country-available" v-for="(storeView, storeCode) in storeViews" :key="storeCode" v-if="!storeView.disabled && typeof storeView === 'object' && storeView.i18n">
-          <h3>{{ $t(storeView.i18n.fullCountryName) }}</h3>
-          <ul>
-            <li><a :href="storeView.url">{{ $t(storeView.i18n.fullLanguageName) }}</a></li>
-          </ul>
-        </div>
+  <modal name="modal-switcher" :width="500">
+    <div slot="header">
+      {{ $t('Switch store') }}
+    </div>
+    <div class="t-flex t-flex-wrap t--mx-2 t--mb-4">
+      <div class="t-w-1/2 t-px-2 t-pb-4" v-for="(storeView) in storeViews" :key="storeView.storeCode">
+        <language-button :store-view="storeView" :is-current="storeView.storeId === currentStoreView.storeId" />
       </div>
     </div>
   </modal>
 </template>
+
 <script>
-import Modal from 'theme/components/core/Modal.vue'
 import config from 'config'
+import { mapGetters } from 'vuex'
+import { currentStoreView } from '@vue-storefront/core/lib/multistore'
+
+import Modal from 'theme/components/core/Modal.vue'
+import LanguageButton from 'theme/components/core/blocks/Switcher/LanguageButton'
+
 export default {
   components: {
-    Modal
-  },
-  data () {
-    return {
-      minCountryPerColumn: 3,
-      componentLoaded: false
-    }
+    Modal,
+    LanguageButton
   },
   computed: {
+    ...mapGetters({ storeConfigs: 'icmaaConfig/getMap' }),
+    currentStoreView () {
+      return currentStoreView()
+    },
     storeViews () {
-      return config.storeViews
-    },
-    config () {
-      return config
-    },
-    enableColumns () {
-      var enableStoreViews = Object.keys(config.storeViews).filter((key) => {
-        var value = config.storeViews[key]
-        return (typeof value === 'object' && value.disabled === false)
-      })
-      return enableStoreViews.length > this.minCountryPerColumn
+      return this.storeConfigs.map(s => config.storeViews[s.storeCode])
     }
-  },
-  mounted () {
-    this.$nextTick(() => {
-      this.componentLoaded = true
-      this.$bus.$emit('modal-show', 'modal-switcher')
-    })
   },
   methods: {
     close () {
@@ -62,36 +40,3 @@ export default {
   }
 }
 </script>
-<style lang="scss" scoped>
-  h3 {
-    margin-top: 0;
-    margin-bottom: 0.5em;
-  }
-  .columns {
-    -moz-column-count: 2;
-    column-count: 2;
-    column-gap: 15px;
-    .country {
-      -webkit-column-break-inside: avoid;
-      page-break-inside: avoid;
-      break-inside: avoid;
-    }
-  }
-  .country {
-    margin-bottom: 2em;
-    color: #4f4f4f;
-  }
-  ul {
-    margin: 0;
-    padding: 0;
-    list-style-type: none;
-    margin-left: -1em;
-    li {
-      display: inline-block;
-      margin-left: 1em;
-      a {
-        font-size: 0.9em;
-      }
-    }
-  }
-</style>
---
+++ src/themes/icmaa-imp/components/core/blocks/Switcher/LanguageButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <a :href="storeView.url" class="t-flex t-items-center t-rounded-sm t-border t-border-base-lightest t-p-2 t-cursor-pointer" :class="{ 't-bg-base-lightest': isCurrent }">
+    <flag-icon :iso="i18n.defaultCountry" class="t-flex-fix t-mr-2" format="4x3" width="40" height="30" />
+    <div class="t-text-sm t-leading-none">{{ i18n.fullCountryName }}</div>
+  </a>
+</template>
+
+<script>
+import config from 'config'
+import FlagIcon from 'theme/components/core/blocks/FlagIcon'
+
+export default {
+  components: {
+    FlagIcon
+  },
+  props: {
+    storeView: {
+      type: Object,
+      required: true
+    },
+    isCurrent: {
+      type: Boolean,
+      required: true
+    }
+  },
+  computed: {
+    i18n () {
+      return this.getI18n(this.storeView)
+    }
+  },
+  methods: {
+    getI18n (storeView) {
+      if (!storeView.i18n) {
+        return storeView.extend ? this.getI18n(config.storeViews[storeView.extend]) : config.i18n
+      }
+
+      return storeView.i18n
+    }
+  }
+}
+</script>
