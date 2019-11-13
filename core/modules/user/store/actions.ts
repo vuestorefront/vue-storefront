99540d1109aab9273d9971c4887172587eb07dc7
Petar Petrov <p.petrov@codecoda.com>
2019-11-13 11:49:01 +0200

    fix empty space

--- core/modules/user/store/actions.ts
+++ core/modules/user/store/actions.ts
@@ -16,6 +16,7 @@ const actions: ActionTree<UserState, RootState> = {
   async startSession ({ commit, dispatch, getters }) {
     const usersCollection = StorageManager.get('user')
     const userData = await usersCollection.getItem('current-user')
+
     if (isServer || getters.isLocalDataLoaded) return
     commit(types.USER_LOCAL_DATA_LOADED, true)
 
