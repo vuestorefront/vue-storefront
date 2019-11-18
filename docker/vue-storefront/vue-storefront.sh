206ec18ab6293802c9d645649dfad2d94db68273
Dan <d.irinkovn1@gmail.com>
2019-11-19 00:44:37 +0200

    asdad unknown issue with yarn build and NODE_ENV=docker

--- docker/vue-storefront/vue-storefront.sh
+++ docker/vue-storefront/vue-storefront.sh
@@ -6,7 +6,6 @@ rm config_repo/production.js
 cp -a -r -f config_repo/. config
 
 yarn install || exit $?
-npm install --save-dev cross-env@3.1.4
 
 yarn build:client && yarn build:server && yarn build:sw || exit $?
 
