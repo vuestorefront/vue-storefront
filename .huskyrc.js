83080f91cd7587e35a85d3ba9aefbc4c77c68605
cewald <c.ewald@impericon.com>
2019-12-04 06:50:25 +0000

    Disable unit tests on pre-commit

--- .huskyrc.js
+++ .huskyrc.js
@@ -4,9 +4,11 @@ module.exports = {
   'hooks': {
     'pre-commit': tasks([
       'lint-staged'
-    ]),
-    'pre-push': tasks([
-      'yarn test:unit'
     ])
+    // We don't have valid tests yet
+    // And it's slow as f$*! ...
+    // 'pre-push': tasks([
+    //   'yarn test:unit'
+    // ])
   }
 }
