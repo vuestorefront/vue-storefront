52f9f0a95092b5b4b2473c75b67e7d6e2eaf00c2
Dan <d.irinkovn1@gmail.com>
2019-12-17 19:34:23 +0200

    Polishing isServer URL switcher for kube

--- core/lib/search/adapter/api/searchAdapter.ts
+++ core/lib/search/adapter/api/searchAdapter.ts
@@ -80,10 +80,10 @@ export class SearchAdapter {
       },
       body: config.elasticsearch.queryMethod === 'POST' ? JSON.stringify(ElasticsearchQueryBody) : null
     })
-    .then(resp => { return resp.json() })
-    .catch(error => {
-      throw new Error('FetchError in request to ES: ' + error.toString())
-    })
+      .then(resp => { return resp.json() })
+      .catch(error => {
+        throw new Error('FetchError in request to ES: ' + error.toString())
+      })
   }
 
   public handleResult (resp, type, start = 0, size = 50): SearchResponse {
