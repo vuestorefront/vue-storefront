8ca34208bbf189d59d070ff29cc86b2d471556b2
Dan <d.irinkovn1@gmail.com>
2019-12-17 04:07:37 +0200

    fixing config url issue in prod

--- core/modules/order/types/Order.ts
+++ core/modules/order/types/Order.ts
@@ -13,8 +13,8 @@ export interface Order {
   transaction?: any,  // Added by Dan
   transactionId?: any,// Added by Dan
   store_id?: number | string,
-  transaction?: any,  // Added by Dan
-  transactionId?: any,// Added by Dan
+  transaction?: any, // Added by Dan
+  transactionId?: any, // Added by Dan
   /**
    * Products list
    */
