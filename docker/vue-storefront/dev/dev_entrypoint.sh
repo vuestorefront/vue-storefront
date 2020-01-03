b1749d4b426cd3665237337700207cfd1722112a
Dan <d.irinkovn1@gmail.com>
2020-01-03 20:06:46 +0200

    fixed entrypoint scripts

--- docker/vue-storefront/dev/dev_entrypoint.sh
+++ docker/vue-storefront/dev/dev_entrypoint.sh
@@ -6,5 +6,7 @@ rm -f ./config_repo/production.json
 cp -a -r -f config_repo/. config
 cp -a -f config_bin/local_docker.json config/local.json
 
+yarn install
+
 npm install pm2 -g
 pm2 start 'yarn dev'
