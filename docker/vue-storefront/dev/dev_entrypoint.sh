f65039d22c38dc8388faa1cd4466c518d05cf30c
Dan <d.irinkovn1@gmail.com>
2020-01-03 16:43:16 +0200

    fixing script issue

--- docker/vue-storefront/dev/dev_entrypoint.sh
+++ docker/vue-storefront/dev/dev_entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 cd /var/www
+
+#copy local.config to Container
+rm -f ./config_repo/production.json
+cp -a -r -f config_repo/. config
+cp -a -f config_bin/local_docker.json config/local.json
+
 npm install pm2 -g
 pm2 start 'yarn dev'
