9063ef73f5546ac83327dc42a73e1a440416e320
Dan <d.irinkovn1@gmail.com>
2020-01-04 05:09:13 +0200

    working on dev script polish

--- docker/vue-storefront/dev/dev_entrypoint.sh
+++ docker/vue-storefront/dev/dev_entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-cd /var/www
 
+cd /var/www
 #copy local.config to Container
-rm -f ./config_repo/production.json
-cp -a -r -f config_repo/. config
+rm -f /var/config_repo/production.json
+cp -a -r -f /var/config_repo/. config
+
 cp -a -f config_bin/local_docker.json config/local.json
 
 yarn install
 
-npm install pm2 -g
-pm2 start 'yarn dev'
+pm2-runtime start 'yarn dev'
