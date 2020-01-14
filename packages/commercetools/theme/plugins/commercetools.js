aadde05d83afd99ee8c0c43b68092ca1b07611d3
Patryk Andrzejewski <vox3r69@gmail.com>
2020-01-14 09:47:41 +0100

    set new api client (#107)
    Co-authored-by: Filip Rakowski <f.rakowskI@hotmail.com>

--- packages/commercetools/theme/plugins/commercetools.js
+++ packages/commercetools/theme/plugins/commercetools.js
@@ -2,14 +2,22 @@ import { setup } from '@vue-storefront/commercetools-api'
 
 setup({
   api: {
-    uri: 'https://api.commercetools.com/vue-storefront-next/graphql',
+    uri: 'https://api.commercetools.com/vsf-ct-dev/graphql',
     authHost: 'https://auth.sphere.io',
-    projectKey: 'vue-storefront-next',
-    clientId: 'ULi2QVos7ZoeBD_cY90aFNmc',
-    clientSecret: '2eX7tGiZsZt0uexGQlcF2tgwbWEXIgbf',
+    projectKey: 'vsf-ct-dev',
+    clientId: 'E4kSLZbP-XGL6KDEJU1Pd5Sl',
+    clientSecret: 'U_5IRgdcKHkRlSMeCMabo57gYybDBIUU',
     scopes: [
-      'manage_products:vue-storefront-next',
-      'manage_orders:vue-storefront-next'
+      'view_types:vsf-ct-dev',
+      'create_anonymous_token:vsf-ct-dev',
+      'manage_my_orders:vsf-ct-dev',
+      'view_customers:vsf-ct-dev',
+      'view_shopping_lists:vsf-ct-dev',
+      'manage_my_profile:vsf-ct-dev',
+      'manage_my_shopping_lists:vsf-ct-dev',
+      'manage_my_payments:vsf-ct-dev',
+      'view_products:vsf-ct-dev',
+      'view_published_products:vsf-ct-dev'
     ],
   },
   locale: 'en',
