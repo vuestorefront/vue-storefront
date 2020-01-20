4cd4e55be1c22d497d7ad9b0c7074a763d30f091
Filip Rakowski <frakowski@divante.pl>
2020-01-20 12:29:16 +0100

    set image sizes for product cards in sfui

--- packages/commercetools/helpers/src/index.ts
+++ packages/commercetools/helpers/src/index.ts
@@ -7,6 +7,7 @@ export const getProductName = (product: ProductVariant): string => product ? (pr
 
 export const getProductSlug = (product: ProductVariant): string => product ? (product as any)._slug : ''
 
+// todo change to getProductPrices returning different types of prices https://github.com/DivanteLtd/next/issues/128
 export const getProductPrice = (product: ProductVariant): number | null => product ? product.price.value.centAmount / 100 : null
 
 export const getProductGallery = (product: ProductVariant): UiMediaGalleryItem[] =>
