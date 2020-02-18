855eae0b7a116efd98bc9ade3784557addc9a045
Tony T <ttruong@jsproducts.com>
2020-02-18 09:55:31 -0800

    Add yarn build to dev docker start script

--- docker/vue-storefront/vue-storefront.sh
+++ docker/vue-storefront/vue-storefront.sh
@@ -3,6 +3,7 @@ set -e
 
 if [ "$VS_ENV" = 'dev' ]; then
   yarn install
+  yarn build
   yarn dev
 else
   yarn cross-env NODE_ENV=production TS_NODE_PROJECT=\"tsconfig-build.json\" ts-node ./core/scripts/server.ts --no-daemon
