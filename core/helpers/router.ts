76d52943b27dc40e03f72e30118551295abc4765
Dominik Deimel <infodominikdeimel.com>
2020-02-28 13:26:52 +0100

    added wrapper for adding new push method prototype.

--- core/helpers/router.ts
+++ core/helpers/router.ts
@@ -1,52 +1,48 @@
-import VueRouter, { RouteConfig } from "vue-router";
-import { RouterManager } from "@vue-storefront/core/lib/router-manager";
-import { ErrorHandler, RawLocation, Route } from "vue-router/types/router";
+import VueRouter, { RouteConfig } from 'vue-router'
+import { RouterManager } from '@vue-storefront/core/lib/router-manager'
+import { ErrorHandler, RawLocation, Route } from 'vue-router/types/router'
+import { once } from 'core/helpers/index'
 
-const originalPush = VueRouter.prototype.push;
-VueRouter.prototype.push = function push(
-  location: RawLocation,
-  onComplete: Function = () => {},
-  onAbort?: ErrorHandler
-): Promise<Route> {
-  if (onComplete || onAbort)
-    return originalPush.call(this, location, onComplete, onAbort);
-  return originalPush.call(this, location).catch(err => err);
-};
-
+once('__VUE_EXTEND_PUSH_RR__', () => {
+  const originalPush = VueRouter.prototype.push
+  VueRouter.prototype.push = function push (location: RawLocation, onComplete: Function = () => {}, onAbort?: ErrorHandler): Promise<Route> {
+    if (onComplete || onAbort) return originalPush.call(this, location, onComplete, onAbort)
+    return originalPush.call(this, location).catch(err => err)
+  }
+}
 export const createRouter = (): VueRouter => {
   return new VueRouter({
-    mode: "history",
+    mode: 'history',
     base: __dirname,
     scrollBehavior: (to, from, savedPosition) => {
       if (to.hash) {
         return {
           selector: to.hash
-        };
+        }
       }
       if (savedPosition) {
-        return savedPosition;
-      } else if (to.path !== from.path) {
-        // do not change scroll position when navigating on the same page (ex. change filters)
-        return { x: 0, y: 0 };
+        return savedPosition
+      } else if (to.path !== from.path) { // do not change scroll position when navigating on the same page (ex. change filters)
+        return { x: 0, y: 0 }
       }
     }
-  });
-};
+  })
+}
 
 export const createRouterProxy = (router: VueRouter): VueRouter => {
-  const ProxyConstructor = Proxy || require("proxy-polyfill/src/proxy");
+  const ProxyConstructor = Proxy || require('proxy-polyfill/src/proxy')
 
   return new ProxyConstructor(router, {
-    get(target, propKey) {
-      const origMethod = target[propKey];
+    get (target, propKey) {
+      const origMethod = target[propKey]
 
-      if (propKey === "addRoutes") {
-        return function(routes: RouteConfig[], ...args): void {
-          return RouterManager.addRoutes(routes, ...args);
-        };
+      if (propKey === 'addRoutes') {
+        return function (routes: RouteConfig[], ...args): void {
+          return RouterManager.addRoutes(routes, ...args)
+        }
       }
 
-      return origMethod;
+      return origMethod
     }
-  });
-};
+  })
+}
