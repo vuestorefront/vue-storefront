734c61e9ee0ffac5658c368f9a8b42198db5b994
cewald <c.ewald@impericon.com>
2020-03-03 18:14:17 +0000

    Bugfix for unisex product images in pdp

--- src/modules/icmaa-catalog/store/product/actions.ts
+++ src/modules/icmaa-catalog/store/product/actions.ts
@@ -1,10 +1,39 @@
 import { ActionTree } from 'vuex'
 import RootState from '@vue-storefront/core/types/RootState'
 import ProductState from '@vue-storefront/core/modules/catalog/types/ProductState'
+import * as types from '@vue-storefront/core/modules/catalog/store/product/mutation-types'
 import config, { icmaa } from 'config'
+
 import cloneDeep from 'lodash-es/cloneDeep'
+import uniqBy from 'lodash-es/uniqBy'
+
+import { getMediaGallery, configurableChildrenImages, attributeImages } from '@vue-storefront/core/modules/catalog/helpers'
 
 const actions: ActionTree<ProductState, RootState> = {
+  /**
+   * Clone of originial `product/setProductGallery`
+   *
+   * Changes:
+   * * When `mergeConfigurableChildren` is disabled, just show media-gallery items.
+   * * Refactor to avoid redundant code.
+   */
+  setProductGallery (context, { product }) {
+    let gallery = []
+    if (product.type_id === 'configurable' && product.hasOwnProperty('configurable_children')) {
+      if (!config.products.gallery.mergeConfigurableChildren && product.is_configured) {
+        gallery = uniqBy(getMediaGallery(product), 'src')
+      } else {
+        gallery = uniqBy(configurableChildrenImages(product).concat(getMediaGallery(product)), 'src')
+      }
+    } else {
+      gallery = uniqBy(configurableChildrenImages(product).concat(getMediaGallery(product)), 'src')
+    }
+
+    context.commit(
+      types.PRODUCT_SET_GALLERY,
+      gallery.filter(f => f.src && f.src !== config.images.productPlaceholder)
+    )
+  },
   /**
    * Clone of originial `product/loadProductBreadcrumbs`
    *
