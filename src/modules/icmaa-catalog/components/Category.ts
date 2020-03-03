5e5899ceb7c1de0da720fe5aaaef936bc035282c
cewald <c.ewald@impericon.com>
2020-03-03 17:33:44 +0000

    Add cache-tags to home, product and category page

--- src/modules/icmaa-catalog/components/Category.ts
+++ src/modules/icmaa-catalog/components/Category.ts
@@ -3,5 +3,10 @@ import { mapGetters } from 'vuex'
 export default {
   computed: {
     ...mapGetters({ viewport: 'ui/getViewport' })
+  },
+  async asyncData ({ context }) {
+    if (context) {
+      context.output.cacheTags.add(`category`)
+    }
   }
 }
