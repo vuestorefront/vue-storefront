524623e3e7eaedb0bf4c8a0054ebe07ff4e9d8d7
tkostuch <tkostuch@divante.pl>
2020-03-27 12:37:50 +0100

    add ssrAppId to avoid second meta render on csr

--- core/app.ts
+++ core/app.ts
@@ -60,8 +60,10 @@ const createApp = async (ssrContext, config, storeCode = null): Promise<{app: Vu
   // @deprecated from 2.0
   once('__VUE_EXTEND__', () => {
     Vue.use(Vuelidate)
-    Vue.use(VueLazyload, { attempt: 2, preLoad: 1.5 })
-    Vue.use(Meta)
+    Vue.use(VueLazyload, {attempt: 2, preLoad: 1.5})
+    Vue.use(Meta, {
+      ssrAppId: 1
+    })
     Vue.use(VueObserveVisibility)
 
     Object.keys(corePlugins).forEach(key => {
