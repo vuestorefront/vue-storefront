cf625d2e0bfe86fec6581966998547330a1bf949
tkostuch <tkostuch@divante.pl>
2020-04-06 14:31:42 +0200

    add fastly module

---
+++ src/modules/fastly/README.md
@@ -0,0 +1,27 @@
+# VSF Cache Fastly
+This module extends default caching docs/guide/basics/ssr-cache.md to allow using fastly as cache provider.
+
+## How to install
+Add to config:
+```json
+"fastly": {
+  "enabled": true,
+  "serviceId": "xyz", // (https://docs.fastly.com/en/guides/finding-and-managing-your-account-info#finding-your-service-id)
+  "token": "xyz" // fastly api token (https://docs.fastly.com/api/auth#tokens)
+}
+```
+
+Change those values in `server` section:
+```json
+"useOutputCacheTagging": true,
+"useOutputCache": true
+```
+
+## How to purge cache?
+Open:
+```
+http://localhost:3000/invalidate?key=aeSu7aip&tag=home
+```
+
+## Problems
+Please be aware that you can send maximum 256 keys per request, more info https://docs.fastly.com/api/purge#purge_db35b293f8a724717fcf25628d713583
---
+++ src/modules/fastly/server.ts
@@ -0,0 +1,36 @@
+import { serverHooks } from '@vue-storefront/core/server/hooks'
+import fetch from 'isomorphic-fetch'
+import config from 'config'
+
+serverHooks.beforeOutputRenderedResponse(({ output, res, context }) => {
+  if (!config.get('fastly.enabled')) {
+    return output
+  }
+
+  const tagsArray = Array.from(context.output.cacheTags)
+  const cacheTags = tagsArray.join(' ')
+  res.setHeader('Surrogate-Key', cacheTags)
+
+  return output
+})
+
+serverHooks.beforeCacheInvalidated(async ({ tags }) => {
+  if (!config.get('fastly.enabled') || !config.get('server.useOutputCache') || !config.get('server.useOutputCacheTagging')) {
+    return
+  }
+
+  console.log('Invalidating Fastly Surrogate-Key')
+  const surrogate_keys = tags.filter((tag) =>
+    config.server.availableCacheTags.indexOf(tag) >= 0 ||
+    config.server.availableCacheTags.find(t => tag.indexOf(t) === 0)
+  )
+  console.log(surrogate_keys)
+
+  const response = await fetch(`https://api.fastly.com/service/${config.get('fastly.serviceId')}/purge`, {
+    method: 'POST',
+    headers: { 'Fastly-Key': config.get('fastly.token') },
+    body: JSON.stringify({ surrogate_keys })
+  })
+  const text = await response.text()
+  console.log(text)
+})
