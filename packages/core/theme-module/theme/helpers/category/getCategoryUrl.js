b3781944de0fdf64256e6cb0db04efbe19c23d49
Luke Romanowicz <lromanowicz1@gmail.com>
2020-04-22 14:57:35 +0200

    fix(core-theme): fix CT getCategoryUrl

--- packages/core/theme-module/theme/helpers/category/getCategoryUrl.js
+++ packages/core/theme-module/theme/helpers/category/getCategoryUrl.js
@@ -1,3 +1,3 @@
 export function getCategoryUrl(category, context = this) {
-  return `/c/${context.root.$route.params.slug_1}/${category.slug}`;
+  return `/c/${context.$route.params.slug_1}/${category.slug}`;
 }
