9364b1d4a4c5f202ded6a6e648b686baf0030b30
andrzejewsky <vox3r69@gmail.com>
2020-05-06 14:49:21 +0200

    fix price

--- packages/commercetools/composables/src/getters/_utils.ts
+++ packages/commercetools/composables/src/getters/_utils.ts
@@ -97,9 +97,5 @@ export const createPrice = (product: ProductVariant | LineItem): AgnosticPrice =
 };
 
 export const createFormatPrice = (price: number) => {
-  if (!price) {
-    return null;
-  }
-
   return new Intl.NumberFormat(`${locale}-${country}`, { style: 'currency', currency }).format(price);
 };
