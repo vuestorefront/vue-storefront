de93d9529d4ed93376aa208c10b399c4d102fd1f
tkostuch <tkostuch@divante.pl>
2020-05-18 17:33:50 +0200

    disable visibility check

--- core/modules/catalog/helpers/associatedProducts/buildQuery.ts
+++ core/modules/catalog/helpers/associatedProducts/buildQuery.ts
@@ -6,9 +6,8 @@ import { SearchQuery } from 'storefront-query-builder'
  */
 export default function buildQuery (skus: string[]): SearchQuery {
   let productsQuery = new SearchQuery()
-  productsQuery = productsQuery.applyFilter({ key: 'sku', value: { 'in': skus } })
   productsQuery = productsQuery
-    .applyFilter({ key: 'visibility', value: { 'in': [2, 3, 4] } })
+    .applyFilter({ key: 'sku', value: { 'in': skus } })
     .applyFilter({ key: 'status', value: { 'in': [1] } })
   if (config.products.listOutOfStockProducts === false) {
     productsQuery = productsQuery.applyFilter({ key: 'stock.is_in_stock', value: { 'eq': true } })
