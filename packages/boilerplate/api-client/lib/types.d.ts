3b72633bb42b799e8dd5cf6bb6157683b4a1db8d
krskibin <krskibin@gmail.com>
2020-05-19 11:39:15 +0200

    Stash lib changes in boilerplate package

--- packages/boilerplate/api-client/lib/types.d.ts
+++ packages/boilerplate/api-client/lib/types.d.ts
@@ -3,15 +3,3 @@ export declare type Wishlist = {};
 export declare type ProductVariant = {};
 export declare type Category = {};
 export declare type CategoryFilter = {};
-export declare type ShippingMethod = {};
-export declare type LineItem = {};
-export interface ApiClientMethods {
-    getCategory(params: {}): Promise<Category[]>;
-    getProduct(params: {}): Promise<ProductVariant[]>;
-}
-export interface ApiClientSettings {
-    overrides: {
-        getCategory?(): Promise<Category[]>;
-        getProduct?(params: {}): Promise<ProductVariant[]>;
-    };
-}
