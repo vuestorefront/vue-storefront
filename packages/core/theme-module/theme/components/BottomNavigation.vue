709242f14e8fd91e6ed73c4b2b4b86ed57481805
Carlo Kok <carlo@rb2.nl>
2020-05-25 16:19:52 +0200

    Toggle cart, wishlist and account from bottom navigation (mobile)

--- packages/core/theme-module/theme/components/BottomNavigation.vue
+++ packages/core/theme-module/theme/components/BottomNavigation.vue
@@ -5,12 +5,13 @@
       <SfBottomNavigationItem :class="$route.path == '/' ? 'sf-bottom-navigation__item--active' : ''" icon="home" size="20px" label="Home"/>
     </nuxt-link>
     <SfBottomNavigationItem data-cy="bottom-navigation-url_menu" icon="menu" size="20px" label="Menu"/>
-    <SfBottomNavigationItem data-cy="bottom-navigation-url_wishlist" icon="heart" size="20px" label="Wishlist"/>
-    <SfBottomNavigationItem data-cy="bottom-navigation-url_account" icon="profile" size="20px" label="Account"/>
+    <SfBottomNavigationItem data-cy="bottom-navigation-url_wishlist" icon="heart" size="20px" label="Wishlist" @click="toggleWishlistSidebar()" />
+    <SfBottomNavigationItem data-cy="bottom-navigation-url_account" icon="profile" size="20px" label="Account" @click="onAccountClicked()"/>
     <!-- TODO: add logic for label - if on Home then Basket, if on PDC then AddToCart etc. -->
     <SfBottomNavigationItem data-cy="bottom-navigation-url_add-to-cart"
       label="Basket"
       icon="add_to_cart"
+      @click="toggleCartSidebar()"
       >
       <template #icon>
         <SfCircleIcon aria-label="Add to cart">
@@ -28,12 +29,24 @@
 
 <script>
 import { SfBottomNavigation, SfIcon, SfCircleIcon } from '@storefront-ui/vue';
+import uiState from '~/assets/ui-state';
+import { useUser } from '<%= options.composables %>';
+const { toggleCartSidebar, toggleWishlistSidebar, toggleLoginModal } = uiState;
 
 export default {
   components: {
     SfBottomNavigation,
     SfIcon,
     SfCircleIcon
+  },
+  setup(props, { root }) {
+    const { isAuthenticated } = useUser();
+    const onAccountClicked = () => {
+      isAuthenticated && isAuthenticated.value ? root.$router.push('/my-account') : toggleLoginModal();
+    };
+    return {
+      toggleCartSidebar, onAccountClicked, toggleWishlistSidebar
+    }
   }
 };
 </script>
