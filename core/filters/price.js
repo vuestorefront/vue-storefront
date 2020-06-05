aaa38b822422ef7aa27c9cdc5412f82ed38ada63
tkostuch <tkostuch@divante.pl>
2020-06-05 06:47:16 +0200

    revert toFixed changes and use .abs on value

--- core/filters/price.js
+++ core/filters/price.js
@@ -27,15 +27,14 @@ export function price (value, storeView) {
   }
   const _storeView = storeView || currentStoreView();
   if (!_storeView.i18n) {
-    return Number(value).toFixed(2)
+    return value
   }
 
   const { defaultLocale, currencySign, currencyDecimal, currencyGroup, fractionDigits, priceFormat } = _storeView.i18n;
 
   const options = { minimumFractionDigits: fractionDigits, maximumFractionDigits: fractionDigits };
 
-  const absPrice = Math.abs(Number(value)).toFixed(2)
-  let localePrice = absPrice.toLocaleString(defaultLocale, options);
+  let localePrice = Math.abs(value).toLocaleString(defaultLocale, options);
 
   if (currencyDecimal !== '' || currencyGroup !== '') {
     localePrice = replaceSeparators(localePrice, { decimal: currencyDecimal, group: currencyGroup }, getLocaleSeparators(defaultLocale));
