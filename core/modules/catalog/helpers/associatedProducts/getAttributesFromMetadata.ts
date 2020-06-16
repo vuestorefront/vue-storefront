41171b4c9d1dee8b5473669dd2651ef6ee54d57e
cewald <c.ewald@impericon.com>
2020-06-16 13:08:12 +0000

    Change name of `setAttributesMetaFromProducts` to `getAttributesFromMetadata`

--- core/modules/catalog/helpers/associatedProducts/getAttributesFromMetadata.ts
+++ core/modules/catalog/helpers/associatedProducts/getAttributesFromMetadata.ts
@@ -4,7 +4,7 @@ import Product from '@vue-storefront/core/modules/catalog/types/Product'
 /**
  * Set associated attributes by meta data of product links
  */
-export default async function setAttributesMetaFromProducts (context: any, products: Product[]) {
+export default async function getAttributesFromMetadata (context: any, products: Product[]) {
   if (config.entities.attribute.loadByAttributeMetadata) {
     context.dispatch('attribute/loadProductAttributes', { products, merge: true }, { root: true })
   }
