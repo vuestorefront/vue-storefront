aecc0369eec400554f397d2bb443d7d265589901
cewald <c.ewald@impericon.com>
2020-06-16 08:37:58 +0000

    Only load products meta-data if `loadByAttributeMetadata` is enabled

--- core/modules/catalog/helpers/associatedProducts/setAttributesMetaFromProducts.ts
+++ core/modules/catalog/helpers/associatedProducts/setAttributesMetaFromProducts.ts
@@ -1,8 +1,11 @@
+import config from 'config'
 import Product from '@vue-storefront/core/modules/catalog/types/Product'
 
 /**
  * Set associated attributes by meta data of product links
  */
 export default async function setAttributesMetaFromProducts (context: any, products: Product[]) {
-  context.dispatch('attribute/loadProductAttributes', { products, merge: true }, { root: true })
+  if (config.entities.attribute.loadByAttributeMetadata) {
+    context.dispatch('attribute/loadProductAttributes', { products, merge: true }, { root: true })
+  }
 }
