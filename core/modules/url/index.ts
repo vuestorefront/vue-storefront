d767fe882cce10bc55ded7bbf18309ea9b08efa6
Anders Ekelund <anders.emil.ekelund@gmail.com>
2020-07-03 14:41:09 +0200

    Init StorageManager in the right place

--- core/modules/url/index.ts
+++ core/modules/url/index.ts
@@ -3,9 +3,8 @@ import { StorefrontModule } from '@vue-storefront/core/lib/modules'
 import { beforeEachGuard } from './router/beforeEach'
 import { StorageManager } from '@vue-storefront/core/lib/storage-manager'
 
-export const cacheStorage = StorageManager.init('url')
-
 export const UrlModule: StorefrontModule = function ({ store, router }) {
+  StorageManager.init('url')
   store.registerModule('url', urlStore)
   router.beforeEach(beforeEachGuard)
 }
