6a4e10079f23dd554c8fc140d21c79cc31de241e
Fifciuu <filip.jdrasik@gmail.com>
2020-07-06 13:22:21 +0200

    Log as external file & getThemePath function

--- packages/core/cli/src/scripts/createProject.ts
+++ packages/core/cli/src/scripts/createProject.ts
@@ -1,35 +1,26 @@
 const compileTemplate = require('@vue-storefront/nuxt-theme/scripts/compileTemplate');
 const getAllFilesFromDir = require('@vue-storefront/nuxt-theme/scripts/getAllFilesFromDir.js');
 const { copyFile } = require('@vue-storefront/nuxt-theme/scripts/copyThemeFiles');
-const consola = require('consola');
-const chalk = require('chalk');
-
+const log = require('@vue-storefront/cli/src/utils/log');
 const fs = require('fs');
 const path = require('path');
 
-const log = {
-  info: (message) => consola.info(chalk.bold('VSF'), message),
-  success: (message) => consola.success(chalk.bold('VSF'), message),
-  warning: (message) => consola.warning(chalk.bold('VSF'), message),
-  error: (message) => consola.error(chalk.bold('VSF'), message)
-};
-
 const buildFileTargetPath = (file: string, targetPath: string, chopPhrase: string): string => targetPath + (file.replace(chopPhrase, ''));
+const getThemePath = (themeName: string): string => `../../node_modules/@vue-storefront/${themeName}`;
 
 const copyThemeFiles = (filesDir: string | Array<string>, targetPath: string, chopPhrase: string) => {
   if (fs.statSync(filesDir).isDirectory()) {
     return Promise.all(getAllFilesFromDir(filesDir as Array<string>).map(
       file => copyFile(file, buildFileTargetPath(file, targetPath, chopPhrase))
     ));
-  } else {
-    return copyFile(filesDir, buildFileTargetPath(filesDir as string, targetPath, chopPhrase));
   }
+  return copyFile(filesDir, buildFileTargetPath(filesDir as string, targetPath, chopPhrase));
 };
 
 const removeDevMagicComment = (source: string): string => source.replace(/\s+(\/\/ @core-development-only-start)(.*?)(\/\/ @core-development-only-end)/sg, '');
 
 async function createProject(integration: string, targetPath: string): Promise<void> {
-  const integrationThemePath = `../../node_modules/@vue-storefront/${integration}-theme`;
+  const integrationThemePath = getThemePath(`${integration}-theme`);
   const absoluteTargetPath = path.join(__dirname, targetPath);
 
   const omitFiles = ['.theme', '.nuxt', 'node_modules'];
@@ -43,7 +34,7 @@ async function createProject(integration: string, targetPath: string): Promise<v
     )
   );
 
-  const agnosticThemePath = '../../node_modules/@vue-storefront/nuxt-theme/theme';
+  const agnosticThemePath = getThemePath('nuxt-theme/theme');
   const agnosticThemeFiles = getAllFilesFromDir(agnosticThemePath).filter(file => !file.includes(path.sep + 'static' + path.sep));
 
   const compileAgnosticTemplate = (filePath: string, targetPath: string, chopPhrase: string) => {
