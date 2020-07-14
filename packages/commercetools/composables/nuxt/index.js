89276ce6cdf528a5295d2487a74808bfc5c5b4b2
Fifciuu <filip.jdrasik@gmail.com>
2020-07-14 13:48:38 +0200

    Long condition -> external function

--- packages/commercetools/composables/nuxt/index.js
+++ packages/commercetools/composables/nuxt/index.js
@@ -1,11 +1,16 @@
 import path from 'path';
 
+const hasDefinedMiddleware = (options) => options.router && options.router.middleware && options.router.middleware.includes('commercetools');
+
 export default function (moduleOptions) {
-  if (!moduleOptions.disableMiddleware && this.options.router && this.options.router.middleware && !this.options.router.middleware.includes('commercetools')) {
+
+  if (!moduleOptions.disableMiddleware && !hasDefinedMiddleware(this.options)) {
     this.options.router.middleware.push('commercetools');
   }
+
   this.addPlugin({
     src: path.resolve(__dirname, './plugin.js'),
     options: moduleOptions
   });
+
 }
