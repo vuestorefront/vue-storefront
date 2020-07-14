739136d25374603cb84119a6a4c81bff0b804e30
Fifciuu <filip.jdrasik@gmail.com>
2020-07-14 13:46:53 +0200

    Util function for build locales

--- packages/commercetools/composables/nuxt/helpers/index.js
+++ packages/commercetools/composables/nuxt/helpers/index.js
@@ -1,10 +1,27 @@
 import defaultConfig from '@vue-storefront/commercetools/nuxt/defaultConfig';
 
+const getLocaleSettings = (moduleOptions, app) => {
+  if (!moduleOptions.cookies) {
+    return {
+      locale: moduleOptions.locale || defaultConfig.locale,
+      country: moduleOptions.currency || defaultConfig.currenc,
+      currency: moduleOptions.country || defaultConfig.country
+    };
+  }
+
+  return {
+    locale: app.$cookies.get(moduleOptions.cookies.localeCookieName),
+    country: app.$cookies.get(moduleOptions.cookies.currencyCookieName),
+    currency: app.$cookies.get(moduleOptions.cookies.countryCookieName)
+  };
+};
+
 export const mapConfigToSetupObject = ({ moduleOptions, app, additionalProperties = {} }) => {
   return {
     ...defaultConfig,
     ...moduleOptions,
     ...additionalProperties,
+    ...getLocaleSettings(moduleOptions, app),
     api: {
       uri: moduleOptions.api.uri,
       authHost: moduleOptions.api.authHost,
