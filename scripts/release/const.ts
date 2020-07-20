45690b72a31b5fa1dcff604ec757b5ca21fefa0d
Fifciuu <filip.jdrasik@gmail.com>
2020-07-20 10:07:59 +0200

    NPM Registry

--- scripts/release/const.ts
+++ scripts/release/const.ts
@@ -1,4 +1,4 @@
 export const base = '../../packages/';
-// export const registry = 'https://registry.npmjs.org';
-export const registry = 'http://localhost:4873';
-// npm publish --registry http://localhost:4873
+export const registry = 'https://registry.npmjs.org';
+// export const registry = 'http://localhost:4873';
+// npm publish --registry ${registry}
