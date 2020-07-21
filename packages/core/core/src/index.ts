89e530fc35b61b6d71f2a4cbc9ff1c8cd350069a
Filip Rakowski <frakowski@divante.pl>
2020-07-21 14:23:30 +0200

    shameless push to the next master

--- packages/core/core/src/index.ts
+++ packages/core/core/src/index.ts
@@ -3,3 +3,19 @@
 export * from './utils';
 export * from './factories';
 export * from './types';
+
+declare global {
+  interface Window { $VSF: any }
+}
+
+if (typeof window !== 'undefined') {
+  window.$VSF = window.$VSF || { integrations: [] };
+}
+
+export function track(id: string): void {
+  if (typeof window !== 'undefined') {
+    if (window.$VSF) {
+      window.$VSF.integrations.push(id);
+    }
+  }
+}
