be1c423a2441ce007c313a20801f0a51a76b4af8
Fifciuu <filip.jdrasik@gmail.com>
2020-07-23 17:55:11 +0200

    Simple wishlist mock

--- packages/boilerplate/composables/src/composables/getters/wishlistGetters.ts
+++ packages/boilerplate/composables/src/composables/getters/wishlistGetters.ts
@@ -6,37 +6,66 @@ import {
 import { Wishlist, WishlistProduct } from '../../types';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getWishlistItems = (wishlist: Wishlist): WishlistProduct[] => [];
+export const getWishlistItems = (wishlist: Wishlist): WishlistProduct[] => [
+  {
+    _id: 1,
+    _description: 'Some description',
+    _categoriesRef: [
+      '1',
+      '2'
+    ],
+    name: 'Black jacket',
+    sku: 'black-jacket',
+    images: [
+      'https://s3-eu-west-1.amazonaws.com/commercetools-maximilian/products/081223_1_large.jpg'
+    ],
+    price: {
+      original: 12.34,
+      current: 10.00
+    },
+    qty: 1
+  }
+];
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getWishlistItemName = (product: WishlistProduct): string => '';
+export const getWishlistItemName = (product: any): string => product?.name || 'Product\'s name';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getWishlistItemImage = (product: WishlistProduct): string => '';
+export const getWishlistItemImage = (product: any): string => product?.images[0] || 'https://s3-eu-west-1.amazonaws.com/commercetools-maximilian/products/081223_1_large.jpg';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getWishlistItemPrice = (product: WishlistProduct): AgnosticPrice => ({ regular: 0, special: 0 });
+export const getWishlistItemPrice = (product: any): AgnosticPrice => {
+  return {
+    regular: product?.price?.original || 12,
+    special: product?.price?.current || 10
+  };
+};
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getWishlistItemQty = (product: WishlistProduct): number => 1;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getWishlistItemAttributes = (product: WishlistProduct, filterByAttributeName?: string[]) => ({'': ''});
+export const getWishlistItemAttributes = (product: WishlistProduct, filterByAttributeName?: string[]) => ({ color: 'red' });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getWishlistItemSku = (product: WishlistProduct): string => '';
+export const getWishlistItemSku = (product: any): string => product?.sku || 'some-sku';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getWishlistTotals = (wishlist: Wishlist): AgnosticTotals => ({ total: 0, subtotal: 0 });
+export const getWishlistTotals = (wishlist: Wishlist): AgnosticTotals => {
+  return {
+    total: 10,
+    subtotal: 10
+  };
+};
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getWishlistShippingPrice = (wishlist: Wishlist): number => 0;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getWishlistTotalItems = (wishlist: Wishlist): number => 0;
+export const getWishlistTotalItems = (wishlist: Wishlist): number => 1;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getFormattedPrice = (price: number): string => '';
+export const getFormattedPrice = (price: number): string => String(price);
 
 const wishlistGetters: WishlistGetters<Wishlist, WishlistProduct> = {
   getTotals: getWishlistTotals,
