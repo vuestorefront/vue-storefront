02ff7dfee5c4fe13a05f9bb74063d4ea53b2d4fa
Fifciuu <filip.jdrasik@gmail.com>
2020-07-23 13:55:40 +0200

    Mocked categories

--- packages/boilerplate/api-client/src/api/getCategory/index.ts
+++ packages/boilerplate/api-client/src/api/getCategory/index.ts
@@ -1,6 +1,59 @@
 import {Category} from '../../types';
-import { settings } from '../../index';
 
-export default settings.overrides.getCategory || async function getCategory(): Promise<Category[]> {
-  return new Promise<Category[]>(() => {});
-};
+export default async function getCategory(): Promise<Category[]> {
+  return Promise.resolve([
+    {
+      id: 1,
+      name: 'Women',
+      slug: 'women',
+      items: [
+        {
+          id: 4,
+          name: 'Women jackets',
+          slug: 'women-jackets'
+        },
+        {
+          id: 5,
+          name: 'Skirts',
+          slug: 'skirts'
+        }
+      ]
+    },
+    {
+      id: 2,
+      name: 'Men',
+      slug: 'men',
+      items: [
+        {
+          id: 6,
+          name: 'Men T-shirts',
+          slug: 'men-tshirts'
+        }
+      ]
+    },
+    {
+      id: 3,
+      name: 'Kids',
+      slug: 'kids',
+      items: [
+        {
+          id: 7,
+          name: 'Toys',
+          slug: 'toys',
+          items: [
+            {
+              id: 8,
+              name: 'Toy Cars',
+              slug: 'toy-cars'
+            },
+            {
+              id: 8,
+              name: 'Dolls',
+              slug: 'dolls'
+            }
+          ]
+        }
+      ]
+    }
+  ]);
+}
