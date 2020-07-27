a84b3931c51e4d39ccc00de250b8c2f3d63c25a7
Filip Rakowski <frakowski@divante.pl>
2020-07-27 08:36:47 +0200

    spryker

---
+++ packages/spryker/composables/src/types/index.ts
@@ -0,0 +1,33 @@
+import { UseCategory, UseProduct } from '@vue-storefront/core';
+
+type Product = {}
+type Category = {}
+type User = {}
+type UserAddress = {}
+type Cart = {}
+type CartItem = {}
+type Coupon = {}
+type Order = {}
+type OrderItem = {}
+type OrderSearchParams = {}
+type ShippingMethod = {}
+type WishlistProduct = {}
+type Wishlist = {}
+
+export {
+  Cart,
+  CartItem,
+  Category,
+  Coupon,
+  Order,
+  OrderItem,
+  OrderSearchParams,
+  Product,
+  ShippingMethod,
+  User,
+  UserAddress,
+  Wishlist,
+  WishlistProduct,
+  UseCategory,
+  UseProduct
+};
