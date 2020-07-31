24a8ae0399e694411b669f4aa3005d10ca302a7a
Fifciu <filip.jdrasik@gmail.com>
2020-07-31 12:55:18 +0200

    Exported useCko

--- packages/checkout-com/src/index.ts
+++ packages/checkout-com/src/index.ts
@@ -1,5 +1,6 @@
 import { setup, getPublicKey, getCkoWebhookUrl, getStyles } from './configuration';
 import useCkoCard from './useCkoCard';
+import useCko from './useCko';
 
-export { setup, getPublicKey, getCkoWebhookUrl, getStyles, useCkoCard };
+export { setup, getPublicKey, getCkoWebhookUrl, getStyles, useCkoCard, useCko };
 
