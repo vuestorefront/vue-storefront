181dd43788cfbcab6941a9df5f6900ab8f9e6046
Fifciu <filip.jdrasik@gmail.com>
2020-08-03 17:19:27 +0200

    Exported cko payment type

--- packages/checkout-com/src/index.ts
+++ packages/checkout-com/src/index.ts
@@ -1,6 +1,7 @@
 import { setup, getPublicKey, getCkoWebhookUrl, getFramesStyles } from './configuration';
 import useCkoCard from './useCkoCard';
 import useCko from './useCko';
+import { CKO_PAYMENT_TYPE } from './helpers';
 
-export { setup, getPublicKey, getCkoWebhookUrl, getFramesStyles, useCkoCard, useCko };
+export { setup, getPublicKey, getCkoWebhookUrl, getFramesStyles, useCkoCard, useCko, CKO_PAYMENT_TYPE };
 
