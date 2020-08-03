f8b968e35aeea4d5de7dc67811eeab9a8bcef4b2
vitaly.tartynov <Vitaly.Tartynov@GMail.com>
2020-08-03 14:01:48 +0200

    integrate(virtocommerce): added virtocommerce template.

---
+++ packages/virtocommerce/composables/jest.config.js
@@ -0,0 +1,9 @@
+const baseConfig = require('./../../jest.base.config');
+
+module.exports = {
+  ...baseConfig,
+  coverageReporters: ['html', 'lcov', 'text'],
+  rootDir: __dirname,
+  setupFilesAfterEnv: ['./__tests__/setup.ts'],
+  watchPathIgnorePatterns: ['/node_modules/']
+};
---
+++ packages/virtocommerce/composables/rollup.config.js
@@ -0,0 +1,4 @@
+import pkg from './package.json';
+import { generateBaseConfig } from '../../rollup.base.config';
+
+export default generateBaseConfig(pkg);
