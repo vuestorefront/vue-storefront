56fa9801002102f3a6102b98b12d25bbfe1df1a0
Fifciu <filip.jdrasik@gmail.com>
2020-08-07 13:36:45 +0200

    It generates project

--- packages/core/cli/src/commands/init.ts
+++ packages/core/cli/src/commands/init.ts
@@ -1,10 +1,27 @@
 import log from '../utils/log';
+import getIntegrationsFromPackage from '../utils/getIntegrationsFromPackage';
+import inquirer from 'inquirer';
+import createProject from '../scripts/createProject';
+import path from 'path';
 
-const program = () => {
+const program = async () => {
   const [projectName] = process.argv.slice(2);
   if (!projectName) {
     log.error('Provide projectName: yarn cli:init <projectName>');
   }
+
+  const integrations = getIntegrationsFromPackage();
+  const { chosenIntegration } = await inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'chosenIntegration',
+        message: 'Choose integration',
+        choices: integrations
+      }
+    ]);
+
+  return createProject(chosenIntegration, path.resolve(__dirname, 'projectName'));
 };
 
 program();
