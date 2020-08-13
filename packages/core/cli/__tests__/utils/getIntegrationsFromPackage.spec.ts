82d9259c54f8c66baee29aeb0d3240cc7a999e49
Fifciu <filip.jdrasik@gmail.com>
2020-08-13 15:18:23 +0200

    Approach with hardcoded available integrations list

--- packages/core/cli/__tests__/utils/getIntegrationsFromPackage.spec.ts
+++ packages/core/cli/__tests__/utils/getIntegrationsFromPackage.spec.ts
@@ -1,24 +1,10 @@
 import getIntegrationsFromPackage from '../../src/utils/getIntegrationsFromPackage';
 
-jest.mock('fs', () => ({
-  readFileSync: () => `{
-        "dependencies": {
-            "@vue-storefront/about-you-theme": "^0.0.1",
-            "@vue-storefront/boilerplate-theme": "^0.0.3",
-            "@vue-storefront/commercetools-theme": "^0.0.3",
-            "@vue-storefront/nuxt-theme": "^0.0.1",
-            "inquirer": "^7.3.3"
-        }
-    }`
-}));
-
 const expectedIntegrations = ['about-you', 'commercetools'];
 
 describe('[vsf-next-cli] getIntegrationsFromPackage', () => {
   it('gets dependencies from package.json and transform them to list of integrations with filtering', () => {
-
     const integrations = getIntegrationsFromPackage();
     expect(integrations.sort()).toEqual(expectedIntegrations.sort());
-
   });
 });
--- packages/core/cli/src/utils/getIntegrationsFromPackage.ts
+++ packages/core/cli/src/utils/getIntegrationsFromPackage.ts
@@ -1,23 +1,4 @@
-import path from 'path';
-import fs from 'fs';
-
-const notAnIntegration = [
-  'boilerplate',
-  'nuxt'
+export default (): string[] => [
+  'about-you',
+  'commercetools'
 ];
-
-const getIntegrationNameFromPackageName = (packageName: string): string => {
-  const resolved = /@vue-storefront\/(.*?)-theme/.exec(packageName);
-  return resolved && resolved[1];
-};
-
-export default (): string[] => {
-
-  const { dependencies } = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../package.json'), 'utf-8'));
-  const integrations = Object.keys(dependencies)
-    .map(getIntegrationNameFromPackageName)
-    .filter(integration => integration)
-    .filter(integration => !notAnIntegration.includes(integration));
-
-  return integrations;
-};
