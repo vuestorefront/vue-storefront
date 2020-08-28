e30390d9171bcb9e57d886ee62f11b428c54c91e
Fifciuu <filip.jdrasik@gmail.com>
2020-08-28 14:12:03 +0200

    prepublish to generateTemplates

--- packages/core/cli/src/scripts/generateTemplates/index.ts
+++ packages/core/cli/src/scripts/generateTemplates/index.ts
@@ -2,10 +2,10 @@ import getIntegrations from '@vue-storefront/cli/src/utils/getIntegrations';
 import createProject from '@vue-storefront/cli/src/scripts/createProject';
 import path from 'path';
 
-const prepublishScript = async () => {
+const generateTemplatesScript = async () => {
   for (const integration of getIntegrations()) {
     await createProject(integration, path.resolve('./templates', integration));
   }
 };
 
-prepublishScript();
+generateTemplatesScript();
