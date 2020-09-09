1427605c04e915f6471fa6c1c443f1c5a3e8ee53
Filip Sobol <sobol.filip@gmail.com>
2020-09-09 11:19:28 +0200

    Remove unnecessary whitespaces

--- packages/commercetools/api-client/src/api/addReview/index.ts
+++ packages/commercetools/api-client/src/api/addReview/index.ts
@@ -7,8 +7,7 @@ const addReview = () => {
         createdAt: (new Date()).toDateString(),
         lastModifiedAt: (new Date()).toDateString(),
         authorName: 'Jane D.Smith',
-        text:
-            'I was looking for a bright light for the kitchen but wanted some item more modern than a strip light. this one is perfect, very bright and looks great. I can\'t comment on interlation as I had an electrition instal it. Would recommend',
+        text: 'I was looking for a bright light for the kitchen but wanted some item more modern than a strip light. this one is perfect, very bright and looks great. I can\'t comment on interlation as I had an electrition instal it. Would recommend',
         rating: 4
       },
       {
@@ -17,8 +16,7 @@ const addReview = () => {
         createdAt: (new Date()).toDateString(),
         lastModifiedAt: (new Date()).toDateString(),
         authorName: 'Mari',
-        text:
-            'Excellent light output from this led fitting. Relatively easy to fix to the ceiling,but having two people makes it easier, to complete the installation. Unable to comment on reliability at this time, but I am hopeful of years of use with good light levels. Excellent light output from this led fitting. Relatively easy to fix to the ceiling,',
+        text: 'Excellent light output from this led fitting. Relatively easy to fix to the ceiling,but having two people makes it easier, to complete the installation. Unable to comment on reliability at this time, but I am hopeful of years of use with good light levels. Excellent light output from this led fitting. Relatively easy to fix to the ceiling,',
         rating: 5
       }
     ],
