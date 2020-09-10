b5ae708d0febf8d3213511fdb157b6f3f4b66391
Filip Sobol <sobol.filip@gmail.com>
2020-09-10 15:48:29 +0200

    Simplify mocked review getters in Boilerplate

--- packages/boilerplate/composables/src/composables/getters/reviewGetters.ts
+++ packages/boilerplate/composables/src/composables/getters/reviewGetters.ts
@@ -3,25 +3,35 @@ import { ReviewGetters, AgnosticRateCount } from '@vue-storefront/core';
 type Review = any;
 type ReviewItem = any;
 
-export const getItems = (review: Review): ReviewItem[] => review?.results || [];
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const getItems = (review: Review): ReviewItem[] => [];
 
-export const getReviewId = (item: ReviewItem): string => item?.id || '';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const getReviewId = (item: ReviewItem): string => '';
 
-export const getReviewAuthor = (item: ReviewItem): string => item?.authorName || '';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const getReviewAuthor = (item: ReviewItem): string => '';
 
-export const getReviewMessage = (item: ReviewItem): string => item?.text || '';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const getReviewMessage = (item: ReviewItem): string => '';
 
-export const getReviewRating = (item: ReviewItem): number => item?.rating || 0;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const getReviewRating = (item: ReviewItem): number => 0;
 
-export const getReviewDate = (item: ReviewItem): string => item?.createdAt || '';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const getReviewDate = (item: ReviewItem): string => '';
 
-export const getTotalReviews = (review: Review): number => review?.total || 0;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const getTotalReviews = (review: Review): number => 0;
 
-export const getAverageRating = (review: Review): number => review?.averageRating || 0;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const getAverageRating = (review: Review): number => 0;
 
-export const getRatesCount = (review: Review): AgnosticRateCount[] => review?.ratingsDistribution || [];
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const getRatesCount = (review: Review): AgnosticRateCount[] => [];
 
-export const getReviewsPage = (review: Review): number => review ? (review.offset / review.limit) + 1 : 1;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const getReviewsPage = (review: Review): number => 1;
 
 const reviewGetters: ReviewGetters<Review, ReviewItem> = {
   getItems,
