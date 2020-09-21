24c1fb16d6a4006daba1229f0a7ec8dc2fca7402
lukasborawski <lukas.borawski@gmail.com>
2020-09-21 16:20:12 +0200

    fix: wrong param value for getMe method

--- packages/commercetools/composables/src/useCart/currentCart.ts
+++ packages/commercetools/composables/src/useCart/currentCart.ts
@@ -2,7 +2,7 @@ import { createCart, getMe } from '@vue-storefront/commercetools-api';
 
 const loadCurrentCart = async (customQueryFn = (user = null, cart = null) => ({ cart, user })) => {
   const { user, cart } = customQueryFn();
-  const { data: profileData } = await getMe({ customer: true }, user);
+  const { data: profileData } = await getMe({ customer: false }, user);
   if (profileData.me.activeCart) return profileData.me.activeCart;
   const { data } = await createCart({}, cart);
   return data.cart;
