6144e5e97c4b398eb36eea5188d63e2f0b11f7c8
lukasborawski <lukas.borawski@gmail.com>
2020-09-24 17:41:45 +0200

    chore: typo

---
+++ packages/core/core/__tests__/factories/useContentFactory.spec.ts
@@ -0,0 +1,8 @@
+/* import { useContentFactory } from '../../src/factories';
+import { UseContent } from "../../src/types";
+
+const contentResponse = {
+  result: [
+    { id: 1 }
+  ]
+}; */
--- packages/core/nuxt-theme-module/theme/pages/Category.vue
+++ packages/core/nuxt-theme-module/theme/pages/Category.vue
@@ -4,7 +4,6 @@
       class="breadcrumbs desktop-only"
       :breadcrumbs="breadcrumbs"
     />
-
     <div class="navbar section">
       <div class="navbar__aside desktop-only">
         <SfHeading :level="3" :title="$t('Categories')" class="navbar__title" />
@@ -255,11 +254,18 @@ import { useUiHelpers } from '~/composables';
 import uiState from '~/assets/ui-state';
 import { onSSR } from '@vue-storefront/core';
 import Filters from '../components/Filters';
+import { useContent } from '@vue-storefront/commercetools';
 
 export default {
   transition: 'fade',
   setup(props, context) {
-    onMounted(() => context.root.$scrollTo(context.root.$el, 2000));
+    onMounted(() => {
+      const { search } = useContent();
+      console.log(search({
+        contentId: 'bb1ce5fc-0dfd-49dc-80fe-bdac41bb7d6a'
+      }));
+      return context.root.$scrollTo(context.root.$el, 2000);
+    });
     const th = useUiHelpers();
     const { loadCart, addToCart, isOnCart } = useCart();
     const { addToWishlist } = useWishlist();
