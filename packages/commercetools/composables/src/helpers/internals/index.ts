adcf15db9b500e8b7d47a18bb9b322900eb8335c
Fifciuu <filip.jdrasik@gmail.com>
2020-09-28 18:22:00 +0200

    chore(coupon): fix

--- packages/commercetools/composables/src/helpers/internals/index.ts
+++ packages/commercetools/composables/src/helpers/internals/index.ts
@@ -1,9 +1,11 @@
 import enhanceProduct from './enhanceProduct';
 import mapPaginationParams from './mapPaginationParams';
 import getFiltersFromProductsAttributes from './getFiltersFromProductsAttributes';
+import getCouponsFromCart from './getCouponsFromCart';
 
 export {
   enhanceProduct,
   mapPaginationParams,
-  getFiltersFromProductsAttributes
+  getFiltersFromProductsAttributes,
+  getCouponsFromCart
 };
