a5bace07123b4edd5039906d854a140b14eaeb12
Filip Sobol <sobol.filip@gmail.com>
2020-10-05 15:33:04 +0200

    Block guest access to user profile

--- packages/core/nuxt-theme-module/theme/pages/MyAccount.vue
+++ packages/core/nuxt-theme-module/theme/pages/MyAccount.vue
@@ -54,7 +54,6 @@ import MyNewsletter from './MyAccount/MyNewsletter';
 import OrderHistory from './MyAccount/OrderHistory';
 import MyReviews from './MyAccount/MyReviews';
 
-// TODO: protect this route: https://github.com/DivanteLtd/next/issues/379
 export default {
   name: 'MyAccount',
   components: {
@@ -68,6 +67,17 @@ export default {
     OrderHistory,
     MyReviews
   },
+
+  async middleware({ redirect }) {
+    const { load, isAuthenticated } = useUser();
+
+    await load();
+
+    if (!isAuthenticated.value) {
+      return redirect('/');
+    }
+  },
+
   setup(props, context) {
     const { $router, $route } = context.root;
     const { logout } = useUser();
