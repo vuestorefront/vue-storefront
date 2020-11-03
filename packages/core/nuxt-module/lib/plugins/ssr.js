3d0584e34aeaf726f90f482045c75157a3f0a55f
andrzejewsky <vox3r69@gmail.com>
2020-11-03 12:15:49 +0100

    changelog

--- packages/core/nuxt-module/lib/plugins/ssr.js
+++ packages/core/nuxt-module/lib/plugins/ssr.js
@@ -2,7 +2,7 @@
 import { configureSSR } from '@vue-storefront/core'
 import { ssrRef, getCurrentInstance, onServerPrefetch } from '@nuxtjs/composition-api';
 
-const canRenderOnCSR = (vm) => {
+const hasRouteChanged = (vm) => {
   const { from } = vm.$router.app.context;
   const { current } = vm.$router.history
 
@@ -21,7 +21,7 @@ const ssrPlugin = () => {
       if (typeof window !== 'undefined') {
         const vm = getCurrentInstance();
 
-        if (canRenderOnCSR(vm)) {
+        if (hasRouteChanged(vm)) {
           fn();
         }
       }
