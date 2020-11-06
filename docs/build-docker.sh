5558185e58d42f3817ac97f121aafe4912548ae2
Mateusz Koszutowski <mkoszut@gmail.com>
2020-11-06 08:13:05 +0100

    Fixing build script

--- docs/build-docker.sh
+++ docs/build-docker.sh
@@ -1,1 +1,3 @@
-docker build -t registry.storefrontcloud.io/docs/v1:test -f Dockerfile .
+TAG=`git rev-parse HEAD`
+docker build -t registry.storefrontcloud.io/docs-storefrontcloud-io/v1:${TAG:0:8} -f Dockerfile .
+docker push registry.storefrontcloud.io/docs-storefrontcloud-io/v1:${TAG:0:8}
