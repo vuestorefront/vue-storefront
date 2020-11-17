fc0d7920d334e68ca4995bb77f671e3aaee113fa
Fifciuu <filip.jdrasik@gmail.com>
2020-11-17 13:47:05 +0100

    feat: http2 server push support with doc update

---
+++ src/modules/http2-server-push/server.ts
@@ -0,0 +1,24 @@
+import { serverHooks } from '@vue-storefront/core/server/hooks'
+import config from 'config'
+
+if (config.server.http2ServerPush) {
+  serverHooks.beforeOutputRenderedResponse(({
+    res,
+    context,
+    isProd
+  }) => {
+    if (isProd) {
+      console.log('HTTP2 Server Push is enabled')
+      const serverPushItems = [];
+      for (let { file, asType, extension } of (context as any).getPreloadFiles()) {
+        if (extension !== 'js') {
+          continue;
+        }
+        serverPushItems.push(`</dist/${file}>;rel=preload;as=${asType}`)
+      }
+      if (serverPushItems.length) {
+        res.setHeader('Link', serverPushItems)
+      }
+    }
+  })
+}
