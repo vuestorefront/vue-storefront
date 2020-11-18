3e790c61f9b3a81a994c6047ffa9ff87ba39e92b
Fifciuu <filip.jdrasik@gmail.com>
2020-11-18 12:55:38 +0100

    refactor: chunk names for services

--- core/data-resolver/index.ts
+++ core/data-resolver/index.ts
@@ -1,7 +1,7 @@
-export const CategoryService = async () => (await import('./CategoryService')).CategoryService;
-export const UserService = async () => (await import('./UserService')).UserService;
-export const CartService = async () => (await import('./CartService')).CartService;
-export const OrderService = async () => (await import('./OrderService')).OrderService;
-export const StockService = async () => (await import('./StockService')).StockService;
-export const ReviewsService = async () => (await import('./ReviewsService')).ReviewsService;
-export const NewsletterService = async () => (await import('./NewsletterService')).NewsletterService;
+export const CategoryService = async () => (await import(/* webpackChunkName: "vsf-service-category" */ './CategoryService')).CategoryService;
+export const UserService = async () => (await import(/* webpackChunkName: "vsf-service-user" */ './UserService')).UserService;
+export const CartService = async () => (await import(/* webpackChunkName: "vsf-service-cart" */ './CartService')).CartService;
+export const OrderService = async () => (await import(/* webpackChunkName: "vsf-service-order" */ './OrderService')).OrderService;
+export const StockService = async () => (await import(/* webpackChunkName: "vsf-service-stock" */ './StockService')).StockService;
+export const ReviewsService = async () => (await import(/* webpackChunkName: "vsf-service-reviews" */ './ReviewsService')).ReviewsService;
+export const NewsletterService = async () => (await import(/* webpackChunkName: "vsf-service-newsletter" */ './NewsletterService')).NewsletterService;
