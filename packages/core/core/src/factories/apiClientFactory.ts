0db6efa461d6e7e27e9fa1d70645e5108c1585cb
andrzejewsky <vox3r69@gmail.com>
2020-11-20 10:49:48 +0100

    update api client

--- packages/core/core/src/factories/apiClientFactory.ts
+++ packages/core/core/src/factories/apiClientFactory.ts
@@ -11,13 +11,15 @@ type ApiClientInstance<T, F = any> = { settings: T } & F;
 
 export function apiClientFactory<ALL_SETTINGS, ALL_FUNCTIONS extends Interface>(factoryParams: FactoryParams<ALL_SETTINGS, ALL_FUNCTIONS>) {
   return {
-    createApiClient (config: ALL_SETTINGS): ApiClientInstance<ALL_SETTINGS, ALL_FUNCTIONS> {
+    createApiClient (config: ALL_SETTINGS, customApi: any = {}): ApiClientInstance<ALL_SETTINGS, ALL_FUNCTIONS> {
       const settings = factoryParams.onSetup ? merge(config, factoryParams.onSetup(config)) as ALL_SETTINGS : config;
 
       Logger.debug('apiClientFactory.setup', settings);
 
-      const api = Object.entries(factoryParams.api)
-        .reduce((prev, [key, fn]) => ({
+      const apis = { ...factoryParams.api, ...customApi };
+
+      const api = Object.entries(apis)
+        .reduce((prev, [key, fn]: any) => ({
           ...prev,
           [key]: (...args) => fn(settings, ...args)
         }), {}) as ALL_FUNCTIONS;
