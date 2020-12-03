188308c2adcc46ebc30db9b2f728c9bdefe198d6
Filip Sobol <sobol.filip@gmail.com>
2020-12-03 15:18:05 +0100

    Update

--- packages/boilerplate/composables/nuxt/plugin.js
+++ packages/boilerplate/composables/nuxt/plugin.js
@@ -1,4 +1,11 @@
 import { integrationPlugin } from '@vue-storefront/boilerplate';
 
+const moduleOptions = JSON.parse('<%= JSON.stringify(options) %>');
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-unused-vars
-export default integrationPlugin(({ app, integration }) => {});
+export default integrationPlugin(({ app, integration }) => {
+  integration.configure({
+    ...moduleOptions
+    // other options
+  });
+});
