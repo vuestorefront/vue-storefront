7195328bea7145084d5a64d73fe89b5f31716a41
Fifciuu <filip.jdrasik@gmail.com>
2020-12-08 11:51:21 +0100

    fix: useUserOrders & tests

--- packages/commercetools/composables/src/useUserOrders/index.ts
+++ packages/commercetools/composables/src/useUserOrders/index.ts
@@ -1,10 +1,10 @@
-import { CustomQuery, OrdersSearchResult, useUserOrdersFactory, UseUserOrdersFactoryParams, Context } from '@vue-storefront/core';
+import { OrdersSearchResult, useUserOrdersFactory, UseUserOrdersFactoryParams, Context } from '@vue-storefront/core';
 import { Order } from '../types/GraphQL';
 import { OrderSearchParams } from '../types';
 
 const params: UseUserOrdersFactoryParams<Order, OrderSearchParams> = {
-  searchOrders: async (context: Context, params: OrderSearchParams = {}, customQuery?: CustomQuery): Promise<OrdersSearchResult<Order>> => {
-    const result = await context.$ct.api.getOrders(params, customQuery);
+  searchOrders: async (context: Context, { searchParams, customQuery } = { searchParams: {} }): Promise<OrdersSearchResult<Order>> => {
+    const result = await context.$ct.api.getOrders(searchParams, customQuery);
     const { results: data, total } = result.data?.me.orders || { results: [], total: 0 };
     return { data, total };
   }
