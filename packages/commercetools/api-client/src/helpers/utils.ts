a6c523e8feb81cd5c093ba4249d5a33f4d2c250f
andrzejewsky <vox3r69@gmail.com>
2021-01-06 13:47:38 +0100

    test proxy

--- packages/commercetools/api-client/src/helpers/utils.ts
+++ packages/commercetools/api-client/src/helpers/utils.ts
@@ -1,3 +1,3 @@
-export const isAnonymousSession = (token) => token && token.scope.includes('anonymous_id');
-export const isUserSession = (token) => token && token.scope.includes('customer_id');
+export const isAnonymousSession = (token) => token && token.scope && token.scope.includes('anonymous_id');
+export const isUserSession = (token) => token && token.scope && token.scope.includes('customer_id');
 export const getAccessToken = (token) => token ? token.access_token : null;
