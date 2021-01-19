1f387b6f292e7af9e8ad11ddaaa0ab3fa4e02c69
Fifciuu <fjedrasik@vuestorefront.io>
2021-01-19 16:54:58 +0100

    feat(ct): saving shipping method

--- packages/commercetools/composables/src/useCheckoutShippingMethod/index.ts
+++ packages/commercetools/composables/src/useCheckoutShippingMethod/index.ts
@@ -1,14 +1,31 @@
 import { useCheckoutShippingFactory, UseCheckoutShippingParams, Context } from '@vue-storefront/core';
+import useCart from '../useCart';
+import { cartActions } from '@vue-storefront/commercetools-api';
 
 const params: UseCheckoutShippingParams<any, any> = {
+  provide() {
+    return {
+      cart: useCart()
+    };
+  },
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   load: async (context: Context) => {
-    return Promise.resolve({});
+    const shippingMethodsResponse = await context.$ct.api.getShippingMethods(context.cart.cart.value.id);
+    return shippingMethodsResponse.shippingMethods;
   },
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   save: async (context: Context, params) => {
-    return Promise.resolve({});
+    const cartResponse = await context.$ct.api.updateCart({
+      id: context.cart.cart.value.id,
+      version: context.cart.cart.value.version,
+      actions: [
+        cartActions.setShippingMethodAction(params.method.id)
+      ]
+    });
+
+    context.cart.setCart(cartResponse.data.cart);
+    return cartResponse;
   }
 };
 
