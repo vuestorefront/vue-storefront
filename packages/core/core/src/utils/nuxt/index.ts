37cff85f05092dba55e8339241a50f48e3b40264
andrzejewsky <vox3r69@gmail.com>
2021-01-28 12:19:10 +0100

    cr

--- packages/core/core/src/utils/nuxt/index.ts
+++ packages/core/core/src/utils/nuxt/index.ts
@@ -1,10 +1,12 @@
 import { createExtendIntegrationInCtx, createAddIntegrationToCtx } from './context';
 import { getIntegrationConfig, createProxiedApi } from './_proxyUtils';
+import { Context as NuxtContext, Plugin as NuxtPlugin } from '@nuxt/types';
 import axios from 'axios';
 
-export type IntegrationPlugin = (pluginFn) => (nuxtCtx, inject) => void
+type InjectFn = (key: string, value: any) => void;
+export type IntegrationPlugin = (pluginFn: NuxtPlugin) => NuxtPlugin
 
-export const integrationPlugin = (pluginFn) => (nuxtCtx, inject) => {
+export const integrationPlugin = (pluginFn: NuxtPlugin) => (nuxtCtx: NuxtContext, inject: InjectFn) => {
   const configure = (tag, configuration) => {
     const injectInContext = createAddIntegrationToCtx({ tag, nuxtCtx, inject });
     const config = getIntegrationConfig(nuxtCtx, configuration);
@@ -20,5 +22,5 @@ export const integrationPlugin = (pluginFn) => (nuxtCtx, inject) => {
 
   const integration = { configure, extend };
 
-  pluginFn({ ...nuxtCtx, integration }, inject);
+  pluginFn({ ...nuxtCtx, integration } as NuxtContext, inject);
 };
