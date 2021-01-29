99b1c54370730f3ecf1546295c9796af77988c71
Filip Sobol <sobol.filip@gmail.com>
2021-01-29 09:40:52 +0100

    Use cookies to determine if user is logged in

--- packages/commercetools/theme/middleware/authenticated.js
+++ packages/commercetools/theme/middleware/authenticated.js
@@ -1,5 +1,5 @@
-export default async ({ $vsf, redirect }) => {
-  if (await $vsf.$ct.api.isGuest()) {
+export default async ({ app, redirect }) => {
+  if (!app.$cookies.get('vsf-commercetools-token')?.scope?.includes('customer_id')) {
     return redirect('/');
   }
 };
