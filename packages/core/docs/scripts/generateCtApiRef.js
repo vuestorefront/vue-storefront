5f1299f4aeb8d7c2fe3c1d6907f401ec42552226
Łukasz <ljedrasik@vuestorefront.io>
2021-02-02 15:51:49 +0100

    chore: created empty custom template

---
+++ packages/core/docs/scripts/generateCtApiRef.js
@@ -0,0 +1,20 @@
+const TypeDoc = require('typedoc');
+
+const app = new TypeDoc.Application();
+
+// If you want TypeDoc to load tsconfig.json / typedoc.json files
+app.options.addReader(new TypeDoc.TSConfigReader());
+app.options.addReader(new TypeDoc.TypeDocReader());
+
+app.bootstrap({
+  tsconfig: '../../commercetools/api-client/tsconfig.json',
+  entryPoints: ['../../commercetools/api-client/src/api'],
+  name: 'API Client Reference'
+});
+
+const project = app.convert();
+
+if (project) {
+  const outputDir = './commercetools/api-client-reference';
+  app.generateDocs(project, outputDir);
+}
---
+++ packages/core/docs/scripts/generateCtComposablesRef.js
@@ -0,0 +1,32 @@
+const TypeDoc = require('typedoc');
+
+const app = new TypeDoc.Application();
+
+// If you want TypeDoc to load tsconfig.json / typedoc.json files
+app.options.addReader(new TypeDoc.TSConfigReader());
+app.options.addReader(new TypeDoc.TypeDocReader());
+
+app.bootstrap({
+  tsconfig: '../../commercetools/composables/tsconfig.json',
+  entryPoints: [
+    '../../commercetools/composables/src/useCart',
+    '../../commercetools/composables/src/useCategory',
+    '../../commercetools/composables/src/useCheckout',
+    '../../commercetools/composables/src/useFacet',
+    '../../commercetools/composables/src/useProduct',
+    '../../commercetools/composables/src/useReview',
+    '../../commercetools/composables/src/useUser',
+    '../../commercetools/composables/src/useUserBilling',
+    '../../commercetools/composables/src/useUserOrders',
+    '../../commercetools/composables/src/useUserShipping',
+    '../../commercetools/composables/src/useWishlist'
+  ],
+  name: 'API Reference'
+});
+
+const project = app.convert();
+
+if (project) {
+  const outputDir = './commercetools/api-composables-reference';
+  app.generateDocs(project, outputDir);
+}
