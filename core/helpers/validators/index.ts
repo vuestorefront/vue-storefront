29b593f72be065a7adc815f6c8cc30f8b0bb48be
Łukasz <ljedrasik@vuestorefront.io>
2021-02-05 17:18:58 +0100

    more chars

--- core/helpers/validators/index.ts
+++ core/helpers/validators/index.ts
@@ -1,5 +1,5 @@
 /** check if string contains some any unicode alphabet characters */
-export const unicodeAlpha = (value: string): boolean => /^[a-zA-ZàâäôéèëêïîçùûüÿæœÀÂÄÔÉÈËÊÏÎŸÇÙÛÜÆŒäöüßÄÖÜẞąćęłńóśźżĄĆĘŁŃÓŚŹŻàèéìíîòóùúÀÈÉÌÍÎÒÓÙÚáéíñóúüÁÉÍÑÓÚÜ_ ]*$/.test(value)
+export const unicodeAlpha = (value: string): boolean => /^[a-zA-ZÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķĸĹĺĻļĽľŁłĿŀŃńŅņŇňŉŊŋŌōŎŏŐőŒœŔŕŖŗŘřŚśŜŝŞşŠšŢţŤťŦŧŨũŪūŬŭŮůŰűŲųŴŵŶŷŸŹźŻżŽžſ_ ]*$/.test(value)
 
 /** check if string contains some any unicode alphabet characters or digits */
 export const unicodeAlphaNum = (value: string): boolean => unicodeAlpha(value) || /[0-9]+/.test(value)
