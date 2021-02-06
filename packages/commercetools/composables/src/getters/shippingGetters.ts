b9b9a667c94edd3f7b19571b4bca756d06494601
Fifciuu <fjedrasik@vuestorefront.io>
2021-02-06 13:11:18 +0100

    refactor: shippingGetters fallback

--- packages/commercetools/composables/src/getters/shippingGetters.ts
+++ packages/commercetools/composables/src/getters/shippingGetters.ts
@@ -2,20 +2,20 @@ import { ShippingGetters } from '@vue-storefront/core';
 import { Address } from './../types/GraphQL';
 
 export const getAddress = (shipping: any) => shipping;
-export const getAddressPostCode = (address: Address) => address?.postalCode;
-export const getAddressStreetName = (address: Address) => address?.streetName;
-export const getAddressStreetNumber = (address: Address) => address?.streetNumber;
-export const getAddressCity = (address: Address) => address?.city;
-export const getAddressFirstName = (address: Address) => address?.firstName;
-export const getAddressLastName = (address: Address) => address?.lastName;
-export const getAddressCountry = (address: Address) => address?.country;
-export const getAddressPhone = (address: Address) => address?.phone;
-export const getAddressEmail = (address: Address) => address?.email;
-export const getAddressProvince = (address: Address) => address?.state;
-export const getAddressCompanyName = (address: Address) => address?.company;
+export const getAddressPostCode = (address: Address) => address?.postalCode || '';
+export const getAddressStreetName = (address: Address) => address?.streetName || '';
+export const getAddressStreetNumber = (address: Address) => address?.streetNumber || '';
+export const getAddressCity = (address: Address) => address?.city || '';
+export const getAddressFirstName = (address: Address) => address?.firstName || '';
+export const getAddressLastName = (address: Address) => address?.lastName || '';
+export const getAddressCountry = (address: Address) => address?.country || '';
+export const getAddressPhone = (address: Address) => address?.phone || '';
+export const getAddressEmail = (address: Address) => address?.email || '';
+export const getAddressProvince = (address: Address) => address?.state || '';
+export const getAddressCompanyName = (address: Address) => address?.company || '';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getAddressTaxNumber = (address: Address) => '';
-export const getAddressApartmentNumber = (address: Address) => address?.apartment;
+export const getAddressApartmentNumber = (address: Address) => address?.apartment || '';
 
 const shippingGetters: ShippingGetters<any, Address> = {
   getAddress,
