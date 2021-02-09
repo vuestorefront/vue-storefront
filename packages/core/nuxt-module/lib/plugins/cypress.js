8854f272a04f41896106f8cbcb86588183afee21
Filip Sobol <sobol.filip@gmail.com>
2021-02-09 16:20:16 +0100

    Add GitHub Action

--- packages/core/nuxt-module/lib/plugins/cypress.js
+++ packages/core/nuxt-module/lib/plugins/cypress.js
@@ -2,13 +2,7 @@ import Vue from 'vue';
 
 const cypressPlugin = (ctx) => {
   Vue.directive('cypress', {
-    bind: (element, binding) => {
-      console.log({
-        1: binding.value,
-        element
-      });
-      ctx.isDev && element.setAttribute(`data-cypress`, binding.value);
-    }
+    bind: (element, binding) => ctx.isDev && element.setAttribute(`data-cypress`, binding.value)
   });
 };
 
