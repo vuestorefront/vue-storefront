4705edb2225e04bd8f4ae63be0aa9418a7a5def1
Filip Sobol <sobol.filip@gmail.com>
2021-02-10 12:13:26 +0100

    Update

--- packages/core/nuxt-module/lib/plugins/cypress.js
+++ packages/core/nuxt-module/lib/plugins/cypress.js
@@ -2,7 +2,11 @@ import Vue from 'vue';
 
 const cypressPlugin = (ctx) => {
   Vue.directive('cypress', {
-    bind: (element, binding) => ctx.isDev && element.setAttribute(`data-cypress`, binding.value)
+    bind: (element, binding) => {
+      const enabled = ctx.isDev || ctx.env.NUXT_ENV_CYPRESS === true.toString();
+
+      return enabled && element.setAttribute(`data-cypress`, binding.value);
+    }
   });
 };
 
