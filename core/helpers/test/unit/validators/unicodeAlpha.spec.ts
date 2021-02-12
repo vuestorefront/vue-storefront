c14e7d14a602aaa748c0206e8756df89385bb1c4
Łukasz <ljedrasik@vuestorefront.io>
2021-02-12 08:45:23 +0100

    chore: added more test cases

--- core/helpers/test/unit/validators/unicodeAlpha.spec.ts
+++ core/helpers/test/unit/validators/unicodeAlpha.spec.ts
@@ -2,20 +2,30 @@ import { unicodeAlpha } from '../../../validators';
 
 describe('unicodeAlpha', () => {
   it('returns false if string contains unacceptable characters', () => {
-    const someValue = unicodeAlpha('John$@!#! Doe@!#');
+    const example1 = unicodeAlpha('John$@!#! Doe@!#');
+    const example2 = unicodeAlpha('Jo$%n Do');
+    const example3 = unicodeAlpha('Al#!C');
+    const example4 = unicodeAlpha('#$!@');
+    const example5 = unicodeAlpha('John ć∑efz');
 
-    expect(someValue).toBe(false);
+    expect(example1).toBe(false);
+    expect(example2).toBe(false);
+    expect(example3).toBe(false);
+    expect(example4).toBe(false);
+    expect(example5).toBe(false);
   })
 
   it('returns true if string contains acceptable characters', () => {
-    const polishExample = unicodeAlpha('Rafał');
-    const frenchExample = unicodeAlpha('Émilien');
-    const germanExample = unicodeAlpha('Heiner Schönherr');
-    const spainExample = unicodeAlpha('Fabián Gálvez');
+    const example1 = unicodeAlpha('Rafał');
+    const example2 = unicodeAlpha('Émilien');
+    const example3 = unicodeAlpha('Heiner Schönherr');
+    const example4 = unicodeAlpha('Fabián Gálvez');
+    const example5 = unicodeAlpha('Adrián Sørina');
 
-    expect(polishExample).toBe(true);
-    expect(frenchExample).toBe(true);
-    expect(germanExample).toBe(true);
-    expect(spainExample).toBe(true);
+    expect(example1).toBe(true);
+    expect(example2).toBe(true);
+    expect(example3).toBe(true);
+    expect(example4).toBe(true);
+    expect(example5).toBe(true);
   })
 })
