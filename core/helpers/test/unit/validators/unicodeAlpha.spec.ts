73f685c67c84037b5f4316e50c6236e79a303a09
Łukasz <ljedrasik@vuestorefront.io>
2021-02-16 16:06:10 +0100

    chore: added greek alpha

--- core/helpers/test/unit/validators/unicodeAlpha.spec.ts
+++ core/helpers/test/unit/validators/unicodeAlpha.spec.ts
@@ -6,7 +6,7 @@ describe('unicodeAlpha', () => {
     const example2 = unicodeAlpha('Jo$%n Do');
     const example3 = unicodeAlpha('Al#!C');
     const example4 = unicodeAlpha('#$!@');
-    const example5 = unicodeAlpha('John ć∑efz');
+    const example5 = unicodeAlpha('#$!@ Te#$');
 
     expect(example1).toBe(false);
     expect(example2).toBe(false);
@@ -21,11 +21,13 @@ describe('unicodeAlpha', () => {
     const example3 = unicodeAlpha('Heiner Schönherr');
     const example4 = unicodeAlpha('Fabián Gálvez');
     const example5 = unicodeAlpha('Adrián Sørina');
+    const example6 = unicodeAlpha('John ćΣefz');
 
     expect(example1).toBe(true);
     expect(example2).toBe(true);
     expect(example3).toBe(true);
     expect(example4).toBe(true);
     expect(example5).toBe(true);
+    expect(example6).toBe(true);
   })
 })
--- core/helpers/validators/index.ts
+++ core/helpers/validators/index.ts
@@ -1,5 +1,5 @@
 /** check if string contains some any unicode alphabet characters */
-export const unicodeAlpha = (value: string): boolean => /^[a-zA-ZÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķĸĹĺĻļĽľŁłĿŀŃńŅņŇňŉŊŋŌōŎŏŐőŒœŔŕŖŗŘřŚśŜŝŞşŠšŢţŤťŦŧŨũŪūŬŭŮůŰűŲųŴŵŶŷŸŹźŻżŽžſ ]*$/.test(value)
+export const unicodeAlpha = (value: string): boolean => /^[a-zA-ZÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķĸĹĺĻļĽľŁłĿŀŃńŅņŇňŉŊŋŌōŎŏŐőŒœŔŕŖŗŘřŚśŜŝŞşŠšŢţŤťŦŧŨũŪūŬŭŮůŰűŲųŴŵŶŷŸŹźŻżŽžſαβΓγΔδεζηΘθικΛλμνΞξΠπρΣσ,ςτυΦφχΨψΩω ]*$/.test(value)
 
 /** check if string contains some any unicode alphabet characters or digits */
 export const unicodeAlphaNum = (value: string): boolean => unicodeAlpha(value) || /[0-9]+/.test(value)
