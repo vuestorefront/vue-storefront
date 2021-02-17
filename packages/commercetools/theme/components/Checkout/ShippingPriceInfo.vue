83f624691caf8439388f7cd5b82febc4bf132dc3
Fifciuu <fjedrasik@vuestorefront.io>
2021-02-17 14:07:47 +0100

    refactor: agnostic CartPreview with ShippingPriceInfo per integration

---
+++ packages/commercetools/theme/components/Checkout/ShippingPriceInfo.vue
@@ -0,0 +1,27 @@
+<template>
+  <SfProperty
+    name="Shipping"
+    v-if="chosenShippingMethod && chosenShippingMethod.zoneRates"
+    :value="$n(getShippingMethodPrice(chosenShippingMethod), 'currency')"
+    class="sf-property--full-width sf-property--large property"
+  />
+</template>
+
+<script>
+import { SfProperty } from '@storefront-ui/vue';
+import getShippingMethodPrice from '@/helpers/Checkout/getShippingMethodPrice';
+import { ref } from '@vue/composition-api';
+
+export default {
+  components: {
+    SfProperty
+  },
+  setup () {
+    const chosenShippingMethod = ref(0);
+    return {
+      getShippingMethodPrice,
+      chosenShippingMethod
+    };
+  }
+};
+</script>
