2aebf2f00fdff97aee555ec2775643da838a7a4b
andrzejewsky <vox3r69@gmail.com>
2021-02-17 10:24:26 +0100

    changelog

---
+++ packages/core/docs/changelog/5530.js
@@ -0,0 +1,7 @@
+module.exports = {
+  description: 'Add typings for middleeware',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/5530',
+  isBreaking: false,
+  author: 'Patryk Andrzejewski',
+  linkToGitHubAccount: 'https://github.com/andrzejewsky'
+};
