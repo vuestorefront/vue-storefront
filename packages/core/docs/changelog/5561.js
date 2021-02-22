cfd30fcba455c52d931035e3a6e0ef16209b21c9
Filip Sobol <sobol.filip@gmail.com>
2021-02-22 10:14:05 +0100

    Add changelog

---
+++ packages/core/docs/changelog/5561.js
@@ -0,0 +1,7 @@
+module.exports = {
+  description: 'Added \'components/Checkout/ShippingPriceInfo.vue\' component to display integration-specific shipping price information',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/5561',
+  isBreaking: false,
+  author: 'Filip Sobol',
+  linkToGitHubAccount: 'https://github.com/filipsobol'
+};
