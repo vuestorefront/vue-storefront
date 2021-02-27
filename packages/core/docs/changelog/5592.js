2abcb06c6b536f927f34b90e01c1aaacb0e06a4c
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-02-27 16:58:28 +0100

    bug: update changelog for issue #5592

---
+++ packages/core/docs/changelog/5592.js
@@ -0,0 +1,21 @@
+module.exports = {
+  description: 'fix naming convention for isOnCart and isOnWishlist to isInCart and isInWishlist',
+  link: 'https://github.com/vuestorefront/vue-storefront/issues/5592',
+  isBreaking: true,
+  breakingChanges: [
+    {
+      module: 'commercetools',
+      before: 'variables and methods had names isOnCart and isOnWishlist',
+      after: 'variables and methods have names isInCart and isInWishlist',
+      comment: 'fix naming convention for isOnCart and isOnWishlist to isInCart and isInWishlist fro commercetools'
+    },
+    {
+      module: 'core',
+      before: 'variables and methods had names isOnCart and isOnWishlist',
+      after: 'variables and methods have names isInCart and isInWishlist',
+      comment: 'fix naming convention for isOnCart and isOnWishlist to isInCart and isInWishlist for core'
+    }
+  ],
+  author: 'Jakub Andrzejewski',
+  linkToGitHubAccount: 'https://github.com/Baroshem'
+};
