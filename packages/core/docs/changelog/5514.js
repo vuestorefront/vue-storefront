4afd5892cffa5590dae7cf9897433f37670ebf87
Przemysław Rakowski <przemek.rakowski@gmail.com>
2021-02-27 15:51:50 +0100

    changelog

---
+++ packages/core/docs/changelog/5514.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'removed  overflow-x: hidden from html tag',
+  link: 'https://github.com/vuestorefront/vue-storefront/issues/5514',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Przemysław Rakowski',
+  linkToGitHubAccount: 'https://github.com/RakowskiPrzemyslaw'
+};
