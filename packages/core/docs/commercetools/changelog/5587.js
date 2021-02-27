0a7b6ac09c587c0c303efbab803930b931dd9c1d
Fifciuu <filip.jdrasik@gmail.com>
2021-02-27 13:03:18 +0100

    chore: changelog

---
+++ packages/core/docs/commercetools/changelog/5587.js
@@ -0,0 +1,7 @@
+module.exports = {
+  description: 'Mocked of VsfPaymentProvider',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/5587',
+  isBreaking: false,
+  author: 'Fifciu',
+  linkToGitHubAccount: 'https://github.com/Fifciu'
+};
