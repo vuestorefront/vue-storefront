bbf449625584ce55dc6f4df9a5a3057c9c1cd831
Fifciuu <filip.jdrasik@gmail.com>
2021-02-27 15:43:05 +0100

    refactor: vsfpaymentprovidermock

--- packages/commercetools/theme/components/Checkout/VsfPaymentProviderMock.vue
+++ packages/commercetools/theme/components/Checkout/VsfPaymentProviderMock.vue
@@ -6,12 +6,6 @@
       class="sf-heading--left sf-heading--no-underline title"
     />
     <div class="form">
-      <div v-if="error.loadMethods">
-        {{ $t('There was some error while trying to fetch payment methods. We are sorry, please try with different billing details or later.') }}
-      </div>
-      <div v-else-if="error.saveMethod">
-        {{ $t('There was some error while trying to select this payment method. We are sorry, please try with different payment method or later.') }}
-      </div>
       <div class="form__radio-group">
           <SfRadio
             v-for="method in paymentMethods"
@@ -48,7 +42,7 @@ import {
   SfButton,
   SfRadio
 } from '@storefront-ui/vue';
-import { ref, reactive, onMounted } from '@vue/composition-api';
+import { ref } from '@vue/composition-api';
 
 export default {
   name: 'VsfPaymentProviderMock',
@@ -57,106 +51,25 @@ export default {
     SfButton,
     SfRadio
   },
-  props: {
-    beforeLoad: Function,
-    afterLoad: Function,
-    onSelected: Function,
-    onSelectedDetailsChanged: Function,
-    onError: Function
-  },
   setup (props, context) {
-    const loading = ref(false);
-    const paymentMethods = ref([]);
     const selectedPaymentMethod = ref({});
-
-    const error = reactive({
-      loadMethods: null,
-      saveMethod: null
-    });
-
-    const callHookWithFallback = async (hookFn, arg = null, fallbackValue = null) => {
-      if (typeof hookFn === 'function') {
-        return await hookFn(arg);
+    const paymentMethods = ref([
+      {
+        id: 'mocked-id',
+        name: 'Cash on delivery',
+        description: 'Pay when you get the package'
       }
-      return fallbackValue;
-    };
+    ]);
 
-    const loadMethods = async () => {
-      try {
-        error.loadMethods = null;
-        return [
-          {
-            id: 'mocked-id',
-            name: 'Cash on delivery',
-            description: 'Pay when you get the package'
-          }
-        ];
-      } catch (err) {
-        error.loadMethods = err;
-        await callHookWithFallback(
-          props.onError,
-          {
-            action: 'loadMethods',
-            error: error.loadMethods
-          }
-        );
-      }
-    };
-
-    const saveMethod = async ({ paymentMethod }) => {
-      try {
-        error.saveMethod = null;
-        loading.value = true;
-        return paymentMethod;
-      } catch (err) {
-        error.saveMethod = err;
-        await callHookWithFallback(
-          props.onError,
-          {
-            action: 'saveMethod',
-            error: error.saveMethod
-          }
-        );
-      } finally {
-        loading.value = false;
-      }
-    };
-
-    const selectPaymentMethod = async paymentMethod => {
-      if (loading.value) {
-        return;
-      }
-      const newPaymentMethod = await saveMethod({ paymentMethod });
-      if (error.saveMethod) {
-        selectedPaymentMethod.value = {};
-        context.emit('status', false);
-        return;
-      }
-      selectedPaymentMethod.value = await callHookWithFallback(props.onSelected, { paymentMethod: newPaymentMethod }, newPaymentMethod);
+    const selectPaymentMethod = paymentMethod => {
+      selectedPaymentMethod.value = paymentMethod;
       context.emit('status', true);
     };
 
-    onMounted(async () => {
-      loading.value = true;
-      await callHookWithFallback(props.beforeLoad);
-      const paymentMethodsResponse = await loadMethods();
-      if (error.loadMethods) {
-        return;
-      }
-      paymentMethods.value = await callHookWithFallback(
-        props.afterLoad,
-        { paymentMethods: paymentMethodsResponse },
-        paymentMethodsResponse
-      );
-      loading.value = false;
-    });
-
     return {
-      loading,
       paymentMethods,
       selectedPaymentMethod,
-      selectPaymentMethod,
-      error
+      selectPaymentMethod
     };
   }
 };
