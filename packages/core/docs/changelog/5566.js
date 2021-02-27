6774438905d3a171b65582f33f4a50c929c4bee6
Justyna <jgieracka@vuestorefront.io>
2021-02-27 14:09:42 +0100

    changelog file

--- packages/core/docs/changelog/5566.js
+++ packages/core/docs/changelog/5566.js
@@ -1,8 +1,21 @@
 module.exports = {
   description: 'Quick search',
   link: 'https://github.com/vuestorefront/vue-storefront/issues/5566',
-  isBreaking: false,
-  breakingChanges: [],
+  isBreaking: true,
+  breakingChanges: [
+    {
+      module: 'useUiHelpers/index.ts',
+      before: '{ changeSearchTerm } = useUiHelpers()',
+      after: '{ setTermForUrl } = useUiHelpers();',
+      comment: 'Changed changeSearchTerm name to setTermForUrl'
+    },
+    {
+      module: 'useUiHelpers/index.ts',
+      before: '',
+      after: '{ getSearchTermFromUrl } = useUiHelpers();',
+      comment: 'Created new function'
+    }
+  ],
   author: 'Justyna Gieracka',
   linkToGitHubAccount: 'https://github.com/justyna-13'
 };
