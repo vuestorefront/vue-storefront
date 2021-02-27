efbb0eacb368b64a243709c65e9852105fdd4caf
Filip Rakowski <f.rakowskI@hotmail.com>
2021-02-27 10:18:12 +0100

    Update packages/core/docs/commercetools/changelog/5419.js

--- packages/core/docs/commercetools/changelog/5419.js
+++ packages/core/docs/commercetools/changelog/5419.js
@@ -3,18 +3,6 @@ module.exports = {
   link: 'https://github.com/vuestorefront/vue-storefront/pull/5552',
   isBreaking: false,
   breakingChanges: [
-    {
-      module: 'commercetools',
-      before: 'Checkout/UserShippingAddresses.vue - shippingAddresses props, emiting changeSetAsDefault event, not using useUserShipping',
-      after: 'accessing shippingAddresses via useUserShipping, emiting update:setAsDefault to use props.sync',
-      comment: 'Refactor of Checkout/UserShippingAddresses.vue component'
-    },
-    {
-      module: 'commercetools',
-      before: 'Using checkoutGetters in pages/Checkout/OrderReview.vue',
-      after: 'Accessing properties directly. Using new helper - getShippingMethodPrice for transforming price (to do not break the DRY rule)',
-      comment: 'Got rid of getters in pages/Checkout/OrderReview.vue'
-    }
   ],
   author: 'Fifciu',
   linkToGitHubAccount: 'https://github.com/Fifciu'
