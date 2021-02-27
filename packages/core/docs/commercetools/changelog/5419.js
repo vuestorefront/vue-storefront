94860fead3f9919fa70ff2f5826913e743897d16
Fifciuu <filip.jdrasik@gmail.com>
2021-02-27 10:22:30 +0100

    refactor

--- packages/core/docs/commercetools/changelog/5419.js
+++ packages/core/docs/commercetools/changelog/5419.js
@@ -2,8 +2,7 @@ module.exports = {
   description: 'New part of checkout - shipping details, inside core and commercetools',
   link: 'https://github.com/vuestorefront/vue-storefront/pull/5552',
   isBreaking: false,
-  breakingChanges: [
-  ],
+  breakingChanges: [],
   author: 'Fifciu',
   linkToGitHubAccount: 'https://github.com/Fifciu'
 };
