e776d0635136f89abad575d8e6be57b12b50640b
Fifciuu <fjedrasik@vuestorefront.io>
2021-03-01 15:49:05 +0100

    chore: changelog update

---
+++ packages/core/docs/changelog/5598.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'save method for useShippingProvider',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/5598',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Fifciu',
+  linkToGitHubAccount: 'https://github.com/Fifciu'
+};
