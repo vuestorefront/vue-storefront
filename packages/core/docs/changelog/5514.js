c828ff5cdfe32dea0e657e496488b450c210b486
Przemysław Rakowski <przemek.rakowski@gmail.com>
2021-03-02 10:03:19 +0100

    fix: remove double space

--- packages/core/docs/changelog/5514.js
+++ packages/core/docs/changelog/5514.js
@@ -1,5 +1,5 @@
 module.exports = {
-  description: 'removed  overflow-x: hidden from html tag',
+  description: 'removed overflow-x: hidden from html tag',
   link: 'https://github.com/vuestorefront/vue-storefront/issues/5514',
   isBreaking: false,
   breakingChanges: [],
