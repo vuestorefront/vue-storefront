44aff9f7a1ccc2822ba0cc3a9ef9c5d0a0401ddd
Fifciuu <fjedrasik@vuestorefront.io>
2021-03-02 13:09:03 +0100

    chore: changelog

--- packages/core/docs/changelog/5598.js
+++ packages/core/docs/changelog/5598.js
@@ -1,5 +1,5 @@
 module.exports = {
-  description: 'save method for useShippingProvider',
+  description: 'save method for useShippingProvider, deprecated field ShippingMethod.description switched to ShippingMethod.localizedDescription, fixed cart getter for counting totals',
   link: 'https://github.com/vuestorefront/vue-storefront/pull/5598',
   isBreaking: false,
   breakingChanges: [],
