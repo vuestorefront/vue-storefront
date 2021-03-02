5c9088384e58b2a049650de26720c4679b45b111
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-03-02 08:22:00 +0100

    bug-#5537: create useNewsletterFactory in core

---
+++ packages/core/core/src/factories/useNewsletterFactory.ts
@@ -0,0 +1,61 @@
+import { computed, Ref } from '@vue/composition-api';
+import { FactoryParams, Context, UseNewsletterErrors, UseNewsletter, NewsletterSections } from '../types';
+import { sharedRef, Logger, configureFactoryParams } from '../utils';
+
+export interface UseNewsletterFactoryParams extends FactoryParams {
+  updateNewsletterData: (
+    context: Context,
+    params: NewsletterSections) => Promise<NewsletterSections>;
+  load: () => Promise<NewsletterSections>;
+}
+
+export const useNewsletterFactory = (
+  factoryParams: UseNewsletterFactoryParams
+) => {
+  return function useNewsletter (): UseNewsletter {
+    const loading: Ref<boolean> = sharedRef(false, 'useNewsletter-loading');
+    const newsletterRef: Ref<NewsletterSections> = sharedRef({}, 'useNewsletter-newsletter');
+    const _factoryParams = configureFactoryParams(factoryParams);
+    const error: Ref<UseNewsletterErrors> = sharedRef({}, 'useNewsletter-error');
+
+    const load = async (): Promise<NewsletterSections> => {
+      Logger.debug('useNewsletter.load');
+
+      try {
+        loading.value = true;
+        error.value = null;
+        newsletterRef.value = await _factoryParams.load();
+        return newsletterRef.value;
+      } catch (err) {
+        error.value = err;
+        Logger.error('useNewsletter/load', err);
+      } finally {
+        loading.value = false;
+      }
+    };
+
+    const updateNewsletterData = async (newsletter) => {
+      Logger.debug('useNewsletter.updateNewsletterData', newsletter);
+
+      try {
+        loading.value = true;
+        error.value = null;
+        const newsletterValue = await _factoryParams.updateNewsletterData(newsletter);
+        newsletterRef.value = newsletterValue;
+      } catch (err) {
+        error.value = err;
+        Logger.error('useNewsletter/updateNewsletterData', err);
+      } finally {
+        loading.value = false;
+      }
+    };
+
+    return {
+      newsletter: computed(() => newsletterRef.value),
+      loading: computed(() => loading.value),
+      error: computed(() => error.value),
+      updateNewsletterData,
+      load
+    };
+  };
+};
