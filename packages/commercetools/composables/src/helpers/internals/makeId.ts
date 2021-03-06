b0e94bf333f8f4cc89f63ab1b1e0f6667ec9c2a9
Fifciuu <fjedrasik@vuestorefront.io>
2021-03-06 14:24:41 +0100

    refactor: makeId as internal helper

---
+++ packages/commercetools/composables/src/helpers/internals/makeId.ts
@@ -0,0 +1,9 @@
+export default length => {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
--- packages/commercetools/composables/src/useUserShipping/index.ts
+++ packages/commercetools/composables/src/useUserShipping/index.ts
@@ -1,14 +1,5 @@
 import { useUserShippingFactory, UseUserShippingFactoryParams, Context } from '@vue-storefront/core';
-
-const makeId = length => {
-  let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-};
+import { makeId } from '../helpers/internals';
 
 const addresses: any[] = [
   {
