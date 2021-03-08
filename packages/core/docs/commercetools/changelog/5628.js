0194874754d465af65c42d96da94b339eb7719fb
Fifciuu <fjedrasik@vuestorefront.io>
2021-03-08 11:02:08 +0100

    chore: changelog

---
+++ packages/core/docs/commercetools/changelog/5628.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'State as a select field at both Checkout and MyAccount (shipping & billing). Support for freeAbove in shipping methods',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/5628',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Fifciu',
+  linkToGitHubAccount: 'https://github.com/Fifciu'
+};
