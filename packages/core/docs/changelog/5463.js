51752b393e6e668d993577bf33c018dc5ed57dfe
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-03-10 11:42:33 +0100

    bug: add changelogs

---
+++ packages/core/docs/changelog/5463.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Fix VsfShippingProvider to correctly show errors related to no shipping methods available for certain country',
+  link: 'https://github.com/vuestorefront/vue-storefront/issues/5463',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Baroshem',
+  linkToGitHubAccount: 'https://github.com/Baroshem'
+};
