177facc22cae22715ab1e1be39df2af3ff70d50b
lukasborawski <lukas.borawski@gmail.com>
2021-03-17 18:47:24 +0100

    docs: changelog update for 2.3.0-rc.2

--- packages/core/docs/changelog/5624.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  description: 'state field in useShippingProvider instead of response, setState method',
-  link: 'https://github.com/vuestorefront/vue-storefront/pull/5624',
-  isBreaking: false,
-  breakingChanges: [],
-  author: 'Fifciu',
-  linkToGitHubAccount: 'https://github.com/Fifciu'
-};
--- packages/core/docs/changelog/5654.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  description: 'Fix for error states reactivity in core factories and example of watching errors in docs',
-  link: 'https://github.com/vuestorefront/vue-storefront/pull/5654',
-  isBreaking: false,
-  breakingChanges: [],
-  author: 'Fifciu',
-  linkToGitHubAccount: 'https://github.com/Fifciu'
-};
