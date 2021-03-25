58f6194a5a92a6e569c39c000c22b90b1dfe2868
Filip Sobol <sobol.filip@gmail.com>
2021-03-25 15:09:48 +0100

    Add changelog

---
+++ packages/core/docs/changelog/5708.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Fixed mapping for product attributes with type `set`',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/5708',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Filip Sobol',
+  linkToGitHubAccount: 'https://github.com/filipsobol'
+};
