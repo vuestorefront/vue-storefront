e23330140d63e81943e8155b2a06f28e28b900b4
cewald <c.ewald@impericon.com>
2021-03-26 13:41:28 +0000

    Enhanced support for multistore mode * Only trigger event if it is for the current store in case you have opened tabs for multiple store-views

--- core/modules/cart/helpers/syncCartWhenLocalStorageChange.ts
+++ core/modules/cart/helpers/syncCartWhenLocalStorageChange.ts
@@ -1,7 +1,16 @@
-import rootStore from '@vue-storefront/core/store';
+import rootStore from '@vue-storefront/core/store'
+import { storeViews } from 'config'
+import { currentStoreView } from '@vue-storefront/core/lib/multistore'
 
 function getItemsFromStorage ({ key }) {
-  if (key.endsWith('shop/cart/current-cart')) {
+  if (
+    key === 'shop/cart/current-cart' ||
+    (
+      storeViews.multistore &&
+      !storeViews.commonCache &&
+      key === `${currentStoreView().storeCode}-shop/cart/current-cart`
+    )
+  ) {
     const storedItems = JSON.parse(localStorage[key])
     rootStore.dispatch('cart/syncCartWhenLocalStorageChange', { items: storedItems })
   }
