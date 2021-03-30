96164fc987f21d526bde32c0dc26ab46b64fe35a
Filip Sobol <filipsobol@users.noreply.github.com>
2021-03-30 10:30:45 +0200

    Fix CT wishlist throwing CAPI error (#5716)
    * Fix CT wishlist throwing CAPI error
    
    * Add changelog

---
+++ packages/core/docs/changelog/5716.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Fixed CT wishlist throwing CAPI error',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/5716',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Filip Sobol',
+  linkToGitHubAccount: 'https://github.com/filipsobol'
+};
