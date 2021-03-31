5093253d2fd6e0621fb4f4afabcc2c856830b8e1
Łukasz <ljedrasik@vuestorefront.io>
2021-03-31 08:11:12 +0200

    chore: revert svg instead SfLoader

--- packages/commercetools/theme/components/Header/MobileMenu.vue
+++ packages/commercetools/theme/components/Header/MobileMenu.vue
@@ -15,8 +15,7 @@
           @click="changeActive(title); handleClickCategory(category.slug)"
         />
       </template>
-      <div v-if="!subCategoriesLoading">
-        <transition name="sf-fade" mode="out-in">
+      <SfLoader :loading="subCategoriesLoading">
           <SfList v-if="activeCategory && activeCategory[0] && activeCategory[0].children">
             <SfListItem
               v-for="subCategory in activeCategory[0].children"
@@ -32,40 +31,14 @@
               </SfMenuItem>
             </SfListItem>
           </SfList>
-        </transition>
-      </div>
-      <svg
-        v-else
-        role="img"
-        width="38"
-        height="38"
-        viewBox="0 0 38 38"
-        xmlns="http://www.w3.org/2000/svg"
-        class="sf-loader__spinner"
-      >
-        <g fill="none" fill-rule="evenodd">
-          <g transform="translate(1 1)" stroke-width="2">
-            <circle stroke-opacity=".5" cx="18" cy="18" r="18" />
-            <path d="M36 18c0-9.94-8.06-18-18-18">
-              <animateTransform
-                attributeName="transform"
-                type="rotate"
-                from="0 18 18"
-                to="360 18 18"
-                dur="1s"
-                repeatCount="indefinite"
-              />
-            </path>
-          </g>
-        </g>
-      </svg>
+      </SfLoader>
       <NewCatBanners v-if="hasBanners()" />
     </SfMegaMenuColumn>
   </SfMegaMenu>
 </template>
 
 <script>
-import { SfMegaMenu, SfMenuItem, SfList } from '@storefront-ui/vue';
+import { SfMegaMenu, SfMenuItem, SfList, SfLoader } from '@storefront-ui/vue';
 import { useCategory } from '@vue-storefront/commercetools';
 import { useUiState } from '~/composables';
 import { onSSR } from '@vue-storefront/core';
@@ -77,6 +50,7 @@ export default {
     SfMegaMenu,
     SfMenuItem,
     SfList,
+    SfLoader,
     NewCatBanners: () => import('./NewCatBanners')
   },
   setup (_, { root }) {
