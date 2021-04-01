02797c0a15b3a9eb0613bdf335aa4802376f5aaf
lukasborawski <lukas.borawski@gmail.com>
2021-04-01 14:30:30 +0200

    chore: building image object

--- storyblok/src/composables/renderContent.ts
+++ storyblok/src/composables/renderContent.ts
@@ -1,6 +1,29 @@
 import { RenderComponent } from '@vue-storefront/core'
 import { Component } from '../types'
 
+const buildImageObject = (
+  items: { image: string }[],
+  actions: { replace: boolean } = { replace: false },
+) => {
+  return items.map((item: { image }) => {
+    const { replace } = actions
+    if (item.image) {
+      const image = item.image
+      const itemObject = replace
+        ? { ...item, url: image }
+        : {
+            ...item,
+            image: {
+              url: image,
+            },
+          }
+      if (replace) delete itemObject.image
+      return itemObject
+    }
+    return item
+  })
+}
+
 export const extractComponents = (
   contentData: {} | [] = [],
 ): RenderComponent[] => {
@@ -8,15 +31,21 @@ export const extractComponents = (
   if (!Array.isArray(contentData)) content = [content]
   return (content as []).map((component: Component): {
     componentName: string
-    props: {}
+    props: any
   } => {
     const props = Object.keys(component).reduce(
-      (res: {}, key: string) => ({
+      (res: any, key: string) => ({
         ...res,
         [key]: component[key],
       }),
       {},
     )
+    if (props.items) {
+      props.items = buildImageObject(props.items)
+    }
+    if (props.images) {
+      props.images = buildImageObject(props.images, { replace: true })
+    }
     return {
       componentName: component.component || 'CustomComponent',
       props,
