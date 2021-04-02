5f147ec91938c6ab4b72dd6b545c718c77f14596
Kochnov Denis <d.kochnov.optimus@gmail.com>
2021-04-02 14:26:15 +0300

    Refs #24143: add landing page route

--- src/modules/vsf-storyblok-module/pages/routes.ts
+++ src/modules/vsf-storyblok-module/pages/routes.ts
@@ -7,6 +7,11 @@ export const StoryblokRoutes = [
     component: StoryblokPage
   },
   {
+    name: 'petsies-custom-stuffed-animal-manufacture',
+    path: '/bulk-custom-stuffed-animal-manufacture',
+    component: StoryblokPage
+  },
+  {
     name: 'storyblok-single-component-visual-editor-mode',
     path: '/storyblok-empty',
     component: StoryblokPage,
