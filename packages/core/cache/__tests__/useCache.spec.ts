98af0a2e291aa0ddc997a2d648ac0be289ce8fc6
Mateusz Pietrusinski <mpietrusinski@vuestorefront.io>
2021-04-05 21:30:11 +0200

    Providing module mock per test. Checking behaviour when req is not in context.

--- packages/core/cache/__tests__/useCache.spec.ts
+++ packages/core/cache/__tests__/useCache.spec.ts
@@ -1,40 +1,65 @@
-import { useCache, CacheTagPrefix } from '../src';
+import { CacheTagPrefix } from '../src';
 
 /**
  * Mocks
  */
-jest.mock('@nuxtjs/composition-api', () => ({
-  useContext: () => {
-    return {
-      req: {
-        $vsfCache: {
-          tagsSet: new Set()
-        }
-      }
-    };
-  }
-}));
+const mockedModuleName = '@nuxtjs/composition-api';
 
 /**
  * Variables
  */
 const tags = [
-  { prefix: CacheTagPrefix.Product, value: '1' },
-  { prefix: CacheTagPrefix.Category, value: '2' },
-  { prefix: CacheTagPrefix.View, value: '3' }
+  {
+    prefix: CacheTagPrefix.Product,
+    value: '1'
+  },
+  {
+    prefix: CacheTagPrefix.Category,
+    value: '2'
+  },
+  {
+    prefix: CacheTagPrefix.View,
+    value: '3'
+  }
 ];
 
 /**
  * Tests
  */
 describe('useCache', () => {
+  beforeEach(() => jest.resetModules());
+
   it('returns empty array by default', () => {
+    jest.mock(mockedModuleName, () => ({
+      useContext: () => {
+        return {
+          req: {
+            $vsfCache: {
+              tagsSet: new Set()
+            }
+          }
+        };
+      }
+    }));
+    const { useCache } = require('../src');
     const { getTags } = useCache();
 
     expect(getTags()).toEqual([]);
   });
 
   it('can add tags', () => {
+    jest.mock(mockedModuleName, () => ({
+      useContext: () => {
+        return {
+          req: {
+            $vsfCache: {
+              tagsSet: new Set()
+            }
+          }
+        };
+      }
+    }));
+    const { useCache } = require('../src');
     const { addTags, getTags } = useCache();
 
     addTags(tags);
@@ -43,19 +68,57 @@ describe('useCache', () => {
   });
 
   it('can set / override tags', () => {
+    jest.mock(mockedModuleName, () => ({
+      useContext: () => {
+        return {
+          req: {
+            $vsfCache: {
+              tagsSet: new Set()
+            }
+          }
+        };
+      }
+    }));
+    const { useCache } = require('../src');
     const { addTags, setTags, getTags } = useCache();
 
     addTags(tags);
-    setTags(currentTags => currentTags.filter(tag => tag.prefix === 'P'));
+    setTags((currentTags) => currentTags.filter((tag) => tag.prefix === 'P'));
 
     expect(getTags()).toEqual([tags[0]]);
   });
 
   it('can clear tags', () => {
+    jest.mock(mockedModuleName, () => ({
+      useContext: () => {
+        return {
+          req: {
+            $vsfCache: {
+              tagsSet: new Set()
+            }
+          }
+        };
+      }
+    }));
+    const { useCache } = require('../src');
     const { addTags, clearTags, getTags } = useCache();
 
     addTags(tags);
     clearTags();
+
+    expect(getTags()).toEqual([]);
+  });
+
+  it('runs when req in context is undefined', () => {
+    jest.mock(mockedModuleName, () => ({
+      useContext: () => {
+        return {
+          req: undefined
+        };
+      }
+    }));
+    const { useCache } = require('../src');
+    const { getTags } = useCache();
 
     expect(getTags()).toEqual([]);
   });
