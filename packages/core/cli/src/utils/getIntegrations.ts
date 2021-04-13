c23e2f152b0ef21f43e8eccb37ac73f2b16d10f2
Mateusz Pietrusinski <1325258@gmail.com>
2021-04-13 20:25:28 +0200

    changing format of data storing

--- packages/core/cli/src/utils/getIntegrations.ts
+++ packages/core/cli/src/utils/getIntegrations.ts
@@ -1,3 +1,7 @@
-export default (): string[] => [
-  'commercetools'
-];
+export default (): { [key: string]: string } => {
+  return {
+    commercetools:
+      'https://github.com/vuestorefront/template-commercetools.git',
+    spryker: 'https://github.com/spryker/vsf-theme.git'
+  };
+};
