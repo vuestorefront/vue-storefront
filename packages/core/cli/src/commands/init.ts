9b8804e608ec013ce4b4c14416181f255b57fc64
Mateusz Pietrusinski <1325258@gmail.com>
2021-04-13 21:46:19 +0200

    Adding ability to provide custom integration template

--- packages/core/cli/src/commands/init.ts
+++ packages/core/cli/src/commands/init.ts
@@ -2,6 +2,9 @@ import getIntegrations from '../utils/getIntegrations';
 import inquirer from 'inquirer';
 import copyProject from '../scripts/copyProject';
 import path from 'path';
+import createProject from '../scripts/createProject';
+
+const CUSTOM_TEMPLATE = 'custom integration template';
 
 export default async (args) => {
   let projectName = args[0];
@@ -11,7 +14,7 @@ export default async (args) => {
         type: 'input',
         name: 'typedProjectName',
         message: 'What\'s your project name?',
-        validate (value) {
+        validate(value) {
           if (value.trim().length > 0) {
             return true;
           }
@@ -28,13 +31,56 @@ export default async (args) => {
       type: 'list',
       name: 'chosenIntegration',
       message: 'Choose integration',
-      choices: integrations
+      choices: [...integrations, CUSTOM_TEMPLATE]
     }
   ]);
 
+  if (chosenIntegration !== CUSTOM_TEMPLATE) {
+    return copyProject(
+      chosenIntegration,
+      path.resolve(process.cwd(), projectName),
+      projectName
+    );
+  }
+
+  const { otherIntegrationName } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'otherIntegrationName',
+      message: 'Provide integration name:',
+      validate(value) {
+        if (value.trim().length === 0) {
+          return 'Please provide longer name';
+        }
+        return true;
+      }
+    }
+  ]);
+
+  const { otherIntegrationGitLink } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'otherIntegrationGitLink',
+      message: 'Provide integration repository git link via https:',
+      validate(value) {
+        const gitLinkRegex = /https?:(\/\/)?(.*?)(\.git)(\/?|\#[-\d\w._]+?)$/;
+        if (value.trim().length === 0 || !gitLinkRegex.test(value)) {
+          return 'Please provide git repository https link';
+        }
+        return true;
+      }
+    }
+  ]);
+  const cwd = process.cwd();
+  const integrationTemplatesDirectory = path.resolve('./templates');
+  await createProject({
+    integration: otherIntegrationName,
+    targetPath: integrationTemplatesDirectory,
+    repositoryLink: otherIntegrationGitLink
+  });
   return copyProject(
-    chosenIntegration,
-    path.resolve(process.cwd(), projectName),
+    otherIntegrationName,
+    path.resolve(cwd, projectName),
     projectName
   );
 };
