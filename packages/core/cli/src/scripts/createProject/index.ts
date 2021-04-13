efcf171d8cacfc1f4f5e2ede01029792e9024045
Mateusz Pietrusinski <1325258@gmail.com>
2021-04-13 21:40:32 +0200

    Adding better error handling

--- packages/core/cli/src/scripts/createProject/index.ts
+++ packages/core/cli/src/scripts/createProject/index.ts
@@ -2,26 +2,42 @@ import path from 'path';
 const shell = require('shelljs');
 import log from '../../utils/log';
 import processMagicComments from './processMagicComments';
-import getIntegrations from '../../utils/getIntegrations';
+import * as process from 'process';
 
-async function createProject(
+interface ICreateProjectProps {
   integration: string,
-  targetPath: string
-): Promise<void> {
+  targetPath: string,
+  repositoryLink: string
+}
+
+async function createProject({
+  integration,
+  targetPath,
+  repositoryLink
+}: ICreateProjectProps): Promise<void> {
   const templatePath = `${targetPath}/${integration}`;
-  shell.rm('-rf', templatePath);
-  shell.mkdir('-p', targetPath);
-  shell.cd(targetPath);
-  await shell.exec(
-    `git clone ${getIntegrations()[integration]} ${integration}`
-  );
+  try {
+    shell.rm('-rf', templatePath);
+    shell.mkdir('-p', targetPath);
+    shell.cd(targetPath);
+    await shell.exec(`git clone ${repositoryLink} ${integration}`);
+  } catch (error) {
+    log.error('Unable to get integration template from repository');
+    return;
+  }
 
   log.info('Updating Nuxt config');
-  const absoluteTargetPath = path.isAbsolute(templatePath)
-    ? templatePath
-    : path.join(__dirname, templatePath);
-  const nuxtConfigPath = path.join(absoluteTargetPath, 'nuxt.config.js');
-  await processMagicComments(nuxtConfigPath);
+  try {
+    const absoluteTargetPath = path.isAbsolute(templatePath)
+      ? templatePath
+      : path.join(__dirname, templatePath);
+    const nuxtConfigPath = path.join(absoluteTargetPath, 'nuxt.config.js');
+    await processMagicComments(nuxtConfigPath);
+  } catch (error) {
+    log.error('No nuxt.config.js has been found in integration template');
+    process.exit();
+  }
+
 }
 
 export default createProject;
