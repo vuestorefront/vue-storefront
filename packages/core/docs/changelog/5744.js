c036772d99ee172f8c235c0d7e31fa67d31756ee
AdamPawlinski <apawlinski@vuestorefront.io>
2021-04-13 21:25:10 +0200

    chore: changelog fix after CR

--- packages/core/docs/changelog/5744.js
+++ packages/core/docs/changelog/5744.js
@@ -2,14 +2,7 @@ module.exports = {
   description: 'Fix hydration bug on category page',
   link: 'https://github.com/vuestorefront/vue-storefront/issues/5744',
   isBreaking: false,
-  breakingChanges: [
-    {
-      module: 'affected module',
-      before: 'how it was',
-      after: 'how it is',
-      comment: 'quick migration guide'
-    }
-  ],
+  breakingChanges: [],
   author: 'Adam Pawliński',
   linkToGitHubAccount: 'https://github.com/AdamPawlinski'
 };
