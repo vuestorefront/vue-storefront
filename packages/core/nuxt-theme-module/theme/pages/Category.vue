aeb9a9d6393563f1b30dc4cedbc6ca1d4738fbbc
Igor Wojciechowski <igorxwojciechowski@gmail.com>
2021-04-14 16:39:31 +0200

    Update wishlist logic on category page

--- packages/core/nuxt-theme-module/theme/pages/Category.vue
+++ packages/core/nuxt-theme-module/theme/pages/Category.vue
@@ -169,11 +169,11 @@
               :max-rating="5"
               :score-rating="productGetters.getAverageRating(product)"
               :show-add-to-cart-button="true"
-              :isOnWishlist="false"
+              :isOnWishlist="isInWishlist({ product })"
               :isAddedToCart="isInCart({ product })"
               :link="localePath(`/p/${productGetters.getId(product)}/${productGetters.getSlug(product)}`)"
               class="products__product-card"
-              @click:wishlist="addItemToWishlist({ product })"
+              @click:wishlist="!isInWishlist({ product }) ? addItemToWishlist({ product }) : removeItemFromWishlist({ product })"
               @click:add-to-cart="addItemToCart({ product, quantity: 1 })"
             />
           </transition-group>
@@ -196,9 +196,9 @@
               :special-price="productGetters.getPrice(product).special && $n(productGetters.getPrice(product).special, 'currency')"
               :max-rating="5"
               :score-rating="3"
-              :is-on-wishlist="false"
+              :isOnWishlist="isInWishlist({ product })"
               class="products__product-card-horizontal"
-              @click:wishlist="addItemToWishlist({ product })"
+              @click:wishlist="!isInWishlist({ product }) ? addItemToWishlist({ product }) : removeItemFromWishlist({ product })"
               @click:add-to-cart="addItemToCart({ product, quantity: 1 })"
               :link="localePath(`/p/${productGetters.getId(product)}/${productGetters.getSlug(product)}`)"
             >
@@ -374,7 +374,7 @@ export default {
     const th = useUiHelpers();
     const uiState = useUiState();
     const { addItem: addItemToCart, isInCart } = useCart();
-    const { addItem: addItemToWishlist } = useWishlist();
+    const { addItem: addItemToWishlist, isInWishlist, removeItem: removeItemFromWishlist } = useWishlist();
     const { result, search, loading } = useFacet();
 
     const products = computed(() => facetGetters.getProducts(result.value));
@@ -453,6 +453,8 @@ export default {
       facets,
       breadcrumbs,
       addItemToWishlist,
+      removeItemFromWishlist,
+      isInWishlist,
       addItemToCart,
       isInCart,
       isFacetColor,
