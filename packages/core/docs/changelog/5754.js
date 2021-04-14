3d7754d113297a8c41378dd05a3fe2a05ac0923e
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-04-14 12:57:08 +0200

    feat: add changelog

---
+++ packages/core/docs/changelog/5754.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'added better typings for all occurences of customQuery',
+  link: 'https://github.com/vuestorefront/vue-storefront/issues/5754',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Baroshem',
+  linkToGitHubAccount: 'https://github.com/baroshem'
+};
