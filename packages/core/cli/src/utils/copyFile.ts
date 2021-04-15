66f19ce4b6c0808160c4e4188b8cb4ead3c69d46
Mateusz Pietrusinski <1325258@gmail.com>
2021-04-15 19:32:51 +0200

    Removing typo

--- packages/core/cli/src/utils/copyFile.ts
+++ packages/core/cli/src/utils/copyFile.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
 const isImage = require('./isImage');
-const ensureDirectoryExists = require('./ensureDirectoryExists');
+const ensureDirectoryExists = require('./ensureDirectoryExist');
 
 export async function copyFile(fileDir, outDir) {
   const data = fs.readFileSync(fileDir, !isImage(fileDir) ? 'utf8' : undefined);
