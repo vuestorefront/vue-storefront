33a4e2a7538d6bdb0990c30f88a64907256ce2d4
Mateusz Pietrusinski <1325258@gmail.com>
2021-04-15 19:10:45 +0200

    Removing external lib functions into utils scope

--- packages/core/cli/src/utils/helpers.ts
+++ packages/core/cli/src/utils/helpers.ts
@@ -1,18 +1,32 @@
 import path from 'path';
-import { copyFile } from '@vue-storefront/nuxt-theme/scripts/copyThemeFiles';
-import getAllFilesFromDir from '@vue-storefront/nuxt-theme/scripts/getAllFilesFromDir.js';
+import { copyFile } from './copyFile';
+import { getAllFilesFromDir } from './getAllFilesFromDir';
 import fs from 'fs';
 
-export const buildFileTargetPath = (file: string, targetPath: string, chopPhrase: string): string => targetPath + (file.replace(chopPhrase, ''));
+export const buildFileTargetPath = (
+  file: string,
+  targetPath: string,
+  chopPhrase: string
+): string => targetPath + file.replace(chopPhrase, '');
 
-export const getDependencyPath = (themeName: string): string => path.dirname(require.resolve(`@vue-storefront/${themeName}/package.json`));
+export const getDependencyPath = (themeName: string): string =>
+  path.dirname(require.resolve(`@vue-storefront/${themeName}/package.json`));
 
-export const copyThemeFiles = (filesDir: string, targetPath: string, chopPhrase: string) => {
+export const copyThemeFiles = (
+  filesDir: string,
+  targetPath: string,
+  chopPhrase: string
+) => {
   if (fs.statSync(filesDir).isDirectory()) {
-    return Promise.all(getAllFilesFromDir(filesDir).map(
-      file => copyFile(file, buildFileTargetPath(file, targetPath, chopPhrase))
-    ));
+    return Promise.all(
+      getAllFilesFromDir(filesDir).map((file) =>
+        copyFile(file, buildFileTargetPath(file, targetPath, chopPhrase))
+      )
+    );
   }
 
-  return copyFile(filesDir, buildFileTargetPath(filesDir, targetPath, chopPhrase));
+  return copyFile(
+    filesDir,
+    buildFileTargetPath(filesDir, targetPath, chopPhrase)
+  );
 };
