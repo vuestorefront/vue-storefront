39dd4b1168d431414124636156db973dfa5e7668
v.vlad <vladyslav.vakhnovskyi@spryker.com>
2021-04-20 16:00:00 +0300

    changelog added

---
+++ packages/core/docs/changelog/5746.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'customQuery support for user billing/shipping',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/5781',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'vn-vlad',
+  linkToGitHubAccount: 'https://github.com/vn-vlad'
+};
