2473f28bf704841cf25e936f7ee621632fd17d0d
Filip Rakowski <frakowski@divante.pl>
2021-04-20 19:13:49 +0200

    publish cli 2.3.10

--- packages/core/cli/src/utils/getIntegrations.ts
+++ packages/core/cli/src/utils/getIntegrations.ts
@@ -3,5 +3,6 @@ export default {
       'https://github.com/vuestorefront/template-commercetools.git',
   'Salesforce Commerce Cloud (beta)': 'https://github.com/ForkPoint/vsf-sfcc-template.git',
   'Shopify (beta)': 'https://github.com/vuestorefront/template-shopify.git',
-  'Spryker (beta)': 'https://github.com/spryker/vsf-theme.git'
+  'Spryker (beta)': 'https://github.com/spryker/vsf-theme.git',
+  'Magento 2 (WIP - beta end of April)': 'https://github.com/vuestorefront/template-magento.git'
 };
