e57390583406e29035ad9f18b22d545f2e7f8e59
Łukasz <ljedrasik@vuestorefront.io>
2021-04-26 13:54:26 +0200

    chore: update changelog

--- packages/core/docs/changelog/5267.js
+++ packages/core/docs/changelog/5267.js
@@ -1,8 +1,19 @@
 module.exports = {
   description: 'added MegaMenu to theme',
   link: 'https://github.com/vuestorefront/vue-storefront/issues/5267',
-  isBreaking: false,
-  breakingChanges: [],
+  isBreaking: true,
+  breakingChanges: [
+    {
+      module: '/components/Header',
+      before: 'not exist',
+      after: 'Header.vue, LocaleSelector.vue, TopBar.vue have been moved into Header directory'
+    },
+    {
+      module: '/components/Header/AppHeader.vue',
+      before: 'Navigation as a list',
+      after: 'Moved into /components/Header/HeaderNav.vue'
+    }
+  ],
   author: 'Łukasz Jędrasik',
   linkToGitHubAccount: 'https://github.com/lukaszjedrasik'
 };
