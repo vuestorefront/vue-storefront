3405ea89775fc123c2f9ecdb1b71296e71cb0e4e
AdamPawlinski <apawlinski@vuestorefront.io>
2021-04-26 10:45:35 +0200

    fix: indent

--- packages/core/docs/changelog/5684.js
+++ packages/core/docs/changelog/5684.js
@@ -1,8 +1,8 @@
 module.exports = {
-    description: 'remove filters sidebar to separate component and add ignore to nuxt config',
-    link: 'https://github.com/vuestorefront/vue-storefront/pull/5822',
-    isBreaking: false,
-    breakingChanges: [],
-    author: 'Adam Pawliński',
-    linkToGitHubAccount: 'https://github.com/AdamPawlinski'
-  };
+  description: 'remove filters sidebar to separate component and add ignore to nuxt config',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/5822',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Adam Pawliński',
+  linkToGitHubAccount: 'https://github.com/AdamPawlinski'
+};
