ff6ffec30941a6f70ad2b714f10b23cbcc735820
Filip Sobol <sobol.filip@gmail.com>
2021-04-26 17:08:45 +0200

    Add 2.3.0 migration guide

--- packages/core/docs/.vuepress/config.js
+++ packages/core/docs/.vuepress/config.js
@@ -31,11 +31,27 @@ module.exports = {
 		sidebar: {
 			'/migrate/': [
 				{
-					title: 'Migration guide 2.3.0-rc.1',
+					title: 'Migration guide 2.3.0',
 					children: [
-						['/migrate/2.3.0-rc.1/overview', 'Overview'],
-						['/migrate/2.3.0-rc.1/integrators', 'Integrators'],
-						['/migrate/2.3.0-rc.1/commercetools', 'commercetools']
+						['/migrate/2.3.0/overview', 'Overview'],
+						['/migrate/2.3.0/integrators', 'Integrators'],
+						['/migrate/2.3.0/commercetools', 'commercetools']
+					]
+				},
+				{
+					title: 'Migration guide 2.3.0-rc.3',
+					children: [
+						['/migrate/2.3.0-rc.3/overview', 'Overview'],
+						['/migrate/2.3.0-rc.3/integrators', 'Integrators'],
+						['/migrate/2.3.0-rc.3/commercetools', 'commercetools']
+					]
+				},
+				{
+					title: 'Migration guide 2.3.0-rc.2',
+					children: [
+						['/migrate/2.3.0-rc.2/overview', 'Overview'],
+						['/migrate/2.3.0-rc.2/integrators', 'Integrators'],
+						['/migrate/2.3.0-rc.2/commercetools', 'commercetools']
 					]
 				},
 				{
