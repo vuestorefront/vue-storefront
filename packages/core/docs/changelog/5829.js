4f1d216803391e2127a2844c1c8f09b4d30600c9
lukasborawski <lukas.borawski@gmail.com>
2021-04-28 11:30:04 +0200

    chore: changelog release

--- packages/core/docs/changelog/5829.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  description: 'add _key to enhanceProduct',
-  link: 'https://github.com/vuestorefront/vue-storefront/issues/5829',
-  isBreaking: false,
-  breakingChanges: [],
-  author: 'Baroshem',
-  linkToGitHubAccount: 'https://github.com/baroshem'
-};
