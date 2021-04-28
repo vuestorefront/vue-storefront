6edb3ddf28feeb08ec9e94cd794e696382fa0194
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-04-28 13:42:38 +0200

    feat-#5851: update changelog

---
+++ packages/core/docs/changelog/5851.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'add yarn scripts to publish packages in local registry',
+  link: 'https://github.com/vuestorefront/vue-storefront/issues/5851',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Baroshem',
+  linkToGitHubAccount: 'https://github.com/Baroshem'
+};
