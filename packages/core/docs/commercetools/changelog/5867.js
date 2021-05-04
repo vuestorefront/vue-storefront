d0da109004dcba5833871c129a36cb01de5e3223
Filip Sobol <sobol.filip@gmail.com>
2021-05-04 13:59:18 +0200

    Add changelog

---
+++ packages/core/docs/commercetools/changelog/5867.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Allow more control over how Apollo links can be overwritten',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/5867',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Filip Sobol',
+  linkToGitHubAccount: 'https://github.com/filipsobol'
+};
