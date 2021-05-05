1926b2e8e6dcdc5b8bd4e01ab905ae49599ab255
Filip Sobol <filipsobol@users.noreply.github.com>
2021-05-05 12:49:14 +0200

    Make useUser "login" parameters optional (#5868)

--- packages/core/core/src/types.ts
+++ packages/core/core/src/types.ts
@@ -57,8 +57,8 @@ export interface UseUser
   user: ComputedProperty<USER>;
   setUser: (user: USER) => void;
   updateUser: (params: { user: UPDATE_USER_PARAMS }) => Promise<void>;
-  register: (params: { user: UseUserRegisterParams }) => Promise<void>;
-  login: (params: { user: UseUserLoginParams }) => Promise<void>;
+  register: (params?: { user: UseUserRegisterParams }) => Promise<void>;
+  login: (params?: { user: UseUserLoginParams }) => Promise<void>;
   logout: () => Promise<void>;
   changePassword: (params: { current: string; new: string }) => Promise<void>;
   load: () => Promise<void>;
