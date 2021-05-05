e12f1c3928c30b07f469079a40fa756b682f2076
Filip Sobol <sobol.filip@gmail.com>
2021-05-05 13:24:58 +0200

    Add changelog

---
+++ packages/core/docs/commercetools/changelog/5873.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Export helper Apollo Link functions for easier overriding',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/5873',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Filip Sobol',
+  linkToGitHubAccount: 'https://github.com/filipsobol'
+};
