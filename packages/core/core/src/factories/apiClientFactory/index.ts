276ddfa280b8791ca7bb1d9a1958b8232e27f795
v.vlad <vladyslav.vakhnovskyi@spryker.com>
2021-05-07 16:33:35 +0300

    minor changes

--- packages/core/core/src/factories/apiClientFactory/index.ts
+++ packages/core/core/src/factories/apiClientFactory/index.ts
@@ -3,41 +3,68 @@ import {
   ApiClientConfig,
   ApiInstance,
   ApiClientFactory,
-  ApiClientExtension
+  ApiClientExtension,
+  ApiClientExtensionHooks
 } from './../../types';
 import { Logger } from './../../utils';
 import { applyContextToApi } from './context';
 
-const isFn = (x) => typeof x === 'function';
+type Fn<A extends any [] = any [], B = any> = (...xs: A) => B;
+type HooksNames = keyof ApiClientExtensionHooks;
+
+const isFn = (x): x is Fn => typeof x === 'function';
+
+const callWithProp = (prop: string, rest: any = {}) => (result: any, hook: any) =>
+  isFn(hook)
+    ? hook({...rest, [prop]: result})
+    : result;
+
+const callThenWithProp = (prop: string, rest: any = {}) => (result: any, hook: any) =>
+  isFn(hook)
+    ? result.then(result => hook({...rest, [prop]: result}))
+    : result;
+
+const handleHook = (hookName: HooksNames, call: Fn) => (result: any, next: ApiClientExtensionHooks) =>
+  call(result, next[hookName]);
 
 const apiClientFactory = <ALL_SETTINGS extends ApiClientConfig, ALL_FUNCTIONS>(factoryParams: ApiClientFactoryParams<ALL_SETTINGS, ALL_FUNCTIONS>): ApiClientFactory => {
   function createApiClient (config: any, customApi: any = {}): ApiInstance {
     const rawExtensions: ApiClientExtension[] = this?.middleware?.extensions || [];
+
     const lifecycles = Object.values(rawExtensions)
       .filter(ext => isFn(ext.hooks))
       .map(({ hooks }) => hooks(this?.middleware?.req, this?.middleware?.res));
+
     const extendedApis = Object.keys(rawExtensions)
       .reduce((prev, curr) => ({ ...prev, ...rawExtensions[curr].extendApiMethods }), customApi);
 
-    const _config = lifecycles
-      .filter(ext => isFn(ext.beforeCreate))
-      .reduce((configuration, curr) => curr.beforeCreate({ configuration }), config);
+    const _config = lifecycles.reduce(
+      handleHook('beforeCreate', callWithProp('configuration')),
+      config
+    );
 
-    const settings = factoryParams.onCreate ? factoryParams.onCreate(_config) : { config, client: config.client };
+    const settings = factoryParams.onCreate
+      ? factoryParams.onCreate(_config)
+      : { config, client: config.client };
 
     Logger.debug('apiClientFactory.create', settings);
 
-    settings.config = lifecycles
-      .filter(ext => isFn(ext.afterCreate))
-      .reduce((configuration, curr) => curr.afterCreate({ configuration }), settings.config);
+    settings.config = lifecycles.reduce(
+      handleHook('afterCreate', callWithProp('configuration')),
+      config
+    );
 
-    const before = async (params) => lifecycles
-      .filter(e => isFn(e.beforeCall))
-      .reduce((args, e) => args.then(args => e.beforeCall({ ...params, configuration: settings.config, args})), Promise.resolve(params.args));
+    const before = (params) => lifecycles
+      .reduce(
+        handleHook('beforeCall', callThenWithProp('args', {...params, configuration: settings.config})),
+        Promise.resolve(params.args)
+      );
 
-    const after = async (params) => lifecycles
-      .filter(e => isFn(e.afterCall))
-      .reduce((response, e) => response.then(response => e.afterCall({ ...params, configuration: settings.config, response })), Promise.resolve(params.response));
+    const after = (params) => lifecycles
+      .reduce(
+        handleHook('afterCall', callThenWithProp('response', {...params, configuration: settings.config})),
+        Promise.resolve(params.response)
+      );
 
     const api = applyContextToApi(
       { ...factoryParams.api, ...extendedApis },
