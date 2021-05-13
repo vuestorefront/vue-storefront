04bc810066376d7e746d0e344e2d3a5241ac2c8c
Filip Sobol <sobol.filip@gmail.com>
2021-05-13 15:23:47 +0200

    Add changelog

---
+++ packages/core/docs/changelog/5904.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Fix reactivity of "error" objects in composables',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/5904/',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Filip Sobol',
+  linkToGitHubAccount: 'https://github.com/filipsobol'
+};
