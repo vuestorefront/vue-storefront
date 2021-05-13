472e62780054c11659b953bee988b47eb418321a
Filip Sobol <sobol.filip@gmail.com>
2021-05-13 15:22:00 +0200

    Add changelogs

---
+++ packages/core/docs/changelog/5906.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Fix calculation of prices displayed in CartSidebar.vue',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/5906/',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Filip Sobol',
+  linkToGitHubAccount: 'https://github.com/filipsobol'
+};
