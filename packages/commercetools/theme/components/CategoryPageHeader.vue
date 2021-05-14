97683341fd8d12af42a1924ee9c853e4af881fa3
AdamPawlinski <apawlinski@vuestorefront.io>
2021-05-14 14:52:27 +0200

    fix: category page styling, and overwriting on CT

---
+++ packages/commercetools/theme/components/CategoryPageHeader.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="navbar__main">
+    <div class="navbar__counter">
+      <span class="navbar__label desktop-only">{{ $t('Products found') }}: </span>
+      <span class="desktop-only">{{ pagination.totalItems }}</span>
+      <span class="navbar__label smartphone-only">{{ pagination.totalItems }} {{ $t('Items') }}</span>
+    </div>
+    <div class="navbar__view">
+      <span class="navbar__view-label desktop-only">{{ $t('View') }}</span>
+      <SfIcon
+        class="navbar__view-icon"
+        :color="isCategoryGridView ? 'black' : 'dark-secondary'"
+        icon="tiles"
+        size="12px"
+        role="button"
+        aria-label="Change to grid view"
+        :aria-pressed="isCategoryGridView"
+        @click="toggleCategoryGridView"
+      />
+      <SfIcon
+        class="navbar__view-icon"
+        :color="!isCategoryGridView ? 'black' : 'dark-secondary'"
+        icon="list"
+        size="12px"
+        role="button"
+        aria-label="Change to list view"
+        :aria-pressed="!isCategoryGridView"
+        @click="toggleCategoryGridView"
+      />
+    </div>
+  </div>
+</template>
+<script>
+import { useUiHelpers, useUiState } from '~/composables';
+import FiltersSidebar from './FiltersSidebar';
+
+import {
+  SfButton,
+  SfIcon
+} from '@storefront-ui/vue';
+import LazyHydrate from 'vue-lazy-hydration';
+
+export default {
+  name: 'CategoryPageHeader',
+  components: {
+    SfButton,
+    SfIcon,
+    LazyHydrate,
+    FiltersSidebar
+  },
+  props: {
+    pagination: {
+      type: Object
+    }
+  },
+  setup() {
+    const th = useUiHelpers();
+    const uiState = useUiState();
+
+    return {
+      ...uiState,
+      th
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+  .navbar {
+    position: relative;
+    display: flex;
+    border: 1px solid var(--c-light);
+    border-width: 0 0 1px 0;
+    @include for-desktop {
+      border-width: 1px 0 1px 0;
+    }
+    &.section {
+      padding: var(--spacer-sm);
+      @include for-desktop {
+        padding: 0;
+      }
+    }
+    &__main {
+      display: flex;
+      flex: 1;
+      align-items: center;
+      padding: 0;
+      justify-content: space-between;
+      @include for-desktop {
+        padding: var(--spacer-xs) var(--spacer-xl);
+      }
+    }
+    &__label {
+      font-family: var(--font-family--secondary);
+      font-weight: var(--font-weight--normal);
+      color: var(--c-text-muted);
+      @include for-desktop {
+        color: var(--c-link);
+        margin: 0 var(--spacer-2xs) 0 0;
+      }
+    }
+    &__counter {
+      font-family: var(--font-family--secondary);
+      order: 1;
+      @include for-desktop {
+        order: 0;
+      }
+    }
+    &__view {
+      display: flex;
+      align-items: center;
+      order: 0;
+      @include for-desktop {
+        margin: 0 0 0 var(--spacer-2xl);
+        order: 0;
+      }
+      &-icon {
+        cursor: pointer;
+        margin: 0 var(--spacer-base) 0 0;
+        &:last-child {
+          margin: 0;
+        }
+      }
+      &-label {
+        margin: 0 var(--spacer-sm) 0 0;
+        font: var(--font-weight--normal) var(--font-size--base) / 1.6
+          var(--font-family--secondary);
+        text-decoration: none;
+        color: var(--c-link);
+      }
+    }
+  }
+</style>
---
+++ packages/commercetools/theme/pages/Category.vue
@@ -0,0 +1,433 @@
+<template>
+  <div id="category">
+    <SfBreadcrumbs
+      class="breadcrumbs desktop-only"
+      :breadcrumbs="breadcrumbs"
+    />
+    <div class="navbar section">
+      <div class="navbar__aside desktop-only">
+        <LazyHydrate never>
+          <SfHeading
+            :level="3"
+            :title="$t('Categories')"
+            class="navbar__title" />
+        </LazyHydrate>
+      </div>
+      <CategoryPageHeader :pagination="pagination"/>
+    </div>
+
+    <div class="main section">
+      <div class="sidebar desktop-only">
+        <LazyHydrate when-idle>
+          <SfLoader
+          :class="{ 'loading--categories': loading }"
+          :loading="loading">
+            <SfAccordion
+              :open="activeCategory"
+              :show-chevron="true"
+            >
+              <SfAccordionItem
+                v-for="(cat, i) in categoryTree && categoryTree.items"
+                :key="i"
+                :header="cat.label"
+              >
+                <template>
+                  <SfList class="list">
+                    <SfListItem class="list__item">
+                      <SfMenuItem
+                        :count="cat.count || ''"
+                        :label="cat.label"
+                      >
+                        <template #label>
+                          <nuxt-link
+                            :to="localePath(th.getCatLink(cat))"
+                            :class="cat.isCurrent ? 'sidebar--cat-selected' : ''"
+                          >
+                            All
+                          </nuxt-link>
+                        </template>
+                      </SfMenuItem>
+                    </SfListItem>
+                    <SfListItem
+                      class="list__item"
+                      v-for="(subCat, j) in cat.items"
+                      :key="j"
+                    >
+                      <SfMenuItem
+                        :count="subCat.count || ''"
+                        :label="subCat.label"
+                      >
+                        <template #label="{ label }">
+                          <nuxt-link
+                            :to="localePath(th.getCatLink(subCat))"
+                            :class="subCat.isCurrent ? 'sidebar--cat-selected' : ''"
+                          >
+                            {{ label }}
+                          </nuxt-link>
+                        </template>
+                      </SfMenuItem>
+                    </SfListItem>
+                  </SfList>
+                </template>
+              </SfAccordionItem>
+            </SfAccordion>
+          </SfLoader>
+        </LazyHydrate>
+      </div>
+      <SfLoader :class="{ loading }" :loading="loading">
+        <div class="products" v-if="!loading">
+          <transition-group
+            v-if="isCategoryGridView"
+            appear
+            name="products__slide"
+            tag="div"
+            class="products__grid"
+          >
+            <SfProductCard
+              v-for="(product, i) in products"
+              :key="productGetters.getSlug(product)"
+              :style="{ '--index': i }"
+              :title="productGetters.getName(product)"
+              :image="productGetters.getCoverImage(product)"
+              :regular-price="$n(productGetters.getPrice(product).regular, 'currency')"
+              :special-price="productGetters.getPrice(product).special && $n(productGetters.getPrice(product).special, 'currency')"
+              :max-rating="5"
+              :score-rating="productGetters.getAverageRating(product)"
+              :show-add-to-cart-button="true"
+              :isOnWishlist="false"
+              :isAddedToCart="isInCart({ product })"
+              :link="localePath(`/p/${productGetters.getId(product)}/${productGetters.getSlug(product)}`)"
+              class="products__product-card"
+              @click:wishlist="addItemToWishlist({ product })"
+              @click:add-to-cart="addItemToCart({ product, quantity: 1 })"
+            />
+          </transition-group>
+          <transition-group
+            v-else
+            appear
+            name="products__slide"
+            tag="div"
+            class="products__list"
+          >
+            <SfProductCardHorizontal
+              v-for="(product, i) in products"
+              :key="productGetters.getSlug(product)"
+              :style="{ '--index': i }"
+              :title="productGetters.getName(product)"
+              :description="productGetters.getDescription(product)"
+              :image="productGetters.getCoverImage(product)"
+              :regular-price="$n(productGetters.getPrice(product).regular, 'currency')"
+              :special-price="productGetters.getPrice(product).special && $n(productGetters.getPrice(product).special, 'currency')"
+              :max-rating="5"
+              :score-rating="3"
+              :is-on-wishlist="false"
+              class="products__product-card-horizontal"
+              @click:wishlist="addItemToWishlist({ product })"
+              @click:add-to-cart="addItemToCart({ product, quantity: 1 })"
+              :link="localePath(`/p/${productGetters.getId(product)}/${productGetters.getSlug(product)}`)"
+            >
+              <template #configuration>
+                <SfProperty
+                  class="desktop-only"
+                  name="Size"
+                  value="XS"
+                  style="margin: 0 0 1rem 0;"
+                />
+                <SfProperty class="desktop-only" name="Color" value="white" />
+              </template>
+              <template #actions>
+                <SfButton
+                  class="sf-button--text desktop-only"
+                  style="margin: 0 0 1rem auto; display: block;"
+                  @click="() => {}"
+                >
+                  {{ $t('Save for later') }}
+                </SfButton>
+              </template>
+            </SfProductCardHorizontal>
+          </transition-group>
+
+          <LazyHydrate on-interaction>
+            <SfPagination
+              v-if="!loading"
+              class="products__pagination desktop-only"
+              v-show="pagination.totalPages > 1"
+              :current="pagination.currentPage"
+              :total="pagination.totalPages"
+              :visible="5"
+            />
+          </LazyHydrate>
+
+          <div
+            v-show="pagination.totalPages > 1"
+            class="products__show-on-page"
+          >
+            <span class="products__show-on-page__label">{{ $t('Show on page') }}</span>
+            <LazyHydrate on-interaction>
+              <SfSelect
+                :value="pagination.itemsPerPage.toString()"
+                class="products__items-per-page"
+                @input="th.changeItemsPerPage"
+              >
+                <SfSelectOption
+                  v-for="option in pagination.pageOptions"
+                  :key="option"
+                  :value="option"
+                  class="products__items-per-page__option"
+                >
+                  {{ option }}
+                </SfSelectOption>
+              </SfSelect>
+            </LazyHydrate>
+          </div>
+        </div>
+      </SfLoader>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  SfButton,
+  SfList,
+  SfIcon,
+  SfHeading,
+  SfMenuItem,
+  SfFilter,
+  SfProductCard,
+  SfProductCardHorizontal,
+  SfPagination,
+  SfAccordion,
+  SfSelect,
+  SfBreadcrumbs,
+  SfLoader,
+  SfProperty
+} from '@storefront-ui/vue';
+import CategoryPageHeader from '../components/CategoryPageHeader';
+import { computed } from '@vue/composition-api';
+import { useCart, useWishlist, productGetters, useFacet, facetGetters } from '@vue-storefront/commercetools';
+import { useUiHelpers, useUiState } from '~/composables';
+import { onSSR } from '@vue-storefront/core';
+import LazyHydrate from 'vue-lazy-hydration';
+
+// TODO(addToCart qty, horizontal): https://github.com/vuestorefront/storefront-ui/issues/1606
+export default {
+  transition: 'fade',
+  components: {
+    SfButton,
+    SfIcon,
+    SfList,
+    SfFilter,
+    SfProductCard,
+    SfProductCardHorizontal,
+    SfPagination,
+    SfMenuItem,
+    SfAccordion,
+    SfSelect,
+    SfBreadcrumbs,
+    SfLoader,
+    SfHeading,
+    SfProperty,
+    LazyHydrate,
+    CategoryPageHeader
+  },
+  setup() {
+    const th = useUiHelpers();
+    const uiState = useUiState();
+    const { addItem: addItemToCart, isInCart } = useCart();
+    const { addItem: addItemToWishlist } = useWishlist();
+    const { result, search, loading } = useFacet();
+
+    const products = computed(() => facetGetters.getProducts(result.value));
+    const categoryTree = computed(() => facetGetters.getCategoryTree(result.value));
+    const breadcrumbs = computed(() => facetGetters.getBreadcrumbs(result.value));
+    const pagination = computed(() => facetGetters.getPagination(result.value));
+    const activeCategory = computed(() => {
+      const items = categoryTree.value.items;
+
+      if (!items) {
+        return '';
+      }
+
+      const category = items.find(({ isCurrent, items }) => isCurrent || items.find(({ isCurrent }) => isCurrent));
+
+      return category?.label || items[0].label;
+    });
+
+    onSSR(async () => {
+      await search(th.getFacetsFromURL());
+    });
+
+    return {
+      ...uiState,
+      th,
+      products,
+      categoryTree,
+      loading,
+      productGetters,
+      pagination,
+      activeCategory,
+      breadcrumbs,
+      addItemToWishlist,
+      addItemToCart,
+      isInCart
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#category {
+  box-sizing: border-box;
+  @include for-desktop {
+    max-width: 1240px;
+    margin: 0 auto;
+  }
+}
+.main {
+  &.section {
+    padding: var(--spacer-xs);
+    @include for-desktop {
+      padding: 0;
+    }
+  }
+}
+.breadcrumbs {
+  margin: var(--spacer-base) auto var(--spacer-lg);
+}
+.navbar {
+    position: relative;
+    display: flex;
+    border: 1px solid var(--c-light);
+    border-width: 0 0 1px 0;
+    @include for-desktop {
+      border-width: 1px 0 1px 0;
+    }
+    &.section {
+      padding: var(--spacer-sm);
+      @include for-desktop {
+        padding: 0;
+      }
+    }
+    &__aside,
+    {
+      display: flex;
+      align-items: center;
+      padding: var(--spacer-sm) 0;
+    }
+    &__aside {
+      flex: 0 0 15%;
+      padding: var(--spacer-sm) var(--spacer-sm);
+      border: 1px solid var(--c-light);
+      border-width: 0 1px 0 0;
+    }
+    &__title {
+      --heading-title-font-weight: var(--font-weight--semibold);
+      --heading-title-font-size: var(--font-size--xl);
+    }
+  }
+.main {
+  display: flex;
+}
+.list {
+  --menu-item-font-size: var(--font-size--sm);
+  &__item {
+    &:not(:last-of-type) {
+      --list-item-margin: 0 0 var(--spacer-sm) 0;
+    }
+
+    .nuxt-link-exact-active {
+      text-decoration: underline;
+    }
+  }
+}
+.products {
+  box-sizing: border-box;
+  flex: 1;
+  margin: 0;
+  &__grid {
+    justify-content: center;
+    @include for-desktop {
+      justify-content: flex-start;
+    }
+  }
+  &__grid,
+  &__list {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  &__product-card {
+    --product-card-title-margin: var(--spacer-base) 0 0 0;
+    --product-card-title-font-weight: var(--font-weight--medium);
+    --product-card-title-margin: var(--spacer-xs) 0 0 0;
+    flex: 1 1 50%;
+    @include for-desktop {
+      --product-card-title-font-weight: var(--font-weight--normal);
+      --product-card-add-button-bottom: var(--spacer-base);
+      --product-card-title-margin: var(--spacer-sm) 0 0 0;
+    }
+  }
+  &__product-card-horizontal {
+    flex: 0 0 100%;
+    @include for-mobile {
+      ::v-deep .sf-image {
+      --image-width: 5.3125rem;
+      --image-height: 7.0625rem;
+      }
+    }
+  }
+  &__slide-enter {
+    opacity: 0;
+    transform: scale(0.5);
+  }
+  &__slide-enter-active {
+    transition: all 0.2s ease;
+    transition-delay: calc(0.1s * var(--index));
+  }
+  @include for-desktop {
+    &__grid {
+      margin: var(--spacer-sm) 0 0 var(--spacer-sm);
+    }
+    &__pagination {
+      display: flex;
+      justify-content: flex-start;
+      margin: var(--spacer-xl) 0 0 0;
+    }
+    &__product-card-horizontal {
+      margin: var(--spacer-lg) 0;
+    }
+    &__product-card {
+      flex: 1 1 25%;
+    }
+    &__list {
+      margin: 0 0 0 var(--spacer-sm);
+    }
+  }
+  &__show-on-page {
+    display: flex;
+    justify-content: flex-end;
+    align-items: baseline;
+    &__label {
+      font-family: var(--font-family--secondary);
+      font-size: var(--font-size--sm);
+    }
+  }
+}
+.sidebar {
+  flex: 0 0 15%;
+  padding: var(--spacer-sm);
+  border: 1px solid var(--c-light);
+  border-width: 0 1px 0 0;
+}
+.loading {
+  margin: var(--spacer-3xl) auto;
+  @include for-desktop {
+    margin-top: 6.25rem;
+  }
+  &--categories {
+    @include for-desktop {
+      margin-top: 3.75rem;
+    }
+  }
+}
+</style>
