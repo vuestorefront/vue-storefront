62fb9923c605bc091053f8159cd6a26af0b732e9
lukasborawski <lukas.borawski@gmail.com>
2021-05-14 10:15:53 +0200

    chore: missing changelog thread

---
+++ packages/core/docs/commercetools/changelog/5858.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Allow searching by orderNumber',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/5858',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'samriley',
+  linkToGitHubAccount: 'https://github.com/samriley'
+};
