f7ae07f0dfceec61a545e2316b8dfe604eb8aaad
andrzejewsky <vox3r69@gmail.com>
2021-05-20 08:52:59 +0100

    update changelog

---
+++ packages/core/docs/commercetools/changelog/5920.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Resetting comemrcetools token when clearing user session',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/5920',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'andrzejewsky',
+  linkToGitHubAccount: 'https://github.com/andrzejewsky'
+};
