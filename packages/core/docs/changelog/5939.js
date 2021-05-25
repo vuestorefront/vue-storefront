a504d364cba6a6acb0165a8a84168b53ba029bcc
Filip Sobol <sobol.filip@gmail.com>
2021-05-25 15:21:16 +0200

    Add changelog

---
+++ packages/core/docs/changelog/5939.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Make `register` params in `useUser` optional',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/5939',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Filip Sobol',
+  linkToGitHubAccount: 'https://github.com/filipsobol'
+};
