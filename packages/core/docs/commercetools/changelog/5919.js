32cb46a8bcb8c48fd4e1dee200d85c202e861094
lukasborawski <lukas.borawski@gmail.com>
2021-05-25 12:03:36 +0200

    chore: changelog update

--- packages/core/docs/commercetools/changelog/5919.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  description: 'Use cookies to read information about i18n',
-  link: 'https://github.com/vuestorefront/vue-storefront/pull/5919',
-  isBreaking: false,
-  breakingChanges: [],
-  author: 'andrzejewsky',
-  linkToGitHubAccount: 'https://github.com/andrzejewsky'
-};
