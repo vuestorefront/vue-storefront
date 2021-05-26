1225d12551c999d7276745a401cfcb0f46dfb6e0
Filip Sobol <sobol.filip@gmail.com>
2021-05-26 11:16:30 +0200

    Add changelog

---
+++ packages/core/docs/changelog/5943.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Update `changePassword` in `useUser` to have optional parameters and removing logging of masked password and customer addresses',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/5943',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Filip Sobol',
+  linkToGitHubAccount: 'https://github.com/filipsobol'
+};
