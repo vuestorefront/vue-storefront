cefbd022273f3bd255f194c999b17eb6a1b68c5d
Philip Bushnell <philip.bushnell@valtech.com>
2021-05-26 14:38:18 +0100

    chore: add #5935 changelog

---
+++ packages/core/docs/changelog/5935.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'fix checkout routing to respect non-default locales',
+  link: 'https://github.com/vuestorefront/vue-storefront/issues/5935',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Philip Bushnell (Valtech)',
+  linkToGitHubAccount: 'https://github.com/philipbushnell-valtech'
+};
