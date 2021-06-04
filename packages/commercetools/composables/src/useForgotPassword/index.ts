45b1c3b99bf8b3286c73eb57496b0f541bc9c8e5
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-06-04 15:41:11 +0200

    feat-#5968: add useForgotPassword CT composable

---
+++ packages/commercetools/composables/src/useForgotPassword/index.ts
@@ -0,0 +1,25 @@
+import { Context, UseForgotPassword, useForgotPasswordFactory, UseForgotPasswordFactoryParams } from '@vue-storefront/core';
+
+const params: UseForgotPasswordFactoryParams<any, any> = {
+  resetPassword: async (context: Context, { email, customQuery }) => {
+    const response = await context.$ct.api.customerCreatePasswordResetToken(email, customQuery);
+    const token = response?.data?.customerCreatePasswordResetToken?.value;
+    const emailObject = {
+      to: email,
+      from: 'password-recovery@vue-storefront.io',
+      subject: `Password recovery for ${email}`,
+      html: `<a href='https://vsf-next-demo.storefrontcloud.io/reset-password?token=${token}'>Reset your password by clicking this link</a>`
+    };
+
+    console.log(JSON.stringify(emailObject));
+    return token;
+  },
+  changePassword: async (context: Context, { tokenValue, newPassword, customQuery }) => {
+    const response = await context.$ct.api.customerResetPassword(tokenValue, newPassword, customQuery);
+    return response?.data?.customerResetPassword.id;
+  }
+};
+
+const useForgotPassword: () => UseForgotPassword<any, any> = useForgotPasswordFactory<any, any>(params);
+
+export default useForgotPassword;
