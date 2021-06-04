a0481125e71267d247a32c4c92a55580583545c5
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-06-04 15:38:20 +0200

    feat-#5968: create reset password default mutation

---
+++ packages/commercetools/api-client/src/api/customerResetPassword/defaultMutation.ts
@@ -0,0 +1,9 @@
+import gql from 'graphql-tag';
+
+export default gql`
+    mutation customerResetPassword($tokenValue: String!, $newPassword: String!, $storeKey: KeyReferenceInput) {
+        user: customerResetPassword(tokenValue: $tokenValue, newPassword: $newPassword, storeKey: $storeKey) {
+            id
+        }
+    }
+`;
