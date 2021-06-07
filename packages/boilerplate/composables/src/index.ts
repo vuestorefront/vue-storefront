58454a330ecea8b2ef0b15f41b38845dc5dc5fce
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-06-07 16:47:07 +0200

    feat-#5968: add to boilerplate composables

--- packages/boilerplate/composables/src/index.ts
+++ packages/boilerplate/composables/src/index.ts
@@ -15,4 +15,5 @@ export { default as useUserBilling } from './useUserBilling';
 export { default as useUserOrder } from './useUserOrder';
 export { default as useUserShipping } from './useUserShipping';
 export { default as useWishlist } from './useWishlist';
+export { default as useForgotPassword } from './useForgotPassword';
 export * from './getters';
---
+++ packages/boilerplate/composables/src/useForgotPassword/index.ts
@@ -0,0 +1,19 @@
+import { Context, UseForgotPassword, useForgotPasswordFactory, UseForgotPasswordFactoryParams } from '@vue-storefront/core';
+
+const params: UseForgotPasswordFactoryParams<any, any> = {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  resetPassword: async (context: Context, { email, customQuery }) => {
+    console.log('Mocked: resetPassword');
+
+    return {};
+  },
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  changePassword: async (context: Context, { tokenValue, newPassword, customQuery }) => {
+    console.log('Mocked: changePassword');
+    return {};
+  }
+};
+
+const useForgotPassword: () => UseForgotPassword<any, any> = useForgotPasswordFactory<any, any>(params);
+
+export default useForgotPassword;
