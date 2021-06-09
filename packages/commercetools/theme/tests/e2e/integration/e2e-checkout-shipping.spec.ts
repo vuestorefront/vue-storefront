9043b68ba2155aa8914a325e9de63e873b69799f
Igor Wojciechowski <igorxwojciechowski@gmail.com>
2021-06-09 14:53:35 +0200

    Add e2e tests for shipping form selects validation

--- packages/commercetools/theme/tests/e2e/integration/e2e-checkout-shipping.spec.ts
+++ packages/commercetools/theme/tests/e2e/integration/e2e-checkout-shipping.spec.ts
@@ -9,18 +9,18 @@ context(['regression'], 'Checkout - Shipping', () => {
       };
     });
   });
-  // it('Should successfully save address - guest customer', function () {
-  //   const data = this.fixtures.data[this.test.title];
-  //   requests.createCart().then((response: CreateCartResponse) => {
-  //     requests.addToCart(response.body.data.cart.id, data.product);
-  //   });
-  //   page.checkout.shipping.visit();
-  //   page.checkout.shipping.fillForm(data.customer);
-  //   page.checkout.shipping.selectShippingButton.click();
-  //   page.checkout.shipping.shippingMethods.contains(data.shippingMethod).click();
-  //   page.checkout.shipping.continueToBillingButton.click();
-  //   page.checkout.billing.heading.should('be.visible');
-  // });
+  it('Should successfully save address - guest customer', function () {
+    const data = this.fixtures.data[this.test.title];
+    requests.createCart().then((response: CreateCartResponse) => {
+      requests.addToCart(response.body.data.cart.id, data.product);
+    });
+    page.checkout.shipping.visit();
+    page.checkout.shipping.fillForm(data.customer);
+    page.checkout.shipping.selectShippingButton.click();
+    page.checkout.shipping.shippingMethods.contains(data.shippingMethod).click();
+    page.checkout.shipping.continueToBillingButton.click();
+    page.checkout.billing.heading.should('be.visible');
+  });
 
   const requiredFields = [
     'First Name',
@@ -33,7 +33,6 @@ context(['regression'], 'Checkout - Shipping', () => {
   ];
 
   requiredFields.forEach(requiredField => {
-
     it(`Should display an error - ${requiredField} empty`, function () {
       const data = this.fixtures.data[this.test.title];
       requests.createCart().then((response: CreateCartResponse) => {
@@ -43,9 +42,29 @@ context(['regression'], 'Checkout - Shipping', () => {
       page.checkout.shipping.fillForm(data.customer);
       page.checkout.shipping.selectShippingButton.click();
       page.checkout.shipping[Cypress._.camelCase(requiredField)].within(() => {
-        return cy.get('input').then(($input) => {
+        cy.get('input').then(($input) => {
           expect($input[0].validationMessage).to.be.eq(data.errorMessage);
         });
+      });
+    });
+  });
+
+  const requiredSelects = [
+    'Country',
+    'State'
+  ];
+
+  requiredSelects.forEach(requiredSelect => {
+    it(`Should display an error - ${requiredSelect} empty`, function () {
+      const data = this.fixtures.data[this.test.title];
+      requests.createCart().then((response: CreateCartResponse) => {
+        requests.addToCart(response.body.data.cart.id, data.product);
+      });
+      page.checkout.shipping.visit();
+      page.checkout.shipping.fillForm(data.customer);
+      page.checkout.shipping.selectShippingButton.click();
+      page.checkout.shipping[Cypress._.camelCase(requiredSelect)].parent().within(() => {
+        cy.contains(data.errorMessage).should('be.visible');
       });
     });
   });
