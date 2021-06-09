47c6caab4d25998b674b81f86fe35f2f83e96fb0
Igor Wojciechowski <igorxwojciechowski@gmail.com>
2021-06-09 13:22:06 +0200

    Add e2e tests for shipping form input validation

--- packages/commercetools/theme/tests/e2e/integration/e2e-checkout-shipping.spec.ts
+++ packages/commercetools/theme/tests/e2e/integration/e2e-checkout-shipping.spec.ts
@@ -9,19 +9,45 @@ context(['regression'], 'Checkout - Shipping', () => {
       };
     });
   });
-  it('Should successfully save address - guest customer', function () {
-    const data = this.fixtures.data[this.test.title];
+  // it('Should successfully save address - guest customer', function () {
+  //   const data = this.fixtures.data[this.test.title];
+  //   requests.createCart().then((response: CreateCartResponse) => {
+  //     requests.addToCart(response.body.data.cart.id, data.product);
+  //   });
+  //   page.checkout.shipping.visit();
+  //   page.checkout.shipping.fillForm(data.customer);
+  //   page.checkout.shipping.selectShippingButton.click();
+  //   page.checkout.shipping.shippingMethods.contains(data.shippingMethod).click();
+  //   page.checkout.shipping.continueToBillingButton.click();
+  //   page.checkout.billing.heading.should('be.visible');
+  // });
 
-    requests.createCart().then((response: CreateCartResponse) => {
-      requests.addToCart(response.body.data.cart.id, data.product);
+  const requiredFields = [
+    'First Name',
+    'Last Name',
+    'Street Name',
+    'Apartment',
+    'City',
+    'Zipcode',
+    'Phone'
+  ];
+
+  requiredFields.forEach(requiredField => {
+
+    it(`Should display an error - ${requiredField} empty`, function () {
+      const data = this.fixtures.data[this.test.title];
+      requests.createCart().then((response: CreateCartResponse) => {
+        requests.addToCart(response.body.data.cart.id, data.product);
+      });
+      page.checkout.shipping.visit();
+      page.checkout.shipping.fillForm(data.customer);
+      page.checkout.shipping.selectShippingButton.click();
+      page.checkout.shipping[Cypress._.camelCase(requiredField)].within(() => {
+        return cy.get('input').then(($input) => {
+          expect($input[0].validationMessage).to.be.eq(data.errorMessage);
+        });
+      });
     });
-
-    page.checkout.shipping.visit();
-    page.checkout.shipping.fillForm(data.customer);
-    page.checkout.shipping.selectShippingButton.click();
-    page.checkout.shipping.shippingMethods.contains(data.shippingMethod).click();
-    page.checkout.shipping.continueToBillingButton.click();
-    page.checkout.billing.heading.should('be.visible');
   });
 
 });
