f2cdd6c2177c5be20126086242dd82560b19ba69
Diana <diana-lex@bk.ru>
2021-06-11 11:59:30 +0300

    Refs #24642: Blok - added computed properties for getting css classes and styles

--- src/modules/vsf-storyblok-module/components/Blok.ts
+++ src/modules/vsf-storyblok-module/components/Blok.ts
@@ -17,6 +17,43 @@ export default Vue.extend({
     isStoryblokPreview () {
       const { id } = getStoryblokQueryParams(this.$route)
       return !!id
+    },
+    cssClasses() {
+      if (!this.item.css_classes) {
+        return '';
+      }
+      return this.item.css_classes;
+    },
+    styles () {
+      let styles = '';
+      if (this.item.alignment) {
+        styles += 'text-align: ' + this.item.alignment.toString() + ';';
+      }
+      if (this.item.margin_top) {
+        styles += 'margin-top: ' + this.item.margin_top.toString() + '%;';
+      }
+      if (this.item.margin_left) {
+        styles += 'margin-left: ' + this.item.margin_left.toString() + '%;';
+      }
+      if (this.item.margin_right) {
+        styles += 'margin-right: ' + this.item.margin_right.toString() + '%;';
+      }
+      if (this.item.margin_bottom) {
+        styles += 'margin-bottom: ' + this.item.margin_bottom.toString() + '%;';
+      }
+      if (this.item.padding_top) {
+        styles += 'padding-top: ' + this.item.padding_top.toString() + '%;';
+      }
+      if (this.item.padding_left) {
+        styles += 'padding-left: ' + this.item.padding_left.toString() + '%;';
+      }
+      if (this.item.padding_right) {
+        styles += 'padding-right: ' + this.item.padding_right.toString() + '%;';
+      }
+      if (this.item.padding_bottom) {
+        styles += 'padding-bottom: ' + this.item.padding_bottom.toString() + '%;';
+      }
+      return styles;
     }
   },
   metaInfo: {
