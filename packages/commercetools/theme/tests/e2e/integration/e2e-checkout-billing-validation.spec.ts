6ad9304cf3ee586dab9ec378bfeff0cbe437d7d3
Igor Wojciechowski <igorxwojciechowski@gmail.com>
2021-06-11 15:32:26 +0200

    Update e2e tests data

--- packages/commercetools/theme/tests/e2e/integration/e2e-checkout-billing-validation.spec.ts
+++ packages/commercetools/theme/tests/e2e/integration/e2e-checkout-billing-validation.spec.ts
@@ -11,7 +11,6 @@ context(['regression'], 'Checkout - Billing', () => {
   });
   it('Should successfully save address - guest customer', function () {
     const data = this.fixtures.data[this.test.title];
-    page.home.visit();
     requests.createCart().then((response: CreateCartResponse) => {
       requests.addToCart(response.body.data.cart.id, data.product);
       requests.updateCart(response.body.data.cart.id, { addresses: { shipping: data.customer.address.shipping }});
@@ -24,5 +23,62 @@ context(['regression'], 'Checkout - Billing', () => {
     page.checkout.billing.fillForm(data.customer);
     page.checkout.billing.continueToPaymentButton.click();
     page.checkout.payment.heading.should('be.visible');
+  });
+
+  const requiredFields = [
+    'First Name',
+    'Last Name',
+    'Street Name',
+    'Apartment',
+    'City',
+    'Zipcode',
+    'Phone'
+  ];
+
+  requiredFields.forEach(requiredField => {
+    it(`Should display an error - ${requiredField} empty`, function () {
+      const data = this.fixtures.data[this.test.title];
+      requests.createCart().then((response: CreateCartResponse) => {
+        requests.addToCart(response.body.data.cart.id, data.product);
+        requests.updateCart(response.body.data.cart.id, { addresses: { shipping: data.customer.address.shipping }});
+      });
+      page.checkout.shipping.visit();
+      page.checkout.shipping.selectShippingButton.click();
+      page.checkout.shipping.shippingMethods.contains(data.shippingMethod).click();
+      page.checkout.shipping.continueToBillingButton.click();
+      page.checkout.billing.heading.should('be.visible');
+      page.checkout.billing.fillForm(data.customer);
+      page.checkout.billing.continueToPaymentButton.click();
+      page.checkout.billing[Cypress._.camelCase(requiredField)].within(() => {
+        cy.get('input').then(($input) => {
+          expect($input[0].validationMessage).to.be.eq(data.errorMessage);
+        });
+      });
+    });
+  });
+
+  const requiredSelects = [
+    'Country',
+    'State'
+  ];
+
+  requiredSelects.forEach(requiredSelect => {
+    it(`Should display an error - ${requiredSelect} empty`, function () {
+      const data = this.fixtures.data[this.test.title];
+      requests.createCart().then((response: CreateCartResponse) => {
+        requests.addToCart(response.body.data.cart.id, data.product);
+        requests.updateCart(response.body.data.cart.id, { addresses: { shipping: data.customer.address.shipping }});
+      });
+      page.checkout.shipping.visit();
+      page.checkout.shipping.selectShippingButton.click();
+      page.checkout.shipping.shippingMethods.contains(data.shippingMethod).click();
+      page.checkout.shipping.continueToBillingButton.click();
+      page.checkout.billing.heading.should('be.visible');
+      page.checkout.billing.fillForm(data.customer);
+      page.checkout.billing.continueToPaymentButton.click();
+      page.checkout.billing[Cypress._.camelCase(requiredSelect)].parent().within(() => {
+        cy.contains(data.errorMessage).should('be.visible');
+      });
+    });
   });
 });
