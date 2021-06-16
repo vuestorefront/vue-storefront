a72d23b0c6f0c23cf1043328cc74b0e61ea3aded
Mateusz Pietrusinski <1325258@gmail.com>
2021-06-16 09:51:02 +0200

    chore: removing unnecessary file

--- packages/core/docs/changelog/5999.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  description: 'Update CLI - magento name, link to docs',
-  link: 'https://github.com/vuestorefront/vue-storefront/issues/5999',
-  isBreaking: false,
-  breakingChanges: [],
-  author: 'Mateusz Pietrusiński',
-  linkToGitHubAccount: 'https://github.com/Mateuszpietrusinski'
-};
