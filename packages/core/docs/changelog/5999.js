6ba3f811338b79339caf59cc766031408be805d9
Mateusz Pietrusinski <1325258@gmail.com>
2021-06-16 09:45:48 +0200

    chore: adding changelog for #5999

---
+++ packages/core/docs/changelog/5999.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Update CLI - magento name, link to docs',
+  link: 'https://github.com/vuestorefront/vue-storefront/issues/5999',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Mateusz Pietrusiński',
+  linkToGitHubAccount: 'https://github.com/Mateuszpietrusinski'
+};
