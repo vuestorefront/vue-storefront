c51ca81cca4274f6ee05db2364650fdd81631ff4
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-06-17 15:32:12 +0200

    feat-#5968: create logEmailExtension

--- packages/commercetools/api-client/src/index.server.ts
+++ packages/commercetools/api-client/src/index.server.ts
@@ -107,10 +107,36 @@ const tokenExtension: ApiClientExtension = {
   }
 };
 
+const logMailExtension: ApiClientExtension = {
+  name: 'logMailExtension',
+  extendApiMethods: {
+    customerCreatePasswordResetToken: async (context, email) => {
+      const response = await api.customerCreatePasswordResetToken(context, email);
+      const token = response?.data?.customerCreatePasswordResetToken?.value;
+
+      const emailObject = {
+        to: email,
+        from: 'password-recovery@vue-storefront.io',
+        subject: `Password recovery for ${email}`,
+        html: `<a href='/reset-password?token=${token}'>Reset your password by clicking this link</a>`
+      };
+
+      console.log(JSON.stringify(emailObject));
+      return response;
+    }
+  }
+};
+
+const extensions = [tokenExtension];
+
+if (process.env.NODE_ENV === 'development') {
+  extensions.push(logMailExtension);
+}
+
 const { createApiClient } = apiClientFactory({
   onCreate,
   api,
-  extensions: [tokenExtension]
+  extensions
 });
 
 export {
--- packages/commercetools/composables/src/useForgotPassword/index.ts
+++ packages/commercetools/composables/src/useForgotPassword/index.ts
@@ -3,18 +3,7 @@ import { Context, UseForgotPassword, useForgotPasswordFactory, UseForgotPassword
 const params: UseForgotPasswordFactoryParams<any, any> = {
   resetPassword: async (context: Context, { email, customQuery }) => {
     const response = await context.$ct.api.customerCreatePasswordResetToken(email, customQuery);
-    const token = response?.data?.customerCreatePasswordResetToken?.value;
-    const emailObject = {
-      to: email,
-      from: 'password-recovery@vue-storefront.io',
-      subject: `Password recovery for ${email}`,
-      html: `<a href='https://vsf-next-demo.storefrontcloud.io/reset-password?token=${token}'>Reset your password by clicking this link</a>`
-    };
-
-    if (process.env.IS_DEMO) {
-      console.log(JSON.stringify(emailObject));
-    }
-    return token;
+    return response?.data?.customerCreatePasswordResetToken?.value;
   },
   setNewPassword: async (context: Context, { tokenValue, newPassword, customQuery }) => {
     const response = await context.$ct.api.customerResetPassword(tokenValue, newPassword, customQuery);
