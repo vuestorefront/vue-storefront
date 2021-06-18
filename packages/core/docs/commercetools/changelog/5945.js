d651f86da5726185e096f3d1983c56a38b9ff46b
v.vlad <vladyslav.vakhnovskyi@spryker.com>
2021-06-18 13:04:04 +0300

    added docs and changelog

---
+++ packages/core/docs/commercetools/changelog/5945.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Add "useStore" implementation and plugin for creating locale, currency and country cookies',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/5945',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'vn-vlad',
+  linkToGitHubAccount: 'https://github.com/vn-vlad'
+};
