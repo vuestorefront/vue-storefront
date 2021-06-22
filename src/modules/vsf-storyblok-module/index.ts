f7b6aab675545b2cf7c299c3f28db64906389555
Andrey Gorbunov <andrey@optimuspro.ru>
2021-06-22 17:32:18 +0300

    Refs #24682: Storyblok module - re-export exposed services and types

--- src/modules/vsf-storyblok-module/index.ts
+++ src/modules/vsf-storyblok-module/index.ts
@@ -1,10 +1,16 @@
 import { StorefrontModule } from '@vue-storefront/core/lib/modules'
 import { module } from './store'
 import { beforeRegistration } from './hooks/beforeRegistration'
+import ComponentWidthCalculator, { SizeValue } from './component-width-calculator.service'
 
 export const KEY = 'storyblok'
 
 export const StoryblokModule: StorefrontModule = function ({ store, router, appConfig }) {
   beforeRegistration(appConfig, store)
   store.registerModule(KEY, module)
+}
+
+export {
+  ComponentWidthCalculator,
+  SizeValue
 }
