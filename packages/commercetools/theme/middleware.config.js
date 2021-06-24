7bad008cc07ae48b96dc7ee6ce945ee2d5d68932
v.vlad <vladyslav.vakhnovskyi@spryker.com>
2021-06-24 15:47:50 +0300

    fix middleware config

--- packages/commercetools/theme/middleware.config.js
+++ packages/commercetools/theme/middleware.config.js
@@ -19,8 +19,7 @@ module.exports = {
             'manage_my_payments:vsf-ct-dev',
             'manage_my_orders:vsf-ct-dev',
             'manage_my_shopping_lists:vsf-ct-dev',
-            'view_published_products:vsf-ct-dev',
-            'view_stores:vsf-ct-dev'
+            'view_published_products:vsf-ct-dev'
           ]
         },
         currency: 'USD',
