f5e4c4ea55984b1e2d169fae593047e7e0cee573
ljedrasik <ljedrasik@vuestorefront.io>
2021-06-29 11:40:07 +0200

    chore: add changelog

---
+++ packages/core/docs/changelog/6035.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Remove generate command from CT and BP themes',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6035',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'lukaszjedrasik',
+  linkToGitHubAccount: 'https://github.com/lukaszjedrasik'
+};
