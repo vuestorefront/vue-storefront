1369641bd0886a8fa22bf1c8af545b6568c0c494
ljedrasik <ljedrasik@vuestorefront.io>
2021-06-29 09:45:53 +0200

    chore: add changelog

---
+++ packages/core/docs/commercetools/changelog/5994.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Click "Add New Address" button on the Checkout does not submit the form',
+  link: '',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'lukaszjedrasik',
+  linkToGitHubAccount: 'https://github.com/lukaszjedrasik'
+};
