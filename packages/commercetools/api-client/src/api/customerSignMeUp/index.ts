5940ff4c9a6d6e682a006bd9cb50f28ed741d76c
Łukasz <lukasz.sliwa@newfantastic.com>
2021-07-07 13:37:52 +0200

    chore: Add gql wrapper to queries where missing

--- packages/commercetools/api-client/src/api/customerSignMeUp/index.ts
+++ packages/commercetools/api-client/src/api/customerSignMeUp/index.ts
@@ -1,3 +1,4 @@
+import gql from 'graphql-tag';
 import { CustomerSignMeUpDraft } from '../../types/GraphQL';
 import CustomerSignMeUpMutation from './defaultMutation';
 import { SignInResponse } from '../../types/Api';
@@ -6,8 +7,13 @@ const customerSignMeUp = async (context, draft: CustomerSignMeUpDraft): Promise<
   const { locale, acceptLanguage, currency } = context.config;
 
   const registerResponse = await context.client.mutate({
-    mutation: CustomerSignMeUpMutation,
-    variables: { draft, locale, acceptLanguage, currency },
+    mutation: gql`${CustomerSignMeUpMutation}`,
+    variables: {
+      draft,
+      locale,
+      acceptLanguage,
+      currency
+    },
     fetchPolicy: 'no-cache'
   }) as SignInResponse;
 
