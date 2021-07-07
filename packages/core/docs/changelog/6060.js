2730a1ee175b99379098be469b03d06097914bf4
Filip Sobol <sobol.filip@gmail.com>
2021-07-07 17:14:45 +0200

    Add changelog

---
+++ packages/core/docs/changelog/6060.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Export `UseFacetFactoryParams` interface, add generics to the `Context` interface, add minor fixes to base theme',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6060/files',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Filip Sobol',
+  linkToGitHubAccount: 'https://github.com/filipsobol'
+};
