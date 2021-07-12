7871a538056394c444abe9586813554ce12ad71f
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-07-12 16:40:54 -0300

    docs(core:docs): added changelog for #6066 PR

---
+++ packages/core/docs/commercetools/changelog/6066.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'refactor(commercetools): fix the frontend client bundling the commercetools-sdk and apollo client',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6066',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'bloodf',
+  linkToGitHubAccount: 'https://github.com/bloodf'
+};
