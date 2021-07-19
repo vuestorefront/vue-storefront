f9bc16c5f9452ab663104a28291d21c1b15391ca
jason.wolf <jason.wolf@borngroup.com>
2021-07-19 13:23:33 -0400

    bug/6089-incorrect-method --> commit changelog

---
+++ packages/core/docs/changelog/6089.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Fixed incorrect method`',
+  link: 'https://github.com/vuestorefront/vue-storefront/issues/6089',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'jaydubb12',
+  linkToGitHubAccount: 'https://github.com/jaydubb12/vue-storefront'
+};
