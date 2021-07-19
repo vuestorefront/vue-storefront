ad004ce8bb3aa1a4316aabb0fdec95f31e8da054
Alef Barbeli <alefbarbeli@gmail.com>
2021-07-19 13:52:05 -0300

    fix(commercetools): fixed unity test

--- packages/commercetools/composables/__tests__/getters/storeGetters.spec.ts
+++ packages/commercetools/composables/__tests__/getters/storeGetters.spec.ts
@@ -2,14 +2,14 @@ import { storeGetters } from '../../src/getters';
 import { StoresData } from '../../src/types';
 import { AgnosticStore } from '@vue-storefront/core';
 
-function makeStore (storeId, storeName, channelID = null, channelName = null) {
+function makeStore (storeId, storeName, storeKey, channelID = null, channelName = null) {
   return {
     name: `${storeName}${channelName ? ` - ${channelName}` : ''}`,
     id: `${storeId}${channelID ? `/${channelID}` : ''}`,
     description: '',
     geoLocation: null,
     locales: [],
-    key: `${storeId}${channelID ? `/${channelID}` : ''}`,
+    key: storeKey,
     address: { addressLine1: '', addressLine2: '', _address: null },
     _storeID: storeId,
     _channelID: channelID
@@ -17,29 +17,29 @@ function makeStore (storeId, storeName, channelID = null, channelName = null) {
 }
 
 const distributionChannels = (id) => ([
-  { id: 'd1', name: 'distribution channel 1', key: `${id}/d1`},
-  { id: 'd2', name: 'distribution channel 2', key: `${id}/d2`},
-  { id: 'd3', name: 'distribution channel 3', key: `${id}/d3` }
+  { id: 'd1', name: 'distribution channel 1', key: `key-${id}`},
+  { id: 'd2', name: 'distribution channel 2', key: `key-${id}`},
+  { id: 'd3', name: 'distribution channel 3', key: `key-${id}` }
 ]);
 
 const supplyChannels = (id) => ([
-  { id: 's1', name: 'supply channel 1', key: `${id}/s1` },
-  { id: 's2', name: 'supply channel 2', key: `${id}/s2` },
-  { id: 's3', name: 'supply channel 3', key: `${id}/s3` }
+  { id: 's1', name: 'supply channel 1', key: `key-${id}` },
+  { id: 's2', name: 'supply channel 2', key: `key-${id}` },
+  { id: 's3', name: 'supply channel 3', key: `key-${id}` }
 ]);
 
 const results = [
-  { id: '1', name: 'store-1', key: '1', distributionChannels: distributionChannels('1'), supplyChannels: supplyChannels('1') },
-  { id: '2', name: 'store-2', key: '2', distributionChannels: [], supplyChannels: supplyChannels('2') },
-  { id: '3', name: 'store-3', key: '3', distributionChannels: distributionChannels('3'), supplyChannels: [] },
-  { id: '4', name: 'store-4', key: '4', distributionChannels: [], supplyChannels: [] }
+  { id: '1', name: 'store-1', key: 'key-1', distributionChannels: distributionChannels('1'), supplyChannels: supplyChannels('1') },
+  { id: '2', name: 'store-2', key: 'key-2', distributionChannels: [], supplyChannels: supplyChannels('2') },
+  { id: '3', name: 'store-3', key: 'key-3', distributionChannels: distributionChannels('3'), supplyChannels: [] },
+  { id: '4', name: 'store-4', key: 'key-4', distributionChannels: [], supplyChannels: [] }
 ];
 
 const stores = {
   offset: 0,
   count: 0,
   total: 0,
-  _selected: '3',
+  _selected: 'key-3',
   results
 };
 
@@ -49,19 +49,19 @@ describe('[commercetools-getters] store getters', () => {
 
     it('returns array of stores', () => {
       const expected = [
-        makeStore('1', 'store-1', 'd1', 'distribution channel 1'),
-        makeStore('1', 'store-1', 'd2', 'distribution channel 2'),
-        makeStore('1', 'store-1', 'd3', 'distribution channel 3'),
-        makeStore('1', 'store-1', 's1', 'supply channel 1'),
-        makeStore('1', 'store-1', 's2', 'supply channel 2'),
-        makeStore('1', 'store-1', 's3', 'supply channel 3'),
-        makeStore('2', 'store-2', 's1', 'supply channel 1'),
-        makeStore('2', 'store-2', 's2', 'supply channel 2'),
-        makeStore('2', 'store-2', 's3', 'supply channel 3'),
-        makeStore('3', 'store-3', 'd1', 'distribution channel 1'),
-        makeStore('3', 'store-3', 'd2', 'distribution channel 2'),
-        makeStore('3', 'store-3', 'd3', 'distribution channel 3'),
-        makeStore('4', 'store-4'),
+        makeStore('1', 'store-1', 'key-1', 'd1', 'distribution channel 1'),
+        makeStore('1', 'store-1', 'key-1', 'd2', 'distribution channel 2'),
+        makeStore('1', 'store-1', 'key-1', 'd3', 'distribution channel 3'),
+        makeStore('1', 'store-1', 'key-1', 's1', 'supply channel 1'),
+        makeStore('1', 'store-1', 'key-1', 's2', 'supply channel 2'),
+        makeStore('1', 'store-1', 'key-1', 's3', 'supply channel 3'),
+        makeStore('2', 'store-2', 'key-2', 's1', 'supply channel 1'),
+        makeStore('2', 'store-2', 'key-2', 's2', 'supply channel 2'),
+        makeStore('2', 'store-2', 'key-2', 's3', 'supply channel 3'),
+        makeStore('3', 'store-3', 'key-3', 'd1', 'distribution channel 1'),
+        makeStore('3', 'store-3', 'key-3', 'd2', 'distribution channel 2'),
+        makeStore('3', 'store-3', 'key-3', 'd3', 'distribution channel 3'),
+        makeStore('4', 'store-4', 'key-4')
       ];
 
       expect(storeGetters.getItems(stores as StoresData)).toStrictEqual(expected);
@@ -74,12 +74,12 @@ describe('[commercetools-getters] store getters', () => {
 
     it('returns array of stores with store filter criteria', () => {
       const expected = [
-        makeStore('1', 'store-1', 'd1', 'distribution channel 1'),
-        makeStore('1', 'store-1', 'd2', 'distribution channel 2'),
-        makeStore('1', 'store-1', 'd3', 'distribution channel 3'),
-        makeStore('1', 'store-1', 's1', 'supply channel 1'),
-        makeStore('1', 'store-1', 's2', 'supply channel 2'),
-        makeStore('1', 'store-1', 's3', 'supply channel 3')
+        makeStore('1', 'store-1', 'key-1', 'd1', 'distribution channel 1'),
+        makeStore('1', 'store-1', 'key-1', 'd2', 'distribution channel 2'),
+        makeStore('1', 'store-1', 'key-1', 'd3', 'distribution channel 3'),
+        makeStore('1', 'store-1', 'key-1', 's1', 'supply channel 1'),
+        makeStore('1', 'store-1', 'key-1', 's2', 'supply channel 2'),
+        makeStore('1', 'store-1', 'key-1', 's3', 'supply channel 3')
       ];
 
       const criteria = {
@@ -93,15 +93,15 @@ describe('[commercetools-getters] store getters', () => {
 
     it('returns array of stores with callable store filter criteria', () => {
       const expected = [
-        makeStore('1', 'store-1', 'd1', 'distribution channel 1'),
-        makeStore('1', 'store-1', 'd2', 'distribution channel 2'),
-        makeStore('1', 'store-1', 'd3', 'distribution channel 3'),
-        makeStore('1', 'store-1', 's1', 'supply channel 1'),
-        makeStore('1', 'store-1', 's2', 'supply channel 2'),
-        makeStore('1', 'store-1', 's3', 'supply channel 3'),
-        makeStore('3', 'store-3', 'd1', 'distribution channel 1'),
-        makeStore('3', 'store-3', 'd2', 'distribution channel 2'),
-        makeStore('3', 'store-3', 'd3', 'distribution channel 3')
+        makeStore('1', 'store-1', 'key-1', 'd1', 'distribution channel 1'),
+        makeStore('1', 'store-1', 'key-1', 'd2', 'distribution channel 2'),
+        makeStore('1', 'store-1', 'key-1', 'd3', 'distribution channel 3'),
+        makeStore('1', 'store-1', 'key-1', 's1', 'supply channel 1'),
+        makeStore('1', 'store-1', 'key-1', 's2', 'supply channel 2'),
+        makeStore('1', 'store-1', 'key-1', 's3', 'supply channel 3'),
+        makeStore('3', 'store-3', 'key-3', 'd1', 'distribution channel 1'),
+        makeStore('3', 'store-3', 'key-3', 'd2', 'distribution channel 2'),
+        makeStore('3', 'store-3', 'key-3', 'd3', 'distribution channel 3')
       ];
 
       const criteria = {
@@ -117,8 +117,8 @@ describe('[commercetools-getters] store getters', () => {
 
     it('returns array of stores with channel filter criteria', () => {
       const expected = [
-        makeStore('1', 'store-1', 'd3', 'distribution channel 3'),
-        makeStore('3', 'store-3', 'd3', 'distribution channel 3')
+        makeStore('1', 'store-1', 'key-1', 'd3', 'distribution channel 3'),
+        makeStore('3', 'store-3', 'key-3', 'd3', 'distribution channel 3')
       ];
 
       const criteria = {
@@ -132,10 +132,10 @@ describe('[commercetools-getters] store getters', () => {
 
     it('returns array of stores with callable channel filter criteria', () => {
       const expected = [
-        makeStore('1', 'store-1', 'd3', 'distribution channel 3'),
-        makeStore('1', 'store-1', 's2', 'supply channel 2'),
-        makeStore('2', 'store-2', 's2', 'supply channel 2'),
-        makeStore('3', 'store-3', 'd3', 'distribution channel 3')
+        makeStore('1', 'store-1', 'key-1', 'd3', 'distribution channel 3'),
+        makeStore('1', 'store-1', 'key-1', 's2', 'supply channel 2'),
+        makeStore('2', 'store-2', 'key-2', 's2', 'supply channel 2'),
+        makeStore('3', 'store-3', 'key-3', 'd3', 'distribution channel 3')
       ];
 
       const criteria = {
@@ -166,7 +166,7 @@ describe('[commercetools-getters] store getters', () => {
   describe('getSelected', () => {
 
     it('returns selected store', () => {
-      const expected = makeStore('3', 'store-3', 'd1', 'distribution channel 1');
+      const expected = makeStore('3', 'store-3', 'key-3', 'd1', 'distribution channel 1');
       expect(storeGetters.getSelected(stores as StoresData)).toStrictEqual(expected);
     });
 
