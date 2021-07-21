fa2bad252fc05943298f7cc3bad2566f28c348aa
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-07-21 12:21:18 +0200

    refactor(ct:theme): update token middleware
    - add if statement to do request for token when it's not there

--- packages/commercetools/theme/middleware/token.js
+++ packages/commercetools/theme/middleware/token.js
@@ -1,10 +1,20 @@
+import { Logger } from '@vue-storefront/core';
+
 export default async (context) => {
   const { $vsf } = context;
   const currentToken = $vsf.$ct.config.auth.onTokenRead();
-  const token = await $vsf.$ct.api.requestAuthToken(currentToken);
+  if (!currentToken) {
+    const { token, errors } = await $vsf.$ct.api.requestAuthToken(currentToken);
 
-  await $vsf.$ct.config.auth.onTokenChange(
-    JSON.stringify(token),
-    token?.expires_at ? { expires: new Date(token.expires_at) } : {}
-  );
+    if (errors.length) {
+      errors.forEach(error => {
+        Logger.error(error);
+      });
+    }
+
+    await $vsf.$ct.config.auth.onTokenChange(
+      JSON.stringify(token),
+      token?.expires_at ? { expires: new Date(token.expires_at), path: '/' } : {}
+    );
+  }
 };
