03acf965f917acacf48c823f7add20862929b271
v.vlad <vladyslav.vakhnovskyi@spryker.com>
2021-07-22 14:14:16 +0300

    changelog added

---
+++ packages/core/docs/changelog/6108.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Initial oreders value in useUserOredersFactory changed to "null"',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6108',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'vn-vlad',
+  linkToGitHubAccount: 'https://github.com/vn-vlad'
+};
---
+++ packages/core/docs/commercetools/changelog/6108.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Implemented pagination for orders history',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6108',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'vn-vlad',
+  linkToGitHubAccount: 'https://github.com/vn-vlad'
+};
