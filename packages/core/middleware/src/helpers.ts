a811315f50e62c2dba390ad474beb2da8feaf8d1
Łukasz <lukasz.sliwa@newfantastic.com>
2021-07-23 14:57:38 +0200

    Satisfy eslint

--- packages/core/middleware/src/helpers.ts
+++ packages/core/middleware/src/helpers.ts
@@ -1,20 +1,18 @@
 export const getAgnosticStatusCode = (obj: unknown, ...keys: string[]) => {
-	if (Array.isArray(obj)) {
-  	for (let o of obj) {    
-    	if (getAgnosticStatusCode(o, ...keys)) {
-      	return getAgnosticStatusCode(o, ...keys)
+  if (Array.isArray(obj)) {
+    for (const o of obj) {
+      if (getAgnosticStatusCode(o, ...keys)) {
+        return getAgnosticStatusCode(o, ...keys);
       }
     }
-  } else if (obj && typeof obj === 'object') {  
-    for (let key of Object.keys(obj)) {
+  } else if (obj && typeof obj === 'object') {
+    for (const key of Object.keys(obj)) {
       if (keys.includes(key)) {
-        return obj[key]
-      } else {
-      	if (getAgnosticStatusCode(obj[key], ...keys)) {
-        	return getAgnosticStatusCode(obj[key], ...keys)
-        }
+        return obj[key];
+      } else if (getAgnosticStatusCode(obj[key], ...keys)) {
+        return getAgnosticStatusCode(obj[key], ...keys);
       }
     }
   }
-  return null
-}
+  return null;
+};
