eead28b17b94b637ffda39d87581b7b7a0e5bcfb
Łukasz <lsliwaradioluz@gmail.com>
2021-07-28 09:27:23 +0200

    Store expected statusCode in a separate const

--- packages/core/middleware/__tests__/helpers/getAgnosticStatusCode.spec.ts
+++ packages/core/middleware/__tests__/helpers/getAgnosticStatusCode.spec.ts
@@ -1,52 +1,53 @@
 import getAgnosticStatusCode from '../../src/helpers/getAgnosticStatusCode';
+const expectedStatusCode = 400;
 
 describe('[middleware-helpers] getAgnosticStatusCode', () => {
   it('retrieves the status code from simple object', () => {
     const testData = {
-      statusCode: 400
+      statusCode: expectedStatusCode
     };
 
     const statusCode = getAgnosticStatusCode(testData, 'statusCode');
 
-    expect(statusCode).toBe(400);
+    expect(statusCode).toBe(expectedStatusCode);
   });
 
   it('retrieves the status code from nested object', () => {
     const testData = {
       a: {
         b: 4,
-        statusCode: 400
+        statusCode: expectedStatusCode
       }
     };
 
     const statusCode = getAgnosticStatusCode(testData, 'statusCode');
 
-    expect(statusCode).toBe(400);
+    expect(statusCode).toBe(expectedStatusCode);
   });
 
   it('retrieves the status code from nested object inside an array', () => {
     const testData = {
       a: [
         {
-          statusCode: 400
+          statusCode: expectedStatusCode
         }
       ]
     };
 
     const statusCode = getAgnosticStatusCode(testData, 'statusCode');
 
-    expect(statusCode).toBe(400);
+    expect(statusCode).toBe(expectedStatusCode);
   });
 
   it('returns only the value of the first matched key', () => {
     const testData = {
-      statusCode: 400,
+      statusCode: expectedStatusCode,
       status: 500
     };
 
     const statusCode = getAgnosticStatusCode(testData, 'statusCode', 'status');
 
-    expect(statusCode).toBe(400);
+    expect(statusCode).toBe(expectedStatusCode);
   });
 
   it('handles values of type other than \'object\' correctly', () => {
