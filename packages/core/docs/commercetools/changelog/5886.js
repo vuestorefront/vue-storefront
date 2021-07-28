1197eb67042cdfdd1066ed8fdcf33daef54f5629
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-07-28 11:46:05 +0200

    fix(ct/core): add changelog

---
+++ packages/core/docs/commercetools/changelog/5886.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Fix locale links in core and commercetools integration',
+  link: 'https://github.com/vuestorefront/vue-storefront/issues/5886',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Baroshem',
+  linkToGitHubAccount: 'https://github.com/Baroshem'
+};
