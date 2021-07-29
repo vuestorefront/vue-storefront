a133d2183baf3accde523f247ec291d4c3e0377a
Filip Sobol <sobol.filip@gmail.com>
2021-07-29 13:52:14 +0200

    Update

--- packages/core/docs/.vuepress/config.js
+++ packages/core/docs/.vuepress/config.js
@@ -48,7 +48,6 @@ module.exports = {
         selector: 'main :not(a):not(.tile) > img'
       }
     ],
-    '@vuepress/last-updated',
     '@vuepress/active-header-links',
     '@vuepress/search'
   ],
@@ -67,7 +66,6 @@ module.exports = {
     docsDir: 'packages/core/docs',
     docsBranch: 'release/next',
     editLinkText: 'Edit this page',
-    lastUpdated: true,
     logo: 'https://camo.githubusercontent.com/48c886ac0703e3a46bc0ec963e20f126337229fc/68747470733a2f2f643968687267346d6e767a6f772e636c6f756466726f6e742e6e65742f7777772e76756573746f726566726f6e742e696f2f32383062313964302d6c6f676f2d76735f3062793032633062793032633030303030302e6a7067',
     nav: [
       { text: 'Integrations', link: '/integrations/' },
