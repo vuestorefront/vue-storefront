60b3e2c3903829a18c720a40f05f41ab1478e6cf
ljedrasik <ljedrasik@vuestorefront.io>
2021-07-30 12:04:07 +0200

    test

--- packages/core/docs/.vuepress/config.js
+++ packages/core/docs/.vuepress/config.js
@@ -24,16 +24,16 @@ module.exports = {
     ['link', { rel: 'icon', href: '/favicon.png' }],
 
     // HubSpot
-    ['script', { async: true, defer: true, src: 'https://js.hs-scripts.com/8443671.js', id: 'hs-script-loader' }],
+    // ['script', { async: true, defer: true, src: 'https://js.hs-scripts.com/8443671.js', id: 'hs-script-loader' }],
 
     // Google Tag Manager
-    ['script', {}, [`
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','${GTM_TAG}');
-    `]],
+    // ['script', {}, [`
+    //   (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    //   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    //   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    //   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    //   })(window,document,'script','dataLayer','${GTM_TAG}');
+    // `]],
   ],
 
   /**
