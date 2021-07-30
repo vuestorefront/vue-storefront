915a3e96267a0b0b697d9cb66abf9da462178261
Razz21 <razzrazz@wp.pl>
2021-07-30 11:39:22 +0200

    fix: font loading optimization

--- packages/boilerplate/theme/nuxt.config.js
+++ packages/boilerplate/theme/nuxt.config.js
@@ -30,10 +30,7 @@ export default {
       },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Raleway:300,400,400i,500,600,700|Roboto:300,300i,400,400i,500,700&display=swap',
-        media: 'print',
-        onload: 'this.media=\'all\'',
-        once: true
+        href: 'https://fonts.googleapis.com/css?family=Raleway:300,400,400i,500,600,700|Roboto:300,300i,400,400i,500,700&display=swap'
       }
     ]
   },
