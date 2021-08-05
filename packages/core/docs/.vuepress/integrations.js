846ea4ec7eb7db20e1adb852964b5d6eaea97972
Fifciuu <fjedrasik@vuestorefront.io>
2021-08-05 15:57:05 +0200

    chore: enterprise

--- packages/core/docs/.vuepress/integrations.js
+++ packages/core/docs/.vuepress/integrations.js
@@ -270,7 +270,7 @@ const INTEGRATIONS = {
       link: 'https://docs.vuestorefront.io/algolia',
       image: 'https://uploads-ssl.webflow.com/5e90e5cd5f86784ad554a3c2/5ed620cebc49f091f5fc7571_logo-algolia-nebula-blue-full.svg',
       status: STATUSES.BETA,
-      availability: AVAILABILITY.OPEN_SOURCE,
+      availability: AVAILABILITY.ENTERPRISE,
       maintainedBy: [
         { name: 'Vue Storefront', link: 'https://vuestorefront.io/' },
       ],
