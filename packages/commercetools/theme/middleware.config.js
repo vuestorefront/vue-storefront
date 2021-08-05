3250e79ca66e4ed3d285f54cbe32aa7d8d70c9ce
Alef Barbeli <alefbarbeli@gmail.com>
2021-08-05 17:09:34 -0300

    fix(commercetools): removed wrong config

--- packages/commercetools/theme/middleware.config.js
+++ packages/commercetools/theme/middleware.config.js
@@ -4,21 +4,13 @@ module.exports = {
       location: '@vue-storefront/commercetools-api/server',
       configuration: {
         api: {
-          uri: 'https://api.commercetools.com/vsf-qa/graphql',
+          uri: 'https://api.commercetools.com/vsf-ct-dev/graphql',
           authHost: 'https://auth.sphere.io',
-          projectKey: 'vsf-qa',
-          clientId: 'LoKnq1faNAdasGqk5R-zDIUd',
-          clientSecret: '6upp22N3H6uR5GdFHVEeaJXJJxAPZXr4',
+          projectKey: 'vsf-ct-dev',
+          clientId: 'kuFT95wdTP4uH_hVOKjqfGEo',
+          clientSecret: 'tklIDic86mgWrFy0oBHRQQmwX7ZC5wIP',
           scopes: [
-            'create_anonymous_token:vsf-qa',
-            'manage_my_profile:vsf-qa',
-            'view_categories:vsf-qa',
-            'introspect_oauth_tokens:vsf-qa',
-            'manage_my_payments:vsf-qa',
-            'manage_my_orders:vsf-qa',
-            'manage_my_shopping_lists:vsf-qa',
-            'view_published_products:vsf-qa',
-            'view_stores:vsf-qa'
+            'manage_project:vsf-ct-dev'
           ]
         },
         currency: 'USD',
@@ -27,25 +19,3 @@ module.exports = {
     }
   }
 };
-
-// module.exports = {
-//   integrations: {
-//     ct: {
-//       location: '@vue-storefront/commercetools-api/server',
-//       configuration: {
-//         api: {
-//           uri: 'https://api.commercetools.com/vsf-ct-dev/graphql',
-//           authHost: 'https://auth.sphere.io',
-//           projectKey: 'vsf-ct-dev',
-//           clientId: 'kuFT95wdTP4uH_hVOKjqfGEo',
-//           clientSecret: 'tklIDic86mgWrFy0oBHRQQmwX7ZC5wIP',
-//           scopes: [
-//             'manage_project:vsf-ct-dev',
-//           ]
-//         },
-//         currency: 'USD',
-//         country: 'US'
-//       }
-//     }
-//   }
-// };
