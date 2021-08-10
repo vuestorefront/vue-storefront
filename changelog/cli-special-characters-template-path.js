cedf8881b48eb3af5dec2d659f6f820bc081c9fc
danieljaywhy <87324619+danieljaywhy@users.noreply.github.com>
2021-08-10 13:50:37 +0100

    Added Changelog file.

---
+++ changelog/cli-special-characters-template-path.js
@@ -0,0 +1,15 @@
+module.exports = {
+  description: 'Updates CLI init command',
+  link: '',
+  isBreaking: false,
+  breakingChanges: [
+    {
+      module: 'CLI',
+      before: 'Fails to clone custom repo when brackets are in the template path',
+      after: 'how it is',
+      comment: ''
+    }
+  ],
+  author: 'Daniel Yeoman',
+  linkToGitHubAccount: 'https://github.com/danieljaywhy'
+};
