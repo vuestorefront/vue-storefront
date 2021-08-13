f203c701264a28f147435aa0d4bf829f40ca4f80
ljedrasik <ljedrasik@vuestorefront.io>
2021-08-13 16:00:35 +0200

    test

--- packages/commercetools/api-client/src/index.ts
+++ packages/commercetools/api-client/src/index.ts
@@ -1,7 +1,9 @@
+import * as cartActions from './helpers/cart/actions';
+
 export * from './fragments';
 export * from './types/Api';
 export * from './types/GraphQL';
 export * from './types/setup';
 export { isAnonymousSession, isUserSession } from './helpers/utils';
 export { createErrorHandler } from './helpers/commercetoolsLink/graphqlError';
-export * as cartActions from './helpers/cart/actions';
+export { cartActions };
--- packages/core/docs/.vuepress/config.js
+++ packages/core/docs/.vuepress/config.js
@@ -119,7 +119,7 @@ module.exports = {
 					title: 'API Client',
 					collapsable: false,
 					children: [
-						['/commercetools/api-client-reference', 'Methods reference']
+						['/commercetools/api-reference', 'Methods reference']
 					]
 				},
 				{
