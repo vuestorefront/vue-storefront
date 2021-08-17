eb9888bb36e35d690e3012fed1dad1b79ecced96
Filip Sobol <sobol.filip@gmail.com>
2021-08-17 11:22:17 +0200

    Add change log

---
+++ packages/core/docs/changelog/6198.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Fix `@vue-storefront/cache` package ',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6198',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Filip Sobol',
+  linkToGitHubAccount: 'https://github.com/filipsobol'
+};
