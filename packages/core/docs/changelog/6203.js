9c048b4ea4c2031d17309534f5bb53c65f9898ff
Filip Sobol <sobol.filip@gmail.com>
2021-08-18 15:23:12 +0200

    Add changelog

---
+++ packages/core/docs/changelog/6203.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Revert changes to Webpack configuration and Google font loading',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6203',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Filip Sobol',
+  linkToGitHubAccount: 'https://github.com/filipsobol'
+};
