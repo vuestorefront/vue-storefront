3deca86020e8d58fe34ed1586354eb732fef5c67
Alef Barbeli <alefbarbeli@gmail.com>
2021-08-19 10:49:42 -0300

    fix(commercetools): updated getInventory API

--- packages/commercetools/api-client/src/fragments/inventory.ts
+++ packages/commercetools/api-client/src/fragments/inventory.ts
@@ -13,7 +13,7 @@ export const InventoryEntryFragment = `
     availableQuantity
     restockableInDays
     expectedDelivery
-    supplyChannels {
+    supplyChannel {
       id
     }
     createdBy {
