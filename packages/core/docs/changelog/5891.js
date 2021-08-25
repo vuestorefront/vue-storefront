fe09f8213716bb1ac22fd22d3fafe21ae3346cde
ljedrasik <ljedrasik@vuestorefront.io>
2021-08-25 09:05:14 +0200

    remove unused checkoutGetters

---
+++ packages/core/docs/changelog/5891.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Remove unused `checkoutGetters`.',
+  link: 'https://github.com/vuestorefront/vue-storefront/issues/5891',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Łukasz Jędrasik',
+  linkToGitHubAccount: 'https://github.com/lukaszjedrasik'
+};
