5863a11c33c7eff689a27465d6d7e2671485f8e5
Justyna <jgieracka@vuestorefront.io>
2021-08-26 13:51:16 +0200

    updated changelog

---
+++ packages/core/docs/commercetools/changelog/6240.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: '',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6240',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Justyna Gieracka',
+  linkToGitHubAccount: 'https://github.com/justyna-13'
+};
