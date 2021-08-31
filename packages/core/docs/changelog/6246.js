2dd6fa1e6ba86264e7bcb488ada80c4bd9d2d709
ljedrasik <ljedrasik@vuestorefront.io>
2021-08-31 08:18:36 +0200

    update changelog

---
+++ packages/core/docs/changelog/6246.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Remove "download all" button from the order list in my account',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6246',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Łukasz Jędrasik',
+  linkToGitHubAccount: 'https://github.com/lukaszjedrasik'
+};
