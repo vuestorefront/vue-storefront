00d1a29aa93b02972d42ee9626ea2913fa9b91b5
Jason Wolf <jaydubb12@gmail.com>
2021-09-02 09:40:31 -0400

    Bug/6099 missing dependency (#6100)
    * bug/6089-incorrect-method --> resolved
    
    * bug/6089-incorrect-method --> commit changelog
    
    * bug/6099-missing-dependency
    
    * bug/6099-missing-dependency -changelog
    
    * Delete 6089.js
    
    Co-authored-by: jason.wolf <jason.wolf@borngroup.com>
    Co-authored-by: Heitor Ramon Ribeiro <heitor.ramon@gmail.com>

---
+++ packages/core/docs/changelog/6099.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Remediated missing dependency`',
+  link: 'https://github.com/vuestorefront/vue-storefront/issues/6099',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'jaydubb12',
+  linkToGitHubAccount: 'https://github.com/jaydubb12/vue-storefront'
+};
