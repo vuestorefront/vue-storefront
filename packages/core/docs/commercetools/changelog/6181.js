aa74a74b5e8bccf0dc937ec9759470824fe3f358
Razz21 <razzrazz@wp.pl>
2021-09-02 12:59:58 +0200

    chore: add changelog

---
+++ packages/core/docs/commercetools/changelog/6181.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Fixes useProduct search by skus',
+  link: 'https://github.com/vuestorefront/vue-storefront/issues/6181',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Andrzej Kurek',
+  linkToGitHubAccount: 'https://github.com/Razz21'
+};
