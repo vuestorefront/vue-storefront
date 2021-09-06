5786b405fcf491e35f1087fbe5f4cd09ca61bd0a
Andrzej K <32792205+Razz21@users.noreply.github.com>
2021-09-06 10:31:45 +0200

    [OS-71]: Product :: Color filter param set to object, not color value (#6251)
    * fix: PDP color filter value
    
    * chore: changelog

---
+++ packages/core/docs/changelog/5787.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Color filter param set correct color value',
+  link: 'https://github.com/vuestorefront/vue-storefront/issues/5787',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Andrzej Kurek',
+  linkToGitHubAccount: 'https://github.com/Razz21'
+};
