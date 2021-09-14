2a135d5e7b57c80fb805a66d9f1c04a7155c8574
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-09-14 15:43:26 +0200

    feat: update changelog

---
+++ packages/core/docs/commercetools/changelog/6307.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Make VSF PWA again',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6307',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Baroshem',
+  linkToGitHubAccount: 'https://github.com/Baroshem'
+};
