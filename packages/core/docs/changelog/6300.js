46762a541833c96ed030cf86e1f85409ce664a87
Alef Barbeli <alefbarbeli@gmail.com>
2021-09-16 09:32:16 -0300

    feat(commercetools): added changelog

---
+++ packages/core/docs/changelog/6300.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Added useInventory composable',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6300/',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Alef Barbeli',
+  linkToGitHubAccount: 'https://github.com/alefbarbeli'
+};
