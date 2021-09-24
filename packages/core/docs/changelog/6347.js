b52365f4ed0a1200bf197f99c910eab59f3d5eab
David Golodetsky <david.golodetsky@lovecrafts.com>
2021-09-24 18:13:50 +0300

    6347 productType field on product object

---
+++ packages/core/docs/changelog/6347.js
@@ -0,0 +1,7 @@
+module.exports = {
+  description: "getProduct custom query - productType doesn't get mapped to product response in enhanceProduct helper",
+  link: 'https://github.com/vuestorefront/vue-storefront/issues/6347',
+  isBreaking: false,
+  author: 'David Golodetsky',
+  linkToGitHubAccount: 'https://github.com/DavidGolodetsky',
+};
