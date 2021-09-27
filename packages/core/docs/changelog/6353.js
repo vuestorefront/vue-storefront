6a6dd7f69138c3cea9b647b712cbce050c398922
Łukasz <lsliwaradioluz@gmail.com>
2021-09-27 11:02:57 +0200

    Add changelog

---
+++ packages/core/docs/changelog/6353.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Cache package invalidation bug',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6353',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Łukasz Śliwa',
+  linkToGitHubAccount: 'https://github.com/lsliwaradioluz'
+};
