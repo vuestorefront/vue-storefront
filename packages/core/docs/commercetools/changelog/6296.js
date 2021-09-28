bbde2890248e7047fda5c34654ea7c915347f076
Marcin Sulowski <msulowski@vuestorefront.io>
2021-09-28 20:22:51 +0200

    fix(OS-169): add changelog

---
+++ packages/core/docs/commercetools/changelog/6296.js
@@ -0,0 +1,7 @@
+module.exports = {
+  description: 'pass cookies obj with cookie names to mapI18nSettings',
+  link: 'https://github.com/vuestorefront/vue-storefront/issues/6296',
+  isBreaking: false,
+  author: 'Marcin Sulowski',
+  linkToGitHubAccount: 'https://github.com/MarcinSulowski'
+};
