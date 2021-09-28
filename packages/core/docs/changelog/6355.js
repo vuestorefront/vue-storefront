ba5f58d61375392ad1a6a0d456a5b4a1ad11a991
lsliwaradioluz <39009379+lsliwaradioluz@users.noreply.github.com>
2021-09-28 09:05:09 +0200

    fix(cache): cache package invalidation bug (#6355)
    * Send response in cache invalidation request handler
    
    * Add changelog

---
+++ packages/core/docs/changelog/6355.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'fix(cache): cache package invalidation bug',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6355',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Łukasz Śliwa',
+  linkToGitHubAccount: 'https://github.com/lsliwaradioluz'
+};
