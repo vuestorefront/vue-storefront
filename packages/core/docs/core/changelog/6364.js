bfc77428c972861ae0cf438051c67b22aec537fe
Dawid Ziobro <dziobro@vuestorefront.io>
2021-09-29 11:48:12 +0200

    fix(6364): removed more action button that does nothing

---
+++ packages/core/docs/core/changelog/6364.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Removed "more actions" button that does nothing',
+  link: 'https://github.com/vuestorefront/vue-storefront/issues/6364',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Dawid Ziobro',
+  linkToGitHubAccount: 'https://github.com/dawid-ziobro'
+};
