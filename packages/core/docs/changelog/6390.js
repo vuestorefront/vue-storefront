7586382ba317fd01680d7aac864965d5b053d8e5
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-09-30 08:23:38 +0200

    perf: update changelog

---
+++ packages/core/docs/changelog/6390.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Remove unused component imports',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6390',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Baroshem',
+  linkToGitHubAccount: 'https://github.com/Baroshem'
+};
