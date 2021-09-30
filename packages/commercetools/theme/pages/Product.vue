ab75feedc1f803ae92baf480a8b4a4e024cb46cf
Alef Barbeli <alefbarbeli@gmail.com>
2021-09-30 11:12:11 -0300

    fix(commercetools): reselect shipping delivery (#6276)
    * fix(commercetools): reselect shipping delivery
    
    * fix(commercetools): removed mock delivery content
    
    * feat(commercetools): added changelog
    
    * fix(commercetools): updated after CR
    
    * fix(commercetools): added missing translations

--- packages/commercetools/theme/pages/Product.vue
+++ packages/commercetools/theme/pages/Product.vue
@@ -75,30 +75,45 @@
               @click="updateFilter({ color: color.value })"
             />
           </div>
-          <SfTabs :open-tab="1" class="product__tabs">
-            <SfTab title="Shipping">
-              Shipping
-            </SfTab>
-            <SfTab
-              v-if="channels.length > 0"
-              title="Click and Collect"
+
+          <div
+            class="product__delivery"
+            v-if="channels.length > 0"
+          >
+            <SfRadio
+              v-e2e="'delivery-option'"
+              name="Delivery"
+              :label="$t('Delivery')"
+              value="delivery"
+              :selected="selectedDelivery"
+              @input="setSelectedDelivery('delivery')"
+            />
+            <SfRadio
+              v-e2e="'click-collect-option'"
+              name="Click & Collect"
+              :label="$t('Pickup in the store')"
+              :details="$t('Free')"
+              value="collect"
+              :selected="selectedDelivery"
+              @input="setSelectedDelivery('collect')"
+            />
+            <SfSelect
+              v-if="selectedDelivery === 'collect'"
+              v-e2e="'channel-select'"
+              v-model="channelId"
+              :label="$t('Select Channel')"
+              class="sf-select--underlined product__select-size"
             >
-              <SfSelect
-                v-e2e="'channel-select'"
-                v-model="channelId"
-                label="Select Channel"
-                class="sf-select--underlined product__select-size"
+              <SfSelectOption
+                v-for="{ channel } in channels"
+                :key="channel.id"
+                :value="channel.id"
               >
-                <SfSelectOption
-                  v-for="{ channel } in channels"
-                  :key="channel.id"
-                  :value="channel.id"
-                >
-                  {{channel.name}}
-                </SfSelectOption>
-              </SfSelect>
-            </SfTab>
-          </SfTabs>
+                {{channel.name}}
+              </SfSelectOption>
+            </SfSelect>
+          </div>
+
           <SfAddToCart
             v-e2e="'product_add-to-cart'"
             :stock="stock"
@@ -187,6 +202,7 @@ import {
   SfHeading,
   SfPrice,
   SfRating,
+  SfRadio,
   SfSelect,
   SfAddToCart,
   SfTabs,
@@ -250,8 +266,11 @@ export default {
       const productChannels = product.value?.availability?.channels?.results ?? [];
       return productChannels;
     });
+    const selectedDelivery = ref(null);
+    const setSelectedDelivery = option => selectedDelivery.value = option;
 
     const selectedChannel = computed(() => {
+      if (selectedDelivery.value !== 'collect') return null;
       const selected = channels.value.find((item) => (item.channel.id === channelId.value));
 
       return (selected?.channel?.roles && selected?.channel?.id) ? {
@@ -309,7 +328,9 @@ export default {
       channels,
       channelId,
       selectedChannel,
-      selectedStore
+      selectedStore,
+      selectedDelivery,
+      setSelectedDelivery
     };
   },
   components: {
@@ -319,6 +340,7 @@ export default {
     SfHeading,
     SfPrice,
     SfRating,
+    SfRadio,
     SfSelect,
     SfAddToCart,
     SfTabs,
