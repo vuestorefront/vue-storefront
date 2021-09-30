e879d6c3e0558b16554f476f2b26a7512f699573
Jakub Andrzejewski <37120330+Baroshem@users.noreply.github.com>
2021-09-30 12:26:21 +0200

    Perf/en 456 (#6397)
    * perf: use @nuxtjs/google-fonts (#6373)
    
    * perf: use @nuxtjs/google-fonts
    
    * perf: add changelog
    
    * perf: add extractCSS (#6377)
    
    * perf: add extractCSS
    
    * perf: update changelog
    
    * perf: add min-height to improve CLS
    
    * perf: fix link to issue

---
+++ packages/core/docs/changelog/6392.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'add min-height to improve CLS',
+  link: 'https://github.com/vuestorefront/vue-storefront/issues/6392',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Baroshem',
+  linkToGitHubAccount: 'https://github.com/Baroshem'
+};
