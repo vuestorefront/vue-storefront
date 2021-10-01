d93376a543a6c342327ba307baad20927112beaf
Filip Sobol <sobol.filip@gmail.com>
2021-10-01 16:06:20 +0200

    Add changelog

---
+++ packages/core/docs/commercetools/changelog/6404.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Update GraphQL API types',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6404',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Filip Sobol',
+  linkToGitHubAccount: 'https://github.com/filipsobol'
+};
