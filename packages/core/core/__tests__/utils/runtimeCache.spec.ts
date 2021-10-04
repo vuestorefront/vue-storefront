2eec77c30bc5c1868427f4bb0bd31d597087e96d
Łukasz <lsliwaradioluz@gmail.com>
2021-10-04 22:50:31 +0200

    Add setCacheTimestamp tests

---
+++ packages/core/core/__tests__/utils/runtimeCache.spec.ts
@@ -0,0 +1,51 @@
+import { setCacheTimestamp } from '../../src/utils/runtimeCache';
+import { useVSFContext, sharedRef } from '../../src/utils';
+
+describe('runtimeCacheHelpers', () => {
+  describe('>setCacheTimestamp', () => {
+    let $sharedRefsMap;
+    let mockedDateValue;
+
+    const mockVSFContext = () => {
+      $sharedRefsMap = new Map();
+      (useVSFContext as any).mockImplementation(() => ({ $sharedRefsMap }));
+    };
+    const mockDateNow = () => {
+      mockedDateValue = 12345;
+      Date.now = jest.fn(() => mockedDateValue);
+    };
+    const mockSharedRef = () => {
+      (sharedRef as any).mockImplementation((value, key) => {
+        $sharedRefsMap.set(key, value);
+        return value;
+      });
+    };
+
+    beforeEach(() => {
+      jest.clearAllMocks();
+      mockVSFContext();
+      mockDateNow();
+      mockSharedRef();
+    });
+
+    it('sets the new timestamp equal to Date.now() on the $sharedRefsMap', () => {
+      const key = 'new-timestamp';
+      setCacheTimestamp(key);
+      expect($sharedRefsMap.get(key)).toBe(mockedDateValue);
+    });
+
+    it('returns the existing timestamp', () => {
+      const key = 'existing-timestamp';
+      const existingTimestamp = 123;
+      $sharedRefsMap.set(key, existingTimestamp);
+      const timestamp = setCacheTimestamp(key);
+      expect(timestamp).toBe(existingTimestamp);
+    });
+
+    it('returns the new timestamp', () => {
+      const key = 'new-timestamp';
+      const timestamp = setCacheTimestamp(key);
+      expect(timestamp).toBe(mockedDateValue);
+    });
+  });
+});
