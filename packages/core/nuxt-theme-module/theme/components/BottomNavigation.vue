0ed0ac2dd21fdb818458c3c8c7e4ea8cbea12235
daaf <44862757+dawid-ziobro@users.noreply.github.com>
2021-10-05 11:24:13 +0200

    fix(os-179): added badge for cart on mobiles (#6418)

--- packages/core/nuxt-theme-module/theme/components/BottomNavigation.vue
+++ packages/core/nuxt-theme-module/theme/components/BottomNavigation.vue
@@ -16,13 +16,14 @@
       @click="toggleCartSidebar"
     >
       <template #icon>
-        <SfCircleIcon aria-label="Add to cart">
+        <SfCircleIcon class="cart-button" aria-label="Add to cart">
           <SfIcon
             icon="add_to_cart"
             color="white"
             size="25px"
             :style="{margin: '0 0 0 -2px'}"
           />
+          <SfBadge v-if="cartTotalItems" class="sf-badge--number cart-badge">{{cartTotalItems}}</SfBadge>
         </SfCircleIcon>
       </template>
     </SfBottomNavigationItem>
@@ -30,19 +31,22 @@
 </template>
 
 <script>
-import { SfBottomNavigation, SfIcon, SfCircleIcon } from '@storefront-ui/vue';
+import { SfBottomNavigation, SfIcon, SfCircleIcon, SfBadge } from '@storefront-ui/vue';
 import { useUiState } from '~/composables';
-import { useUser } from '<%= options.generate.replace.composables %>';
+import { useUser, useCart, cartGetters } from '<%= options.generate.replace.composables %>';
+import { computed } from '@vue/composition-api';
 
 export default {
   components: {
     SfBottomNavigation,
     SfIcon,
-    SfCircleIcon
+    SfCircleIcon,
+    SfBadge
   },
   setup(props, { root }) {
     const { toggleCartSidebar, toggleWishlistSidebar, toggleLoginModal, toggleMobileMenu, isMobileMenuOpen } = useUiState();
     const { isAuthenticated } = useUser();
+    const { cart } = useCart();
 
     const handleAccountClick = async () => {
       if (isAuthenticated.value) {
@@ -56,11 +60,18 @@ export default {
       root.$router.push('/');
     };
 
+    const cartTotalItems = computed(() => {
+      const count = cartGetters.getTotalItems(cart.value);
+
+      return count ? count.toString() : null;
+    });
+
     return {
       isMobileMenuOpen,
       toggleWishlistSidebar,
       toggleCartSidebar,
       toggleMobileMenu,
+      cartTotalItems,
       handleAccountClick,
       handleHomeClick
     };
@@ -70,5 +81,13 @@ export default {
 <style lang="scss" scoped>
 .navigation-bottom {
   --bottom-navigation-z-index: 3;
+}
+.cart-button {
+  position: relative;
+}
+.cart-badge {
+  position: absolute;
+  top: 0;
+  right: 0;
 }
 </style>
