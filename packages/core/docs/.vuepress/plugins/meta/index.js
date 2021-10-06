0a5cee9b1321be93fdd1c4442167b3c00f1e05f7
Filip Sobol <sobol.filip@gmail.com>
2021-10-06 10:54:09 +0200

    Update

--- packages/core/docs/.vuepress/plugins/meta/index.js
+++ packages/core/docs/.vuepress/plugins/meta/index.js
@@ -17,7 +17,7 @@ const VSF_LOGO_BUFFER = new Buffer
  /**
   * Helper function to fill PAGES array
   */
-const pageToContent = ({ outDir, title, fileName }) => {
+function pageToContent({ outDir, title, fileName }) {
   return {
     title,
     image: `data:image/jpeg;base64,${ VSF_LOGO_BUFFER }`,
@@ -25,7 +25,7 @@ const pageToContent = ({ outDir, title, fileName }) => {
     type: 'jpeg',
     selector: '.banner'
   };
-};
+}
 
 module.exports = (options, context) => {
   if (!context.isProd) {
@@ -39,30 +39,66 @@ module.exports = (options, context) => {
       const outDir = resolve(context.outDir, './og-images');
 
       const fallback = pageToContent({
+        outDir,
         title: 'Vue Storefront documentation',
         fileName: FALLBACK_PATH
       });
 
       const pages = context.pages
         .filter(({ title }) => Boolean(title))
-        .map(page => {
+        .map(page => pageToContent({
+          outDir,
+          title: page.title,
+          fileName: page.key
+        }));
 
-          console.log(page.title)
-
-          return pageToContent({
-            outDir,
-            title: page.title,
-            fileName: page.key
-          });
-        });
-  
       if (!existsSync(outDir)) {
         mkdirSync(outDir, { recursive: true });
       }
-  
+
       await nodeHtmlToImage({
         html: readFileSync(resolve(__dirname, './template.html'), 'utf8'),
-        content: pages.concat(fallback)
+        content: pages.concat(fallback),
+        puppeteerArgs: {
+          dumpio: true,
+          args: [
+            '--autoplay-policy=user-gesture-required',
+            '--disable-background-networking',
+            '--disable-background-timer-throttling',
+            '--disable-backgrounding-occluded-windows',
+            '--disable-breakpad',
+            '--disable-client-side-phishing-detection',
+            '--disable-component-update',
+            '--disable-default-apps',
+            '--disable-dev-shm-usage',
+            '--disable-domain-reliability',
+            '--disable-extensions',
+            '--disable-features=AudioServiceOutOfProcess',
+            '--disable-hang-monitor',
+            '--disable-ipc-flooding-protection',
+            '--disable-notifications',
+            '--disable-offer-store-unmasked-wallet-cards',
+            '--disable-popup-blocking',
+            '--disable-print-preview',
+            '--disable-prompt-on-repost',
+            '--disable-renderer-backgrounding',
+            '--disable-setuid-sandbox',
+            '--disable-speech-api',
+            '--disable-sync',
+            '--hide-scrollbars',
+            '--ignore-gpu-blacklist',
+            '--metrics-recording-only',
+            '--mute-audio',
+            '--no-default-browser-check',
+            '--no-first-run',
+            '--no-pings',
+            '--no-sandbox',
+            '--no-zygote',
+            '--password-store=basic',
+            '--use-gl=swiftshader',
+            '--use-mock-keychain',
+          ]
+        }
       });
     },
   
