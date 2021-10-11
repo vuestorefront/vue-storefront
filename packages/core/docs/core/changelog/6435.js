08b8582e5df7b8298d32e9471869164c3f64d6b8
ljedrasik <ljedrasik@vuestorefront.io>
2021-10-11 14:48:40 +0200

    chore: add changelog

--- packages/core/docs/core/changelog/6435.js
+++ packages/core/docs/core/changelog/6435.js
@@ -1,6 +1,6 @@
 module.exports = {
   description: 'Change message when the customer searches for a phrase that has no results',
-  link: 'https://github.com/vuestorefront/vue-storefront/pull/6435',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6436',
   isBreaking: false,
   breakingChanges: [],
   author: 'Łukasz Jędrasik',
