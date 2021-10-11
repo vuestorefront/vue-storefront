8f7c5d49eb8531c9b212166087f9234fe5750250
ljedrasik <ljedrasik@vuestorefront.io>
2021-10-11 14:47:31 +0200

    fix: misleading message for no search results

--- packages/commercetools/theme/lang/en.js
+++ packages/commercetools/theme/lang/en.js
@@ -114,6 +114,7 @@ export default {
   'Your bag is empty': 'Your bag is empty',
   'Cancel': 'Cancel',
   'See all results': 'See all results',
+  'We haven’t found any results for given phrase': 'We haven’t found any results for given phrase',
   'You haven’t searched for items yet': 'You haven’t searched for items yet.',
   'Let’s start now – we’ll help you': 'Let’s start now – we’ll help you.',
   'Search results': 'Search results',
