9fc8aafd37ccc17e329fb2efcfc1c34ccb87464a
Łukasz Jędrasik <ljedrasik@vuestorefront.io>
2021-10-12 15:55:24 +0200

    fix: remove LoyaltyCard and MyReviews (#6439)
    * fix: remove LoyaltyCard and MyReviews
    
    * chore: add changelog

---
+++ packages/core/docs/core/changelog/6439.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Remove LoyaltyCard and MyReviews',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6439',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Łukasz Jędrasik',
+  linkToGitHubAccount: 'https://github.com/lukaszjedrasik'
+};
