63552828bf3a000c5714b46a81c9d8858864dd59
Filip Sobol <filipsobol@users.noreply.github.com>
2021-10-13 09:43:01 +0200

    docs: update logos (#6445)

--- packages/core/docs/.vuepress/config.js
+++ packages/core/docs/.vuepress/config.js
@@ -95,7 +95,7 @@ module.exports = {
     docsDir: 'packages/core/docs',
     docsBranch: 'release/next',
     editLinkText: 'Edit this page',
-    logo: 'https://camo.githubusercontent.com/48c886ac0703e3a46bc0ec963e20f126337229fc/68747470733a2f2f643968687267346d6e767a6f772e636c6f756466726f6e742e6e65742f7777772e76756573746f726566726f6e742e696f2f32383062313964302d6c6f676f2d76735f3062793032633062793032633030303030302e6a7067',
+    logo: '/vsf-full.svg',
     sidebarDepth: 0,
     nav: [
       { text: 'Home', link: '/' },
