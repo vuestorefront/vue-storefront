2c7354efbd13a548fa0d78713f9c9f19b3060e97
Dawid Ziobro <dziobro@vuestorefront.io>
2021-10-13 16:28:01 +0200

    fix(en-440): fix access token cookies override

--- packages/commercetools/composables/nuxt/accessToken.js
+++ packages/commercetools/composables/nuxt/accessToken.js
@@ -1,6 +1,6 @@
 import { CT_COOKIE_NAME } from '@vue-storefront/commercetools-api';
 
-const applyCookies = (token) => `${CT_COOKIE_NAME}=${encodeURIComponent(JSON.stringify(token))}`;
+const applyCookies = (cookies, token) => `${cookies}; ${CT_COOKIE_NAME}=${encodeURIComponent(JSON.stringify(token))}`;
 
 export default async function accessTokenPlugin({ app }) {
   const isServer = typeof window === 'undefined';
@@ -13,6 +13,6 @@ export default async function accessTokenPlugin({ app }) {
      * For this reason, it's required to add access token to axios during SSR, so that new
      * access token is not requested every time we call an API.
      */
-    app.$vsf.$ct.config.axios.headers.cookie = applyCookies(token);
+    app.$vsf.$ct.config.axios.headers.cookie = applyCookies(app.$vsf.$ct.config.axios.headers.cookie, token);
   }
 }
