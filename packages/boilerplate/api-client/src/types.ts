6faa694d7096d15d19fd2cb9a12bcb1bebc96992
Dawid Ziobro <dziobro@vuestorefront.io>
2021-10-14 14:51:57 +0200

    Revert "fix(bp): added useStore to boilerplate"
    This reverts commit e8b94a472e39c5fc81dcebb6fe53e5acfc6bf31f.

--- packages/boilerplate/api-client/src/types.ts
+++ packages/boilerplate/api-client/src/types.ts
@@ -55,5 +55,3 @@ export type ShippingProvider = TODO;
 export type Wishlist = TODO;
 
 export type WishlistItem = TODO;
-
-export type StoresData = TODO;
--- packages/boilerplate/composables/src/index.ts
+++ packages/boilerplate/composables/src/index.ts
@@ -10,7 +10,6 @@ export { useProduct } from './useProduct';
 export { useReview } from './useReview';
 export { useShipping } from './useShipping';
 export { useShippingProvider } from './useShippingProvider';
-export { useStore } from './useStore';
 export { useUser } from './useUser';
 export { useUserBilling } from './useUserBilling';
 export { useUserOrder } from './useUserOrder';
