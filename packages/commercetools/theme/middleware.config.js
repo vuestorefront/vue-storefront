8f59800f3189e17352f37ba90c54cc5d514ed379
Filip Sobol <filipsobol@users.noreply.github.com>
2021-10-15 11:55:20 +0200

    chore(commercetools): use API client from new commercetools project (#6458)

--- packages/commercetools/theme/middleware.config.js
+++ packages/commercetools/theme/middleware.config.js
@@ -5,28 +5,28 @@ module.exports = {
       location: '@vue-storefront/commercetools-api/server',
       configuration: {
         api: {
-          uri: 'https://api.commercetools.com/vsf-ct-dev/graphql',
+          uri: 'https://api.commercetools.com/vsf-generic-ent-demo/graphql',
           authHost: 'https://auth.sphere.io',
-          projectKey: 'vsf-ct-dev',
-          clientId: 'FGUpRp88xDpaDEDpVfbfs0Q-',
-          clientSecret: 'DvQvj3ccl-qwqS6qFnO-n0PUmjb8ok3G',
+          projectKey: 'vsf-generic-ent-demo',
+          clientId: 'w3HqE0p55rhxCWinIjFiq26L',
+          clientSecret: '8ThyrROTeD73Uc9rzfiwSgNpVIku8bOE',
           scopes: [
-            'create_anonymous_token:vsf-ct-dev',
-            'manage_my_profile:vsf-ct-dev',
-            'view_categories:vsf-ct-dev',
-            'manage_my_payments:vsf-ct-dev',
-            'manage_my_orders:vsf-ct-dev',
-            'manage_my_shopping_lists:vsf-ct-dev',
-            'view_published_products:vsf-ct-dev',
-            'view_stores:vsf-ct-dev'
+            'create_anonymous_token:vsf-generic-ent-demo',
+            'view_categories:vsf-generic-ent-demo',
+            'view_published_products:vsf-generic-ent-demo',
+            'view_stores:vsf-generic-ent-demo',
+            'manage_my_profile:vsf-generic-ent-demo',
+            'manage_my_orders:vsf-generic-ent-demo',
+            'manage_my_payments:vsf-generic-ent-demo',
+            'manage_my_shopping_lists:vsf-generic-ent-demo'
           ]
         },
         serverApi: {
-          clientId: 'XPVdGFHqZwAaR2rQQEu0cXU-',
-          clientSecret: 'bpDi7aApbmeQjSnCJT_KL-YymzEjxrUq',
+          clientId: 'y-ia9E9gne-uybwVa9Lqzqfn',
+          clientSecret: 'sX5T3-5zF3ieIoVyvtDDnl3cjxTXNTDs',
           scopes: [
-            'manage_customers:vsf-ct-dev',
-            'manage_products:vsf-ct-dev'
+            'manage_customers:vsf-generic-ent-demo',
+            'manage_products:vsf-generic-ent-demo'
           ]
         },
         currency: 'USD',
