3325a61df8ea2c3ac1e1779b83a6e349fdc44071
Anton Sylka <anton.sylka@gorbunov.studio>
2021-10-21 11:03:30 +0800

    Refs #24743: Move product campaign discount calculations to Promotion Platform store getters

--- src/modules/promotion-platform/store/getters.ts
+++ src/modules/promotion-platform/store/getters.ts
@@ -9,5 +9,26 @@ export const getters: GetterTree<PromotionPlatformState, any> = {
   },
   campaignToken (state): string | undefined {
     return state.campaignToken;
+  },
+  getProductCampaignDiscount (state): (product: any, format: boolean) => string | number | undefined {
+    return (product, format = true) => {
+      const campaignContent = state.campaignContent;
+
+      if (!campaignContent || !campaignContent.discountsContent) {
+        return;
+      }
+
+      const discount = campaignContent.discountsContent[product.id];
+
+      if (!discount) {
+        return format ? '' : 0;
+      }
+
+      if (format) {
+        return discount;
+      }
+
+      return Number.parseInt(discount.split('$')[1], 10);
+    }
   }
 }
