16ba02569466dc26a80271179631dee1b19be703
ljedrasik <ljedrasik@vuestorefront.io>
2021-10-26 12:45:11 +0200

    fix(ct): redirect to localized My Account page

--- packages/commercetools/theme/components/AppHeader.vue
+++ packages/commercetools/theme/components/AppHeader.vue
@@ -138,7 +138,7 @@ export default {
     HeaderNavigation
   },
   directives: { clickOutside },
-  setup() {
+  setup(_, { root }) {
     const router = useRouter();
     const { toggleCartSidebar, toggleWishlistSidebar, toggleLoginModal, isMobileMenuOpen } = useUiState();
     const { setTermForUrl, getFacetsFromURL } = useUiHelpers();
@@ -159,7 +159,8 @@ export default {
 
     const handleAccountClick = async () => {
       if (isAuthenticated.value) {
-        return router.push('/my-account');
+        const localeAccountPath = root.localePath({ name: 'my-account' });
+        return router.push(localeAccountPath);
       }
 
       toggleLoginModal();
