fb524c71ca51240cac96eca626cfb68433814f53
Łukasz <lsliwaradioluz@gmail.com>
2021-10-27 15:37:30 +0200

    Add changelog

---
+++ packages/core/docs/core/changelog/6491.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Update `vsf-locale` cookie client-side on language switch',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6491',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Łukasz Śliwa',
+  linkToGitHubAccount: 'https://github.com/lsliwaradioluz'
+};
