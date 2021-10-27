e1990638fe16ed8c537b1a9c47d30fa43ee4a7bc
Anton Sylka <anton.sylka@gorbunov.studio>
2021-10-27 17:41:16 +0800

    Refs #25346: PromotionPlatform - respect bundle products in discount price calculating

--- src/modules/promotion-platform/helpers/initEventBusListeners.ts
+++ src/modules/promotion-platform/helpers/initEventBusListeners.ts
@@ -1,18 +1,26 @@
+import config from 'config'
 import { Store } from 'vuex'
 import EventBus from '@vue-storefront/core/compatibility/plugins/event-bus'
+import { isBundleProduct } from '@vue-storefront/core/modules/catalog/helpers';
 import RootState from '@vue-storefront/core/types/RootState'
 
 import UpdateProductDiscountPriceEventData, { UPDATE_PRODUCT_DISCOUNT_PRICE_DATA_EVENT_ID } from 'src/modules/shared/types/update-product-discount-price.event';
 
+import getBundleProductDiscountPrice from './bundle-product-discount-price';
+
 export default function initEventBusListeners (store: Store<RootState>) {
   EventBus.$on(UPDATE_PRODUCT_DISCOUNT_PRICE_DATA_EVENT_ID, (productPriceData: UpdateProductDiscountPriceEventData) => {
-    const discountedPrice = store.getters['promotionPlatform/getProductCampaignDiscountPrice'](
-      productPriceData.product
-    );
+    let discountedPrice;
 
-    if (discountedPrice === undefined) {
+    if (config.products.calculateBundlePriceByOptions && isBundleProduct(productPriceData.product)) {
+      discountedPrice = getBundleProductDiscountPrice(productPriceData.product, store);
+      productPriceData.value = discountedPrice;
       return;
     }
+
+    discountedPrice = store.getters['promotionPlatform/getProductCampaignDiscountPrice'](
+      productPriceData.product
+    );
 
     productPriceData.value = discountedPrice;
   })
