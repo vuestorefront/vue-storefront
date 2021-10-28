b97d5c7d982d77ecb07b7d488ac855d342fbaa5e
Rafal Dobrowolski <rafal.dobrowolski77@gmail.com>
2021-10-28 16:54:58 +0200

    feat(os-192): add unit tests for refresh token

--- packages/commercetools/api-client/__tests__/links/authLinks.spec.ts
+++ packages/commercetools/api-client/__tests__/links/authLinks.spec.ts
@@ -1,5 +1,5 @@
 import { handleBeforeAuth, handleAfterAuth } from '../../src/links/authLinks';
-import { createSdkHelpers } from '../../src/links/sdkHelpers';
+import * as sdkHelpers from '../../src/links/sdkHelpers';
 
 const getTokenProvider = (scope) => ({
   setTokenInfo: jest.fn().mockImplementation(() => {}),
@@ -7,13 +7,9 @@ const getTokenProvider = (scope) => ({
   invalidateTokenInfo: jest.fn().mockImplementation(() => {})
 });
 
-jest.mock('../../src/links/sdkHelpers', () => ({
-  createSdkHelpers: jest.fn()
-}));
-
 const auth = {
   onTokenChange: () => {},
-  onTokenRead: () => '',
+  onTokenRead: () => {},
   onTokenRemove: () => {},
   setTokenProvider: () => {},
   getTokenProvider: jest.fn()
@@ -21,6 +17,23 @@ const auth = {
 
 const configuration = {
   auth: auth
+};
+
+const api: any = {
+  authHost: 'https://auth.europe-west1.gcp.commercetools.com',
+  projectKey: 'vsf-generic-ent-demo',
+  clientId: 'w3HqE0p55rhxCWinIjFiq26L',
+  clientSecret: '8ThyrROTeD73Uc9rzfiwSgNpVIku8bOE',
+  scopes: [
+    'create_anonymous_token:vsf-generic-ent-demo',
+    'view_categories:vsf-generic-ent-demo',
+    'view_published_products:vsf-generic-ent-demo',
+    'view_stores:vsf-generic-ent-demo',
+    'manage_my_profile:vsf-generic-ent-demo',
+    'manage_my_orders:vsf-generic-ent-demo',
+    'manage_my_payments:vsf-generic-ent-demo',
+    'manage_my_shopping_lists:vsf-generic-ent-demo'
+  ]
 };
 
 describe('[commercetools-helpers] handleBeforeAuth', () => {
@@ -36,7 +49,7 @@ describe('[commercetools-helpers] handleBeforeAuth', () => {
       apolloReq: { operationName: '' }
     });
 
-    expect(currentToken).toMatchObject({ scope, access_token: 'ACCESS_TOKEN' });
+    expect(currentToken).toMatchObject({access_token: 'ACCESS_TOKEN' });
   });
 
   it('generates server access token for users related operations', async () => {
@@ -52,8 +65,8 @@ describe('[commercetools-helpers] handleBeforeAuth', () => {
 
   it('generates access token for guest on anonymous-session allowed operations', async () => {
     const scope = '';
-    const createSdkHelpersMock = createSdkHelpers as jest.Mock;
-    createSdkHelpersMock.mockImplementation(() => ({ tokenProvider: getTokenProvider(scope)}));
+    const spyCreateSdkHelpers = jest.spyOn(sdkHelpers, 'createSdkHelpers');
+    spyCreateSdkHelpers.mockReturnValue({ sdkAuth: null, tokenProvider: getTokenProvider(scope)});
     const currentToken = await handleBeforeAuth({
       configuration,
       apolloReq: { operationName: 'createCart' }
@@ -65,8 +78,8 @@ describe('[commercetools-helpers] handleBeforeAuth', () => {
   it('returns existing token for anonymous user', async () => {
     const scope = 'anonymous_id';
     auth.onTokenRead = () => 'ACCESS_TOKEN';
-    const createSdkHelpersMock = createSdkHelpers as jest.Mock;
-    createSdkHelpersMock.mockImplementation(() => ({ tokenProvider: getTokenProvider(scope)}));
+    const spyCreateSdkHelpers = jest.spyOn(sdkHelpers, 'createSdkHelpers');
+    spyCreateSdkHelpers.mockReturnValue({ sdkAuth: null, tokenProvider: getTokenProvider(scope)});
     const currentToken = await handleBeforeAuth({
       configuration,
       apolloReq: { operationName: 'customerSignMeIn' }
@@ -78,14 +91,62 @@ describe('[commercetools-helpers] handleBeforeAuth', () => {
   it('returns existing token for logged in user', async () => {
     const scope = 'customer_id';
     auth.onTokenRead = () => 'ACCESS_TOKEN';
-    const createSdkHelpersMock = createSdkHelpers as jest.Mock;
-    createSdkHelpersMock.mockImplementation(() => ({ tokenProvider: getTokenProvider(scope)}));
+    const spyCreateSdkHelpers = jest.spyOn(sdkHelpers, 'createSdkHelpers');
+    spyCreateSdkHelpers.mockReturnValue({ sdkAuth: null, tokenProvider: getTokenProvider(scope)});
     const currentToken = await handleBeforeAuth({
       configuration,
       apolloReq: { operationName: 'customerSignMeIn' }
     });
 
     expect(currentToken).toMatchObject({ scope, access_token: 'ACCESS_TOKEN' });
+  });
+
+  it('fetches new access token when user session is expired', async () => {
+    const scope = 'customer_id';
+    const expiredToken = {
+      access_token: 'om9UleqfwtjDCP6jg4hF9iCm69mxlXO3',
+      expires_in: 172800,
+      token_type: 'Bearer',
+      scope: 'view_categories:vsf-generic-ent-demo customer_id:26b9772d-0d22-4e5c-8fc3-bbc7a668ab86 manage_my_profile:vsf-generic-ent-demo manage_my_orders:vsf-generic-ent-demo view_published_products:vsf-generic-ent-demo create_anonymous_token:vsf-generic-ent-demo manage_my_payments:vsf-generic-ent-demo manage_my_shopping_lists:vsf-generic-ent-demo view_stores:vsf-generic-ent-demo',
+      refresh_token: 'vsf-generic-ent-demo:WPyhTfXtyl-R3DwDLT3LausqJKIejsQTOvNYDV8l9m4',
+      expires_at: 1666267200
+    };
+
+    auth.onTokenRead = () => expiredToken;
+    jest.spyOn(sdkHelpers, 'createSdkHelpers').mockRestore();
+
+    const currentToken = await handleBeforeAuth({
+      configuration: { api, auth },
+      apolloReq: { operationName: 'customerSignMeIn' }
+    });
+
+    const { scope: currentTokenScope } = currentToken;
+    expect(currentToken.access_token).not.toBe(expiredToken.access_token);
+    expect(currentTokenScope).toContain(scope);
+  });
+
+  it('fetches new access token when anonymous session is expired', async () => {
+    const scope = 'anonymous_id';
+    const expiredToken = {
+      access_token: 'ikKnHpgRHA5JEglaF2hedIqq9sKZ8s0h',
+      expires_in: 10800,
+      token_type: 'Bearer',
+      scope: 'manage_my_profile:vsf-generic-ent-demo manage_my_orders:vsf-generic-ent-demo view_published_products:vsf-generic-ent-demo create_anonymous_token:vsf-generic-ent-demo view_categories:vsf-generic-ent-demo anonymous_id:c17fa5a2-8f1a-4152-a07a-13e842f12855 manage_my_payments:vsf-generic-ent-demo manage_my_shopping_lists:vsf-generic-ent-demo view_stores:vsf-generic-ent-demo',
+      refresh_token: 'vsf-generic-ent-demo:XwbYnoyVaeMjcEG31SfOiaUJLjDTNi_j37qy29eQrEk',
+      expires_at: 1666267200
+    };
+
+    auth.onTokenRead = () => expiredToken;
+    jest.spyOn(sdkHelpers, 'createSdkHelpers').mockRestore();
+
+    const currentToken = await handleBeforeAuth({
+      configuration: { api, auth },
+      apolloReq: { operationName: 'customerSignMeIn' }
+    });
+
+    const { scope: currentTokenScope } = currentToken;
+    expect(currentToken.access_token).not.toBe(expiredToken.access_token);
+    expect(currentTokenScope).toContain(scope);
   });
 });
 
@@ -141,8 +202,8 @@ describe('[commercetools-helpers] handleAfterAuth', () => {
     const scope = 'anonymous_id';
     const tokenProvider = getTokenProvider(scope);
     auth.getTokenProvider = jest.fn().mockImplementation(() => tokenProvider);
-    const createSdkHelpersMock = createSdkHelpers as jest.Mock;
-    createSdkHelpersMock.mockImplementation(() => ({ tokenProvider: tokenProvider }));
+    const spyCreateSdkHelpers = jest.spyOn(sdkHelpers, 'createSdkHelpers');
+    spyCreateSdkHelpers.mockReturnValue({ sdkAuth: null, tokenProvider: getTokenProvider(scope)});
     const result = await handleAfterAuth({
       apolloReq: {
         operationName: 'customerSignMeIn',
@@ -159,8 +220,8 @@ describe('[commercetools-helpers] handleAfterAuth', () => {
     const scope = '';
     const tokenProvider = getTokenProvider(scope);
     auth.getTokenProvider = jest.fn().mockImplementation(() => tokenProvider);
-    const createSdkHelpersMock = createSdkHelpers as jest.Mock;
-    createSdkHelpersMock.mockImplementation(() => ({ tokenProvider: tokenProvider}));
+    const spyCreateSdkHelpers = jest.spyOn(sdkHelpers, 'createSdkHelpers');
+    spyCreateSdkHelpers.mockReturnValue({ sdkAuth: null, tokenProvider: getTokenProvider(scope)});
     const result = await handleAfterAuth({
       apolloReq: {
         operationName: 'customerSignMeIn',
