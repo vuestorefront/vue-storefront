a7eea6e758a2fec55413747e72557be4b28f4269
daaf <44862757+dawid-ziobro@users.noreply.github.com>
2021-10-28 09:39:18 +0200

    fix(os-202): fixed share data for faceting composable (#6493)
    * fix(os-202): fixed share data for faceting composable

---
+++ packages/core/docs/core/changelog/6492.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Fixed data sharing between components in the `useFacet` composable',
+  link: 'https://github.com/vuestorefront/vue-storefront/issues/6492',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Dawid Ziobro',
+  linkToGitHubAccount: 'https://github.com/dawid-ziobro'
+};
