7c974900f1e3d86efa0941010575d0b8ebddce10
Anton Sylka <anton.sylka@gorbunov.studio>
2021-11-02 17:24:55 +0800

    Refs #25357: Fix qty calculating for gift cards

--- core/modules/cart/helpers/createCartItemForUpdate.ts
+++ core/modules/cart/helpers/createCartItemForUpdate.ts
@@ -3,10 +3,16 @@ import CartItem from '@vue-storefront/core/modules/cart/types/CartItem';
 
 const createCartItemForUpdate = (clientItem: CartItem, serverItem: any, updateIds: boolean = false, mergeQty: boolean = false): CartItem => {
   const sku = clientItem.parentSku && config.cart.setConfigurableProductOptions ? clientItem.parentSku : clientItem.sku
+  const qty = mergeQty ? (clientItem.qty + serverItem.qty) : clientItem.qty
+
+  if (clientItem.giftcard_options) {
+    clientItem.giftcard_options.qty = qty;
+  }
+
   const cartItem = {
     sku,
     ...((serverItem && serverItem.item_id) ? { item_id: serverItem.item_id } : {}),
-    qty: mergeQty ? (clientItem.qty + serverItem.qty) : clientItem.qty,
+    qty,
     product_option: clientItem.product_option,
     customerImages: clientItem.customerImages ? clientItem.customerImages : undefined,
     plushieId: clientItem.plushieId ? clientItem.plushieId : undefined,
