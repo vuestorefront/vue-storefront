17d4578588dc9c1bb5590a41517a0a5ed227c2a4
Filip Sobol <sobol.filip@gmail.com>
2021-11-04 17:57:48 +0100

    Fix core changelogs

--- packages/core/docs/core/changelog/6475.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  description: 'Reduced cart operations payload size',
-  link: 'https://github.com/vuestorefront/vue-storefront/pull/6475',
-  isBreaking: false,
-  breakingChanges: [],
-  author: 'Łukasz Śliwa',
-  linkToGitHubAccount: 'https://github.com/lsliwaradioluz'
-};
