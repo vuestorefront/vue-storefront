e281ff2f5f3581374f53c780fed908909b5599d5
Filip Sobol <sobol.filip@gmail.com>
2021-11-04 16:46:36 +0100

    Update

--- packages/core/docs/core/changelog/6370.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  description: 'Added missing badge for cart on mobiles',
-  link: 'https://github.com/vuestorefront/vue-storefront/pull/6370',
-  isBreaking: false,
-  breakingChanges: [],
-  author: 'Dawid Ziobro',
-  linkToGitHubAccount: 'https://github.com/dawid-ziobro'
-};
--- packages/core/docs/core/changelog/6439.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  description: 'Remove LoyaltyCard and MyReviews',
-  link: 'https://github.com/vuestorefront/vue-storefront/pull/6439',
-  isBreaking: false,
-  breakingChanges: [],
-  author: 'Łukasz Jędrasik',
-  linkToGitHubAccount: 'https://github.com/lukaszjedrasik'
-};
