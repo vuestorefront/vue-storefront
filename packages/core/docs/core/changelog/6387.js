82dad5c6ad98138989975513120d043222fd5eef
Filip Sobol <filipsobol@users.noreply.github.com>
2021-11-05 09:52:56 +0100

    Fix core changelogs (#6508)

--- packages/core/docs/core/changelog/6387.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  description: 'Fixed login modal screens behavior',
-  link: 'https://github.com/vuestorefront/vue-storefront/issues/6387',
-  isBreaking: false,
-  breakingChanges: [],
-  author: 'Dawid Ziobro',
-  linkToGitHubAccount: 'https://github.com/dawid-ziobro'
-};
