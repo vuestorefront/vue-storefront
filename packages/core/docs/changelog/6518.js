8d0818f45c315929993607ba64f8fefa0c165444
Łukasz <lsliwaradioluz@gmail.com>
2021-11-09 15:01:10 +0100

    chore: add changelog

---
+++ packages/core/docs/changelog/6518.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Display and remove coupon codes in cart preview',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6518',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Łukasz Śliwa',
+  linkToGitHubAccount: 'https://github.com/lsliwaradioluz'
+};
