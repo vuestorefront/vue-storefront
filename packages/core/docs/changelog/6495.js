eb01fb99dc355966f0bab4ae9ca8f0ee5ca4f090
lukasborawski <lukas.borawski@gmail.com>
2021-11-09 13:16:35 +0100

    docs: changelog update

--- packages/core/docs/changelog/6495.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  description: 'Fixed duplicated search requests',
-  link: 'https://github.com/vuestorefront/vue-storefront/pull/6495',
-  isBreaking: false,
-  breakingChanges: [],
-  author: 'Dawid Ziobro',
-  linkToGitHubAccount: 'https://github.com/dawid-ziobro'
-};
--- packages/core/docs/commercetools/changelog/6474.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  description: 'Fixed redirect to localized My Account page.',
-  link: 'https://github.com/vuestorefront/vue-storefront/issues/6474',
-  isBreaking: false,
-  breakingChanges: [],
-  author: 'Łukasz Jędrasik',
-  linkToGitHubAccount: 'https://github.com/lukaszjedrasik'
-};
