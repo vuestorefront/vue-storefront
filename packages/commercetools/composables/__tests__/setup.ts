4de2bbb29935f365b930e349a9c960017c13e2f4
Alef Barbeli <alefbarbeli@gmail.com>
2021-11-12 06:42:48 -0300

    chore: removed commercetools package

--- packages/commercetools/composables/__tests__/setup.ts
+++
@@ -1,8 +0,0 @@
-require('jsdom-global')();
-import Vue from 'vue';
-import VueCompositionApi from '@vue/composition-api';
-
-Vue.config.productionTip = false;
-Vue.config.devtools = false;
-
-Vue.use(VueCompositionApi);
