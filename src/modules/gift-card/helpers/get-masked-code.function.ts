2e81e0e7bcfc2742dd8300843273b2d803a5395f
Anton Sylka <anton.sylka@gorbunov.studio>
2021-11-19 18:51:26 +0800

    Refs #25476: getMaskedCode - fix regexp

--- src/modules/gift-card/helpers/get-masked-code.function.ts
+++ src/modules/gift-card/helpers/get-masked-code.function.ts
@@ -3,7 +3,7 @@ export default function getMaskedCode (code: string): string {
   const maskChar = 'X';
 
   const prefixCode = code.slice(0, prefixLength);
-  let suffixCode = code.slice(prefixLength).replace(/([A-Z,0-9]{1})/g, maskChar);
+  let suffixCode = code.slice(prefixLength).replace(/[^-]/g, maskChar);
 
   return prefixCode + suffixCode;
 }
