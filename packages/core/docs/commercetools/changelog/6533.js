c6e66aed885bfe79ae0cb6576aea377194a83928
RafalDobrowolski <rafal.dobrowolski77@gmail.com>
2021-11-23 18:21:01 +0100

    fix(en-496): fixed handleAfterAuth method for customToken (#6533)
    * fix(en-496): fixed handleAfterAuth method for customToken
    
    * chore: add changelog

---
+++ packages/core/docs/commercetools/changelog/6533.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Fixed handleAfterAuth method to handle customToken scenario correctly',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6533',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Rafał Dobrowolski',
+  linkToGitHubAccount: 'https://github.com/RafalDobrowolski'
+};
