5224176ac2847453429c7aa0d9eccaadccf3a9f8
Filip Sobol <filipsobol@users.noreply.github.com>
2021-12-02 10:19:25 +0100

    chore: add missing changelog and update GH Action for conventional commits (#6546)

---
+++ packages/core/docs/changelog/6545.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Fix errors in the `Product` and `OrderHistory` components',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6545',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Filip Sobol',
+  linkToGitHubAccount: 'https://github.com/filipsobol'
+};
