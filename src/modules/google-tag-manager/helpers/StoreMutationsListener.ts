b77607fabdd81f9bf9652dac30573eb8bbc627b5
Anton Sylka <anton.sylka@gorbunov.studio>
2021-12-03 16:29:34 +0800

    Refs #25517: GTM module - fix imports

--- src/modules/google-tag-manager/helpers/StoreMutationsListener.ts
+++ src/modules/google-tag-manager/helpers/StoreMutationsListener.ts
@@ -1,8 +1,8 @@
 import { Store } from 'vuex';
 import VueGtm from 'vue-gtm';
 import Product from 'core/modules/catalog/types/Product';
-import { PRODUCT_SET_CURRENT } from 'core/modules/catalog/store/product/mutation-types';
-import { CART_ADD_ITEM } from 'core/modules/cart/store/mutation-types';
+import { PRODUCT_SET_CURRENT } from '@vue-storefront/core/modules/catalog/store/product/mutation-types';
+import { CART_ADD_ITEM } from '@vue-storefront/core/modules/cart/store/mutation-types';
 
 export default class StoreMutationsListener {
   public constructor (private store: Store<any>, private gtm: typeof VueGtm) {}
