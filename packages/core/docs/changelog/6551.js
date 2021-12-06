e896a4959ec1e29af9678d6f57eb70f7d4deade5
Łukasz <lsliwaradioluz@gmail.com>
2021-12-06 14:58:51 +0100

    chore: add changelog

---
+++ packages/core/docs/changelog/6551.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'fix(core): allow disabling locale redirects',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6551',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Łukasz Śliwa',
+  linkToGitHubAccount: 'https://github.com/lsliwaradioluz'
+};
