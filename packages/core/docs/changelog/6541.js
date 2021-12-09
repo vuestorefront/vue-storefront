a708ce7c7e009dcc742e5a2f9777ee1231ac06db
ljedrasik <ljedrasik@vuestorefront.io>
2021-12-09 11:25:40 +0100

    chore: add changelog

---
+++ packages/core/docs/changelog/6541.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Add health check under /healthz',
+  link: 'https://github.com/vuestorefront/vue-storefront/issues/6541',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Łukasz Jędrasik',
+  linkToGitHubAccount: 'https://github.com/lukaszjedrasik'
+};
