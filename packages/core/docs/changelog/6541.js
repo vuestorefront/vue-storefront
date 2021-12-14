490c70ba70efaf4682581161270a3ab051b8e735
Łukasz Jędrasik <ljedrasik@vuestorefront.io>
2021-12-14 11:22:16 +0100

    feat: add `@vue-storefront/health-check` module (#6566)
    * feat: add `@vue-storefront/health-check` module
    
    * chore: add changelog

---
+++ packages/core/docs/changelog/6541.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Add `@vue-storefront/health-check` module ',
+  link: 'https://github.com/vuestorefront/vue-storefront/issues/6541',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Łukasz Jędrasik',
+  linkToGitHubAccount: 'https://github.com/lukaszjedrasik'
+};
