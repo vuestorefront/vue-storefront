e706d2e3971bc44dde280f1fab4da30fd45f141f
Łukasz Jędrasik <ljedrasik@vuestorefront.io>
2021-12-14 10:31:24 +0100

    feat: add `@vue-storefront/health-check` module (#6563)
    * feat: add @vue-storefront/health-check module
    
    * chore: cr fix
    
    * chore: add Readme
    
    * chore: fix readme
    
    * chore: fix readme
    
    * chore: fix readme

--- packages/core/nuxt-module/lib/plugins/health-check.js
+++
@@ -1,8 +0,0 @@
-import Middleware from './middleware'
-
-Middleware['health-check'] = function ({ app, res }) {
-  const currentPath = app.context.route.fullPath;
-  if (currentPath === '/healthz') {
-    res.end('ok');
-  }
-};
