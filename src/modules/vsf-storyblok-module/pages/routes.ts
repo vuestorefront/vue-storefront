e8b087ad064bd72592d1f75aa4103fc2bede5014
Anton Sylka <anton.sylka@gorbunov.studio>
2021-12-21 16:20:20 +0800

    Refs #25690: Vsf Storyblok Module - fix types

--- src/modules/vsf-storyblok-module/pages/routes.ts
+++ src/modules/vsf-storyblok-module/pages/routes.ts
@@ -1,21 +1,24 @@
+import Vue, { ComponentOptions, AsyncComponent } from 'vue'
+import { RouteConfig } from 'vue-router/types/router'
+
 const StoryblokPage = () => import(/* webpackChunkName: "vsf-storyblok" */ './StoryblokPage.vue')
 const StoryblokBlock = () => import(/* webpackChunkName: "vsf-storyblok" */ './StoryblokBlock.vue')
 
-export const StoryblokRoutes = [
+export const StoryblokRoutes: RouteConfig[] = [
   {
     name: 'storyblok-page',
     path: '/stub',
-    component: StoryblokPage
+    component: StoryblokPage as unknown as ComponentOptions<Vue> | typeof Vue | AsyncComponent
   },
   {
     name: 'storyblok-block',
     path: '/stub',
-    component: StoryblokBlock
+    component: StoryblokBlock as unknown as ComponentOptions<Vue> | typeof Vue | AsyncComponent
   },
   {
     name: 'storyblok-single-component-visual-editor-mode',
     path: '/storyblok-empty',
-    component: StoryblokPage,
+    component: StoryblokPage as unknown as ComponentOptions<Vue> | typeof Vue | AsyncComponent,
     meta: { layout: 'empty' }
   }
 ]
