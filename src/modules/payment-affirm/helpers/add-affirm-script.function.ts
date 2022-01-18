f516ada41bf083bda85a31d0a37991fcca84b447
Anton Sylka <anton.sylka@gorbunov.studio>
2022-01-18 20:00:23 +0800

    Fix config for affirm

--- src/modules/payment-affirm/helpers/add-affirm-script.function.ts
+++ src/modules/payment-affirm/helpers/add-affirm-script.function.ts
@@ -5,7 +5,7 @@ export default function addAffirmScript (appConfig: any): void {
   script.text = `
       _affirm_config = {
         public_api_key:  "${appConfig.affirm.publicKey}",
-        script:          "${appConfig.affirm.promoMessaging.script}"
+        script:          "${appConfig.affirm.script}"
       };
       (function(l,g,m,e,a,f,b){var d,c=l[m]||{},h=document.createElement(f),n=document.getElementsByTagName(f)[0],k=function(a,b,c){return function(){a[b]._.push([c,arguments])}};c[e]=k(c,e,"set");d=c[e];c[a]={};c[a]._=[];d._=[];c[a][b]=k(c,a,b);a=0;for(b="set add save post open empty reset on off trigger ready setProduct".split(" ");a<b.length;a++)d[b[a]]=k(c,e,b[a]);a=0;for(b=["get","token","url","items"];a<b.length;a++)d[b[a]]=function(){};h.async=!0;h.src=g[f];n.parentNode.insertBefore(h,n);delete g[f];d(g);l[m]=c})(window,_affirm_config,"affirm","checkout","ui","script","ready");
       `;
