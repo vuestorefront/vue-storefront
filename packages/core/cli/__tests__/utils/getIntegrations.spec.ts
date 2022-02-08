2ab45d91c4248e3f242b8ef2d86e16ae99cf2b0b
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-02-08 19:36:48 -0300

    chore: delete our current CLI package

--- packages/core/cli/__tests__/utils/getIntegrations.spec.ts
+++
@@ -1,7 +0,0 @@
-import integrations from '@vue-storefront/cli/src/utils/getIntegrations';
-
-describe('[@core/cli/src/utils] Integrations', () => {
-  it('integrations should be an object', () => {
-    expect(integrations).toEqual(expect.any(Object));
-  });
-});
