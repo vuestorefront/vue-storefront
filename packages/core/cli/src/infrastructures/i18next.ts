66c8aa1aefe0f7bff9e0cdd62b1be4653fa0f367
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-02-09 23:21:32 -0300

    fix(core:cli): setup i18next in the script instead of init hook

--- packages/core/cli/src/infrastructures/i18next.ts
+++ packages/core/cli/src/infrastructures/i18next.ts
@@ -1,11 +1,10 @@
 // eslint-disable-next-line unicorn/prefer-node-protocol
 import path from 'path'
-import {Hook} from '@oclif/core'
 import i18next from 'i18next'
 import Backend from 'i18next-fs-backend'
 import getLocale from 'os-locale'
 
-const init: Hook<'init'> = async (): Promise<void> => {
+export const setupI18Next = async (): Promise<void> => {
   await i18next.use(Backend).init({
     backend: {
       // eslint-disable-next-line unicorn/prefer-module
@@ -13,13 +12,9 @@ const init: Hook<'init'> = async (): Promise<void> => {
     },
     fallbackLng: 'en-US',
     interpolation: {
-      // Escaping is only required to prevent XSS attacks in the front-end.
+      // Escaping is only required to prevent XSS attacks in the front-end
       escapeValue: false,
     },
     lng: await getLocale(),
   })
-
-  console.log('init was calld')
 }
-
-export default init
