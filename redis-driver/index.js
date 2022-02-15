dc2312f51177862094d10fd16283ccd667214995
Filip Sobol <sobol.filip@gmail.com>
2022-02-15 15:48:18 +0100

    chore: add driver

---
+++ redis-driver/index.js
@@ -0,0 +1,56 @@
+import Redis from 'redis-tag-cache';
+
+export default function RedisCache (options) {
+  const client = new Redis(options);
+
+  return {
+    async invoke({ route, render, getTags }) {
+      const key = `page:${ route }`;
+      const cachedResponse = await client.get(key);
+
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      const content = await render();
+      const tags = getTags();
+
+      if (!tags.length) {
+        return content;
+      }
+
+      // We could add "await" here, but saving content in cache doesn't have to block the request
+      client.set(
+        key,
+        content,
+        tags
+      );
+
+      return content;
+    },
+
+    invalidate({ tags }) {
+      const clearAll = tags.includes('*');
+
+      if (!clearAll) {
+        return client.invalidate(...tags)
+      }
+
+      return new Promise((resolve, reject) => {
+        const prefix = `${options.redis.keyPrefix || ''}tags:`;
+        const stream = client.redis.scanStream({ match: `${prefix}*` });
+
+        const tags = [];
+
+        stream.on('data', rawTags => tags.push(...rawTags.map(tag => tag.replace(prefix, ''))));
+        stream.on('end', async () => {
+          if (tags.length) {
+            await client.invalidate(...tags);
+          }
+          resolve();
+        });
+        stream.on('error', reject);
+      });
+    }
+  };
+};
