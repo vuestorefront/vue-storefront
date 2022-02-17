09d7161f3214ccd426356424df41effda335327b
Mateusz Pietrusinski <1325258@gmail.com>
2022-02-17 17:40:13 +0100

    chore: adding ability to select plugins by passing custom config from nuxt.config.js

--- packages/core/nuxt-module/lib/module.js
+++ packages/core/nuxt-module/lib/module.js
@@ -8,22 +8,26 @@ const performanceModule = require('./modules/performance');
 const storefrontUiModule = require('./modules/storefront-ui');
 const rawSourcesModule = require('./modules/raw-sources-loader');
 
-module.exports = function VueStorefrontNuxtModule (moduleOptions) {
+module.exports = function VueStorefrontNuxtModule(moduleOptions) {
   const defaultOptions = {
     coreDevelopment: false,
+    i18n: true,
+    e2e: true,
+    logger: true,
+    ssr: true,
+    context: true,
+    sfui: true,
     performance: {
       httpPush: true,
       purgeCSS: {
         enabled: false,
-        paths: [
-          '**/*.vue'
-        ]
-      }
+        paths: ['**/*.vue'],
+      },
     },
     useRawSource: {
       dev: [],
-      prod: []
-    }
+      prod: [],
+    },
   };
 
   const options = merge(defaultOptions, moduleOptions);
@@ -31,7 +35,7 @@ module.exports = function VueStorefrontNuxtModule (moduleOptions) {
   // Add meta data
   this.options.head.meta.push({
     name: 'generator',
-    content: 'Vue Storefront 2'
+    content: 'Vue Storefront 2',
   });
 
   log.info('Starting Vue Storefront Nuxt Module');
@@ -45,39 +49,47 @@ module.exports = function VueStorefrontNuxtModule (moduleOptions) {
           return preloadFiles
             .filter(({ asType }) => asType === 'script')
             .map(({ file, asType }) => `<${publicPath}${file}>; rel=preload; as=${asType}`);
-        }
-      }
+        },
+      },
     });
   }
+  if (option.context) {
+    // Context plugin
+    this.addPlugin(path.resolve(__dirname, 'plugins/context.js'));
+    log.success('Installed Vue Storefront Context plugin');
+  }
+  if (option.ssr) {
+    // SSR plugin
+    this.addPlugin(path.resolve(__dirname, 'plugins/ssr.js'));
+    log.success('Installed Vue Storefront SSR plugin');
+  }
 
-  // Context plugin
-  this.addPlugin(path.resolve(__dirname, 'plugins/context.js'))
-  log.success('Installed Vue Storefront Context plugin');
+  if (options.logger) {
+    // Logger plugin
+    this.addPlugin({
+      src: path.resolve(__dirname, 'plugins/logger.js'),
+      options: moduleOptions.logger || {},
+    });
+    log.success('Installed VSF Logger plugin');
+  }
 
-  // SSR plugin
-  this.addPlugin(path.resolve(__dirname, 'plugins/ssr.js'));
-  log.success('Installed Vue Storefront SSR plugin');
-
-  // Logger plugin
-  this.addPlugin({
-    src: path.resolve(__dirname, 'plugins/logger.js'),
-    options: moduleOptions.logger || {}
-  });
-  log.success('Installed VSF Logger plugin');
-
-  // Context plugin
-  this.addPlugin(path.resolve(__dirname, 'plugins/e2e-testing.js'))
-  log.success('Installed Vue Storefront E2E testing plugin');
+  if (options.e2e) {
+    // Context plugin
+    this.addPlugin(path.resolve(__dirname, 'plugins/e2e-testing.js'));
+    log.success('Installed Vue Storefront E2E testing plugin');
+  }
 
   // i18n-cookies plugin
-  this.addPlugin({
-    src: path.resolve(__dirname, 'plugins/i18n-cookies.js'),
-    options: this.options.i18n
-  });
-  log.success('Installed Internationalization Cookies plugin');
+  if (options.i18n) {
+    this.addPlugin({
+      src: path.resolve(__dirname, 'plugins/i18n-cookies.js'),
+      options: this.options.i18n,
+    });
+    log.success('Installed Internationalization Cookies plugin');
+  }
 
   // StorefrontUI module
-  if (fs.existsSync(resolveDependency('@storefront-ui/vue'))) {
+  if (fs.existsSync(resolveDependency('@storefront-ui/vue')) && options.sfui) {
     storefrontUiModule.call(this, options);
     log.success('Installed StorefrontUI Module');
   }
