c8920d0437df590eabba97e15538d16a06c317df
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-02-23 13:59:02 -0300

    fix(core:cli): update integrations API URL

--- packages/core/cli/src/domains/integration/fetchIntegrations.ts
+++ packages/core/cli/src/domains/integration/fetchIntegrations.ts
@@ -1,7 +1,7 @@
 import type Integration from './Integration';
 import fetch from 'node-fetch';
 
-const API_URL = 'https://raw.githubusercontent.com/vuestorefront/integrations/main/data.json';
+const API_URL = 'https://raw.githubusercontent.com/vuestorefront/cli-integrations/main/data.json';
 
 const fetchIntegrations = async (): Promise<Integration[]> => {
   const response = await fetch(API_URL);
