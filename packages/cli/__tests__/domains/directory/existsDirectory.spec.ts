76f386427c167150cd3a164399ae5be3366616aa
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-02-28 18:56:01 -0300

    test(cli): add existsDirectory and removeFileOrDirectory tests

---
+++ packages/cli/__tests__/domains/directory/existsDirectory.spec.ts
@@ -0,0 +1,22 @@
+import mockFileSystem from 'mock-fs';
+import { existsDirectory } from '../../../src/domains/directory';
+
+describe('existsDirectory | integration test', () => {
+  beforeEach(() => {
+    mockFileSystem({
+      templates: {
+        'getting-started.md': '# Getting Started\n'
+      }
+    });
+  });
+
+  afterEach(() => {
+    mockFileSystem.restore();
+  });
+
+  it('checks if repository exists', async () => {
+    expect(await existsDirectory('templates/getting-started.md')).toBe(false);
+    expect(await existsDirectory('sources')).toBe(false);
+    expect(await existsDirectory('templates')).toBe(true);
+  });
+});
---
+++ packages/cli/__tests__/domains/directory/removeFileOrDirectory.spec.ts
@@ -0,0 +1,25 @@
+import mockFileSystem from 'mock-fs';
+import { existsDirectory, removeFileOrDirectory } from '../../../src/domains/directory';
+
+describe('removeFileOrDirectory | integration test', () => {
+  beforeEach(() => {
+    mockFileSystem({
+      src: {
+        'index.js': 'console.log("Hello world!");'
+      }
+    });
+  });
+
+  afterEach(() => {
+    mockFileSystem.restore();
+  });
+
+  it('removes files and directories', async () => {
+    expect(await existsDirectory('src')).toBe(true);
+
+    await removeFileOrDirectory('src/index.js');
+    await removeFileOrDirectory('src');
+
+    expect(await existsDirectory('src')).toBe(false);
+  });
+});
