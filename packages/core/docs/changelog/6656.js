a1dca54055f47323b5f3214a7ca72570459d4ab7
lukasborawski <lukas.borawski@gmail.com>
2022-03-10 15:04:08 +0100

    chore: version bump for core to 2.5.6

---
+++ packages/core/docs/changelog/6656.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Added the VSF cookie name',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6667',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Alef Barbeli',
+  linkToGitHubAccount: 'https://github.com/vuestorefront'
+};
