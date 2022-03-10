e0fc3a2341b3802ad7073187d0c0d8b60c51edb3
lukasborawski <lukas.borawski@gmail.com>
2022-03-10 15:06:00 +0100

    docs: changelog update

--- packages/core/docs/changelog/6641.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  description: 'Added the ability to select plugins used by @vue-storefront/nuxt',
-  link: 'https://github.com/vuestorefront/vue-storefront/pull/6641',
-  isBreaking: false,
-  breakingChanges: [],
-  author: 'Mateusz Pietrusiński',
-  linkToGitHubAccount: 'https://github.com/Mateuszpietrusinski'
-};
