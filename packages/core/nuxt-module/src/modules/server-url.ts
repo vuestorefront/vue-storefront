459c24723948b6528eac996247f1f6a8c1da5538
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2022-03-30 13:03:52 -0300

    refactor(nuxt-module): improved validation in server-url

--- packages/core/nuxt-module/src/modules/server-url.ts
+++ packages/core/nuxt-module/src/modules/server-url.ts
@@ -1,8 +1,7 @@
 export default function VueStorefrontServerUrl(): void {
   const hasMiddlewareUrl = this.options?.publicRuntimeConfig?.middlewareUrl;
-  const hasSSRUrl = this.options?.publicRuntimeConfig?.ssrMiddlewareUrl;
 
-  if (!hasMiddlewareUrl || !hasSSRUrl) {
+  if (!hasMiddlewareUrl) {
     this.nuxt.hook('listen', (server, { https, host, port }) => {
       const baseURL = new URL('', `http${https ? 's' : ''}://${host}:${port}`);
 
