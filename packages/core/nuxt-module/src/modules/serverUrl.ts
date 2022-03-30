4e74a3d95aa787b19bd816e44bcce0d7b812cfbd
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2022-03-29 21:53:05 -0300

    refactor(nuxt-module): changed merge strategy

--- packages/core/nuxt-module/src/modules/serverUrl.ts
+++ packages/core/nuxt-module/src/modules/serverUrl.ts
@@ -1,17 +1,15 @@
-import { merge } from 'lodash';
-
 export default function VueStorefrontServerUrl(): void {
   const hasMiddlewareUrl = this.options?.publicRuntimeConfig?.middlewareUrl;
   const hasSSRUrl = this.options?.publicRuntimeConfig?.ssrMiddlewareUrl;
+
   if (!hasMiddlewareUrl || !hasSSRUrl) {
     this.nuxt.hook('listen', (server, { https, host, port }) => {
       const baseURL = new URL('', `http${https ? 's' : ''}://${host}:${port}`);
 
-      this.options = merge({
-        publicRuntimeConfig: {
-          middlewareUrl: baseURL.origin
-        }
-      }, this.options);
+      this.options.publicRuntimeConfig = {
+        ...this.options.publicRuntimeConfig,
+        middlewareUrl: baseURL.origin
+      };
     });
   }
 }
