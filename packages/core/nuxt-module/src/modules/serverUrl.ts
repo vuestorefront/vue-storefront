8598230f318e7b2db7e05e07da08ec26cbd8a1c9
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2022-03-29 21:11:05 -0300

    refactor(nuxt-module): fixed configuration merge

--- packages/core/nuxt-module/src/modules/serverUrl.ts
+++ packages/core/nuxt-module/src/modules/serverUrl.ts
@@ -1,11 +1,15 @@
 export default function VueStorefrontServerUrl(): void {
-  if (!(this.options?.publicRuntimeConfig?.middlewareUrl || this.options?.publicRuntimeConfig?.ssrMiddlewareUrl)) {
+  const hasMiddlewareUrl = this.options?.publicRuntimeConfig?.middlewareUrl;
+  const hasSSRUrl = this.options?.publicRuntimeConfig?.ssrMiddlewareUrl;
+  if (!hasMiddlewareUrl || !hasSSRUrl) {
     this.nuxt.hook('listen', (server, { https, host, port }) => {
       const baseURL = new URL('', `http${https ? 's' : ''}://${host}:${port}`);
 
-      this.options.publicRuntimeConfig = {
-        ...this.options.publicRuntimeConfig,
-        middlewareUrl: baseURL.origin
+      this.options = {
+        publicRuntimeConfig: {
+          ...(this.options.publicRuntimeConfig || {}),
+          middlewareUrl: baseURL.origin
+        }
       };
     });
   }
