15aa819af3ec6d93c9b9e13d49acd7d8c3536ca5
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2022-04-03 00:54:30 -0300

    refactor(middleware): added serverUrl logic to package

--- packages/core/middleware/nuxt/index.js
+++ packages/core/middleware/nuxt/index.js
@@ -1,9 +1,29 @@
 const { createServer } = require('@vue-storefront/middleware');
+const consola = require('consola');
 
-module.exports = function VueStorefrontMiddleware () {
+module.exports = function VueStorefrontMiddleware() {
+  const apiPath = 'api';
+  // Validating Server URL
+  this.nuxt.hook('listen', (server, { url }) => {
+    try {
+      if (!url) consola.fatal('Nuxt was\'t able to fetch your url.');
+
+      if (!(this.options && this.options.publicRuntimeConfig && this.options.publicRuntimeConfig.middlewareUrl)) {
+        Object.assign(this.options.publicRuntimeConfig, {
+          ...this.options.publicRuntimeConfig,
+          middlewareUrl: new URL(apiPath, url).toString()
+        });
+      }
+    } catch (error) {
+      consola.warn(error);
+      consola.fatal('Nuxt wasn\'t able to fetch the middlewareUrl. Please follow the guide at https://docs.vuestorefront.io/v2/getting-started/configuration.html#nuxt-config-js to configure your nuxt.config.js');
+    }
+  });
+
   const { integrations } = require(this.nuxt.options.rootDir + '/middleware.config.js');
+
   const handler = createServer({ integrations });
-  const serverMiddleware = { path: '/api', handler };
+  const serverMiddleware = { path: `/${apiPath}`, handler };
 
   this.addServerMiddleware(serverMiddleware);
 };
