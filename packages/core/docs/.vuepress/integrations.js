8ca56ffc88f2b9364ab5d5d1778f1b9518e8b670
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2022-04-04 11:10:40 -0300

    docs: change Sylius integration (#6695)
    * docs: change Sylius integration
    
    * docs: removed integration link

--- packages/core/docs/.vuepress/integrations.js
+++ packages/core/docs/.vuepress/integrations.js
@@ -103,7 +103,7 @@ const INTEGRATIONS = {
       name: 'Sylius',
       link: '',
       image: 'https://uploads-ssl.webflow.com/5e90e5cd5f86784ad554a3c2/5ed6275b7df0e61000941a54_sylius_logo.svg',
-      status: STATUS.WIP,
+      status: STATUS.BETA,
       license: LICENSE.OPEN_SOURCE,
       maintainedBy: [
         { name: 'BitBag', link: 'https://bitbag.io/' },
