442f3260ae7840e2fa62626f3d6e5e3541beb11d
Anton Sylka <anton.sylka@gorbunov.studio>
2022-04-22 18:55:44 +0800

    Refs #26302: Fix settings sync

--- src/modules/backend-settings/index.ts
+++ src/modules/backend-settings/index.ts
@@ -1,15 +1,8 @@
 import { StorefrontModule } from '@vue-storefront/core/lib/modules';
-import { coreHooks } from '@vue-storefront/core/hooks';
 
 import { module } from './store';
 import { SN_BACKEND_SETTINGS } from './types/StoreMutations';
 
-export const BackendSettings: StorefrontModule = function ({ store, app }) {
+export const BackendSettings: StorefrontModule = async function ({ store }) {
   store.registerModule(SN_BACKEND_SETTINGS, module);
-
-  coreHooks.afterAppInit(() => {
-    if (app.$isServer) {
-      store.dispatch(`${SN_BACKEND_SETTINGS}/fetchSettings`);
-    }
-  });
 }
