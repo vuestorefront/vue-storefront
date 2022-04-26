a52d5f46ac85cfe6dcbcd81ebec4101d49422e1e
Anton Sylka <anton.sylka@gorbunov.studio>
2022-04-26 14:17:42 +0800

    Refs #26391: IsCustomProduct - use product ids from backend settings module

--- src/modules/shared/helpers/is-custom-product.function.ts
+++ src/modules/shared/helpers/is-custom-product.function.ts
@@ -1,15 +1,6 @@
-const customProductsSkus = [
-  'customPrintedSocks_bundle',
-  'customPrintedMasks_bundle',
-  'customPrintedKeychains_bundle',
-  'customFeltedMagnets_bundle',
-  'customPillow_bundle',
-  'petsiesPhrasePillow_bundle',
-  'ForeversDog_bundle',
-  'ForeversCat_bundle',
-  'ForeversOther_bundle'
-];
+import rootStore from '@vue-storefront/core/store';
 
 export default function isCustomProduct (productSku: string): boolean {
+  const customProductsSkus = rootStore.getters['backend-settings/getSettingByCompositeKey']('products/customProductsSkus');
   return customProductsSkus.includes(productSku);
 }
