fb55f4526c0e09fda4e63d64a4487726b8ad81f5
Anton Sylka <anton.sylka@gorbunov.studio>
2022-05-05 17:12:55 +0800

    Refs #26428: CustomerImage - move to shared module

--- src/modules/shared/index.ts
+++ src/modules/shared/index.ts
@@ -2,6 +2,7 @@ import isVue from './is-vue.typeguard';
 import { AspectRatio } from './types/aspect-ratio.value';
 import { InjectType } from './types/inject-type';
 import { VideoProvider } from './types/video-provider.value';
+import CustomerImage from './types/customer-image.interface';
 
 import StreamingVideo from './components/streaming-video.vue';
 
@@ -10,5 +11,6 @@ export {
   isVue,
   AspectRatio,
   VideoProvider,
-  StreamingVideo
+  StreamingVideo,
+  CustomerImage
 }
