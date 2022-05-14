b534145f3b84d94cade5ddbdf573a81a71afec70
Łukasz Śliwa <39009379+lsliwaradioluz@users.noreply.github.com>
2022-05-14 10:53:31 +0200

    fix(core): prevent http cache module from setting headers after they had been sent (#6720)
    * fix: prevent setting response headers when sent
    
    * chore: add test

--- packages/http-cache/nuxt/renderRouteCallback.ts
+++ packages/http-cache/nuxt/renderRouteCallback.ts
@@ -2,6 +2,7 @@ import isUrlMatchingRule from './isUrlMatchingRule';
 import { HttpCacheModuleParams } from './types';
 
 const renderRouteCallback = ({ default: defaultHeaderValue = 'max-age=60', matchRoute = {} }: HttpCacheModuleParams) => (url, result, { res }): void => {
+  if (res.headersSent) return;
   res.setHeader('Cache-Control', defaultHeaderValue);
 
   Object.entries(matchRoute).map(([rule, headerValue]: [string, string]): void => {
