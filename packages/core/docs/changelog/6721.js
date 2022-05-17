cc01c8b4b48cccd271923ff60ad206f6f1626924
Łukasz Śliwa <lsliwaradioluz@gmail.com>
2022-05-17 13:02:25 +0200

    feat: changelog

---
+++ packages/core/docs/changelog/6721.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Support for slash-containing locales in i18n',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6721',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Łukasz Śliwa',
+  linkToGitHubAccount: 'https://github.com/lsliwaradioluz'
+};
