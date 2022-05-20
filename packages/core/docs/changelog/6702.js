eb112ab3b106706029493af697a5750b7f81965d
lukasborawski <lukas.borawski@gmail.com>
2022-05-20 10:28:48 +0100

    docs: missing changelog notes

---
+++ packages/core/docs/changelog/6702.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'Allow to use external Middleware URL',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6702',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Heitor Ramon Ribeiro | Lukas Borawski',
+  linkToGitHubAccount: 'https://github.com/vuestorefront/vue-storefront'
+};
