a3f6666202125ca6afd12968537db86e8f45e0ce
Dawid Ziobro <dziobro@vuestorefront.io>
2022-06-01 11:03:52 +0200

    refactor: removed bp and ct also changed paths

--- packages/boilerplate/theme/tests/e2e/pages/components/cart-sidebar.ts
+++
@@ -1,7 +0,0 @@
-class Cart {
-  get goToCheckoutButton(): Cypress.Chainable {
-    return cy.contains('Go to checkout');
-  }
-}
-
-export default new Cart();
--- packages/commercetools/api-client/src/api/customerSignOut/index.ts
+++
@@ -1,5 +0,0 @@
-const customerSignOut = async ({ config }) => {
-  config.auth.onTokenRemove?.();
-};
-
-export default customerSignOut;
