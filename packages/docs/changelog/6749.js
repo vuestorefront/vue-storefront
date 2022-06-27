8e102c9794863b6fce4cd12148a93ffbff29a3cf
Bartosz Herba <bherba@vuestorefront.io>
2022-06-27 10:44:07 +0200

    Add changelog

---
+++ packages/docs/changelog/6749.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: 'feat: init function in the middleware',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6749',
+  isBreaking: false,
+  breakingChanges: [],
+  author: 'Bartosz Herba',
+  linkToGitHubAccount: 'https://github.com/bartoszherba'
+};
