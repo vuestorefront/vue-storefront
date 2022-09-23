78b56125a12420c6891b01671442316a86a074ea
Fifciu <fjedrasik@vuestorefront.io>
2022-09-23 16:49:59 +0200

    chore: added changelog entry for the #6810 feature

---
+++ packages/docs/changelog/6810.js
@@ -0,0 +1,7 @@
+module.exports = {
+  description: 'feat!: plugins pass Host header in server to server communication #6810. Previously headers other than Cookie have been lost in server to server communication. Now it\'s passed to the axios client of plugins and works as expected.',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6810',
+  isBreaking: false,
+  author: 'Filip Jędrasik',
+  linkToGitHubAccount: 'https://github.com/Fifciu'
+};
