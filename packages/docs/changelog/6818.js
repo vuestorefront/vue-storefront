21afd3a7f379dadc74951ba286bee608e93c8cc3
Fifciu <fjedrasik@vuestorefront.io>
2022-10-28 22:50:02 +0200

    chore: changelog

---
+++ packages/docs/changelog/6818.js
@@ -0,0 +1,7 @@
+module.exports = {
+  description: 'feat!: plugins pass X-Forwarded-Host header with fallback to the Host header in server to server communication #6818.',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6818',
+  isBreaking: false,
+  author: 'Filip Jędrasik',
+  linkToGitHubAccount: 'https://github.com/Fifciu'
+};
