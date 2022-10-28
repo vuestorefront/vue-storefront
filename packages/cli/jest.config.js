0ca26e36c5f658c8c2c8dfd61d021a9187ec38c1
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2022-10-28 17:26:44 -0300

    chore(cli): fixed eslint rules

--- packages/cli/jest.config.js
+++ packages/cli/jest.config.js
@@ -1,23 +1,22 @@
 // @ts-check
+const baseConfig = require('../jest.base.config.js');
 
 /**
  * Jest's configuration object.
  * @type {import('@jest/types').Config.InitialOptions}
  */
 module.exports = {
-  ...require('../jest.base.config.js'),
-  preset: 'ts-jest/presets/js-with-babel-esm', // or other ESM presets
+  ...baseConfig,
+  preset: 'ts-jest/presets/js-with-babel-esm',
   moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^(\\.{1,2}/.*)\\.js$': '$1'
   },
   transform: {
-    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
-    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
-    '^.+\\.tsx?$': [
+    '^.+\\.m?[tj]sx?$': [
       'ts-jest',
       {
-        useESM: true,
-      },
-    ],
-  },
+        useESM: true
+      }
+    ]
+  }
 };
