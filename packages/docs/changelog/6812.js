19db8b08b6c275946dc83cede9035145cb268006
Wojciech Sikora <wsikora@vuestorefront.io>
2022-11-03 15:39:13 +0100

    docs: add changelog

---
+++ packages/docs/changelog/6812.js
@@ -0,0 +1,7 @@
+module.exports = {
+  description: 'fix: allow using spread operator on request body.',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6815',
+  isBreaking: false,
+  author: 'Bartosz Herba',
+  linkToGitHubAccount: 'https://github.com/bartoszherba'
+};
