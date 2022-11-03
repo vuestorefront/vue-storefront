65d891df60124ece7250e363a726d2d2b2714c28
Wojciech Sikora <wsikora@vuestorefront.io>
2022-11-03 12:05:01 +0100

    docs: add changelog

---
+++ packages/docs/changelog/6815.js
@@ -0,0 +1,7 @@
+module.exports = {
+  description: 'fix: prevent middleware from crashing when sending a request with non-existing integration name.',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6815',
+  isBreaking: false,
+  author: 'Bartosz Herba',
+  linkToGitHubAccount: 'https://github.com/bartoszherba'
+};
