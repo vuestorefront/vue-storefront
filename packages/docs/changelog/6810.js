39a8dff333c921e2eb41caf37bae070a8414edbb
Wojciech Sikora <wsikora@vuestorefront.io>
2022-11-04 12:52:12 +0100

    docs: update #6810 description

--- packages/docs/changelog/6810.js
+++ packages/docs/changelog/6810.js
@@ -1,5 +1,5 @@
 module.exports = {
-  description: 'feat!: plugins pass Host header in server to server communication #6810. Previously headers other than Cookie have been lost in server to server communication. Now it\'s passed to the axios client of plugins and works as expected.',
+  description: 'feat: plugins pass Host header in server to server communication #6810. Previously headers other than Cookie have been lost in server to server communication. Now it\'s passed to the axios client of plugins and works as expected.',
   link: 'https://github.com/vuestorefront/vue-storefront/pull/6810',
   isBreaking: false,
   author: 'Filip Jędrasik',
