3de84e45df02b0f9dba351802a24b20a9875a2fc
Łukasz Śliwa <lsliwaradioluz@gmail.com>
2022-11-29 17:46:30 +0200

    feat: changelog

---
+++ packages/docs/changelog/6850.js
@@ -0,0 +1,7 @@
+module.exports = {
+  description: 'feat: allow overwriting i18n configuration through plugins',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6850',
+  isBreaking: false,
+  author: 'Łukasz Śliwa',
+  linkToGitHubAccount: 'https://github.com/lsliwaradioluz'
+};
