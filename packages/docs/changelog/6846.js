0cff869e21757a0f373342c694d98aa726454f38
Wojciech Sikora <wsikora@vuestorefront.io>
2022-12-07 10:50:28 +0100

    chore: add changelog

---
+++ packages/docs/changelog/6846.js
@@ -0,0 +1,7 @@
+module.exports = {
+  description: 'feat: make new url constructor in core package able to work with api',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6846',
+  isBreaking: false,
+  author: 'Bartosz Herba',
+  linkToGitHubAccount: 'https://github.com/bartoszherba'
+};
