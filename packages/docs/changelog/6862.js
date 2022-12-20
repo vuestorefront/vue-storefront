7c3ff546b7c9a86e9956e86b121ac053f11c3f57
Wojciech Sikora <wsikora@vuestorefront.io>
2022-12-20 12:59:13 +0100

    docs: add changelog

---
+++ packages/docs/changelog/6862.js
@@ -0,0 +1,7 @@
+module.exports = {
+  description: 'fix: use any type as configuration in ApiClientExtension',
+  link: 'https://github.com/vuestorefront/vue-storefront/pull/6862',
+  isBreaking: false,
+  author: 'Wojciech Sikora',
+  linkToGitHubAccount: 'https://github.com/WojtekTheWebDev'
+};
