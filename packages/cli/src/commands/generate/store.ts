d9e701cf3ef52c813fce6c41b40f6fc44fdddace
Sergii Kirianov <s.kirianov@gmail.com>
2023-01-27 10:12:24 +0000

    add additional files

--- packages/cli/src/commands/generate/store.ts
+++ packages/cli/src/commands/generate/store.ts
@@ -3,10 +3,13 @@ import { t } from 'i18next';
 import inquirer from 'inquirer';
 import picocolors from 'picocolors';
 import * as path from 'path';
+import fs from 'fs';
 import { getIntegration } from '../../domains/integration';
 import { getProjectName } from '../../domains/project-name';
 import { cloneGitRepository, terminateGitRepository } from '../../domains/git-repository';
 import { existsDirectory } from '../../domains/directory';
+import { confirmOverwrite, getMagentoDirName, installMg2Prompt, handleMagentoKeys, isMagentoKeys, copyAuth, handleGraphQL, isGenerateSampleData, handleSampleData, copyEnv } from '../../domains/magento2';
+import { checkDocker, getMagentoDomainName, installMagentoImage } from '../../domains/magento2/docker';
 
 export default class GenerateStore extends Command {
   static override description = t('command.generate_store.description');
@@ -25,6 +28,8 @@ export default class GenerateStore extends Command {
       customIntegrationRepositoryMessage: t('command.generate_store.input.custom_integration_repository')
     });
 
+    const { name: integrationName } = integration;
+
     const projectDir = path.resolve(projectName);
 
     if (await existsDirectory(projectDir)) {
@@ -40,10 +45,74 @@ export default class GenerateStore extends Command {
       }
     }
 
+    if (integrationName === 'Magento 2') {
+      let magentoAccessKey: string;
+      let magentoSecretKey: string;
+
+      const isInstallMagento = await installMg2Prompt('Do you want to install Magento?');
+
+      if (isInstallMagento) {
+        const magentoDirName = await getMagentoDirName('Magento directory name');
+
+        // check if the directory with the name magentoDirName exists in the current directory
+        // if yes, ask if the user wants to overwrite it
+        // if no, create the directory
+        if (!fs.existsSync(magentoDirName)) {
+          fs.mkdirSync(magentoDirName);
+        } else {
+          await confirmOverwrite({
+            message: `The directory ${magentoDirName} already exists. Do you want to overwrite it?`,
+            magentoDirName,
+            self: this
+          });
+        }
+
+        // prompt user if they have Magento access keys
+        // if yes, ask for the keys
+        // if no, provide a link to the Magento website to get the keys
+        const hasMagentoKeys = await isMagentoKeys('Do you have Magento access keys?');
+
+        if (hasMagentoKeys) {
+          const { accessKey, secretKey } = await handleMagentoKeys('Please provide your Magento access keys');
+
+          magentoAccessKey = accessKey;
+          magentoSecretKey = secretKey;
+        } else {
+          this.log('Please go to https://experienceleague.adobe.com/docs/commerce-operations/installation-guide/prerequisites/authentication-keys.html and follow instructions to get your Magento access keys');
+
+          // prompt user to enter the keys
+          const { accessKey, secretKey } = await handleMagentoKeys('Please provide your Magento access keys');
+
+          magentoAccessKey = accessKey;
+          magentoSecretKey = secretKey;
+        }
+
+        const isDockerInstalled = await checkDocker();
+
+        if (isDockerInstalled) {
+          const magentoDomainName = await getMagentoDomainName('Magento domain name');
+          await installMagentoImage(magentoDirName, magentoDomainName, this);
+
+          await copyAuth(magentoDirName, magentoAccessKey, magentoSecretKey);
+
+          await handleGraphQL(magentoDirName);
+
+          const isGenerateData = await isGenerateSampleData('Do you want to generate sample data for the store?');
+
+          // eslint-disable-next-line max-depth
+          if (isGenerateData) {
+            await handleSampleData(magentoDirName);
+          }
+        }
+      }
+    }
+
     await cloneGitRepository({
       projectDir,
       gitRepositoryURL: integration.gitRepositoryURL
     });
+
+    await copyEnv(projectDir);
 
     await terminateGitRepository(projectDir);
 
