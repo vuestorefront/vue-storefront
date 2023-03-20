1b6888cb81210ebd7a508264ca699c724003c9e9
Anton Sylka <anton.sylka@gorbunov.studio>
2023-03-20 11:16:01 +0700

    Refs #28628: BodyPartFactory - move getting children body parts to separate function

--- src/modules/budsies/factories/bodypart.factory.ts
+++ src/modules/budsies/factories/bodypart.factory.ts
@@ -2,6 +2,24 @@ import ObjectBuilderInterface from '../types/object-builder.interface';
 import Bodypart from '../models/bodypart.model';
 import BodypartApiResponse from '../models/bodypart-api-response.interface';
 
+function getChildBodyPartsFromData (data: BodypartApiResponse): Bodypart[] | undefined {
+  if (!data.child_bodyparts) {
+    return;
+  }
+
+  return data.child_bodyparts.map((bodyPart) => {
+    return new Bodypart(
+      bodyPart.id + '',
+      bodyPart.code,
+      bodyPart.name,
+      +bodyPart.is_required === 1,
+      +bodyPart.max_values,
+      +bodyPart.sn,
+      false
+    )
+  });
+}
+
 const factory: ObjectBuilderInterface<Bodypart, BodypartApiResponse> = (data) => {
   const value = new Bodypart(
     data.id + '',
@@ -12,19 +30,7 @@ const factory: ObjectBuilderInterface<Bodypart, BodypartApiResponse> = (data) =>
     +data.sn,
     false,
     data.detailing_flag_text,
-    data.child_bodyparts
-      ? data.child_bodyparts.map((bodyPart) => {
-        return new Bodypart(
-          bodyPart.id + '',
-          bodyPart.code,
-          bodyPart.name,
-          +bodyPart.is_required === 1,
-          +bodyPart.max_values,
-          +bodyPart.sn,
-          false
-        )
-      })
-      : undefined
+    getChildBodyPartsFromData(data)
   );
 
   return value;
