59760c04da8c820fbcfa753e924c07bc707d5737
Maciej Rybaniec <maciej.rybaniec@gmail.com>
2023-04-05 08:57:00 +0200

    feat: 🎸 prettier eslint + json eslint

---
+++ engineering-toolkit/packages/eslint-config/src/json.ts
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ["plugin:jsonc/recommended-with-jsonc"],
+
+  overrides: [
+    {
+      files: ["**/*.{json,json5,jsonc}"],
+      parser: "jsonc-eslint-parser",
+      excludedFiles: ["package-lock.json"],
+    },
+  ],
+};
