5d71441c1ceb028835f81d25149cb1e03b0620bb
Maciej Rybaniec <maciej.rybaniec@gmail.com>
2023-04-11 19:05:42 +0200

    fix: 🐛 dependency circular

--- engineering-toolkit/github-actions/circular-dependency-action/src/index.ts
+++ engineering-toolkit/github-actions/circular-dependency-action/src/index.ts
@@ -3,10 +3,9 @@ const { parseDependencyTree, parseCircular, prettyCircular } = require('dpdm');
 
 try {
   const filesPath = core.getInput('filesPath');
-  parseDependencyTree(filesPath).then((tree: any) => {
+  parseDependencyTree(filesPath, {}).then((tree: any) => {
     const circulars = parseCircular(tree);
     console.log(prettyCircular(circulars));
-    
   });
 } catch (error) {
   core.setFailed(error.message);
