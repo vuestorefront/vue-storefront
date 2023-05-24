0ce2b4c24f2fa11dc490d665d501f4911e638c00
Matt Maribojoc <matthewmaribojoc@gmail.com>
2023-05-24 08:23:52 -0400

    docs: remove beta tag from sylius

--- packages/docs/.vuepress/integrations.js
+++ packages/docs/.vuepress/integrations.js
@@ -74,7 +74,7 @@ const INTEGRATIONS = {
       name: 'Sylius',
       link: 'https://github.com/BitBagCommerce/SyliusVueStorefront2Plugin',
       image: 'https://uploads-ssl.webflow.com/5e90e5cd5f86784ad554a3c2/5ed6275b7df0e61000941a54_sylius_logo.svg',
-      status: STATUS.BETA,
+      status: STATUS.STABLE,
       license: LICENSE.OPEN_SOURCE,
       maintainedBy: [{ name: 'BitBag', link: 'https://bitbag.io/' }],
     },
