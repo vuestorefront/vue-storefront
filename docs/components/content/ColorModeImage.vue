0e81fe19e45a950f6d0d0b843a4d8fe7b611a70d
Filip <f.rakowski@hotmail.com>
2023-07-21 13:29:42 +0200

    baseline docs

---
+++ docs/components/content/ColorModeImage.vue
@@ -0,0 +1,17 @@
+<template>
+  <img v-if="colorMode === 'dark'" :src="addDarkToImagePath(src)" :alt="alt" />
+  <img v-else :src="src" :alt="alt" />
+</template>
+
+<script setup>
+const colorMode = useColorMode();
+
+function addDarkToImagePath(imagePath) {
+  const parts = imagePath.split('.');
+  const extension = parts.pop();
+  const darkImagePath = `${parts.join('.')}_dark.${extension}`;
+  return darkImagePath;
+}
+
+defineProps(['src','alt'])
+</script>
