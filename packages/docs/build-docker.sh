aacc85ec31988dd7b1161ab07d451ec4dbe8b57a
Matt Maribojoc <matthewmaribojoc@gmail.com>
2023-08-13 20:05:34 -0500

    docs: remove old docs

--- packages/docs/build-docker.sh
+++
@@ -1,3 +0,0 @@
-TAG=`git rev-parse HEAD`
-docker build -t registry.storefrontcloud.io/docs-storefrontcloud-io/v2:${TAG:0:8} -f Dockerfile .
-docker push registry.storefrontcloud.io/docs-storefrontcloud-io/v2:${TAG:0:8}
