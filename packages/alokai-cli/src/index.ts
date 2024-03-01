10a23c58d8f65763d8f073df10194c6175572ced
Sergii Kirianov <s.kirianov@gmail.com>
2024-03-01 11:16:06 -0700

    alokai-cli poc

---
+++ packages/alokai-cli/src/index.ts
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+import path, { dirname } from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+
+process.on("SIGINT", () => process.exit(0));
+process.on("SIGTERM", () => process.exit(0));
+
+// Function to import and initialize command modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const nodeModulesPath = path.join(__dirname, "..", "node_modules");
+
+// Function to import and initialize command modules
+const loadCommandModules = (program, dir) => {
+  fs.readdirSync(nodeModulesPath).forEach(async (moduleName) => {
+    // Filter based on naming convention (e.g., cli-command-*)
+    try {
+      if (moduleName.startsWith("alokai-")) {
+        try {
+          // Construct the full path to the module
+          const modulePath = path.join(nodeModulesPath, moduleName);
+
+          console.log(moduleName, modulePath);
+          // Dynamically import the command module
+          const commandModule = await import(
+            `file://${modulePath}/dist/index.js`
+          );
+          // Initialize the command module with the Commander instance
+          if (typeof commandModule.default === "function") {
+            commandModule.default(program);
+          } else {
+            console.warn(
+              `Module ${moduleName} does not export a default function and was not loaded.`
+            );
+          }
+        } catch (error) {
+          console.error(`Error loading module ${moduleName}:`, error);
+        }
+      }
+    } catch (error) {
+      console.error(`Error loading module ${moduleName}:`, error);
+    }
+  });
+};
+
+async function main() {
+  const program = new Command()
+    .name("Alokai CLI")
+    .description("Alokai CLI is a tool to help you manage your Alokai projects")
+    .version("0.0.1", "-v, --version", "display the version number");
+
+  // Load command modules
+  loadCommandModules(program, __dirname);
+
+  program.parse();
+}
+
+main();
