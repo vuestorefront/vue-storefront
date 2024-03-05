59b595bd61d7b07a93daf4a0ac30b0c824d71a53
Sergii Kirianov <s.kirianov@gmail.com>
2024-03-05 06:50:03 -0700

    updates to package.json

--- packages/alokai-cli/src/index.ts
+++ packages/alokai-cli/src/index.ts
@@ -4,6 +4,12 @@ import { Command } from "commander";
 import path, { dirname } from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
+import {
+  spin,
+  intro,
+  print,
+  colors,
+} from "../node_modules/@alokai/cli-ui/dist/index.js";
 
 process.on("SIGINT", () => process.exit(0));
 process.on("SIGTERM", () => process.exit(0));
@@ -17,14 +23,12 @@ const nodeModulesPath = path.join(__dirname, "..", "node_modules");
 // Function to import and initialize command modules
 const loadCommandModules = (program, dir) => {
   fs.readdirSync(nodeModulesPath).forEach(async (moduleName) => {
-    // Filter based on naming convention (e.g., cli-command-*)
     try {
       if (moduleName.startsWith("alokai-")) {
         try {
           // Construct the full path to the module
           const modulePath = path.join(nodeModulesPath, moduleName);
 
-          console.log(moduleName, modulePath);
           // Dynamically import the command module
           const commandModule = await import(
             `file://${modulePath}/dist/index.js`
@@ -51,7 +55,54 @@ async function main() {
   const program = new Command()
     .name("Alokai CLI")
     .description("Alokai CLI is a tool to help you manage your Alokai projects")
-    .version("0.0.1", "-v, --version", "display the version number");
+    .version("0.0.1", "-v, --version", "display the version number")
+    .action(() => {
+
+      intro();
+    //   print("Example standard message");
+    //   print.info("Example info message");
+    //   print.success("Example success message");
+    //   print.warning("Example warning message");
+    //   print.error("Example error message");
+
+    //   print.highlight("Example highlight message standard");
+    //   print.highlight("Example highlight message with one **word**");
+    //   print.highlight(
+    //     "Example highlight message with **multiple** - **words**"
+    //   );
+    //   print.link("Example link %link% message", "https://www.alokai.io");
+    //   print.stepper([
+    //     "Step 1",
+    //     "Step 2",
+    //     "Step 3",
+    //     "Step 4",
+    //     "Step 5",
+    //     "Step 6",
+    //   ]);
+    //   print.stepper(
+    //     ["Step 1", "Step 2", "Step 3", "Step 4", "Step 5", "Step 6"],
+    //     "numbers"
+    //   );
+    //   print.stepper(
+    //     ["Step 1", "Step 2", "Step 3", "Step 4", "Step 5", "Step 6"],
+    //     "dashes"
+    //   );
+
+    //   print.info(`${colors.emojis.rocket} Example emoji message`);
+
+    //   print.debug("Example debug title", {
+    //     key: "value",
+    //     key2: "value2",
+    //   });
+
+      // const spinner = spin();
+
+      // spinner.start("Starting Alokai CLI Spinner");
+
+      // setTimeout(() => {
+      //   spinner.succeed("Alokai CLI Spinner Succeeded");
+      // }, 15000);
+    });
 
   // Load command modules
   loadCommandModules(program, __dirname);
