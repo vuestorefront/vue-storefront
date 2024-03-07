93ff8a9431fcf2ef7526f247aa3f3a90d60a0393
Wojciech Sikora <wsikora@vuestorefront.io>
2024-03-07 15:45:08 +0100

    refactor: use getRequestHeaders naming for both nuxt and next

--- packages/storefront/packages/nuxt/__tests__/app/sdk.config.ts
+++ packages/storefront/packages/nuxt/__tests__/app/sdk.config.ts
@@ -1,11 +1,11 @@
-import { Endpoints, exampleSdkModule } from "@storefront/shared";
+import { Endpoints } from "@storefront/shared";
 
 export default defineSdkConfig(
-  ({ buildModule, moduleFromEndpoints, middlewareUrl, getCookieHeader }) => {
+  ({ buildModule, moduleFromEndpoints, middlewareUrl, getRequestHeaders }) => {
     return {
       example: buildModule(moduleFromEndpoints<Endpoints>, {
         apiUrl: `${middlewareUrl}/test_integration`,
-        defaultRequestConfig: { headers: { ...getCookieHeader() } },
+        defaultRequestConfig: { headers: { ...getRequestHeaders() } },
       }),
     };
   }
