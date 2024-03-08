8f22db29c9589029a4fdb9cade35481cd6dc70e4
Wojciech Sikora <wsikora@vuestorefront.io>
2024-03-08 15:31:14 +0100

    add comments

--- packages/sdk/jest.config.unit.cjs
+++ packages/sdk/jest.config.unit.cjs
@@ -1,3 +1,4 @@
+// TODO: make this file work with ts after https://github.com/vuestorefront/engineering-toolkit/pull/40/files
 const { baseConfig } = require("@vue-storefront/jest-config");
 
 module.exports = {
