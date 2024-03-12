193b31757858e189529bcae028bc606ee6239b6d
Wojciech Sikora <wsikora@vuestorefront.io>
2024-03-12 09:55:13 +0100

    exit pre mode and refactor

--- packages/storefront/shared/src/index.ts
+++ packages/storefront/shared/src/index.ts
@@ -1,5 +1,4 @@
 /* eslint-disable import/no-relative-packages */
-export * from "./__tests__/sdk";
 export * from "./__tests__/middleware/types";
 export * from "./helpers";
 export * from "./types";
