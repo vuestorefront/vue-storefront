f5b18468dd642dd0b8d54254c42ad233d5af3dce
Sergii Kirianov <s.kirianov@gmail.com>
2024-03-28 10:42:33 -0600

    alokai-cli core

--- packages/alokai-cli/src/index.ts
+++ packages/alokai-cli/src/index.ts
@@ -4,12 +4,6 @@ import { Command } from "commander";
 import path, { dirname } from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
-import {
-  spin,
-  intro,
-  print,
-  colors,
-} from "../node_modules/@alokai/cli-ui/dist/index.js";
 
 process.on("SIGINT", () => process.exit(0));
 process.on("SIGTERM", () => process.exit(0));
@@ -57,57 +51,11 @@ async function main() {
     .description("Alokai CLI is a tool to help you manage your Alokai projects")
     .version("0.0.1", "-v, --version", "display the version number")
     .action(() => {
+      // Load command modules
+      loadCommandModules(program, __dirname);
 
-      intro();
-    //   print("Example standard message");
-    //   print.info("Example info message");
-    //   print.success("Example success message");
-    //   print.warning("Example warning message");
-    //   print.error("Example error message");
-
-    //   print.highlight("Example highlight message standard");
-    //   print.highlight("Example highlight message with one **word**");
-    //   print.highlight(
-    //     "Example highlight message with **multiple** - **words**"
-    //   );
-    //   print.link("Example link %link% message", "https://www.alokai.io");
-    //   print.stepper([
-    //     "Step 1",
-    //     "Step 2",
-    //     "Step 3",
-    //     "Step 4",
-    //     "Step 5",
-    //     "Step 6",
-    //   ]);
-    //   print.stepper(
-    //     ["Step 1", "Step 2", "Step 3", "Step 4", "Step 5", "Step 6"],
-    //     "numbers"
-    //   );
-    //   print.stepper(
-    //     ["Step 1", "Step 2", "Step 3", "Step 4", "Step 5", "Step 6"],
-    //     "dashes"
-    //   );
-
-    //   print.info(`${colors.emojis.rocket} Example emoji message`);
-
-    //   print.debug("Example debug title", {
-    //     key: "value",
-    //     key2: "value2",
-    //   });
-
-      // const spinner = spin();
-
-      // spinner.start("Starting Alokai CLI Spinner");
-
-      // setTimeout(() => {
-      //   spinner.succeed("Alokai CLI Spinner Succeeded");
-      // }, 15000);
+      program.parse();
     });
-
-  // Load command modules
-  loadCommandModules(program, __dirname);
-
-  program.parse();
 }
 
 main();
