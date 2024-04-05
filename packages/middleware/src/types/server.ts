1810537ef4ca24331ec3c10170f30d6c2940ec84
Tomasz Góral <tomasz.gl69@gmail.com>
2024-04-05 12:31:07 +0200

    feat: configuration options in create server (#7104)
    * feat: configuration options in create server
    
    * chore: add changeset
    
    * chore: revert default cors options

--- packages/middleware/src/types/server.ts
+++ packages/middleware/src/types/server.ts
@@ -1,3 +1,6 @@
+import { CorsOptions, CorsOptionsDelegate } from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 import { TObject } from "./base";
 import {
   ApiClientExtension,
@@ -115,3 +118,33 @@ export type CreateApiProxyFn = <CONFIG, API, CLIENT>(
   givenConfig: any,
   customApi?: any
 ) => ApiInstance<CONFIG, API, CLIENT>;
+
+export interface CreateServerOptions {
+  /**
+   * The options for the `express.json` middleware.
+   * If not provided, the default options will be used.
+   * @see https://www.npmjs.com/package/body-parser
+   */
+  bodyParser?: bodyParser.OptionsJson;
+  /**
+   * The options for the `cookie-parser` middleware.
+   * If not provided, the default options will be used.
+   * @see https://www.npmjs.com/package/cookie-parser
+   */
+  cookieParser?: {
+    secret: string | string[];
+    options: cookieParser.CookieParseOptions;
+  };
+  /**
+   * The options for the `cors` middleware.
+   * If not provided, the following configuration will be used:
+   * ```json
+   * {
+   *  "credentials": true,
+   *  "origin": ["http://localhost:3000", "http://localhost:4000"]
+   * }
+   * ```
+   * @see https://www.npmjs.com/package/cors
+   */
+  cors?: CorsOptions | CorsOptionsDelegate;
+}
