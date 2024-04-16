d6ddd159218f661f2972d38b980176829de6de93
Artur Tagisow <artur@tagisow.dev>
2024-04-16 12:47:10 +0200

    Don't cache TS results
    This is EXTREMELY annyoing that if you run build once and it fails,
    the failed build will be cached. Now even if you fix the underlying
    issue and rebuild, the result will still be cached and you'll be
    getting a build error even though you fixed everything.
    
    This wastes a lot of time and it must be the third time I've come
    across this.

--- engineering-toolkit/rollup-config/src/apiClient.ts
+++ engineering-toolkit/rollup-config/src/apiClient.ts
@@ -25,7 +25,7 @@ export function generateServerConfig(pkg: any) {
       nodeResolve({
         extensions,
       }),
-      typescript(),
+      typescript({ clean: true }),
       commonjs({
         extensions,
       }),
