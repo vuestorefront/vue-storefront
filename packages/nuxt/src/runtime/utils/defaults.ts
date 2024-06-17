a130cd16bd9ad487e40d92e064431e1013d87bd8
Michael Kurowski <2772942+michaelKurowski@users.noreply.github.com>
2024-06-17 13:14:00 +0300

    fix: shared package wasn't being bundled with the release of the package (#7194)

--- packages/nuxt/src/runtime/utils/defaults.ts
+++ packages/nuxt/src/runtime/utils/defaults.ts
@@ -1,5 +1,22 @@
-import { contextConfig } from "@storefront/shared";
-
 export function getDefaults() {
-  return contextConfig;
+  return {
+    unifiedCommerce: {
+      middlewareModule: {
+        defaultMethodsRequestConfig: {
+          getCategories: { method: "GET" },
+          getProductDetails: { method: "GET" },
+          getProductReviews: { method: "GET" },
+          getProducts: { method: "GET" },
+          searchProducts: { method: "GET" },
+        },
+      },
+    },
+    unifiedCms: {
+      middlewareModule: {
+        defaultMethodsRequestConfig: {
+          getEntries: { method: "GET" },
+        },
+      },
+    },
+  } as const;
 }
