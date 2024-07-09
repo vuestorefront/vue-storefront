f46438623bd509389837d2325d492d22068b1714
Szymon Dziewoński <szymon.dziewonski@gmail.com>
2024-07-09 13:34:56 +0200

    chore: middleware not available error handling

--- packages/sdk/src/error.ts
+++ packages/sdk/src/error.ts
@@ -9,6 +9,12 @@ export class SDKError extends Error {
 }
 
 export const handleError = (err: Error) => {
+  if ((err.cause as { code: unknown })?.code === "ECONNREFUSED")
+    return new SDKError(
+      "Please check if the middleware is running and is accessible by frontend app",
+      err
+    );
+
   const errMsg = err?.message ?? "[SDK] Error: something went wrong.";
 
   return new SDKError(errMsg, err);
