95ff33d93c207f31fe94849bc357c8f352d69d7c
Artur Tagisow <artur@tagisow.dev>
2024-08-27 18:16:33 +0200

    Add terminus

--- packages/middleware/src/createServer.ts
+++ packages/middleware/src/createServer.ts
@@ -1,10 +1,17 @@
 import consola from "consola";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import type { Express } from "express";
 import express from "express";
 import type { HelmetOptions } from "helmet";
 import helmet from "helmet";
+import http, { Server } from "node:http";
+import {
+  HealthCheckError,
+  TerminusOptions,
+  createTerminus,
+} from "@godaddy/terminus";
+import { setTimeout } from "node:timers/promises";
+
 import { registerIntegrations } from "./integrations";
 import type {
   Helmet,
@@ -29,7 +36,7 @@ async function createServer<
 >(
   config: MiddlewareConfig<TIntegrationContext>,
   options: CreateServerOptions = {}
-): Promise<Express> {
+): Promise<Server> {
   const app = express();
 
   app.use(express.json(options.bodyParser));
@@ -78,12 +85,48 @@ async function createServer<
     callApiFunction
   );
 
+  // This could instead be implemented as a healthcheck within terminus, but we don't want /healthz to change response if app received SIGTERM
   app.get("/healthz", (_req, res) => {
     res.end("ok");
   });
 
+  const terminusOptions: TerminusOptions = {
+    // health check options
+    healthChecks: {
+      verbatim: true,
+      "/readyz": async () => {
+        if (
+          !Array.isArray(options.readinessChecks) ||
+          options.readinessChecks.length === 0
+        ) {
+          return;
+        }
+        const calledReadinessChecks = options.readinessChecks.map((fn) => fn());
+
+        const readinessErrors = (
+          await Promise.allSettled(calledReadinessChecks)
+        ).reduce<unknown[]>(
+          (errors, settledReadinessCheck) =>
+            settledReadinessCheck.status === "rejected"
+              ? [...errors, settledReadinessCheck.reason]
+              : errors,
+          []
+        );
+
+        if (readinessErrors.length) {
+          throw new HealthCheckError("Readiness check failed", readinessErrors);
+        }
+      },
+    },
+    // In case some requests are still being handled when SIGTERM was received, naively wait in hopes that they will be resolved in that time, and only then shut down the process
+    beforeShutdown: () => setTimeout(10 ** 4),
+    useExit0: true,
+  };
+
+  const server = http.createServer(app);
+  createTerminus(server, terminusOptions);
   consola.success("Middleware created!");
-  return app;
+  return server;
 }
 
 export { createServer };
