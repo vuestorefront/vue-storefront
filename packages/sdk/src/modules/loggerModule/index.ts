7b9df5b683ca19f68505c5fdb9ceb309aefe9741
Dawid Ziobro <dziobro@vuestorefront.io>
2024-10-08 16:22:12 +0200

    feat: sdk logger wrapper

--- packages/sdk/src/modules/loggerModule/index.ts
+++ packages/sdk/src/modules/loggerModule/index.ts
@@ -1,14 +1,21 @@
 import { LoggerFactory, LoggerType } from "@vue-storefront/logger";
 import type { Module } from "../../types";
 import type { LoggerModuleConfig } from "./types";
+import { wrapLogger } from "./wrapper";
 
 export const loggerModule = (options?: LoggerModuleConfig) => {
   const logger =
     options?.handler ?? LoggerFactory.create(LoggerType.ConsolaGcp, options);
 
+  const wrappedLogger = wrapLogger(logger, {
+    alokai: {
+      context: "storefront",
+    },
+  });
+
   return {
     connector: {
-      ...logger,
+      ...wrappedLogger,
     },
   } satisfies Module;
 };
---
+++ packages/sdk/src/modules/loggerModule/wrapper.ts
@@ -0,0 +1,27 @@
+import type { LoggerInterface } from "./types";
+
+const methodsToExclude = ["log"];
+
+export function wrapLogger(
+  logger: LoggerInterface,
+  externalData: Record<string, any>
+): LoggerInterface {
+  return new Proxy(logger, {
+    get(target, prop) {
+      const targetProp = target[prop as keyof LoggerInterface];
+      if (
+        typeof targetProp === "function" &&
+        !methodsToExclude.includes(prop as string)
+      ) {
+        return (...args: any[]) => {
+          const [logData, metadata] = args;
+          targetProp(logData, {
+            ...metadata,
+            ...externalData,
+          });
+        };
+      }
+      return targetProp;
+    },
+  });
+}
