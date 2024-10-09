0191275e241bd9fd487f999809e739f8b28ae494
Dawid Ziobro <dziobro@vuestorefront.io>
2024-10-09 11:04:13 +0200

    feat: validate config

--- packages/sdk/src/modules/loggerModule/index.ts
+++ packages/sdk/src/modules/loggerModule/index.ts
@@ -2,10 +2,17 @@ import { LoggerFactory, LoggerType } from "@vue-storefront/logger";
 import type { Module } from "../../types";
 import type { LoggerModuleConfig } from "./types";
 import { wrapLogger } from "./wrapper";
+import { isInvalidConfig } from "./utils";
 
 export const loggerModule = (options?: LoggerModuleConfig) => {
   const logger =
     options?.handler ?? LoggerFactory.create(LoggerType.ConsolaGcp, options);
+
+  if (isInvalidConfig(options)) {
+    console.warn(
+      "Both handler and options are provided. Using handler, options will be ignored."
+    );
+  }
 
   const wrappedLogger = wrapLogger(logger, {
     alokai: {
---
+++ packages/sdk/src/modules/loggerModule/utils.ts
@@ -0,0 +1,13 @@
+import type { LoggerModuleConfig } from "./types";
+
+const isEmptyObject = (obj = {}) => {
+  return Object.keys(obj).length === 0;
+};
+
+export const isInvalidConfig = (config?: LoggerModuleConfig) => {
+  if (config) {
+    const { handler, ...rest } = config;
+    return !!handler && !isEmptyObject(rest);
+  }
+  return false;
+};
