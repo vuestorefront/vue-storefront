507dba5fe80097e4b107d6483dc4a626ce977d83
Filip <f.rakowski@hotmail.com>
2024-10-24 15:54:15 +0200

    Change integrations orchestration to federation

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ["sf-docs-base"],
+  routeRules: {
+    "/middleware/guides/orchestration": {
+      redirect: "/middleware/guides/federation",
+    },
+  },
 });
+
