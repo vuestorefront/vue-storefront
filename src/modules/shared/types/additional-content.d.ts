24c0ed8860bde68187555204a18a3501e26842ae
Anton Sylka <antonsylka1004@gmail.com>
2024-11-15 16:59:34 +0700

    Refs #33411: Additional Content - move type definition to shared module

--- src/modules/shared/types/additional-content.d.ts
+++ src/modules/shared/types/additional-content.d.ts
@@ -3,6 +3,7 @@ import Vue, { VueConstructor } from 'vue';
 declare module 'core/types/additional-content' {
   interface AdditionalContent {
     privacyPolicyAdditionalLinks?: {component: VueConstructor<Vue>, key: string}[],
-    financialIncentivesLinks?: {component: VueConstructor<Vue>, key: string}[]
+    financialIncentivesLinks?: {component: VueConstructor<Vue>, key: string}[],
+    footerLinks?: {component: VueConstructor<Vue>, key: string}[]
   }
 }
