62fbd745948ab0143902312722bc4cce390d62e3
Tomasz Góral <tomasz.gl69@gmail.com>
2024-12-11 15:15:47 +0700

    feat!: remove `generate` command from cli

--- packages/cli/src/domains/generate/project-name/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as getProjectName } from "./getProjectName";
-export { default as formatToProjectName } from "./formatToProjectName";
--- packages/cli/src/domains/generate/theme/uncommentProjectOnlyBlocks.ts
+++
@@ -1,8 +0,0 @@
-const uncommentProjectOnlyBlocks = (source: string): string => {
-  return source.replace(
-    /\s+(\/\* project-only-start)(.*?)\s+(project-only-end \*\/)/gs,
-    (_, __, block) => block
-  );
-};
-
-export default uncommentProjectOnlyBlocks;
