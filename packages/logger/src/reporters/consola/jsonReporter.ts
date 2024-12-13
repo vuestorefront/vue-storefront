35e7274c5d9ce36cec00f22b1ea8c3e352e3fe27
lukasborawski <lukas.borawski@gmail.com>
2024-12-13 11:29:59 +0000

    chore(in-4422): logger prettier

--- packages/logger/src/reporters/consola/jsonReporter.ts
+++ packages/logger/src/reporters/consola/jsonReporter.ts
@@ -1,13 +1,14 @@
 import type { LogObject } from "consola";
+import { jsonReporterPrettier } from "./prettier";
 
 export const jsonReporter = (logObject: LogObject) => {
   const defLogType = "log";
   const logType = logObject?.type ?? defLogType;
   const logFn = console[logType] ?? console[defLogType];
 
-  if (process.env.NODE_ENV === "development" || typeof window !== "undefined") {
-    logFn(logObject.args[0].structuredLog);
+  if (process.env.NODE_ENV === "development") {
+    jsonReporterPrettier(logObject.args[0].structuredLog, logFn);
   } else {
-    logFn(JSON.stringify(logObject.args[0].structuredLog));
+    // logFn(JSON.stringify(logObject.args[0].structuredLog));
   }
 };
---
+++ packages/logger/src/reporters/consola/prettier.ts
@@ -0,0 +1,99 @@
+/**
+ * Formats the severity level of a log message with ANSI escape codes for color.
+ *
+ * @param {String} severity - The severity level of the log message.
+ * @returns {String} - The formatted severity level with ANSI escape codes for color.
+ */
+const getSeverityLog = (severity) => {
+  switch (severity) {
+    case "INFO":
+    case "info":
+      return "\x1b[30;104mINFO\x1b[0m"; // Blue background
+    case "ERROR":
+    case "error":
+      return "\x1b[41mERROR\x1b[0m"; // Red background
+    case "EMERGENCY":
+    case "emergency":
+      return "\x1b[45mEMERGENCY\x1b[0m"; // Magenta background
+    case "ALERT":
+    case "alert":
+      return "\x1b[45mALERT\x1b[0m"; // Magenta background
+    case "CRITICAL":
+    case "critical":
+      return "\x1b[45mCRITICAL\x1b[0m"; // Magenta background
+    case "WARNING":
+    case "warning":
+      return "\x1b[48;5;208mWARNING\x1b[0m"; // Yellow background
+    case "NOTICE":
+    case "notice":
+      return "\x1b[30;104mNOTICE\x1b[0m"; // Blue background
+    case "DEBUG":
+    case "debug":
+      return "\x1b[40mDEBUG\x1b[0m"; // Black background
+    default:
+      return severity; // Return the severity as is if it doesn't match any case
+  }
+};
+
+// Prettier and lint are disabled due to the way the code is formatted / printed within the console.
+// eslint-disable-file no-console
+// prettier-ignore
+/**
+ * Formats and logs the logObject in a prettier way based on the mode.
+ *
+ * @param {Object} logObject - The log object to be formatted and logged.
+ * @param {Function} logFn - The logging function to use.
+ * @param {String} mode - The mode in which the log is being generated. Defaults to "server".
+ */
+export const jsonReporterPrettier = (logObject: Record<any, any>, logFn: Function, mode: string = "server") => {
+ if (mode === "server") {
+   const { timestamp, severity, message, alokai, metadata } = logObject;
+   const severityLog = getSeverityLog(severity);
+   const isSSR = typeof globalThis.window === "undefined";
+
+   const troubleshooting = {
+     message:
+       "This issue is likely caused by a version mismatch in the middleware stack.",
+     steps: [
+       "Update all middleware packages.",
+       "Ensure API client packages are also updated",
+     ],
+   };
+
+   if (isSSR && alokai?.context === "middleware") {
+     logFn(`\x1b[90m:: Alokai Log: Middleware ::\x1b[0m
+
+🔥 Severity: ${severityLog}
+🕓 Timestamp: \x1b[93m${new Date(timestamp).toLocaleString()}\x1b[0m (${timestamp})
+💬 Message: ${message}
+`);
+     if (troubleshooting) {
+       logFn(`🛠️ Troubleshooting: ${troubleshooting.message}
+🚶 Steps to follow:
+${troubleshooting.steps.map((step) => `➡️ ${step}`).join("\n")}
+`);
+     }
+     if (alokai.context) {
+       logFn(`🛍️ Alokai Context: ${alokai.context}`);
+     }
+     if (metadata) {
+       logFn(`📁 Metadata: ${JSON.stringify(metadata)}`);
+     }
+     logFn(``);
+   } else if (isSSR && alokai?.context === "storefront") {
+     console.log(`\x1b[90m:: Alokai Log: Storefront ::\x1b[0m
+
+🔥 Severity: ${severityLog}
+🕓 Timestamp: \x1b[93m${new Date(timestamp).toLocaleString()}\x1b[0m (${timestamp})
+💬 Message: ${message}
+
+${troubleshooting ? `🛠️ Troubleshooting: ${troubleshooting.message}\n🚶 Steps to follow:\n${troubleshooting.steps.map((step) => `➡️ ${step}`).join("\n")}` : ""}
+
+${alokai?.context ? `🛍️ Alokai Context: ${alokai.context}` : ""}
+${metadata ? `📁 Metadata: ${JSON.stringify(metadata)}` : ""}
+`);
+   } else {
+     console.log(`Alokai Log ($raw): ${JSON.stringify(logObject)}`);
+   }
+ }
+};
