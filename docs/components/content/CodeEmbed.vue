6b026fc720a38fed72cb21cfe5102ab147c20171
Wojciech Sikora <wsikora@vuestorefront.io>
2025-06-03 22:27:14 +0200

    chore: remove docs

--- docs/components/content/CodeEmbed.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<div>
-  <iframe :src="config" width="100%" height="100%" fetchpriority="low"></iframe>
-</div>
-</template>
-
-<script setup>
-defineProps(['config'])
-</script>
--- docs/nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  extends: ["sf-docs-base"],
-  routeRules: {
-    "/middleware/guides/orchestration": {
-      redirect: "/middleware/guides/federation",
-    },
-  },
-});
