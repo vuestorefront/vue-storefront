829e38a9f9adc60a883e79d2f5b68e38934c4372
Anton Sylka <antonsylka1004@gmail.com>
2025-07-03 15:51:43 +0800

    Refs #34906: Merge Actions - wait for totals to update to avoid unexpected behavior

--- core/modules/cart/store/actions/itemActions.ts
+++ core/modules/cart/store/actions/itemActions.ts
@@ -124,7 +124,7 @@ const itemActions = {
   async updateClientAndServerItem ({ dispatch }, { product, forceClientState = false, forceUpdateServerItem = false }) {
     await dispatch('updateItem', { product });
 
-    const diffLog = await dispatch('sync', { forceClientState, forceUpdateServerItem });
+    const diffLog = await dispatch('sync', { forceClientState, forceUpdateServerItem, waitForTotalsUpdate: false });
     throwServerErrorFromDiffLog(diffLog);
   }
 }
