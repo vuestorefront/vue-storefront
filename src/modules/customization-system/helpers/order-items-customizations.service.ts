ba9c7cadcebab6de6e41ee6f2dd09ec19f3678db
Anton Sylka <antonsylka1004@gmail.com>
2025-07-05 13:55:16 +0800

    Refs #34784: Order Item Customization - add service for load/update order items customization state

---
+++ src/modules/customization-system/helpers/order-items-customizations.service.ts
@@ -0,0 +1,56 @@
+import config from 'config';
+
+import { TaskQueue } from '@vue-storefront/core/lib/sync';
+import Task from '@vue-storefront/core/lib/sync/types/Task';
+
+import { DraftPlushie } from '../types/draft-plushie.interface';
+
+export async function fetchOrderItemCustomizationsState (orderItemId: string): Promise<DraftPlushie> {
+  const url = `${config.budsies.endpoint}/customizations/order-items/states?token={{token}}&orderItemId=${orderItemId}`;
+
+  const result = await TaskQueue.execute({
+    url,
+    payload: {
+      headers: { 'Accept': 'application/json' },
+      mode: 'cors',
+      method: 'GET'
+    },
+    silent: true
+  });
+
+  if (result.code !== 200) {
+    throw new Error(`Failed to fetch order item customizations state: ${result.result}`);
+  }
+
+  return result.result as DraftPlushie;
+}
+
+export async function saveOrderItemCustomizationsState (payload: DraftPlushie): Promise<Task> {
+  const url = `${config.budsies.endpoint}/customizations/order-items/states?token={{token}}`;
+
+  return TaskQueue.execute({
+    url,
+    payload: {
+      headers: { 'Accept': 'application/json' },
+      mode: 'cors',
+      method: 'POST',
+      body: JSON.stringify(payload)
+    },
+    silent: true
+  });
+}
+
+export async function submitOrderItemCustomizationsState (payload: { orderItemId: string }): Promise<Task> {
+  const url = `${config.budsies.endpoint}/customizations/order-items/submit-requests`;
+
+  return TaskQueue.execute({
+    url,
+    payload: {
+      headers: { 'Accept': 'application/json' },
+      mode: 'cors',
+      method: 'POST',
+      body: JSON.stringify(payload)
+    },
+    silent: true
+  });
+}
---
+++ src/modules/customization-system/types/draft-plushie.interface.ts
@@ -0,0 +1,6 @@
+import { CustomizationStateItem } from './customization-state-item.interface';
+
+export interface DraftPlushie {
+  id: string,
+  customization_state: CustomizationStateItem[]
+}
