6bbfd58f111bd50269ecfd9a7635bed6ec0a7327
Anton Sylka <antonsylka1004@gmail.com>
2025-07-22 15:16:55 +0800

    Refs #34957: Order Items Customizations Service - avoid user logout after request fails with 401 status code

--- src/modules/customization-system/helpers/order-items-customizations.service.ts
+++ src/modules/customization-system/helpers/order-items-customizations.service.ts
@@ -1,11 +1,32 @@
+import fetch from 'isomorphic-fetch';
 import config from 'config';
 
 import { TaskQueue } from '@vue-storefront/core/lib/sync';
-import Task from '@vue-storefront/core/lib/sync/types/Task';
+import EventBus from '@vue-storefront/core/compatibility/plugins/event-bus'
 
-import { DraftPlushie } from '../types/draft-plushie.interface';
+import { BEFORE_STORE_BACKEND_API_REQUEST } from 'src/modules/shared';
 
-export async function fetchOrderItemCustomizationsState (orderItemId: string): Promise<DraftPlushie> {
+import { DraftOrderItem } from '../types/draft-order-item.interface';
+
+async function postRequest (url: string, body: string): Promise<Response> {
+  const mode: RequestMode = 'cors';
+  const eventPayload = {
+    url
+  };
+
+  EventBus.$emit(BEFORE_STORE_BACKEND_API_REQUEST, eventPayload);
+
+  const requestPayload = {
+    method: 'POST',
+    headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+    mode,
+    body
+  }
+
+  return fetch(url, requestPayload);
+}
+
+export async function fetchOrderItemCustomizationsState (orderItemId: string): Promise<DraftOrderItem> {
   const url = `${config.budsies.endpoint}/customizations/order-items/states?token={{token}}&orderItemId=${orderItemId}`;
 
   const result = await TaskQueue.execute({
@@ -21,33 +42,28 @@ export async function fetchOrderItemCustomizationsState (orderItemId: string): P
     throw new Error(`Failed to fetch order item customizations state: ${result.result}`);
   }
 
-  return result.result as DraftPlushie;
+  return result.result as DraftOrderItem;
 }
 
-export async function saveOrderItemCustomizationsState (payload: DraftPlushie): Promise<Task> {
-  const url = `${config.budsies.endpoint}/customizations/order-items/states?token={{token}}`;
+export async function saveOrderItemCustomizationsState (payload: DraftOrderItem, userToken: string): Promise<void> {
+  const url = `${config.budsies.endpoint}/customizations/order-items/states?token=${userToken}`;
 
-  return TaskQueue.execute({
-    url,
-    payload: {
-      headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-      mode: 'cors',
-      method: 'POST',
-      body: JSON.stringify(payload)
-    }
-  });
+  const response = await postRequest(url, JSON.stringify(payload));
+
+  if (response.status !== 200) {
+    throw new Error(`Failed to save order item customizations state`);
+  }
 }
 
-export async function submitOrderItemCustomizationsState (payload: { order_item_id: string }): Promise<Task> {
-  const url = `${config.budsies.endpoint}/customizations/order-items/submit-requests`;
+export async function submitOrderItemCustomizationsState (
+  payload: { order_item_id: string },
+  userToken: string
+): Promise<void> {
+  const url = `${config.budsies.endpoint}/customizations/order-items/submit-requests?token=${userToken}`;
 
-  return TaskQueue.execute({
-    url,
-    payload: {
-      headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-      mode: 'cors',
-      method: 'POST',
-      body: JSON.stringify(payload)
-    }
-  });
+  const response = await postRequest(url, JSON.stringify(payload));
+
+  if (response.status !== 200) {
+    throw new Error(`Failed to submit order item customizations state`);
+  }
 }
