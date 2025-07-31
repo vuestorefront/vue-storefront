706e3aa13bf46bf7f81963545072d435e6f4e885
Anton Sylka <antonsylka1004@gmail.com>
2025-07-31 11:56:04 +0800

    Refs #35018: Persisted Data - remove redundant changes

--- src/modules/persisted-customer-data/index.ts
+++ src/modules/persisted-customer-data/index.ts
@@ -47,7 +47,6 @@ export const PersistedCustomerDataModule: StorefrontModule = async function ({ s
   store.subscribe(localStorageSynchronization.setItems);
 
   store.dispatch(`${SN_PERSISTED_CUSTOMER_DATA}/synchronize`);
-
   EventBus.$on('user-after-logout', () => {
     store.commit(SET_PERSISTED_CUSTOMER_EMAIL, undefined);
     store.commit(SET_PERSISTED_CUSTOMER_FIRST_NAME, undefined);
