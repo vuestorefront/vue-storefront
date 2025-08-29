1cf560aaacb59b460146a31c3450b34c46ebd5fc
Andrey Gorbunov <andrey@optimuspro.ru>
2025-08-29 19:37:46 +0000

    VSCode diff creation task - include the Braintree module changes into the diff

--- tools/createDiff.sh
+++ tools/createDiff.sh
@@ -10,11 +10,18 @@ cd ..
 
 targetBranch="${1:-petsies-theme}"
 themeTargetBranch="${2:-petsies-theme}"
+moduleBraintreeTargetBranch="${3:-optimuspro}"
 
 git diff $(git merge-base HEAD "$targetBranch")..HEAD > changes.diff
 
 cd "src/themes/petsies-capybara/"
 
 git diff $(git merge-base HEAD "$themeTargetBranch")..HEAD --src-prefix=a/src/themes/petsies-capybara/ --dst-prefix=b/src/themes/petsies-capybara/ >> ../../../changes.diff
+
+cd "$originalPath"
+
+cd "src/modules/payment-braintree"
+
+git diff $(git merge-base HEAD "$moduleBraintreeTargetBranch")..HEAD --src-prefix=a/src/modules/payment-braintree/ --dst-prefix=b/src/modules/payment-braintree/ >> ../../../changes.diff
 
 cd "$originalPath"
