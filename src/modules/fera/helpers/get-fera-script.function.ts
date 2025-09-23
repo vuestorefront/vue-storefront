5d86e3ddce42a611cfc572a4dddad57a9984b4a3
Andrey Gorbunov <andrey@optimuspro.ru>
2025-09-23 09:37:00 +0000

    Revert "Refs #35185: FeraAi - disable temporary"
    This reverts commit 0d7314a2a7f4f1c25b32722aab966b91c8924c59.

--- src/modules/fera/helpers/get-fera-script.function.ts
+++ src/modules/fera/helpers/get-fera-script.function.ts
@@ -1,16 +1,15 @@
 export function getFeraScript (apiKey: string): string {
-  return '';
-  // return `
-  // <script>
-  //   window.feraStandaloneMode = true;
-  //   window.fera = window.fera || [];
-  //   window.fera.push('loadPlatformAdapter', "magento2");
-  //   window.fera.push('configure', {
-  //     api_key: "${apiKey}",
-  //     app_url: "https://app.fera.ai/",
-  //     api_url: "https://app.fera.ai/api/",
-  //   });
-  // </script>
-  // <script async="" type="application/javascript" src="https://cdn.fera.ai/js/v3/fera.js"></script>
-  // `;
+  return `
+  <script>
+    window.feraStandaloneMode = true;
+    window.fera = window.fera || [];
+    window.fera.push('loadPlatformAdapter', "magento2");
+    window.fera.push('configure', {
+      api_key: "${apiKey}",
+      app_url: "https://app.fera.ai/",
+      api_url: "https://app.fera.ai/api/",
+    });
+  </script>
+  <script async="" type="application/javascript" src="https://cdn.fera.ai/js/v3/fera.js"></script>
+  `;
 }
