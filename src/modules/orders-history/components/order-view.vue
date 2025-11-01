25cf436f6a6fb9cc8537a046970b8c7be59083a8
Anton Sylka <antonsylka1004@gmail.com>
2025-11-01 17:07:42 +0800

    Refs #35583: Order Item Actions - mock "Download Result" action

--- src/modules/orders-history/components/order-view.vue
+++ src/modules/orders-history/components/order-view.vue
@@ -60,7 +60,17 @@ export default defineComponent({
     });
 
     const orderItems = computed(() => {
-      return props.order.items;
+      return props.order.items.map((item) => {
+        // TODO: mock
+        item.available_actions.push({
+          code: 'download_results',
+          name: 'Download Result',
+          message: '',
+          blocking_progress: false,
+          url: null
+        })
+        return item;
+      });
     });
 
     return {
