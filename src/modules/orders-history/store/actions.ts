57b2e64c930176be0a3d432bb38326d9f31c1fda
Anton Sylka <antonsylka1004@gmail.com>
2025-12-26 16:59:11 +0800

    Refs #35896: Order History - add action for updating order address

--- src/modules/orders-history/store/actions.ts
+++ src/modules/orders-history/store/actions.ts
@@ -5,10 +5,11 @@ import { processURLAddress } from '@vue-storefront/core/helpers';
 import { TaskQueue } from '@vue-storefront/core/lib/sync';
 import RootState from '@vue-storefront/core/types/RootState';
 
-import { FETCH_ORDERS_HISTORY, FETCH_SUGGESTED_PRODUCTS, REORDER_ITEM, FETCH_ORDER_DETAILS, SUBMIT_TAX_ID_UPDATE_REQUEST } from '../types/store/actions';
+import { FETCH_ORDERS_HISTORY, FETCH_SUGGESTED_PRODUCTS, REORDER_ITEM, FETCH_ORDER_DETAILS, SUBMIT_TAX_ID_UPDATE_REQUEST, SUBMIT_ORDER_ADDRESS_UPDATE_REQUEST } from '../types/store/actions';
 import { OrdersHistoryState } from '../types/store/state';
 import { Order } from '../types/order';
 import { SET_ORDERS_HISTORY, SET_SUGGESTED_PRODUCTS, SET_IS_REORDERING_ITEM } from '../types/store/mutations';
+import { OrderAddress } from '../types/order-address';
 
 export const actions: ActionTree<OrdersHistoryState, RootState> = {
   async [FETCH_ORDERS_HISTORY] ({ commit }): Promise<Order[]> {
@@ -116,6 +117,27 @@ export const actions: ActionTree<OrdersHistoryState, RootState> = {
 
     if (resultCode !== 200) {
       const errorMessage = result?.errorMessage || 'Failed to submit Tax ID';
+      throw new Error(errorMessage);
+    }
+  },
+  async [SUBMIT_ORDER_ADDRESS_UPDATE_REQUEST] (_context, { orderId, address }: { orderId: string, address: OrderAddress }): Promise<void> {
+    const url = processURLAddress(`${config.budsies.endpoint}/order/address/update-requests?token={{token}}`);
+
+    const { country_id, address_type, email, entity_id, parent_id, vat_id, ...addressWithoutCountry } = address;
+
+    const { resultCode, result } = await TaskQueue.execute({
+      url,
+      payload: {
+        method: 'POST',
+        mode: 'cors',
+        headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+        body: JSON.stringify({ data: { address_id: entity_id, ...addressWithoutCountry } })
+      },
+      silent: true
+    });
+
+    if (resultCode !== 200) {
+      const errorMessage = result?.errorMessage || 'Failed to update shipping address';
       throw new Error(errorMessage);
     }
   }
