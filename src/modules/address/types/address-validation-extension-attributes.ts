07a2d9a5c7d8f910020ab8dabf2dcf16e9f166f7
Anton Sylka <antonsylka1004@gmail.com>
2025-12-30 16:33:29 +0800

    Refs #35901: Address Validation Extenstion Attributes - add suggested address field

--- src/modules/address/types/address-validation-extension-attributes.ts
+++ src/modules/address/types/address-validation-extension-attributes.ts
@@ -1,7 +1,10 @@
+import BaseAddressDetails from 'core/modules/checkout/types/BaseAddressDetails';
+
 import { ValidationStatus } from './validation-status';
 
 export interface AddressValidationExtensionAttributes {
   validation_status_id?: ValidationStatus,
   validation_customer_override?: boolean,
-  validation_warnings?: string
+  validation_warnings?: string,
+  validation_suggested_address?: Partial<BaseAddressDetails>
 }
