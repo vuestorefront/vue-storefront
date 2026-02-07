679c699a1e7eb3f6d08c22c906e29914395f762a
Andrey Gorbunov <andrey@optimuspro.ru>
2026-02-07 15:15:23 +0000

    Refs #36081: OrderItemProgressTracker - re-design the compact version

--- src/modules/orders-history/components/order-item-progress-tracker.vue
+++ src/modules/orders-history/components/order-item-progress-tracker.vue
@@ -1,35 +1,55 @@
 <template>
-  <div class="order-item-progress-tracker" :title="$t('Progress Tracker')">
-    <div class="_step-counter -vertical" v-if="isVertical">
-      {{ $t('Step {current} of {total}', {current: currentStepIndex + 1, total: filteredStatusesCount}) }}
-    </div>
+  <div
+    class="order-item-progress-tracker"
+    :class="{'-compact': !isVertical}"
+    :title="$t('Progress Tracker')"
+  >
+    <template v-if="isVertical">
+      <div class="_step-counter -vertical">
+        {{ $t('Step {current} of {total}', {current: currentStepIndex + 1, total: filteredStatusesCount}) }}
+      </div>
 
-    <div
-      class="_progress-tracker"
-      :class="{'-vertical': isVertical}"
-    >
-      <div
-        class="_status-item"
-        :class="{
-          '-active': status.statusData.id === activeStatus.id,
-          '-completed': status.index < currentStepIndex || lastStatus.id === activeStatus.id
-        }"
-        :key="status.statusData.id"
-        v-for="status in statusesToDisplay"
-      >
-        <div class="_step-counter" v-if="!isVertical && activeStatus.id === status.statusData.id">
-          {{ $t('Step {current} of {total}', {current: currentStepIndex + 1, total: filteredStatusesCount}) }}
-        </div>
-
-        <div class="_mark" />
-
-        <template v-if="shouldShowStatusName(status)">
+      <div class="_progress-tracker -vertical">
+        <div
+          class="_status-item"
+          :class="{
+            '-active': status.statusData.id === activeStatus.id,
+            '-completed': status.index < currentStepIndex || lastStatus.id === activeStatus.id
+          }"
+          :key="status.statusData.id"
+          v-for="status in statusesToDisplay"
+        >
+          <div class="_mark" />
           <span class="_name">
             {{ status.statusData.name }}
           </span>
-        </template>
+        </div>
       </div>
-    </div>
+    </template>
+
+    <template v-else>
+      <div
+        class="_progress-bar"
+        role="progressbar"
+        :aria-valuenow="currentStepIndex + 1"
+        aria-valuemin="1"
+        :aria-valuemax="filteredStatusesCount"
+        :aria-label="$t('Order Progress')"
+      >
+        <div class="_bar-track">
+          <div class="_bar-fill" :style="{ width: `${progressPercentage}%` }" />
+        </div>
+      </div>
+
+      <div class="_labels">
+        <span class="_label -start">
+          {{ activeStatus ? activeStatus.name : '' }}
+        </span>
+        <span class="_label -end">
+          {{ lastStatus ? lastStatus.name : '' }}
+        </span>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -57,10 +77,6 @@ export default defineComponent({
     isVertical: {
       type: Boolean,
       default: false
-    },
-    maxStatusesToDisplayHorizontal: {
-      type: Number,
-      default: 3
     }
   },
   components: {
@@ -69,86 +85,33 @@ export default defineComponent({
     const filteredStatusesList = computed<ProgressTrackerStatus[]>(() => {
       return props.filteredStatusesList;
     });
-    const activeStatusIndex = computed<number>(() => {
-      return filteredStatusesList.value.findIndex((status) => status.id === props.activeStatus.id);
-    });
     const filteredStatusesCount = computed<number>(() => {
       return filteredStatusesList.value.length;
     });
 
-    const firstStatus = computed<ProgressTrackerStatus>(() => {
-      return filteredStatusesList.value[0];
-    });
     const lastStatus = computed<ProgressTrackerStatus>(() => {
       return filteredStatusesList.value[filteredStatusesList.value.length - 1];
     });
     const currentStepIndex = computed<number>(() => {
       return filteredStatusesList.value.findIndex((status) => status.id === props.activeStatus.id);
     });
-    const isFirstStepActive = computed<boolean>(() => {
-      return props.activeStatus.id === firstStatus.value.id;
-    });
-    const isLastStepActive = computed<boolean>(() => {
-      return props.activeStatus.id === lastStatus.value.id;
+
+    const progressPercentage = computed<number>(() => {
+      if (filteredStatusesCount.value <= 1) return 0;
+      return (currentStepIndex.value / (filteredStatusesCount.value - 1)) * 100;
     });
 
     const statusesToDisplay = computed<StatusDisplayItem[]>(() => {
       const _statusesList = filteredStatusesList.value;
-
-      if (_statusesList.length <= props.maxStatusesToDisplayHorizontal || props.isVertical) {
-        return _statusesList.map(
-          (status, index) => ({
-            statusData: status,
-            index
-          })
-        );
-      }
-
-      const statuses: StatusDisplayItem[] = [
-        {
-          statusData: firstStatus.value,
-          index: 0
-        }
-      ];
-
-      if (isFirstStepActive.value) {
-        statuses.push({
-          statusData: _statusesList[1],
-          index: 1
-        });
-      } else if (isLastStepActive.value) {
-        const index = _statusesList.length - 2;
-
-        statuses.push({
-          statusData: _statusesList[index],
-          index
-        });
-      } else if (props.activeStatus) {
-        statuses.push({
-          statusData: props.activeStatus,
-          index: activeStatusIndex.value
-        });
-      }
-
-      statuses.push({
-        statusData: lastStatus.value,
-        index: _statusesList.length - 1
-      });
-
-      return statuses;
+      return _statusesList.map((status, index) => ({ statusData: status, index }));
     });
-
-    function shouldShowStatusName (status: StatusDisplayItem): boolean {
-      return [props.activeStatus.id, firstStatus.value.id, lastStatus.value.id].includes(status.statusData.id) || props.isVertical;
-    }
 
     return {
       currentStepIndex,
       filteredStatusesCount,
-      firstStatus,
       lastStatus,
-      shouldShowStatusName,
-      statusesToDisplay
+      statusesToDisplay,
+      progressPercentage
     }
   }
 })
@@ -162,7 +125,45 @@ export default defineComponent({
 
   display: flex;
   flex-direction: column;
-  font-size: var(--font-xs);
+
+  &.-compact {
+    font-size: var(--font-xs);
+
+    ._progress-bar {
+      width: 100%;
+      margin: var(--spacer-xs) 0;
+
+      ._bar-track {
+        height: .6em;
+        background-color: var(--c-divider);
+        overflow: hidden;
+        position: relative;
+      }
+
+      ._bar-fill {
+        height: 100%;
+        background-color: var(--c-primary);
+        transition: width 0.3s ease;
+      }
+    }
+
+    ._labels {
+      display: flex;
+      justify-content: space-between;
+      width: 100%;
+
+      ._label {
+        color: var(--c-text-muted);
+        &.-start {
+            color: var(--c-primary);
+            font-weight: var(--font-bold);
+        }
+        &.-end {
+           text-align: right;
+        }
+      }
+    }
+  }
 
   ._progress-tracker {
     display: flex;
@@ -220,13 +221,10 @@ export default defineComponent({
   }
 
   ._step-counter {
-    position: absolute;
-    bottom: 117%;
     white-space: nowrap;
 
     &.-vertical {
       position: relative;
-      bottom: auto;
       margin-top: var(--spacer-xs);
     }
   }
