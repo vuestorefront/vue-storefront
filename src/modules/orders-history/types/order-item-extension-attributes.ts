d3b1b3f3de38f528ba8267575be7029f41533c75
Anton Sylka <antonsylka1004@gmail.com>
2026-02-20 12:08:16 +0800

    Refs #36181: Order Item - move `plushie_id` to extension attributes

--- src/modules/orders-history/types/order-item-extension-attributes.ts
+++ src/modules/orders-history/types/order-item-extension-attributes.ts
@@ -3,5 +3,6 @@ import { Customization, CustomizationStateItem } from 'src/modules/customization
 export interface OrderItemExtensionAttributes {
   customizations: Customization[],
   customization_states: CustomizationStateItem[],
+  plushie_id?: number,
   support_bulk_customization?: boolean
 }
